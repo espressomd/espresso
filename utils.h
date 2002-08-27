0ca2b0406d8435fef3df047018e8dca76fb2a35a
Frank Mühlbach <>
2002-08-27 12:35:28 +0000

    bug fix

--- utils.h
+++ utils.h
@@ -231,8 +231,7 @@ MDINLINE double distance(double pos1[3], double pos2[3])
 {
   int i;
   double dist = 0;
-  for(i=0;i<3;i++)
-      dist += SQR(pos1[0]-pos2[0]) + SQR(pos1[1]-pos2[1]) + SQR(pos1[2]-pos2[2]);
+  dist += SQR(pos1[0]-pos2[0]) + SQR(pos1[1]-pos2[1]) + SQR(pos1[2]-pos2[2]);
   return sqrt(dist);
 }
 
@@ -242,10 +241,13 @@ MDINLINE double unfolded_distance(double pos1[3], int image_box1[3],
 {
   int i;
   double dist = 0;
+  double lpos1[3],lpos2[3];
   for(i=0;i<3;i++){
-    pos1[i] += image_box1[i]*box_l[i];
-    pos2[i] += image_box2[i]*box_l[i];
-    dist += SQR(pos1[i]-pos2[i]);
+    lpos1[i] = pos1[i];
+    lpos2[i] = pos2[i];
+    lpos1[i] += image_box1[i]*box_l[i];
+    lpos2[i] += image_box2[i]*box_l[i];
+    dist += SQR(lpos1[i]-lpos2[i]);
   }
   return sqrt(dist);
 }
