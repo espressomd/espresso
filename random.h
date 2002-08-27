04f13bd251d0d2e710706ed24c7f256cb5597263
Frank Mühlbach <>
2002-08-27 12:54:17 +0000

    tcl random generator put in random.h/random.h

--- random.h
+++ random.h
@@ -5,14 +5,17 @@
     <b>Responsible:</b>
     <a href="mailto:muehlbac@mpip-mainz.mpg.de">Frank</a>
     \todo Put any debug output in a DEBUG statement!!!
- */
+*/
+
 
 /*----------------------------------------------------------*/
 
 extern long   l_random(void);
 extern int    i_random(int maxint);
-extern double random(void);
+extern double d_random(void);
 extern void   init_random(void);
+int tcl_rand(ClientData data, Tcl_Interp *interp,
+	  int argc, char **argv);
 
 /*----------------------------------------------------------*/
 
