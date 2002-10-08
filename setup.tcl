9e6863669b5a52861939102a17197707d1078619
Axel Arnold <arnolda@icp.uni-stuttgart.de>
2002-10-08 12:06:43 +0000

    Cleared up a lot of stuff. Now writes ASCII file.

--- setup.tcl
+++ setup.tcl
@@ -1,98 +1,81 @@
 #!/bin/sh
-# tricking...\
-    PLATFORM=`uname -s`;
+# tricking... the line after a these comments are interpreted as standard shell script \
+    PLATFORM=`uname -s`; if [ "$1" != "" ]; then NP=$1; else NP=1; fi
 # OSF1 \
-    if test $PLATFORM = OSF1; then  exec dmpirun -np 1 $PLATFORM/tcl_md $0 $*
+    if test $PLATFORM = OSF1; then  exec dmpirun -np $NP $PLATFORM/tcl_md $0 $*
 # AIX \
-    elif test $PLATFORM = AIX; then exec poe $PLATFORM/tcl_md $0 $* -procs 1
+    elif test $PLATFORM = AIX; then exec poe $PLATFORM/tcl_md $0 $* -procs $NP
 # Linux \
-    else lamboot; exec /usr/lib/lam/bin/mpirun -np 8 -nsigs $PLATFORM/tcl_md $0 $*;
+    else lamboot; exec /usr/lib/lam/bin/mpirun -np $NP -nsigs $PLATFORM/tcl_md $0 $*;
 
 # \
     fi;
 
 ########### parameters
-
-set maxpart 999
-set step 10
-setmd box_l 10.0 10.0 10.0
-
-set write finish
-set write_steps 10
-set mdst 1.2
+# number of particles to setup
+set npart 500
+# particle enumeration (1,3,5,...)
+set firstpart 1
+set partstep 2
+# box size
+setmd box_l 20.0 20.0 20.0
+# number of particle types
+set ntypes 2
+# minimal distance of particles at finish
+set mdst "guess"
+# consecutive integration steps between two tests
+set intsteps 100
+# how many tests for minimal distance
 set maxtime 200
+setmd skin 1
 
-setmd periodic 1 1 1
-setmd bjerrum 0
-setmd max_num_cells 512
-setmd skin 0.4
 
-source polywr.tcl
+#######################################################\
 
-if { $write  == "yes" || $write == "finish" } {
-    exec rm -f [glob -nocomplain "configs/c*"] [glob -nocomplain "movie/m*"]
+set Lx [lindex [setmd box_l] 0]
+set Ly [lindex [setmd box_l] 1]
+set Lz [lindex [setmd box_l] 2]
+
+if {$mdst == "guess"} {
+    set mdst [expr 0.9*pow($Lx*$Ly*$Lz/$npart,0.333333333)]
+    puts "trying minimal distance of $mdst"
 }
- 
-if {[setmd n_node] == 8} {
-    setmd node_grid 2 2 2
-}
-if {[setmd n_node] == 1} {
-    setmd node_grid 1 1 1
-}
+
 # setup random particles
 puts "setting up random particles"
-for {set i 0} { $i <= $maxpart } { incr i $step} {
-    part $i pos [expr 10*[tcl_rand]] [expr 10*[tcl_rand]] [expr 10*[tcl_rand]] \
-	q [ expr ($i % 2 == 1) ? -1 : 1 ] \
-	type [ expr round([tcl_rand]*3) ]
-}
-puts "total [part number] particles"
 
-# setup ramp ia
-puts "setting up ramp interaction"
+for {set i $firstpart; set np 0} { $np < $npart } { incr np; incr i $partstep} {
+    part $i pos [expr $Lx*[tcl_rand]] [expr $Ly*[tcl_rand]] [expr $Ly*[tcl_rand]] \
+	q [ expr ($i % 2 == 1) ? -1 : 1 ] \
+	type [ expr round([tcl_rand]*$ntypes) ]
+}
+
 # no electrostatics
 setmd bjerrum 0
 
 #pairwise ramp for all particles
-for {set ia1 0} { $ia1 <= 4 } { incr ia1 } {
-    for {set ia2 0} { $ia2 <= 4 } { incr ia2 } {
-	inter $ia1 $ia2 ramp 1.3 100
+for {set ia1 0} { $ia1 <= $ntypes } { incr ia1 } {
+    for {set ia2 0} { $ia2 <= $ntypes } { incr ia2 } {
+	inter $ia1 $ia2 ramp $mdst 100
     }
 }
-
-# test for bonded interaction parameters
 # friction
 setmd gamma 1e4
 
-if {"$write" == "yes" } {
-    exec rm -f "configs/*" "movie/*"
-}
-# relax
 puts "starting ramp integration"
 integrate init
 
-set cont y
-for {set i 0} { $i < 100 && $cont == "y" } { incr i} {
-    puts "step $i: minimum distance=[mindist] !< $mdst"
+set cont 1
+for {set i 0} { $i < $maxtime && $cont} { incr i} {
     set md [mindist]
-    if {$md > $mdst || $md == "(> ramp cutoffs)"} { set cont n }
-    integrate $write_steps
-    if {"$write" == "yes" } {
-	polywrite [format "configs/c%04d.poly" $i]
-    }
+    puts "step $i minimum distance = $md"
+    if {$md >= $mdst || $md == "(> ramp cutoffs)"} { set cont 0 }
+    integrate $intsteps
 }
-
-puts "final: minimum distance=[mindist]"
 
 # write
 set f [open "|gzip -c - >config.gz" w]
-writemd $f posx posy posz type q
+blockfile $f write particles "id pos type q" all
 close $f
 
-if { $write == "finish" } {
-    polywrite "configs/cfinal.poly"
-}
-if { $write  == "yes" || $write == "finish" } {
-    catch { eval exec poly2pdb -poly [glob -nocomplain "configs/c*"] \
-	-per -psf "movie/m" >/dev/null 2>/dev/null }
-}
+puts "wrote [part number] particles to config.gz with minimal distance [mindist]"
