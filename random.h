36d34e814a1b7758a85abecebecd368cabe49029
Bernward Mann <>
2003-03-18 18:24:57 +0000

    + Header support for the new functions init_random_seed(), init_random_stat(),   print_random_seed(), print_random_stat(), setmd_random. - Moved constant NTAB_RANDOM from .c to header. + Introduced new type structure 'RandomStatus' to be able to save the   random number generators current status completely & reproducebly.

--- random.h
+++ random.h
@@ -7,6 +7,15 @@
     \todo Put any debug output in a DEBUG statement!!!
 */
 
+/*----------------------------------------------------------*/
+
+# define NTAB_RANDOM  32
+
+typedef struct {
+  long  idum;
+  long  iy;
+  long  iv[NTAB_RANDOM];
+} RandomStatus;
 
 /*----------------------------------------------------------*/
 
@@ -14,8 +23,21 @@ extern long   l_random(void);
 extern int    i_random(int maxint);
 extern double d_random(void);
 extern void   init_random(void);
-int tcl_rand(ClientData data, Tcl_Interp *interp,
-	  int argc, char **argv);
+extern void   init_random_seed(long seed);
+extern void   init_random_stat(RandomStatus my_stat);
+extern long   print_random_seed(void);
+RandomStatus  print_random_stat(void);
+
+/**  A random generator for tcl.
+     <li> tcl_rand() for uniform double in ]0;1[
+     <li> tcl_rand(i <n>) for integer between 0 and n-1 */
+int tcl_rand(ClientData data, Tcl_Interp *interp,int argc, char **argv);
+
+/**  Implementation of the tcl-command
+     setmd_random { seed [<seed(0)> ... <seed(n_nodes)>] | stat [status-list] }
+     Without further arguments, it returns the current seeds/status of the nodes as a tcl-list;
+     otherwise it issues the parameters as the new seeds/status to the respective nodes. */
+int setmd_random(ClientData data, Tcl_Interp *interp, int argc, char **argv);
 
 /*----------------------------------------------------------*/
 
