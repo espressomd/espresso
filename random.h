7dcf2f30ba9822a1dba36210454cc7e459f499e8
Bernward Mann <>
2003-03-19 13:49:15 +0000

    + Header support for merged functions tcl_rand() and setmd_random() into new tcl-command   t_random [{ int <n> | seed [<seed(0)> ... <seed(n_nodes-1)>] | stat [status-list] }]   - Without further arguments, it returns a random double between 0 and 1.   - If 'int <n>' is given, it returns a random integer between 0 and n-1.   - If 'seed'/'stat' is given without further arguments, it returns a tcl-list with     the current seeds/status of the n_nodes active nodes; otherwise it issues the     given parameters as the new seeds/status to the respective nodes.

--- random.h
+++ random.h
@@ -4,7 +4,6 @@
 /** \file random.h a random generator
     <b>Responsible:</b>
     <a href="mailto:muehlbac@mpip-mainz.mpg.de">Frank</a>
-    \todo Put any debug output in a DEBUG statement!!!
 */
 
 /*----------------------------------------------------------*/
@@ -29,16 +28,14 @@ extern long   print_random_idum(void);
 extern long   print_random_seed(void);
 RandomStatus  print_random_stat(void);
 
-/**  A random generator for tcl.
-     <li> tcl_rand() for uniform double in ]0;1[
-     <li> tcl_rand(i <n>) for integer between 0 and n-1 */
-int tcl_rand(ClientData data, Tcl_Interp *interp,int argc, char **argv);
-
 /**  Implementation of the tcl-command
-     setmd_random { seed [<seed(0)> ... <seed(n_nodes)>] | stat [status-list] }
-     Without further arguments, it returns the current seeds/status of the nodes as a tcl-list;
-     otherwise it issues the parameters as the new seeds/status to the respective nodes. */
-int setmd_random(ClientData data, Tcl_Interp *interp, int argc, char **argv);
+     t_random [{ int <n> | seed [<seed(0)> ... <seed(n_nodes-1)>] | stat [status-list] }]
+     <li> Without further arguments, it returns a random double between 0 and 1.
+     <li> If 'int <n>' is given, it returns a random integer between 0 and n-1.
+     <li> If 'seed'/'stat' is given without further arguments, it returns a tcl-list with
+          the current seeds/status of the n_nodes active nodes; otherwise it issues the 
+	  given parameters as the new seeds/status to the respective nodes. */
+int t_random(ClientData data, Tcl_Interp *interp, int argc, char **argv);
 
 /*----------------------------------------------------------*/
 
