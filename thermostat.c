ec052d5b86f3157cc5c349fe7c5c891b2293511a
Axel Arnold <arnolda@icp.uni-stuttgart.de>
2003-06-02 09:32:11 +0000

    Fixed particles are now excluded to allow measurements.

--- thermostat.c
+++ thermostat.c
@@ -60,11 +60,16 @@ void thermo_init()
 
 void friction_thermo(Particle *p)
 {
-  p->f[0] = pref1*p->v[0] + pref2*(d_random()-0.5);
-  p->f[1] = pref1*p->v[1] + pref2*(d_random()-0.5);
-  p->f[2] = pref1*p->v[2] + pref2*(d_random()-0.5);
+#ifdef EXTERNAL_FORCES
+  if(p->ext_flag != PARTICLE_FIXED) 
+#endif
+    {
+      p->f[0] = pref1*p->v[0] + pref2*(d_random()-0.5);
+      p->f[1] = pref1*p->v[1] + pref2*(d_random()-0.5);
+      p->f[2] = pref1*p->v[2] + pref2*(d_random()-0.5);
 
-  ONEPART_TRACE(if(p->r.identity==check_id) fprintf(stderr,"%d: OPT: LANG f = (%.3e,%.3e,%.3e)\n",this_node,p->f[0],p->f[1],p->f[2]));
+      ONEPART_TRACE(if(p->r.identity==check_id) fprintf(stderr,"%d: OPT: LANG f = (%.3e,%.3e,%.3e)\n",this_node,p->f[0],p->f[1],p->f[2]));
 
-  THERMO_TRACE(fprintf(stderr,"%d: Thermo: P %d: force=(%.3e,%.3e,%.3e)\n",this_node,p->r.identity,p->f[0],p->f[1],p->f[2]));
+      THERMO_TRACE(fprintf(stderr,"%d: Thermo: P %d: force=(%.3e,%.3e,%.3e)\n",this_node,p->r.identity,p->f[0],p->f[1],p->f[2]));
+    }
 }
