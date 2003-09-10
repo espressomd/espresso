71624ebe23b52d3d9232a8fd27d4b2ebe7dd7c31
Christoph Schneider <>
2003-09-10 13:30:51 +0000

    removed the dependency on GSL by implementing the incomplete Gammafunction for uwerr.

--- uwerr.c
+++ uwerr.c
@@ -1,16 +1,11 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <float.h>
+#include <stdio.h>
 
 #include "uwerr.h"
 
-#ifdef UW_PLOT
-#include <stdio.h>
-#endif
-
-#ifdef UW_HAVE_GSL
-#include <gsl/gsl_specfunc.h>
-#endif
 
 /** This is eps from matlab */
 #define UW_EPS 2.2204e-16
@@ -37,9 +32,7 @@ struct UWerr_t {
   double ddvalue;
   double tau_int;
   double dtau_int;
-#ifdef UW_HAVE_GSL
   double Q_val;
-#endif 
   int error;
   int W;
   double bias;
@@ -61,6 +54,116 @@ char * UWerr_err_to_str(struct UWerr_t s) {
   return 0;
 }
 */
+
+double gammaln(double a)
+{
+    int j;
+    double x, y, tmp, ser;
+    double cof[6] = { 76.18009172947146,
+                     -86.50532032941677,
+                      24.01409824083091,
+                      -1.231739572450155,
+                       0.1208650973866179e-2,
+                      -0.5395239384953e-5};
+
+    y = x = a;
+    tmp = x+5.5;
+    tmp -= (x+0.5)*log(tmp);
+    ser = 1.000000000190015;
+    for (j = 0; j < 6; ++j)
+	ser += cof[j]/++y;
+
+    return -tmp+log(2.5066282746310005*ser/x);
+}
+
+void gammaser(double * gser, double a, double x)
+{
+    int n, ITMAX = 100;
+    double sum, del, ap, gln, eps = DBL_EPSILON;
+
+    gln = gammaln(a);
+
+    if (x <= 0.0) {
+	if (x < 0.0)
+	    puts("uwerr: x less than 0 in gammaser.");
+	*gser = 0.0;
+	return;
+    } else {
+	ap = a;
+	del = sum = 1.0/a;
+	for (n = 0; n < ITMAX; ++n) {
+	    ++ap;
+	    del *= x/ap;
+	    sum += del;
+	    if (fabs(del) < fabs(sum)*eps) {
+		*gser = sum*exp(-x+a*log(x)-gln);
+		return;
+	    }
+	}
+	puts("uwerr: a too large, ITMAX too small in gammaser.");
+    }
+}
+
+
+void gammacf(double * gcf, double a, double x)
+{
+    int i, ITMAX = 100;
+    double an, b, c, d, del, h, gln, eps = DBL_EPSILON, FPMIN = DBL_MIN;
+
+    gln=gammaln(a);
+    b = x+1.0-a;
+    c = 1.0/FPMIN;
+    d = 1.0/b;
+    h = d;
+    for (i = 1; i <= ITMAX; ++i) {
+	an = -i*(i-a);
+	b += 2.0;
+	d = an*d+b;
+	if (fabs(d) < FPMIN)
+	    d = FPMIN;
+	c = b+an/c;
+	if (fabs(c) < FPMIN)
+	    c = FPMIN;
+	d = 1.0/d;
+	del = d*c;
+	h *= del;
+	if (fabs(del-1.0) <= eps)
+	    break;
+    }
+    if (i > ITMAX)
+	puts("uwerr: a too large, ITMAX too small in gammacf.");
+    *gcf = exp(-x+a*log(x)-gln)*h;
+}
+
+/** The incomplete Gammafunction.
+
+    This is the implementation of the incomplete Gammafunction as described in
+    Nummerical Recepies in C++ The Art of Scientific Computing Second Edition.
+
+    The incomplete Gammafunction is defined as
+    \f[
+       Q(a,x):=\frac{1}{\Gamma(a)}\int_x^\infty e^{-t}t^{a-1}\textrm{d}t\quad(a>0)
+    \f]
+ */
+
+double gammaq(double a, double x)
+{
+    double retval;
+
+    if (x < 0.0 || a <= 0.0) {
+	puts("uwerr: Invalid arguments for gammaq.");
+	return 0.0;
+    }
+
+    if (x < a+1.0) {
+	gammaser(&retval, a, x);
+	return 1.0-retval;
+    } else {
+	gammacf(&retval, a, x);
+	return retval;
+    }
+}
+
 /** Sum up a vector.
 
     \param v A pointer to the vector.
@@ -562,16 +665,14 @@ int UWerr_f(Tcl_Interp *interp, Tcl_CmdInfo * cmdInfo, int argc, char ** argv,
   ret.ddvalue = ret.dvalue*sqrt((W_opt + .5)/rows);
   ret.dtau_int = 2 * ret.tau_int * sqrt((W_opt + .5 - ret.tau_int)/rows);
 
-#ifdef UW_HAVE_GSL
   if (len > 1) {
     for (i = 0; i < len; ++i)
       Fbr[i] = (Fbr[i] - Fb)*(Fbr[i] - Fb)*n_rep[i];
     
     ret.Q_val = UWerr_sum(Fbr, len);
     ret.Q_val /= CFbb_opt;
-    ret.Q_val = 1 - gsl_sf_gamma_inc_P((len-1)/2., ret.Q_val/2.);
+    ret.Q_val = gammaq((len-1)/2., ret.Q_val/2.);
   }
-#endif
 
 #ifdef UW_PLOT
   tmp = 0;
@@ -592,10 +693,10 @@ int UWerr_f(Tcl_Interp *interp, Tcl_CmdInfo * cmdInfo, int argc, char ** argv,
   Tcl_AppendResult(interp, str, " ", (char *)NULL);
   Tcl_PrintDouble(interp, ret.dtau_int, str);
   Tcl_AppendResult(interp, str, (char *)NULL);
-#ifdef UW_HAVE_GSL
-  Tcl_PrintDouble(interp, ret.Q_val, str);
-  Tcl_AppendResult(interp, " ", str, (char *)NULL);
-#endif
+  if (len > 1) {
+    Tcl_PrintDouble(interp, ret.Q_val, str);
+    Tcl_AppendResult(interp, " ", str, (char *)NULL);
+  }
 
  err_exit:
   free(abb);
