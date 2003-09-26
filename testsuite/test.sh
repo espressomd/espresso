75e5c76d9c6672af4985cb479d99fa9a65a257ed
Axel Arnold <arnolda@icp.uni-stuttgart.de>
2003-09-26 12:04:05 +0000

    Now also loops over number of processors.

--- testsuite/test.sh
+++ testsuite/test.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-for f in *.tcl; do
-    if test -x $f; then
-	if ! $f; then
-	    set error 1
-	    break
+for np in 1 2 3 4 6 8; do
+    for f in *.tcl; do
+	if test -x $f; then
+	    if ! $f $np; then
+		set error 1
+		break
+	    fi
 	fi
-    fi
+    done
 done
 
 if test "$error" == "1"; then
