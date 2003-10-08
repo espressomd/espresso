25f0a9d3f72f7775f062f8a731145d512a09cf27
Hans-Jörg Limbach <hjlimbach10@gmail.com>
2003-10-08 13:46:56 +0000

    added the sample files

---
+++ samples/espresso_logo.tcl
@@ -0,0 +1,278 @@
+#!/bin/sh
+# tricking... the line after a these comments are interpreted as standard shell script \
+    PLATFORM=`uname -s`; if [ "$1" != "" ]; then NP=$1; else NP=1; fi
+# OSF1 \
+    if test $PLATFORM = OSF1; then  exec dmpirun -np $NP $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $*
+# AIX \
+    elif test $PLATFORM = AIX; then exec poe $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $* -procs $NP
+# Linux \
+    else export EF_ALLOW_MALLOC_0=1; exec mpirun -np $NP -nsigs $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $*;
+# \
+    fi;
+
+#############################################################
+#                                                           #
+#  System: ESPRESSO LOGO                                    #
+#  Even though this system is just for fun                  #
+#  it is actually a real polyelectrolyte solution           #
+#                                                           #
+#  Created:       17.03.2003 by HL                          #
+#  Last modified: 18.03.2003 by HL                          #
+#                                                           #
+#############################################################
+
+puts " "
+puts "======================================================="
+puts "=    Sample script 5: espresso_logo.tcl               ="
+puts "======================================================="
+puts " "
+
+#############################################################
+#  Parameters                                               #
+#############################################################
+
+# System identification: 
+set name  "espresso"
+set ident "_s5"
+
+# 
+set vmd_output "yes"
+
+# System parameters
+#############################################################
+
+set box_l 20.0
+
+set cup_top_circ 21
+set cup_bot_circ 15
+set cup_heigth   6
+
+set saucer_circ  30
+
+set n_steam 5
+set l_steam 6
+
+# Interaction parameters (repulsive Lennard Jones)
+#############################################################
+
+# Lennard Jones
+set n_part_types  3
+set lj1_eps     1.0
+set lj1_sig     1.0
+set lj1_cut     1.12246
+set lj1_shift   $lj1_eps
+
+# FENE
+set fene_cut      2.0
+set fene_k        7.0
+
+# Coulomb
+set bjerrum       2.5
+set accuracy      1.0e-4
+
+
+# Integration parameters
+#############################################################
+
+setmd time_step 0.01
+setmd skin      1.0
+setmd gamma     1.0
+setmd temp      1.0
+
+# warmup integration (with capped LJ potential)
+set fixed_steps   10
+set fixed_n_times 100
+
+# integration
+set int_steps    10
+set int_n_times  100
+
+# Other parameters
+#############################################################
+set tcl_precision 6
+
+#############################################################
+#  Setup System                                             #
+#############################################################
+
+# Interaction setup
+#############################################################
+
+setmd box_l $box_l $box_l $box_l
+
+# fene
+inter 0 fene $fene_k $fene_cut
+# lj
+for {set ia1 0} { $ia1 < $n_part_types } { incr ia1 } {
+    for {set ia2 0} { $ia2 < $n_part_types } { incr ia2 } {
+        inter $ia1 $ia2 lennard-jones $lj1_eps $lj1_sig $lj1_cut $lj1_shift 0.0
+    }
+}
+# coulomb: after particle setup
+
+# Particle setup
+#############################################################
+
+set mypi   3.141592653589793
+
+# espresso cup (polyelectrolytes)
+set pid 0
+for {set i 0} { $i < $cup_heigth } {incr i} {
+    set circ [expr $cup_bot_circ + $i*($cup_top_circ-$cup_bot_circ)/double($cup_heigth-1)]
+    set rad  [expr $circ/(2.0*$mypi)]
+    set alpha [expr 2.0*$mypi/int($circ)]
+    set posy [expr 2.0+$i] 
+    for {set j 0} { $j < [expr int($circ)] } {incr j} {
+        set posx [expr $box_l/2.0 + $rad*sin($j*$alpha+($mypi/2.0))]
+        set posz [expr $box_l/2.0 + $rad*cos($j*$alpha+($mypi/2.0))]
+	part $pid pos $posx $posy $posz type 0 q 1.0
+	if { $j > 0 } { part $pid bond 0 [expr $pid-1] }
+	incr pid
+    }   
+}
+
+set rad [expr $cup_bot_circ/(2.0*$mypi)]
+set posy 2.0
+puts "cup: bottom_rad $rad"
+while { $rad > 1.0 } {
+    set rad   [expr $rad-0.9]
+    set circ  [expr 2.0*$mypi*$rad]
+    set alpha [expr 2.0*$mypi/int($circ)]
+    for {set j 0} { $j < [expr int($circ)] } {incr j} {
+        set posx [expr $box_l/2.0 + $rad*sin($j*$alpha+($mypi/2.0))]
+        set posz [expr $box_l/2.0 + $rad*cos($j*$alpha+($mypi/2.0))]
+	part $pid pos $posx $posy $posz type 0 q 1.0
+	if { $j > 0 } { part $pid bond 0 [expr $pid-1] }
+	incr pid
+    }   
+}
+
+# cup handle (one more polyelectrolyte)
+set hand_rad  [expr ($cup_heigth-4.0)/sqrt(2.0)]
+set hand_circ [expr (1.5*$mypi*$hand_rad)]
+set hand_xoff [expr ($cup_bot_circ+$cup_top_circ)/(4.0*$mypi)+1.2]
+set hand_yoff [expr 2.0+$cup_heigth/2.0 -0.2]
+set alpha [expr 2.0*$mypi/int(4.0*$hand_circ/3.0)]
+set beta  [expr sin(($cup_top_circ-$cup_bot_circ)/(2.0*$mypi*$cup_heigth-1))]
+set beta [expr $beta - $mypi/8.0]
+puts "handle rad=$hand_rad circ=$hand_circ beta $beta"
+set posz [expr ($box_l/2.0)+0.5]
+for {set i 0} { $i < [expr int($hand_circ)]} {incr i} {
+    set posx [expr $hand_xoff+$box_l/2.0 + $hand_rad*sin($i*$alpha+$beta)]
+    set posy [expr $hand_yoff + $hand_rad*cos($i*$alpha+$beta)]
+    part $pid pos $posx $posy $posz type 0 q 1.0
+    if { $i > 0 } { part $pid bond 0 [expr $pid-1] }
+    incr pid
+}
+
+# saucer (counterions)
+set s_rad_o [expr $saucer_circ/(2.0*$mypi)]
+set s_rad_i [expr $cup_bot_circ/(2.0*$mypi)]
+set n_saucer [expr int($s_rad_o-$s_rad_i) + 1]
+puts "saucer: r_out=$s_rad_o, r_in=$s_rad_i, rings=$n_saucer"
+set n_ci 0
+for { set i 0 } { $i < $n_saucer } { incr i } {
+    set n_ci [expr $n_ci + int($saucer_circ-($i*2.0*$mypi))]
+}
+set ci_val [expr -$pid/double($n_ci)]
+puts "Parts: total [expr $pid+$n_ci], cup $pid, saucer $n_ci (valency $ci_val)"
+for { set i 0 } { $i < $n_saucer } { incr i } {
+    set rad  [expr $s_rad_o-$i]
+    set alpha [expr 2.0*$mypi/int($saucer_circ-($i*2.0*$mypi))]
+    set posy [expr 2.3 - 0.5*$i]
+    for {set j 0} { $j < int($saucer_circ-($i*2.0*$mypi)) } {incr j} {
+        set posx [expr $box_l/2.0 + $rad*sin($j*$alpha)]
+        set posz [expr $box_l/2.0 + $rad*cos($j*$alpha)]
+	part $pid pos $posx $posy $posz type 1 q $ci_val
+	incr pid
+    }
+}
+
+for {set i 0} { $i < $pid} {incr i} { part $i fix }
+
+# steam (some neutral polymers)
+for {set i 0} { $i < $n_steam } {incr i} {
+    set posy [expr $cup_heigth] 
+    set rad  [expr ($cup_top_circ-12.0)/(2.0*$mypi)]
+    set alpha [expr 2.0*$mypi/int($n_steam)]
+    set posx [expr $box_l/2.0 + $rad*sin($i*$alpha)]
+    set posz [expr $box_l/2.0 + $rad*cos($i*$alpha)]
+    for {set j 0} { $j < $l_steam } {incr j} {
+	part $pid pos $posx $posy $posz type 2 
+	if { $j > 0 } { 
+	    part $pid bond 0 [expr $pid-1]
+	    part $pid ext_force 0.0 5.0 0.0
+	}
+	if { $j == 0 } { part $pid fix }
+	incr pid
+	set posy [expr $posy+1.0]
+    }
+}
+
+#rerun only when you have changed some parameters
+#puts "[inter coulomb $bjerrum p3m tune accuracy 0.0005]"
+inter coulomb $bjerrum p3m 9.0 16 4 2.92863e-01
+
+puts "Interactions:\n[inter]"
+set act_min_dist [analyze mindist]
+puts "Start with minimal distance $act_min_dist"
+
+#############################################################
+#  Fixed Integration                                       #
+#############################################################
+
+
+# vmd connection
+writepsf "espresso.psf"
+writepdb "espresso.pdb"
+for {set port 10000} { $port < 65000 } { incr port } {
+    catch {imd connect $port} res
+    if {$res == ""} break
+}
+set HOSTNAME [exec hostname]
+set vmdout_file [open "vmd_start.script" "w"]
+puts $vmdout_file "axes location off"
+puts $vmdout_file "mol load psf espresso.psf pdb espresso.pdb"
+puts $vmdout_file "rock y by -2"
+puts $vmdout_file "mol modstyle 0 0 CPK 1.500000 0.600000 8.000000 6.000000"
+puts $vmdout_file "mol modcolor 0 0 SegName"
+puts $vmdout_file "imd connect $HOSTNAME $port"
+close $vmdout_file
+
+exec vmd -e vmd_start.script &
+
+#############################################################
+#      Integration                                          #
+#############################################################
+
+puts "\nStart integration with fixed cup:"
+puts "Run $fixed_n_times times $fixed_steps steps"
+
+set j 0
+for {set i 0} { $i < $fixed_n_times } { incr i} {
+    puts -nonewline "run $i at time=[setmd time]\r"
+    integrate $fixed_steps
+    polyBlockWrite "$name$ident.[format %04d $j]" {time box_l} {id pos type}
+    incr j
+    if { $vmd_output=="yes" } { imd positions }
+    flush stdout
+    incr i
+}
+
+puts "\nStart integration with unfixed particles:"
+puts "Run $int_n_times times $int_steps steps"
+for {set i 0} { $i < $pid} {incr i} { part $i unfix }
+
+for {set i 0} { $i < $int_n_times } { incr i} {
+    puts -nonewline "run $i at time=[setmd time] \r"
+    flush stdout
+
+    integrate $int_steps
+    polyBlockWrite "$name$ident.[format %04d $j]" {time box_l} {id pos type}
+    incr j
+
+    if { $vmd_output=="yes" } { imd positions }
+}
+
+exec rm vmd_start.script
+puts "\n\nFinished"
---
+++ samples/lj_liquid.tcl
@@ -0,0 +1,217 @@
+#!/bin/sh
+# tricking... the line after a these comments are interpreted as standard shell script \
+    PLATFORM=`uname -s`; if [ "$1" != "" ]; then NP=$1; else NP=2; fi
+# OSF1 \
+    if test $PLATFORM = OSF1; then  exec dmpirun -np $NP $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $*
+# AIX \
+    elif test $PLATFORM = AIX; then exec poe $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $* -procs $NP
+# Linux \
+    else export EF_ALLOW_MALLOC_0=1; exec mpirun -np $NP -nsigs $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $*;
+# \
+    fi;
+
+#############################################################
+#                                                           #
+#  Sample System 1: Lennard Jones Liquid                    #
+#                                                           #
+#                                                           #
+#  Created:       17.03.2003 by HL                          #
+#  Last modified: 18.03.2003 by HL                          #
+#                                                           #
+#############################################################
+
+puts " "
+puts "======================================================="
+puts "=       Sample Script 1: lj_liquid.tcl                ="
+puts "======================================================="
+puts " "
+
+puts "Program Information: \n[code_info]\n"
+
+#############################################################
+#  Parameters                                               #
+#############################################################
+
+# System identification: 
+set name  "lj_liquid"
+set ident "_s1"
+
+# 
+set vmd_output "no"
+
+# System parameters
+#############################################################
+
+# 10 000  Particles
+set box_l   10.7437
+# 100 000 Particles
+# set box_l   48.9998
+set density 0.7
+
+# Interaction parameters (repulsive Lennard Jones)
+#############################################################
+
+set lj1_eps     1.0
+set lj1_sig     1.0
+set lj1_cut     1.12246
+set lj1_shift   $lj1_eps
+
+# Integration parameters
+#############################################################
+
+setmd time_step 0.01
+setmd skin      0.4
+setmd gamma     1.0
+setmd temp      1.0
+
+# warmup integration (with capped LJ potential)
+set warm_steps   100
+set warm_n_times 30
+# do the warmup until the particles have at least the distance min__dist
+set min_dist     0.9
+
+# integration
+set int_steps    100
+set int_n_times  100
+
+# Other parameters
+#############################################################
+set tcl_precision 6
+
+#############################################################
+#  Setup System                                             #
+#############################################################
+
+# Interaction setup
+#############################################################
+
+setmd box_l $box_l $box_l $box_l
+
+inter 0 0 lennard-jones $lj1_eps $lj1_sig $lj1_cut $lj1_shift 0
+
+# Particle setup
+#############################################################
+
+set volume [expr $box_l*$box_l*$box_l]
+set n_part [expr floor($volume*$density)]
+
+for {set i 0} { $i < $n_part } {incr i} {
+    set posx [expr $box_l*[t_random]]
+    set posy [expr $box_l*[t_random]]
+    set posz [expr $box_l*[t_random]]
+ 
+    part $i pos $posx $posy $posz type 0
+}
+
+puts "Simulate $n_part particles in a cubic simulation box "
+puts "[setmd box_l] at density $density"
+puts "Interactions:\n[inter]"
+set act_min_dist [analyze mindist]
+puts "[part 0]"
+puts "[part 1]"
+puts "Start with minimal distance $act_min_dist"
+
+setmd max_num_cells 2744
+
+#############################################################
+#  Warmup Integration                                       #
+#############################################################
+
+# prepare vmd connection
+if { $vmd_output=="yes" } {
+    prepare_vmd_connection "$name$ident$"
+}
+
+#open Observable file
+set obs_file [open "$name$ident.obs" "w"]
+puts $obs_file "\# System: $name$ident"
+puts $obs_file "\# Time\tE_tot\tE_kin\t..."
+
+puts "\nStart warmup integration:"
+puts "At maximum $warm_n_times times $warm_steps steps"
+puts "Stop if minimal distance is larger than $min_dist"
+
+# set LJ cap
+set cap 20
+inter ljforcecap $cap
+
+# Warmup Integration Loop
+set i 0
+while { $i < $warm_n_times && $act_min_dist < $min_dist } {
+
+    integrate $warm_steps
+
+    # Visualization
+    if { $vmd_output=="yes" } { imd positions }
+
+    # Warmup criterion
+    set act_min_dist [analyze mindist]
+    puts -nonewline "run $i at time=[setmd time] (LJ cap=$cap) min dist = $act_min_dist\r"
+    flush stdout
+
+#   write observables
+    puts $obs_file "{ time [setmd time] } [analyze energy]"
+
+#   Increase LJ cap
+    set cap [expr $cap+10]
+    inter ljforcecap $cap
+    incr i
+}
+
+# Just to see what else we may get from the c code
+puts "\nro variables:"
+puts "cell_grid     [setmd cell_grid]" 
+puts "cell_size     [setmd cell_size]" 
+puts "local_box_l   [setmd local_box_l]" 
+puts "max_cut       [setmd max_cut]" 
+puts "max_part      [setmd max_part]" 
+puts "max_range     [setmd max_range]" 
+puts "max_skin      [setmd max_skin]" 
+puts "n_nodes       [setmd n_nodes]" 
+puts "n_part        [setmd n_part]" 
+puts "n_part_types  [setmd n_part_types]" 
+puts "periodicity   [setmd periodicity]" 
+puts "transfer_rate [setmd transfer_rate]" 
+puts "verlet_reuse  [setmd verlet_reuse]" 
+
+# write parameter file
+polyBlockWrite "$name$ident.set" {box_l time_step skin temp gamma } "" 
+
+#############################################################
+#      Integration                                          #
+#############################################################
+puts "\nStart integration: run $int_n_times times $int_steps steps"
+
+inter ljforcecap 0
+
+puts [analyze energy]
+
+set j 0
+for {set i 0} { $i < $int_n_times } { incr i} {
+
+
+    puts -nonewline "run $i at time=[setmd time] "
+
+    integrate $int_steps
+    if { $vmd_output=="yes" } { imd positions }
+#   write observables
+    set energies [analyze energy]
+    puts $obs_file "{ time [setmd time] } $energies"
+    puts -nonewline "temp = [expr [lindex $energies 1 1]/(1.5*[setmd n_part])]\r"
+    flush stdout
+
+#   write intermediate configuration
+    if { $i%10==0 } {
+	polyBlockWrite "$name$ident.[format %04d $j]" {time box_l} {id pos type}
+	incr j
+    }
+}
+
+# write end configuration
+polyBlockWrite "$name$ident.end" {time box_l} {id pos type}
+
+close $obs_file
+
+# terminate program
+puts "\n\nFinished"
+exit
