bd56f0cf0615a4c449d15ce339d7eee3ec328354
Bernward Mann <>
2003-10-14 14:46:55 +0000

    + Added testcases for fene- and harmonic-interactions.

---
+++ testsuite/harm.tcl
@@ -0,0 +1,127 @@
+#!/bin/sh
+# tricking... the line after a these comments are interpreted as standard shell script \
+    PLATFORM=`uname -s`; if [ "$1" != "" ]; then NP=$1; else NP=2; fi
+# OSF1 \
+    if test $PLATFORM = OSF1; then  exec dmpirun -np $NP $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $*
+# AIX \
+    elif test $PLATFORM = AIX; then exec poe $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $* -procs $NP
+# Linux \
+    else export EF_ALLOW_MALLOC_0=1; exec mpirun -np $NP -nsigs $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $*;
+# \
+    fi;
+
+puts "------------------------------------------"
+puts "- Testcase harm.tcl running on [format %02d [setmd n_nodes]] nodes: -"
+puts "------------------------------------------"
+setmd time_step 0.006
+setmd skin      0.0
+setmd gamma     0.0
+setmd temp      0.0
+
+set epsilon 1e-4
+
+proc read_data {file} {
+    if { [string compare [lindex [split $file "."] end] "gz"]==0 } { set f [open "|gzip -cd $file" r]
+    } else { set f [open "$file" "r"] }
+    while {![eof $f]} { blockfile $f read auto}
+    close $f
+}
+
+proc write_data {file} {
+    global energy pressure
+    if { [string compare [lindex [split $file "."] end] "gz"]==0 } { set f [open "|gzip -c - >$file" w]
+    } else { set f [open $file "w"] }
+    set energy [analyze energy total]
+    set pressure [analyze pressure total]
+    blockfile $f write tclvariable {energy pressure}
+    blockfile $f write variable box_l
+    blockfile $f write particles {id pos f}
+    blockfile $f write bonds all
+    close $f
+}
+
+if { [catch {
+    ############## harm-specific part
+    set harm_k      30.0
+    set harm_r      1.5
+    inter 0 harm $harm_k $harm_r
+
+    read_data "harm_system.data.gz"
+
+    for { set i 0 } { $i <= [setmd max_part] } { incr i } {
+	set F($i) [part $i pr f]
+    }
+    # to ensure force recalculation
+    invalidate_system
+    integrate 0
+
+    # here you can create the necessary snapshot
+    # write_data "harm_system.data2"
+
+    # ensures that no other forces are on
+    set cureng [analyze energy harm 0 0]
+    # tbrs
+    set curprs [lindex [analyze pressure harm 0 0] 0]
+    ############## end
+
+    set toteng [analyze energy total]
+    set totprs [analyze pressure total]
+
+    if { [expr abs($toteng - $cureng)] > $epsilon } {
+	error "system has unwanted energy contributions"
+    }
+    if { [expr abs($totprs - $curprs)] > $epsilon } {
+	error "system has unwanted pressure contributions"
+    }
+
+    set rel_eng_error [expr abs(($toteng - $energy)/$energy)]
+    puts "relative energy deviations: $rel_eng_error"
+    if { $rel_eng_error > $epsilon } {
+	error "relative energy error too large"
+    }
+
+    set rel_prs_error [expr abs(($totprs - $pressure)/$pressure)]
+    puts "relative pressure deviations: $rel_prs_error"
+    if { $rel_prs_error > $epsilon } {
+	error "relative pressure error too large"
+    }
+
+    set maxdx 0
+    set maxpx 0
+    set maxdy 0
+    set maxpy 0
+    set maxdz 0
+    set maxpz 0
+    for { set i 0 } { $i <= [setmd max_part] } { incr i } {
+	set resF [part $i pr f]
+	set tgtF $F($i)
+	set dx [expr abs([lindex $resF 0] - [lindex $tgtF 0])]
+	set dy [expr abs([lindex $resF 1] - [lindex $tgtF 1])]
+	set dz [expr abs([lindex $resF 2] - [lindex $tgtF 2])]
+
+	if { $dx > $maxdx} {
+	    set maxdx $dx
+	    set maxpx $i
+	}
+	if { $dy > $maxdy} {
+	    set maxdy $dy
+	    set maxpy $i
+	}
+	if { $dz > $maxdz} {
+	    set maxdz $dz
+	    set maxpz $i
+	}
+    }
+    puts "maximal force deviation in x $maxdx for particle $maxpx, in y $maxdy for particle $maxpy, in z $maxdz for particle $maxpz"
+    if { $maxdx > $epsilon || $maxdy > $epsilon || $maxdz > $epsilon } {
+	if { $maxdx > $epsilon} {puts "force of particle $maxpx: [part $maxpx pr f] != $F($maxpx)"}
+	if { $maxdy > $epsilon} {puts "force of particle $maxpy: [part $maxpy pr f] != $F($maxpy)"}
+	if { $maxdz > $epsilon} {puts "force of particle $maxpz: [part $maxpz pr f] != $F($maxpz)"}
+	error "force error too large"
+    }
+} res ] } {
+    puts "Error occured: \"$res\""
+    exit -666
+}
+
+exit 0
