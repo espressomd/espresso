170b79d587e60ab93a187b7f4c0f5f0872abc50b
Bernward Mann <>
2003-10-20 13:09:55 +0000

    + Added license_header to the beginning of all files.

--- testsuite/lj-cos.tcl
+++ testsuite/lj-cos.tcl
@@ -9,7 +9,16 @@
     else export EF_ALLOW_MALLOC_0=1; exec mpirun -np $NP -nsigs $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $*;
 # \
     fi;
-
+# 
+#  This file is part of the ESPResSo distribution (http://www.espresso.mpg.de).
+#  It is therefore subject to the ESPResSo license agreement which you accepted upon receiving the distribution
+#  and by which you are legally bound while utilizing this file in any form or way.
+#  There is NO WARRANTY, not even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#  You should have received a copy of that license along with this program;
+#  if not, refer to http://www.espresso.mpg.de/license.html where its current version can be found, or
+#  write to Max-Planck-Institute for Polymer Research, Theory Group, PO Box 3148, 55021 Mainz, Germany.
+#  Copyright (c) 2002-2003; all rights reserved unless otherwise stated.
+# 
 puts "--------------------------------------------"
 puts "- Testcase lj-cos.tcl running on [format %02d [setmd n_nodes]] nodes: -"
 puts "--------------------------------------------"
