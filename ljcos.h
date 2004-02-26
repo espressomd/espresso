4baa0ce91263f1e79d2bef0dae5d9bc072ad59f6
Axel Arnold <arnolda@icp.uni-stuttgart.de>
2004-02-26 10:28:53 +0000

    Fixed wrong #ifndef

--- ljcos.h
+++ ljcos.h
@@ -9,7 +9,8 @@
 #include "utils.h"
 #include "parser.h"
 
-#ifdef LJCOS_H
+#ifndef LJCOS_H
+#define LJCOS_H
 
 /** \file ljcos.h
  *  Routines to calculate the lennard jones+cosine energy and/or force 
@@ -84,42 +85,44 @@ MDINLINE int ljcos_parser(Tcl_Interp * interp,
 			   int part_type_a, int part_type_b,
 			   int argc, char ** argv, int *change)
 {
-    double tmp;
+  double tmp;
   double eps, sig, cut, offset;
-      if (argc < 5) {
-	Tcl_AppendResult(interp, "lj-cos needs 4 parameters: "
-			 "<ljcos_eps> <ljcos_sig> <ljcos_cut> <ljcos_offset>",
-			 (char *) NULL);
-	return TCL_ERROR;
-      }
+  if (argc < 5) {
+    Tcl_AppendResult(interp, "lj-cos needs 4 parameters: "
+		     "<ljcos_eps> <ljcos_sig> <ljcos_cut> <ljcos_offset>",
+		     (char *) NULL);
+    return TCL_ERROR;
+  }
 
-      /* copy lj-cos parameters */
-      if ((! ARG_IS_D(1, eps))   ||
-	  (! ARG_IS_D(2, sig))   ||
-	  (! ARG_IS_D(3, cut))   ||
-	  (! ARG_IS_D(4, offset)    )) {
-	Tcl_AppendResult(interp, "lj-cos needs 4 DOUBLE parameters: "
-			 "<ljcos_eps> <ljcos_sig> <ljcos_cut> <ljcos_offset>",
-			 (char *) NULL);
-	return TCL_ERROR;
-      }
-      *change = 5;
+  /* copy lj-cos parameters */
+  if ((! ARG_IS_D(1, eps))   ||
+      (! ARG_IS_D(2, sig))   ||
+      (! ARG_IS_D(3, cut))   ||
+      (! ARG_IS_D(4, offset)    )) {
+    Tcl_AppendResult(interp, "lj-cos needs 4 DOUBLE parameters: "
+		     "<ljcos_eps> <ljcos_sig> <ljcos_cut> <ljcos_offset>",
+		     (char *) NULL);
+    return TCL_ERROR;
+  }
+  *change = 5;
 
-      /* fix for the inconsistency in the ljcos parameters.
-	 There are 7 parameters for ljcos, but you read in only four of them.
-	 The rest is calculated in lj_cos_set_params.
-	 This is a problem with the blockfile format (Mehmet) 
-      */
+  /* fix for the inconsistency in the ljcos parameters.
+     There are 7 parameters for ljcos, but you read in only four of them.
+     The rest is calculated in lj_cos_set_params.
+     This is a problem with the blockfile format (Mehmet) 
+  */
 
-      if (argc >= 8 && ARG_IS_D(5, tmp) && ARG_IS_D(6, tmp) && ARG_IS_D(7, tmp))
-	*change += 3;
-      else
-	Tcl_ResetResult(interp);
+  if (argc >= 8 && ARG_IS_D(5, tmp) && ARG_IS_D(6, tmp) && ARG_IS_D(7, tmp))
+    *change += 3;
+  else
+    Tcl_ResetResult(interp);
 
-      if (lj_cos_set_params(part_type_a, part_type_b, eps, sig, cut, offset) == TCL_ERROR) {
-	Tcl_AppendResult(interp, "particle types must be non-negative", (char *) NULL);
-	return TCL_ERROR;
-      }
+  if (lj_cos_set_params(part_type_a, part_type_b, eps, sig, cut, offset) == TCL_ERROR) {
+    Tcl_AppendResult(interp, "particle types must be non-negative", (char *) NULL);
+    return TCL_ERROR;
+  }
+
+  return TCL_OK;
 }
 
 MDINLINE void add_ljcos_pair_force(Particle *p1, Particle *p2, IA_parameters *ia_params,
