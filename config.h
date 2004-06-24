287d37275805f0cb2a9202a5691636442e3fbaf9
Bernward Mann <>
2004-06-24 16:45:28 +0000

    + Incorporating changes and moving to v1.6.3d.

--- config.h
+++ config.h
@@ -166,7 +166,7 @@ This will allow to use such particle properties as quart, omega, and torque. */
 /** callback for version status. */
 MDINLINE int version_callback(Tcl_Interp *interp)
 {
-  Tcl_AppendResult(interp, "ESPRESSO: v1.6.3b (Icheb), Last Change: 12.06.2004", (char *) NULL);
+  Tcl_AppendResult(interp, "ESPRESSO: v1.6.3d (Icheb), Last Change: 23.06.2004", (char *) NULL);
   return (TCL_OK);
 }
 
