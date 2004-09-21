60897d5f3494c3d59d29428fa505dfa6ee67f1b1
Bernward Mann <>
2004-09-21 09:08:46 +0000

    - Moving to v1.7.4c and updating changes.

--- config.h
+++ config.h
@@ -166,7 +166,7 @@ This will allow to use such particle properties as quart, omega, and torque. */
 /** callback for version status. */
 MDINLINE int version_callback(Tcl_Interp *interp)
 {
-  Tcl_AppendResult(interp, "ESPRESSO: v1.7.3e (Mezoti), Last Change: 13.09.2004", (char *) NULL);
+  Tcl_AppendResult(interp, "ESPRESSO: v1.7.4c (Mezoti), Last Change: 20.09.2004", (char *) NULL);
   return (TCL_OK);
 }
 
