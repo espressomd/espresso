6d55e963f8e5a1e77dbcdb3a0ba2e0ea7ecf45de
Axel Arnold <arnolda@icp.uni-stuttgart.de>
2005-01-15 20:08:27 +0000

    Some checks for the non verlet domain decomposition case.

--- verlet.c
+++ verlet.c
@@ -98,6 +98,8 @@ void build_verlet_lists()
   fprintf(stderr,"%d: build_verlet_list_and_force_calc:\n",this_node);
   /* estimate number of interactions: (0.5*n_part*ia_volume*density)/n_nodes */
   estimate = 0.5*n_total_particles*(4.0/3.0*PI*pow(max_range_non_bonded,3.0))*(n_total_particles/(box_l[0]*box_l[1]*box_l[2]))/n_nodes;
+
+  if (!dd.use_vList) { fprintf(stderr, "%d: build_verlet_lists, but use_vList == 0\n", this_node); errexit(); }
 #endif
    
   /* Loop local cells */
@@ -194,6 +196,8 @@ void build_verlet_lists_and_calc_verlet_ia()
   fprintf(stderr,"%d: build_verlet_list_and_calc_verlet_ia:\n",this_node);
   /* estimate number of interactions: (0.5*n_part*ia_volume*density)/n_nodes */
   estimate = 0.5*n_total_particles*(4.0/3.0*PI*pow(max_range_non_bonded,3.0))*(n_total_particles/(box_l[0]*box_l[1]*box_l[2]))/n_nodes;
+
+  if (!dd.use_vList) { fprintf(stderr, "%d: build_verlet_lists, but use_vList == 0\n", this_node); errexit(); }
 #endif
  
   /* Loop local cells */
