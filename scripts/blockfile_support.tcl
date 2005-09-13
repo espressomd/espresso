a5b7e79b6c7ccd9cc0fcaccf92fb1e7305048113
Ira Cooke <cooke@mpip-mainz.mpg.de>
2005-09-13 11:15:42 +0000

    [BUGFIX] Fixed a bug which prevented espresso from reading checkpoints when quoted strings within lists were present

--- scripts/blockfile_support.tcl
+++ scripts/blockfile_support.tcl
@@ -408,7 +408,7 @@ proc blockfile_read_auto_tclvariable {channel read auto} {
 #	puts "----$vname-$data-"
 	if { "$vname" != "array" } {
 	    global $vname
-	    if {[catch {eval "set $vname \"$data\""} error]} {
+	    if {[catch {eval "set $vname {$data}"} error]} {
 		if { $error != "" } {
 		    error "blockfile_read_auto_tclvariable: set $vname $data reported: $error"
 		}
@@ -417,7 +417,7 @@ proc blockfile_read_auto_tclvariable {channel read auto} {
 	    set vname [lindex $vblock 1]
 	    set data [lrange $vblock 2 end]
 	    global $vname
-	    if {[catch {eval "array set $vname \"$data\""} error]} {
+	    if {[catch {eval "array set $vname {$data}"} error]} {
 		if { $error != "" } {
 		    error "blockfile_read_auto_tclvariable: set $vname $data reported: $error"
 		}
