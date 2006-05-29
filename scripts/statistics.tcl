b208eba3a11babe63aa24588ad1a29b436966ec6
Hans-Jörg Limbach <hjlimbach10@gmail.com>
2006-05-29 14:03:35 +0000

    New procedure: ring_puckering for calculation of puckering coordinates of cyclic components

--- scripts/statistics.tcl
+++ scripts/statistics.tcl
@@ -5,7 +5,7 @@
 #  You should have received a copy of that license along with this program;
 #  if not, refer to http://www.espresso.mpg.de/license.html where its current version can be found, or
 #  write to Max-Planck-Institute for Polymer Research, Theory Group, PO Box 3148, 55021 Mainz, Germany.
-#  Copyright (c) 2002-2006; all rights reserved unless otherwise stated.
+#  Copyright (c) 2002-2004; all rights reserved unless otherwise stated.
 #  
 #############################################################
 #                                                           #
@@ -221,4 +221,103 @@ proc plotObs { destinations what {p1 NA} {p2 NA} {p3 NA} {p4 NA} {p5 NA} {p6 NA}
 proc plotJoin { sources final } {
     eval exec  gs -dNOPAUSE -sDEVICE=pswrite -sOutputFile=$final.A.ps $sources -c quit
     catch { eval exec pstops "2:0L@.7(21cm,0)+1L@.7(21cm,14.85cm)" $final.A.ps $final.B.ps }
+}
+
+#
+# ring_puckering
+#
+# Calculates the ring puckering coordinates according to
+# Cremer D. and Pople J. A., J. Am. Chem. Soc. 97(6),1354-1358 (1975)
+# Note that there are n-3 puckering coordinates for a n membered ring.
+#
+# As input you need ro give either the n coorindates of the ring atoms or
+# the corresponding particle identities (internal use of part print pos).
+# The parameter ret_type determines the outcome of the procedure:
+# "puck" return the puckering coordinates: q_2, phi_2, ... (default) 
+# "plane" return the ring plane defined by the center of mass and the unit vector perpendicular to the plane
+# "disp" rerturn the n out of plane displacements z_i of the ring members
+# "polar" returns spherical polar puckering coordinates (Q, theta, phi) for 6-membered rings
+#
+########################################################
+
+proc ring_puckering { pos { ret_type "puck" } } {
+
+    # number of atoms in the ring
+    set n [llength $pos]
+
+    # check for identities or positions
+    if { [llength [lindex $pos 0]] == 1 } {
+	set pid $pos; unset pos
+	for { set i 0 } { $i < $n } { incr i } {
+	    lappend pos [part [lindex $pid $i] print pos]
+	}
+    }
+
+    # center of mass coordinates (here the geometrical center)
+    set com { 0 0 0 }
+    for { set i 0 } { $i < $n } { incr i } { 
+	set com [vecadd $com [lindex $pos $i]] }
+    set com [vecscale [expr 1.0/$n] $com]
+    for { set i 0 } { $i < $n } { incr i } { 
+	lappend pos2 [vecsub [lindex $pos $i] $com] }
+
+    # determine ring plane
+    set r1 {0 0 0}; set r2 {0 0 0}
+    for { set i 0 } { $i < $n } { incr i } { 
+	set r1 [vecadd $r1 [vecscale [expr sin(2*[PI]*$i/$n)] [lindex $pos2 $i] ] ]
+	set r2 [vecadd $r2 [vecscale [expr cos(2*[PI]*$i/$n)] [lindex $pos2 $i] ] ]
+    }
+   
+    set nvec [veccross_product3d $r1 $r2]
+    set nvec [vecnorm $nvec]
+
+    if { $ret_type == "plane" } { 
+	set res "{$com} {$nvec}" 
+	return $res
+    }
+
+    # calculate out of plane displacements
+    for { set i 0 } { $i < $n } { incr i } { 
+	lappend z [vecdot_product [lindex $pos2 $i] $nvec]
+    }
+    if { $ret_type == "disp" } { return $z }
+
+    # calculate puckering coordinates. first pairs q_m, phi_m
+    set mmax [expr ($n-1)/2]
+    for { set m 2 } { $m <= $mmax } { incr m } {
+	set sum1 0; set sum2 0
+	for { set i 0 } { $i < $n } { incr i } { 
+	    set sum1 [expr $sum1 + [lindex $z $i]*cos(2*[PI]*$m*$i/$n)]
+	    set sum2 [expr $sum2 + [lindex $z $i]*sin(2*[PI]*$m*$i/$n)]
+	}
+	set $sum1 [expr sqrt(2.0/$n) * $sum1]
+	set $sum2 [expr - sqrt(2.0/$n) * $sum2]
+
+	set qm [expr sqrt( $sum1*$sum1 + $sum2*$sum2 ) ]
+	if { $sum1 != 0 } { set phim [expr atan($sum2/$sum1)] } else { set phim [expr [PI]/2.0]}
+	if { $phim < 0 } { set phim [expr [PI]+$phim] }
+	lappend res $qm
+	lappend res $phim
+    } 
+    # in case of odd number of atoms: q_(n/2)
+    if { [expr $n%2] == 0 } {
+	set sum 0
+	for { set i 0 } { $i < $n } { incr i } { 
+	    set sum [expr $sum + (pow(-1,$i)*[lindex $z $i])]
+	}
+	lappend res [expr $sum / sqrt($n)]
+    }
+
+    if { $ret_type == "puck" || $n != 6 } { return $res }
+
+    # calculate spherical polar puckering coordinates for n=6 rings:
+    if { $ret_type == "polar" } {
+	set res2 $res; unset res
+	lappend res [expr sqrt([sqr [lindex $res2 0]] + [sqr [lindex $res2 2]])]
+	if { $res > 0 } { lappend res [expr acos([lindex $res2 2]/$res)] } else { lappend $res 0 }
+	lappend res [lindex $res2 1]
+	return $res
+    }
+
+    return "Unknown return type"
 }
