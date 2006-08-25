f38cee27e76ced11582a851ce194256dc8dc76c5
Axel Arnold <arnolda@icp.uni-stuttgart.de>
2006-08-25 09:40:35 +0000

    test.sh now creates symbolic links in the objdir and runs from there.

--- testsuite/test.sh
+++ testsuite/test.sh
@@ -1,7 +1,45 @@
 #!/bin/sh
 
+####### guess srcdir from script location for manual execution
+if test ! -f $srcdir/test.sh; then
+    srcdir=`dirname $0`
+fi
 
+if test -x ../Espresso; then
+    ESPRESSO=`pwd`/../Espresso
+fi
+
+# bail out if the environment is not correctly set
+bail=
+if test ! -f "$srcdir/test.sh"; then
+    echo "please specify the variable srcdir pointing to the testsuite directory" 1>&2
+    bail=y
+fi
+if test ! -x "$ESPRESSO"; then
+    echo "please specify the variable ESPRESSO pointing to the Espresso program" 1>&2
+    bail=y
+fi
+if test -z "$PROCESSORS"; then
+    echo "please specify the processor numbers in PROCESSORS" 1>&2
+    bail=y
+fi
+if test -z "$TESTCASES"; then
+    echo "please specify the testcases to run in TESTCASES" 1>&2
+    bail=y
+fi
+if test -n "$bail"; then
+    exit -1
+fi
+
+# here go the error messages
 errf=_test.sh_error.$$
+
+# absolute path of source directory
+srcpath=`cd $srcdir && pwd`
+####### link all files from the src directory to the run directory, if they are not identical
+if test `pwd` != $srcpath; then
+    for f in $srcpath/*; do ln -f -s $f .; done
+fi
 
 # list of tests that are not supported by this version
 blacklist=
@@ -20,9 +58,7 @@ for np in $PROCESSORS; do
 	if test $ignore -eq 0; then
 	    # this is removed if the script runs through
 	    echo "execution of script failed at unexpected point" > $errf
-	    cdir=`pwd`
-	    odir=$cdir/..
-	    ( cd $srcdir; $odir/Espresso $f $np $cdir/$errf )
+	    $ESPRESSO $srcpath/$f $np $errf
 	    if test -f $errf; then
 		if grep -q -e "^not compiled in:" $errf; then
 		    missing="$missing$f: `cat $errf`\n"
