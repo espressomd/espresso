7fd71f6c980f5276c3189ac73c36303b2adf5404
Hans-Jörg Limbach <hjlimbach10@gmail.com>
2007-10-09 12:53:53 +0000

    Added function calc_lj_shift

--- scripts/ABHmath.tcl
+++ scripts/ABHmath.tcl
@@ -293,6 +293,16 @@ proc part_at_dihedral { p1 p2 p3 theta {phi "rnd"} {len "1.0"} } {
 }
 
 #############################################################
+# INTERACTION RELATED
+#############################################################
+
+# Calculate the shift value for the lennard jones interaction
+proc calc_lj_shift { lj_sigma lj_cutoff lj_offset } {
+    set fcut [expr $lj_sigma / ( $lj_cutoff - $lj_offset )]
+    return [expr -(pow($fcut,12)-pow($fcut,6))]
+}
+
+#############################################################
 # VECTOR OPERATIONS
 #############################################################
 
