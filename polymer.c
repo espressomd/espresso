ffa4d19d24492a4eb938e1aaba0e0e4d86a1c7b6
Nils Binz <>
2007-11-12 17:48:21 +0000

    [fix] removed debug-messages

--- polymer.c
+++ polymer.c
@@ -346,16 +346,12 @@ int constraint_collision(double *p1, double *p2){
   double folded_pos2[3];
   int img[3];
 
-printf("CP1\n");
-
   memcpy(folded_pos1, p1, 3*sizeof(double));
   fold_position(folded_pos1, img);
 
   memcpy(folded_pos2, p2, 3*sizeof(double));
   fold_position(folded_pos2, img);
   
-printf("CP2\n");
-
   for(i=0;i<n_constraints;i++){
     c=&constraints[i];
     switch(c->type){
