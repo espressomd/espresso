161e8ef263ac2ef18cd1bb88bbe1f68d92cf2aae
Stefan Kesselheim <Stefan.Kesselheim@icp.uni-stuttgart.de>
2010-08-18 12:52:43 +0000

    Implemented "lb_boundary sphere" command

--- lb-boundaries.c
+++ lb-boundaries.c
@@ -379,6 +379,7 @@ int lb_boundary(ClientData _data, Tcl_Interp *interp,
   else if(!strncmp(argv[1], "sphere", strlen(argv[1]))) {
     status = lb_boundary_sphere(generate_lb_boundary(),interp, argc - 2, argv + 2);
     mpi_bcast_lb_boundary(-1);
+    printf("created sphere on node %d with center %.1f %.1f %.1f, radius %.1f, direction %.1f\n", this_node, lb_boundaries[n_lb_boundaries-1].c.sph.pos[0], lb_boundaries[n_lb_boundaries-1].c.sph.pos[1], lb_boundaries[n_lb_boundaries-1].c.sph.pos[2], lb_boundaries[n_lb_boundaries-1].c.sph.rad, lb_boundaries[n_lb_boundaries-1].c.sph.direction);
   }
   else if(!strncmp(argv[1], "cylinder", strlen(argv[1]))) {
     status = lb_boundary_cylinder(generate_lb_boundary(),interp, argc - 2, argv + 2);
@@ -451,8 +452,7 @@ static void lb_init_boundary_wall(Constraint_wall* wall) {
   	    if (dist <= 0) {
 
 	      //printf("%3f %3f %3f is boundary w dist %3f\n", pos[0], pos[1], pos[2], dist);
-   	      lbfields[get_linear_index(x,y,z,lblattice.halo_grid)].boundary = 1;
-   	      lbfluid[0][1][get_linear_index(x,y,z,lblattice.halo_grid)] = 0.;   	      
+   	      lbfields[get_linear_index(x,y,z,lblattice.halo_grid)].boundary = 1;     
  	      }
       }
     }
@@ -460,55 +460,69 @@ static void lb_init_boundary_wall(Constraint_wall* wall) {
 }
 
 static void lb_init_boundary_sphere(Constraint_sphere* sphere) {
-/*
-  int x, y, z;
-  double pos[3], dist;
-
-  for (x=0;x<lblattice.halo_grid[0];x++) {
-    for (y=0;y<lblattice.halo_grid[1];y++) {
-      for (z=0;z<lblattice.halo_grid[2];z++) {
-
-	pos[0] = my_left[0] + (x-1)*lblattice.agrid;
-	pos[1] = my_left[1] + (y-1)*lblattice.agrid;
-	pos[2] = my_left[2] + (z-1)*lblattice.agrid;
-
-	dist = scalar(pos,plane->n) - plane->d;
-
-	if (fabs(dist) < lblattice.agrid) {
-	  //printf("%d %d %d\n",x,y,z);
-	  lbfluid[get_linear_index(x,y,z,lblattice.halo_grid)].boundary = 1;
-	}
-
+  int x, y, z, index, node_domain_position[3], offset[3];
+  double pos[3], dist, dist_vec[3];
+  
+	printf("executing lb_init_boundary_sphere on node %d\n", this_node);
+	
+	map_node_array(this_node, node_domain_position);
+	//printf("Position of domain of node %d: (%d, %d, %d)\n", this_node, node_domain_position[0], node_domain_position[1], node_domain_position[2]);
+	//printf("Size of domain of node %d: (%d, %d, %d)\n", this_node, lblattice.grid[0], lblattice.grid[1], lblattice.grid[2]);
+	
+	offset[0] = node_domain_position[0]*lblattice.grid[0];
+	offset[1] = node_domain_position[1]*lblattice.grid[1];
+	offset[2] = node_domain_position[2]*lblattice.grid[2];
+	
+	//printf("Node %d coordinate system offset: (%d, %d, %d)\n", this_node, offset[0], offset[1], offset[2]);
+  
+  for (z=1; z<=lblattice.grid[2]; z++) {
+    for (y=1; y<=lblattice.grid[1]; y++) {
+	    for (x=1; x<=lblattice.grid[0]; x++) {	    
+	      pos[0] = (offset[0]+(x-1))*lblattice.agrid;
+	      pos[1] = (offset[1]+(y-1))*lblattice.agrid;
+	      pos[2] = (offset[2]+(z-1))*lblattice.agrid;
+       
+        calculate_sphere_dist((Particle*) NULL, pos, (Particle*) NULL, sphere, &dist, dist_vec); //wall contains MD coordinates
+        
+        //printf("Distance of node (%d, %d, %d) at position (%.1f, %.1f, %.1f) to sphere: %.2f\n", x, y, z, pos[0], pos[1], pos[2], dist);
+        
+  	    if (dist <= 0)
+   	      lbfields[get_linear_index(x,y,z,lblattice.halo_grid)].boundary = 1;   
       }
     }
   }
-*/
 }
 
 static void lb_init_boundary_cylinder(Constraint_cylinder* cylinder) {
-/*
-  int x, y, z;
-  double pos[3], dist;
-
-  for (x=0;x<lblattice.halo_grid[0];x++) {
-    for (y=0;y<lblattice.halo_grid[1];y++) {
-      for (z=0;z<lblattice.halo_grid[2];z++) {
-
-	pos[0] = my_left[0] + (x-1)*lblattice.agrid;
-	pos[1] = my_left[1] + (y-1)*lblattice.agrid;
-	pos[2] = my_left[2] + (z-1)*lblattice.agrid;
-
-	dist = scalar(pos,plane->n) - plane->d;
-
-	if (fabs(dist) < lblattice.agrid) {
-	  //printf("%d %d %d\n",x,y,z);
-	  lbfluid[get_linear_index(x,y,z,lblattice.halo_grid)].boundary = 1;
-	}
-
+  int x, y, z, index, node_domain_position[3], offset[3];
+  double pos[3], dist, dist_vec[3];
+  
+	printf("executing lb_init_boundary_cylinder on node %d\n", this_node);
+	
+	map_node_array(this_node, node_domain_position);
+	
+	offset[0] = node_domain_position[0]*lblattice.grid[0];
+	offset[1] = node_domain_position[1]*lblattice.grid[1];
+	offset[2] = node_domain_position[2]*lblattice.grid[2];
+	
+	//printf("Node %d coordinate system offset: (%d, %d, %d)\n", this_node, offset[0], offset[1], offset[2]);
+  
+  for (z=1; z<=lblattice.grid[2]; z++) {
+    for (y=1; y<=lblattice.grid[1]; y++) {
+	    for (x=1; x<=lblattice.grid[0]; x++) {	    
+	      pos[0] = (offset[0]+(x-1))*lblattice.agrid;
+	      pos[1] = (offset[1]+(y-1))*lblattice.agrid;
+	      pos[2] = (offset[2]+(z-1))*lblattice.agrid;
+       
+        calculate_sphere_dist((Particle*) NULL, pos, (Particle*) NULL, sphere, &dist, dist_vec); //wall contains MD coordinates
+        
+        //printf("Distance of node (%d, %d, %d) at position (%.1f, %.1f, %.1f) to sphere: %.2f\n", x, y, z, pos[0], pos[1], pos[2], dist);
+        
+  	    if (dist <= 0)
+   	      lbfields[get_linear_index(x,y,z,lblattice.halo_grid)].boundary = 1;   
       }
     }
   }
-*/
 }
 
 /** Initialize boundary conditions for all constraints in the system. */
@@ -535,7 +549,7 @@ void lb_init_boundaries() {
       break;
     default:
       errtxt = runtime_error(128);
-      ERROR_SPRINTF(errtxt, "{109 lb_boundary type %d not implemented in lb_init_constraints()\n",lb_boundaries[n].type);
+      ERROR_SPRINTF(errtxt, "{109 lb_boundary type %d not implemented in lb_init_boundaries()\n",lb_boundaries[n].type);
     }
   }
 }
