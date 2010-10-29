7810038086fd2facc23f65b608d8809487537841
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2010-10-29 17:37:13 +0200

    Added copyright and license headers to some mor3e files.

--- samples/adress/Adress_t.tcl
+++ samples/adress/Adress_t.tcl
@@ -1,29 +1,28 @@
-#!/bin/sh
-# tricking... the line after a these comments are interpreted as standard shell script \
-    PLATFORM=`uname -s`; if [ "$1" != "" ]; then NP=$1; else NP=2; fi
-# OSF1 \
-    if test $PLATFORM = OSF1; then  exec dmpirun -np $NP $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $*
-# AIX \
-    elif test $PLATFORM = AIX; then exec poe $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $* -procs $NP
-# Linux \
-    else export EF_ALLOW_MALLOC_0=1; exec mpirun -np $NP -nsigs $ESPRESSO_SOURCE/$PLATFORM/Espresso $0 $*;
-# \
-    fi;
-# 
-#  This file is part of the ESPResSo distribution (http://www.espresso.mpg.de).
-#  It is therefore subject to the ESPResSo license agreement which you accepted upon receiving the distribution
-#  and by which you are legally bound while utilizing this file in any form or way.
-#  There is NO WARRANTY, not even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#  You should have received a copy of that license along with this program;
-#  if not, refer to http://www.espresso.mpg.de/license.html where its current version can be found, or
-#  write to Max-Planck-Institute for Polymer Research, Theory Group, PO Box 3148, 55021 Mainz, Germany.
-#  Copyright (c) 2002-2004; all rights reserved unless otherwise stated.
-# 
 ######################################################################
 #                                                                    #
 #  System: AdResS simulation of a Liquid of tetrahedral molecules    #
 #                                                                    #
 ######################################################################
+#
+# Copyright (C) 2010 The ESPResSo project
+# Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010 Max-Planck-Institute for Polymer Research, Theory Group, PO Box 3148, 55021 Mainz, Germany
+#  
+# This file is part of ESPResSo.
+#  
+# ESPResSo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#  
+# ESPResSo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#  
+
 
 puts " "
 puts "======================================================="
--- tools/watch.tcl
+++ tools/watch.tcl
@@ -1,24 +1,30 @@
 #!/bin/sh
 # tricking... the line after a these comments are interpreted as standard shell script \
     exec $ESPRESSO_SOURCE/Espresso $0 $*
-# 
-#  This file is part of the ESPResSo distribution (http://www.espresso.mpg.de).
-#  It is therefore subject to the ESPResSo license agreement which you accepted upon receiving the distribution
-#  and by which you are legally bound while utilizing this file in any form or way.
-#  There is NO WARRANTY, not even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#  You should have received a copy of that license along with this program;
-#  if not, refer to http://www.espresso.mpg.de/license.html where its current version can be found, or
-#  write to Max-Planck-Institute for Polymer Research, Theory Group, PO Box 3148, 55021 Mainz, Germany.
-#  Copyright (c) 2002-2006; all rights reserved unless otherwise stated.
-# 
 #############################################################
 #                                                           #
 #  Watch Configurations as animation in vmd                 #
 #                                                           #
-#  Created:       10.04.2003 by HL                          #
-#  Last modified: 10.04.2003 by HL                          #
-#                                                           #
 #############################################################
+#
+# Copyright (C) 2010 The ESPResSo project
+# Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010 Max-Planck-Institute for Polymer Research, Theory Group, PO Box 3148, 55021 Mainz, Germany
+#  
+# This file is part of ESPResSo.
+#  
+# ESPResSo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#  
+# ESPResSo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#  
 
 puts " "
 puts "======================================================="
