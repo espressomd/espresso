feecfe9ec07ef967066ebf52682c0e5d9b8ca921
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2010-10-29 17:58:22 +0200

    Copyrighted and licensed packages/

--- packages/mbtools/examples/protein.tcl
+++ packages/mbtools/examples/protein.tcl
@@ -1,3 +1,22 @@
+# Copyright (C) 2010 The ESPResSo project
+# Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010 Max-Planck-Institute for Polymer Research, Theory Gr
+oup, PO Box 3148, 55021 Mainz, Germany
+#  
+# This file is part of ESPResSo.
+#   
+# ESPResSo is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#  
+# ESPResSo is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 # Parameter file for simulating proteins in
 # a membrane
 #
