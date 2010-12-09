a41206eb8a8014a7a7c654d7c14cc53b35001222
Pedro Ojeda <pojeda@tamarau.ica1.uni-stuttgart.de>
2010-12-09 17:34:47 +0100

    Frequency dependent Structure factor

--- statistics_correlation.c
+++ statistics_correlation.c
@@ -16,7 +16,7 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 */ 
-//comentario nuevo
+//comentario siguiente
 #include "statistics_correlation.h"
 #include "particle_data.h"
 #include "parser.h"
