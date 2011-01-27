d9115ff2410495eb321e23e283918246500c80b8
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2011-01-27 11:53:03 +0100

    Virtual sites update -- Small correction to part 4

--- statistics.c
+++ statistics.c
@@ -3777,9 +3777,9 @@ int tclcommand_analyze(ClientData data, Tcl_Interp *interp, int argc, char **arg
   REGISTER_ANALYSIS("energy_kinetic", tclcommand_analyze_parse_and_print_energy_kinetic);
   REGISTER_ANALYSIS_W_ARG("pressure", tclcommand_analyze_parse_and_print_pressure, 0);
 #ifdef VIRTUAL_SITES
-+  // The following analysis commands apply only to the "center of mass"
-+  // implementation of virtual sites
-+  #ifdef VIRTUAL_SITES_COM
+  // The following analysis commands apply only to the "center of mass"
+  // implementation of virtual sites
+#ifdef VIRTUAL_SITES_COM
   REGISTER_ANALYSIS("energy_kinetic_mol", tclcommand_analyze_parse_and_print_energy_kinetic_mol);
   REGISTER_ANALYSIS("pressure_mol", tclcommand_analyze_parse_and_print_pressure_mol);
   REGISTER_ANALYSIS("check_mol", tclcommand_analyze_parse_and_print_check_mol);
