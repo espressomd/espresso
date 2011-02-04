baeebaad99d7af8b9a8c2efd59af7d5aecd8b04e
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2011-02-04 10:13:28 +0100

    Bugfix: Ghost communication at the wrong place in integrate_vv()
    When using "integrate 0" ghost communication has to happen before torques are converted to body-fixed reference frame.

--- integrate.c
+++ integrate.c
@@ -416,6 +416,9 @@ void integrate_vv(int n_steps)
    distribute_mol_force();
    if (check_runtime_errors()) return;
 #endif
+
+ghost_communicator(&cell_structure.collect_ghost_force_comm);
+
 #ifdef ROTATION
     convert_initial_torques();
 #endif
@@ -423,7 +426,6 @@ void integrate_vv(int n_steps)
     thermo_cool_down();
 
     /* Communication Step: ghost forces */
-    ghost_communicator(&cell_structure.collect_ghost_force_comm);
 
 
     /*apply trap forces to trapped molecules*/
