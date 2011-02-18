2bc6434497c33d81a9eb265810b0caff741f772f
Florian Weik <fweik@icq.uni-stuttgart.de>
2011-02-18 13:36:59 +0100

    mpifake: fixed inplace.

--- mpifake/mpi.c
+++ mpifake/mpi.c
@@ -431,7 +431,7 @@ void mpifake_copy(void *src, void *dest, int *count, MPI_Datatype *dtype) {
     break;
 
   default:
-    memmove((char *)dest, (char *)src, *count*(*dtype)->size);
+    memcpy((char *)dest, (char *)src, *count*(*dtype)->size);
 
   }
 
