04530a16447e1acb2453148cd807b593ca291348
Stefan Kesselheim <kessel@icp.uni-stuttgart.de>
2011-02-25 16:24:42 +0100

    new correlation sample. It still has to be improved!

--- samples/correlation.tcl
+++ samples/correlation.tcl
@@ -27,7 +27,9 @@
 ## with (not so important MD parameters)
 set box_l 10. 
 setmd box_l $box_l $box_l $box_l
-thermostat langevin 1. 01.5
+set friction 10.0
+set force 1.
+thermostat langevin 1. $friction
 setmd time_step 0.01
 setmd skin 0.1
 t_random seed [ pid ]
@@ -48,9 +50,14 @@ analyze correlation 2 autoupdate start
 
 ## Now we want to measure the mobility of particle 1. We use an external force 
 ## and investigate the mean of its velocity in x direction.
-part 2 pos 1 1 1 ext_force 1. 0. 0.
+set part_with_force 100
+set counter 2 
+for { set i 0 } { $i < $part_with_force } { incr i } {
+  part $counter pos 1 1 1 ext_force $force 0. 0. type 1
+  incr counter
+}
+analyze correlation 3 first_obs particle_velocities type { 1 } corr_operation componentwise_product tau_lin 20 tau_max 100. delta_t [ setmd time_step ] compress1 linear
 integrate 1000
-analyze correlation 3 first_obs particle_velocities id { 2 } corr_operation componentwise_product tau_lin 20 tau_max 100. delta_t [ setmd time_step ] compress1 linear
 analyze correlation 3 autoupdate start
 
 
@@ -63,7 +70,7 @@ inter 0 0 lennard-jones 0 0.25 0. 0.
 set var 0.
 set av 0.
 ## Now comes the main integration loop
-set nsteps 500000
+set nsteps 10000
 set ofile [ open "v.dat" "w"]
 for { set i 0 } { $i < $nsteps } { incr i } {
   integrate 1
@@ -88,13 +95,41 @@ analyze correlation 2 write_to_file "msd.dat"
 #analyze correlation 3 finalize
 analyze correlation 3 write_to_file "corr_with_force.dat"
 
-#Lets look at the average velocities of the particle
+#Lets look at the average velocities of the particles
 #with external force
-puts "avarage   [ analyze correlation 3 print average1 ]"
-puts "variance  [ analyze correlation 3 print variance1 ]"
-puts "corr_time [ analyze correlation 3 print correlation_time ]"
-exit
+set average [ analyze correlation 3 print average1 ]
+set variance [ analyze correlation 3 print variance1 ]
+set corrtime [ analyze correlation 3 print correlation_time ]
+set stdev_mean [ list ] 
+set true_value [ list ]
+set true_correlation_time [ list ]
+for { set i 0 } { $i < $part_with_force } { incr i } {
+  lappend true_value [ expr $force/$friction ] 0. 0.
+  lappend true_correlation_time [ expr 1/$friction ]  [ expr 1/$friction ] [ expr 1/$friction ]
+}
+for { set i 0 } { $i < [ llength $average ] } { incr i } {
+  if { [ lindex $corrtime $i  ] > 0 } {
+    lappend stdev_mean [ expr sqrt( [ lindex $variance $i ] / $nsteps / [ setmd time_step ] * [ lindex $corrtime $i ] ) ]
+  } else {
+    lappend stdev_mean 0.
+  }
+}
 
+puts "average    [ lrange $average    0 5 ] "
+puts "stdev_mean [ lrange $stdev_mean 0 5 ] "
+puts "true value [ lrange $true_value 0 5 ] "
+puts "corrtime   [ lrange $corrtime   0 5 ] "
+puts "true ctime [ lrange $true_correlation_time 0 5 ]"
+set counter 0.
+for { set i 0 } { $i < [ llength $average ] } { incr i } {
+  if { abs([ lindex $average $i ] - [ lindex $true_value $i ]) < [ lindex $stdev_mean $i ] } {
+    set counter [ expr $counter + 1 ]
+  }
+}
+puts "[ expr $counter/([llength $average ]) *100] % are within 1 sigma"
+
+
+exit
 ## Finally we print the result to the screen.
 
 #puts "average [ expr $av/$nsteps ] [ lindex [ analyze correlation 0 print average1 ] 0 ]"
