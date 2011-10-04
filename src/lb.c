5911ac91f534ab4a5c539d773aabc10fca8f24e4
Georg Rempfer <github@wo-geht-was.net>
2011-10-05 01:47:06 +0200

    Finished TCL-frontend for the lbfluid print command

--- src/lb.c
+++ src/lb.c
@@ -197,9 +197,6 @@ int tclcommand_lbfluid_cpu(Tcl_Interp *interp, int argc, char **argv) {
   else if (ARG0_IS_S("init")) {
     lbfluid_tcl_print_usage(interp);
     return TCL_ERROR;
-  } 
-  else if (ARG0_IS_S("print_interpolated_velocity")) {
-    return tclcommand_lbfluid_print_interpolated_velocity(interp, argc-1, argv+1);
   }
   else
   	while (argc > 0) {
@@ -352,37 +349,50 @@ int tclcommand_lbfluid_cpu(Tcl_Interp *interp, int argc, char **argv) {
         if ( argc < 3 || (ARG1_IS_S("vtk") && argc < 4) ) {
 	        Tcl_AppendResult(interp, "lbfluid print requires at least 2 arguments. Usage: lbfluid print [vtk] velocity|boundary filename", (char *)NULL);
           return TCL_ERROR;
-        } else {
+        }
+        else {
           argc--; argv++;
           if (ARG0_IS_S("vtk")) {
           	if (ARG1_IS_S("boundary")) {
-				      if ( lb_lbfluid_cpu_print_vtk_boundary() != 0 ) {
+				      if ( lb_lbfluid_cpu_print_vtk_boundary(argv[2]) != 0 ) {
 					      Tcl_AppendResult(interp, "Unknown Error at lbfluid print vtk boundary", (char *)NULL);
 				        return TCL_ERROR;
 				      }
-				    } else if (ARG1_IS_S("velocity")) {
-				      if ( lb_lbfluid_cpu_print_vtk_velocity() != 0 ) {
+				    }
+				    else if (ARG1_IS_S("velocity")) {
+				      if ( lb_lbfluid_cpu_print_vtk_velocity(argv[2]) != 0 ) {
 					      Tcl_AppendResult(interp, "Unknown Error at lbfluid print vtk velocity", (char *)NULL);
 				        return TCL_ERROR;
 				      }
 				    }
+				    else {
+				    	return TCL_ERROR;
+				    }
 				    argc-=3; argv+=3;
-		      } else {
+		      }
+		      else {
 		      	if (ARG0_IS_S("boundary")) {
-			   	  	if ( lb_lbfluid_cpu_print_boundary() != 0 ) {
+			   	  	if ( lb_lbfluid_cpu_print_boundary(argv[1]) != 0 ) {
 				    	  Tcl_AppendResult(interp, "Unknown Error at lbfluid print boundary", (char *)NULL);
 			      	  return TCL_ERROR;
 			      	}
-			    	} else if (ARG1_IS_S("velocity")) {
-			      	if ( lb_lbfluid_cpu_print_velocity() != 0 ) {
+			    	}
+			    	else if (ARG1_IS_S("velocity")) {
+			      	if ( lb_lbfluid_cpu_print_velocity(argv[1]) != 0 ) {
 				    	  Tcl_AppendResult(interp, "Unknown Error at lbfluid print velocity", (char *)NULL);
 			      	  return TCL_ERROR;
 			      	}
 			      }
+				    else {
+				    	return TCL_ERROR;
+				    }
 			      argc-=2; argv+=2;
 		      }
         }
       }
+			else if (ARG0_IS_S("print_interpolated_velocity")) { //this has to come after print
+				return tclcommand_lbfluid_print_interpolated_velocity(interp, argc-1, argv+1);
+			}
       else {
     	  Tcl_AppendResult(interp, "unknown feature \"", argv[0],"\" of lbfluid", (char *)NULL);
     	  return TCL_ERROR ;
@@ -663,6 +673,26 @@ int lb_lbfluid_get_ext_force(double* p_fx, double* p_fy, double* p_fz){
   *p_fy = lbpar.ext_force[1];
   *p_fz = lbpar.ext_force[2];
   return 0;
+}
+
+int lb_lbfluid_cpu_print_vtk_boundary(char* filename) {
+	printf("lb_lbfluid_cpu_print_vtk_boundary: gridsize=(%d, %d, %d)\n", lblattice.grid[0], lblattice.grid[1], lblattice.grid[2]);
+	return 0;
+}
+
+int lb_lbfluid_cpu_print_vtk_velocity(char* filename) {
+	printf("called lb_lbfluid_cpu_print_vtk_velocity(%s)\n", filename);
+	return 0;
+}
+
+int lb_lbfluid_cpu_print_boundary(char* filename) {
+	printf("called lb_lbfluid_cpu_print_boundary(%s)\n", filename);
+	return 0;
+}
+
+int lb_lbfluid_cpu_print_velocity(char* filename) {
+	printf("called lb_lbfluid_cpu_print_velocity(%s)\n", filename);
+	return 0;
 }
 
 int lb_lbnode_get_rho(int* ind, double* p_rho){
