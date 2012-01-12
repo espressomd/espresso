cfae690aeb8e4e0305ff4ee37ac2d30c16875296
Pedro Sanchez <psanchez@icp.uni-stuttgart.de>
2012-01-12 16:24:41 +0100

    thermostat partly done, pending of conditional compiling stuff

--- cython/examples/thermostat_test.py
+++ cython/examples/thermostat_test.py
@@ -4,8 +4,22 @@
 import ctypes
 import sys
 sys.setdlopenflags((sys.getdlopenflags() | ctypes.RTLD_GLOBAL ))
-
-print sys.path
-
 import espresso as es
 print dir(es)
+
+th=es.thermostat.Thermostat()
+print th.getStatus() # not initialized
+th.turnOff()
+print th.getStatus() # off
+th.setLangevin(11., 12.)
+print th.getStatus() # langevin
+
+try:
+    th.setLangevin()
+except ValueError, err:
+    print err # wrong args
+
+try:
+    th.setLangevin("lolailo",-1.)
+except ValueError, err:
+    print err # wrong args type/value
--- cython/thermostat.pxd
+++ cython/thermostat.pxd
@@ -1,6 +1,15 @@
 cdef extern from "communication.h":
-  void mpi_set_time_step(double time_step)
-  void mpi_bcast_parameter(int p)
+    void mpi_bcast_parameter(int p)
 
 cdef extern from "global.h":
-  int FIELD_TEMPERATURE
+    int FIELD_TEMPERATURE
+    int FIELD_THERMO_SWITCH
+    int FIELD_TEMPERATURE
+    int FIELD_LANGEVIN_GAMMA
+
+cdef extern from "thermostat.h":
+    double temperature
+    int thermo_switch
+    double langevin_gamma
+    int THERMO_OFF
+    int THERMO_LANGEVIN
