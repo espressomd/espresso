44d96649204d8a3e5f905ffd285814c41efd3335
Peter Košovan <kosovan@icp.uni-stuttgart.de>
2012-01-13 17:17:32 +0100

    All global variables in cython, except for issues related to conditional compilation

--- cython/global_variables.pxd
+++ cython/global_variables.pxd
@@ -1,16 +1,29 @@
 cdef extern from "global.h":
     int FIELD_MAXNUMCELLS
+    int FIELD_MINNUMCELLS
+    int FIELD_NODEGRID
+    int FIELD_NPTISO_PISTON
+    int FIELD_NPTISO_PDIFF
+    int FIELD_PERIODIC
+    int FIELD_SIMTIME
 
 cdef extern from "communication.h":
+    extern int n_nodes
     void mpi_set_time_step(double time_step)
     void mpi_bcast_parameter(int p)
 
 cdef extern from "integrate.h":
     double time_step
     extern int integ_switch
+    extern double sim_time
+    extern double verlet_reuse
+
 
 cdef extern from "verlet.h":
     double skin
+
+cdef extern from "lattice.h":
+    extern int lattice_switch
 
 cdef extern from "../src/domain_decomposition.h":
     ctypedef struct IA_Neighbor:
@@ -23,18 +36,74 @@ cdef extern from "../src/domain_decomposition.h":
     
     extern DomainDecomposition dd
     extern int max_num_cells
+    extern int min_num_cells
+    extern double max_skin
+    int calc_processor_min_num_cells()
 
   
+cdef extern from "particle_data.h":
+    extern int  n_total_particles
+
+
 cdef extern from "interaction_data.h":
     double dpd_gamma
     double dpd_r_cut
     extern double max_cut
     extern int max_seen_particle
+    extern int n_particle_types
+    extern double max_cut_nonbonded
+    extern double max_cut_bonded
+
 
 cdef extern from "thermostat.h":
     double langevin_gamma
+    extern double nptiso_gamma0
+    extern double nptiso_gammav
+    extern double temperature 
+    extern int thermo_switch     
+
+
+cdef extern from "dpd.h":
+    extern int dpd_wf
+    extern double dpd_tgamma
+    extern double dpd_tr_cut
+    extern int dpd_twf
+
+
+#FIXME this will only work with conditional compilation of address
+#cdef extern from "adresso.h":
+#    extern double adress_vars[7]
+
+
+cdef extern from "cells.h":
+    extern double max_range
+
+cdef extern from "layered.h":
+    extern int n_layers
+
+cdef extern from "rattle.h":
+    extern int n_rigidbonds
+
+
+cdef extern from "tuning.h":
+    extern int timing_samples
+
+cdef extern from "imd.h":
+    extern int transfer_rate
+
 
 cdef extern from "grid.h":
     double box_l[3]
     double local_box_l[3]
+    extern int node_grid[3]
+    extern int periodic
+
+cdef extern from "npt.h":
+    ctypedef struct nptiso_struct:
+        double p_ext
+        double p_inst
+        double p_inst_av
+        double p_diff
+        double piston
+    extern nptiso_struct nptiso
 
--- cython/global_variables.pyx
+++ cython/global_variables.pyx
@@ -8,15 +8,6 @@ cdef class GlobalsHandle:
     def __init__(self):
         pass
   
-    property time_step:
-        def __set__(self, double _time_step):
-            if _time_step <= 0:
-              raise ValueError("Time Step must be positive")
-            mpi_set_time_step(_time_step)
-        def __get__(self):
-            global time_step
-            return time_step
-  
     property box_l:
         def __set__(self, _box_l):
             global box_l
@@ -41,6 +32,7 @@ cdef class GlobalsHandle:
         def __get__(self):
             return np.array( [ dd.cell_size[0], dd.cell_size[1], dd.cell_size[2] ] )
 
+    
     property dpd_gamma:
         def __get__(self):
             return dpd_gamma;
@@ -70,18 +62,141 @@ cdef class GlobalsHandle:
     property max_num_cells:
         def __set__(self, int _max_num_cells):
             global max_num_cells
-	    #FIXME add check if max_num_cells > min_num_cells
-            if _max_num_cells <= 0:
-                raise ValueError("max_num_cells must be >= 0")
+            if _max_num_cells < min_num_cells:
+                raise ValueError("max_num_cells must be >= min_num_cells (currently "+str(min_num_cells)+")")
             max_num_cells=_max_num_cells
             mpi_bcast_parameter(FIELD_MAXNUMCELLS);
         def __get__(self):
             return max_num_cells;
+
+    property min_num_cells:
+        def __set__(self, int _min_num_cells): 
+            global min_num_cells 
+            min = calc_processor_min_num_cells()
+            if _min_num_cells < min:
+                raise ValueError("min_num_cells must be >= processor_min_num_cells (currently "+str(min)+")")
+            if _min_num_cells > max_num_cells:
+                raise ValueError("min_num_cells must be <= max_num_cells (currently "+str(max_num_cells)+")")
+            min_num_cells=_min_num_cells
+            mpi_bcast_parameter(FIELD_MINNUMCELLS);
+        def __get__(self):
+            return min_num_cells;
     
     property max_part:
         def __get__(self):
             return max_seen_particle;
   
+    property max_range:
+        def __get__(self):
+            return max_range;
+  
+    property max_skin:
+        def __get__(self):
+            return max_skin;
+  
+    property n_layers:
+        def __get__(self):
+            return n_layers;
+  
+    property n_nodes:
+        def __get__(self):
+            return n_nodes;
+  
+    property n_part:
+        def __get__(self):
+            return n_total_particles;
+  
+    property n_part_types:
+        def __get__(self):
+            return n_particle_types;
+  
+    property n_rigidbonds:
+        def __get__(self):
+            return n_rigidbonds;
+  
+    property node_grid:
+        def __set__(self, _node_grid):
+            global node_grid
+            if len(_node_grid) != 3:
+                raise ValueError("node_grid must be of length 3")
+            for i in range(3):
+                if _node_grid[i] <= 0:
+                    raise ValueError("node_grid must be > 0 in all directions")
+                node_grid[i]=_node_grid[i]
+            if _node_grid[0]*_node_grid[1]*_node_grid[2] != n_nodes:
+                raise ValueError("node_grid does not fit n_nodes ("+str(n_nodes)+")")
+            for i in range(3):
+                node_grid[i]=_node_grid[i]
+            mpi_bcast_parameter(FIELD_NODEGRID)
+        def __get__(self):
+            return np.array([node_grid[0], node_grid[1], node_grid[2]])
+  
+    property nptiso_gamma0:
+        def __get__(self):
+            return nptiso_gamma0;
+  
+    property nptiso_gammav:
+        def __get__(self):
+            return nptiso_gammav;
+  
+    property npt_p_ext:
+        def __get__(self):
+            return nptiso.p_ext;
+  
+    property npt_p_inst:
+        def __get__(self):
+            return nptiso.p_inst;
+  
+    property npt_p_inst_av:
+        def __get__(self):
+            return nptiso.p_inst_av;
+  
+    property npt_piston:
+        def __set__(self, _npt_piston):
+            global npt_piston
+            if _npt_piston < 0:
+                raise ValueError("npt_piston must be > 0")
+            nptiso.piston=_npt_piston
+            mpi_bcast_parameter(FIELD_NPTISO_PISTON)
+        def __get__(self):
+            global npt_piston
+            return nptiso.piston
+
+    property npt_p_diff:
+        def __set__(self, _npt_p_diff):
+            global npt_p_diff
+            nptiso.p_diff=_npt_p_diff
+            mpi_bcast_parameter(FIELD_NPTISO_PDIFF)
+        def __get__(self):
+            global npt_p_diff
+            return nptiso.p_diff
+
+    property periodicity:
+        def __set__(self, _periodic):
+            global periodic
+            if len(_periodic) != 3:
+                raise ValueError("periodicity must be of length 3, got length "+str(len(_periodic)))
+            periodicity=np.zeros(3);
+            for i in range(3):
+                if _periodic[i] != 1:
+                    raise ValueError("Until we can handle conditional compilation, only periodicity [1,1,1] is supported in python interface")
+# FIXME replace the previous two lines by the following when we have dealt with the conditional compilation
+#                if _periodic[i] != 0 and _periodic[i] != 1:
+#                    raise ValueError("exach entry of periodicity has to be 0 or 1, got "+str(_periodic))
+            for i in range(3):
+                periodicity[i]=_periodic[i];
+            periodic=4*_periodic[2] + 2*_periodic[1] + _periodic[0]; 
+            # first 3 bits of periodic determine the periodicity
+            # until we can handle contitional compilatio, periodic=7 is the only value which makes sense
+            mpi_bcast_parameter(FIELD_PERIODIC);
+        def __get__(self):
+            global periodic 
+            periodicity=np.zeros(3);
+            periodicity[0]=periodic%2; 
+            periodicity[1]=int(periodic/2)%2; 
+            periodicity[2]=int(periodic/4)%2; 
+            return periodicity
+
     property skin:
         def __set__(self, double _skin):
             if _skin <= 0:
@@ -93,3 +208,105 @@ cdef class GlobalsHandle:
             global skin
             return skin
 
+    property temperature:
+        def __get__(self):
+            global temperature;
+            return temperature;
+  
+    property thermo_switch:
+        def __get__(self):
+            global thermo_switch;
+            return thermo_switch;
+  
+    property time:
+        def __set__(self, double _time):
+            if _time <= 0:
+                raise ValueError("Simulation time must be >= 0")
+            global sim_time
+            sim_time=_time
+            mpi_bcast_parameter(FIELD_SIMTIME)
+        def __get__(self): 
+            global sim_time
+            return sim_time
+
+    property time_step:
+        def __set__(self, double _time_step):
+        #FIXME when conditional compilation works: comparison with LB_time_step
+            if _time_step <= 0:
+              raise ValueError("Time Step must be positive")
+            mpi_set_time_step(_time_step)
+        def __get__(self):
+            global time_step
+            return time_step
+  
+    property timings:
+        def __set__(self, int _timings):
+            global timing_samples;
+            if _timings <= 0:
+                timing_samples=0;
+            else:
+                timing_samples=_timings;
+        def __get__(self):
+            global timing_samples
+            return timing_samples
+  
+    property transfer_rate:
+        def __get__(self):
+            global transfer_rate;
+            return transfer_rate;
+  
+    property max_cut_nonbonded:
+        def __get__(self):
+            global max_cut_nonbonded;
+            return max_cut_nonbonded;
+  
+    property verlet_reuse:
+        def __get__(self):
+            global verlet_reuse;
+            return verlet_reuse;
+
+
+    property lattice_switch:
+        def __get__(self):
+            global lattice_switch;
+            return lattice_switch;
+  
+    property dpd_tgamma:
+        def __get__(self):
+            global dpd_tgamma;
+            return dpd_tgamma;
+  
+    property dpd_tr_cut:
+        def __get__(self):
+            global dpd_tr_cut;
+            return dpd_tr_cut;
+  
+    property dpd_twf:
+        def __get__(self):
+            global dpd_twf;
+            return dpd_twf;
+  
+    property dpd_wf:
+        def __get__(self):
+            global dpd_wf;
+            return dpd_wf;
+
+#FIXME this will only work with conditional compilation of address
+#    property dpd_wf:
+#        def __get__(self):
+#            global adress_vars;
+#            return np.array( [ \
+#            adress_vars[0], \
+#            adress_vars[1], \
+#            adress_vars[2], \
+#            adress_vars[3], \
+#            adress_vars[4], \
+#            adress_vars[5], \
+#            adress_vars[6] \
+#            ])
+#  
+    property max_cut_bonded:
+        def __get__(self):
+            global max_cut_bonded;
+            return max_cut_bonded;
+  
