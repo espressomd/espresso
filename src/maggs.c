2bcb4bd678dcbd6db665c79fabdd6e80ceac3c8a
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2013-03-07 09:20:40 +0100

    Header of maggs.h.

--- src/maggs.c
+++ src/maggs.c
@@ -1,8 +1,8 @@
 /*
-  Copyright (C) 2010,2011 Florian Fahrenberger
   Copyright (C) 2010,2011,2012,2013 The ESPResSo project
-  Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010 
-  Max-Planck-Institute for Polymer Research, Theory Group
+  Copyright (C) 2010,2011 Florian Fahrenberger
+  Copyright (C) 2007,2008,2009,2010 
+    Max-Planck-Institute for Polymer Research, Theory Group
  
   This file is part of ESPResSo.
  
