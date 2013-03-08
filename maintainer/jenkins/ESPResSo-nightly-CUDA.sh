4f2538209fcfac582305deaf9fd74a5cc94c82db
Olaf Lenz <olaf@lenz.name>
2013-03-08 22:23:49 +0100

    CUDA job uses 4 cores. (cherry picked from commit 441424aa8d3a6db31f2ca0fbed61bf14d3761ca0)

--- maintainer/jenkins/ESPResSo-nightly-CUDA.sh
+++ maintainer/jenkins/ESPResSo-nightly-CUDA.sh
@@ -6,13 +6,13 @@ bootstrap
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
 
 start "CONFIGURE"
-./configure --with-mpi --with-cuda CPU_COUNT="2"
+./configure --with-mpi --with-cuda CPU_COUNT="4"
 end "CONFIGURE"
 
 use_myconfig LBGPU
 
 start "BUILD"
-make -j 2
+make -j 4
 end "BUILD"
 
 check
