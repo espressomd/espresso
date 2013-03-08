873611bf9210f380c499ac1c09308b3e4a213d5b
Olaf Lenz <olaf@lenz.name>
2013-03-08 23:15:21 +0100

    Took out pushes from script, needs to be done in jenkins.

--- maintainer/jenkins/ESPResSo.sh
+++ maintainer/jenkins/ESPResSo.sh
@@ -1,14 +1,6 @@
 #!/bin/bash --login -e
 source maintainer/jenkins/common.sh
 
-if [ -n "$JENKINS_URL" ]; then
-    echo "Pushing to local git clone..."
-    git push --force /home/jenkins/espresso.git master
-else
-    echo "Would push to local git clone in Jenkins..."
-fi
-
-
 bootstrap
 
 start "CONFIGURE"
@@ -24,17 +16,3 @@ end "BUILD"
 check
 doc
 dist
-
-start "GITPUSH"
-# push to savannah
-if [ -z "$CHECK_UNSTABLE" ]; then
-  echo "Pushing to Savannah..."
-  if [ -n "$JENKINS_URL" ]; then
-      git push ssh://olenz@git.sv.gnu.org/srv/git/espressomd.git master
-  else
-      echo "Would push to Savannah in Jenkins..."
-  fi
-else
-  echo "Build is unstable, not pushing to Savannah."
-fi
-end "GITPUSH"
