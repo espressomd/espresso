3e47c180075123577543eb8692e724dd4664b049
Olaf Lenz <olaf@lenz.name>
2013-03-08 22:04:03 +0100

    Added Jenkins build scripts. (cherry picked from commit cf89aa8441368812af0b5fd732a63b2b3e951a72)

---
+++ maintainer/jenkins/ESPResSo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash --login -e
+source maintainer/jenkins/common.sh
+
+if [ -n "$JENKINS_URL" ]; then
+    echo "Pushing to local git clone..."
+    git push --force /home/jenkins/espresso.git master
+else
+    echo "Would push to local git clone in Jenkins..."
+fi
+
+
+bootstrap
+
+start "CONFIGURE"
+./configure CPU_COUNT=2
+end "CONFIGURE"
+
+use_myconfig maxset
+
+start "BUILD"
+make -j 2
+end "BUILD"
+
+check
+doc
+dist
+
+start "GITPUSH"
+# push to savannah
+if [ -z "$CHECK_UNSTABLE" ]; then
+  echo "Pushing to Savannah..."
+  if [ -n "$JENKINS_URL" ]; then
+      git push ssh://olenz@git.sv.gnu.org/srv/git/espressomd.git master
+  else
+      echo "Would push to Savannah in Jenkins..."
+  fi
+else
+  echo "Build is unstable, not pushing to Savannah."
+fi
+end "GITPUSH"
