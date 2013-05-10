9928f27bf361a4a51f74e5d117d9d0a4018f4f58
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2013-05-10 16:59:14 +0200

    Added missing copyright headers, moved directory fsi to object-in-fluid.

--- src/object-in-fluid/area_force_local.c
+++ src/object-in-fluid/area_force_local.c
@@ -1,6 +1,5 @@
 /*
-  Copyright (C) 2010 The ESPResSo project
-  Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010 Max-Planck-Institute for Polymer Research, Theory Group, PO Box 3148, 55021 Mainz, Germany
+  Copyright (C) 2012,2013 The ESPResSo project
   
   This file is part of ESPResSo.
   
@@ -18,28 +17,31 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 */
 
-/** \file volume_force.h
- *  Routines to calculate the VOLUME_FORCE energy or/and and force 
+/** \file area_force_local.h
+ *  Routines to calculate the AREA_FORCE_LOCAL energy or/and and force 
  *  for a particle triple (triangle from mesh). (Dupin2007)
  *  \ref forces.c
 */
 
-#include "volume_force.h"
+#include "area_force_local.h"
 #include "communication.h"
 
-/** set parameters for the VOLUME_FORCE potential. 
+
+/************************************************************/
+
+/** set parameters for the AREA_FORCE_LOCAL potential. 
 */
-int volume_force_set_params(int bond_type, double V0, double kv)
+int area_force_local_set_params(int bond_type, double A0_l, double ka_l)
 {
   if(bond_type < 0)
     return ES_ERROR;
 
   make_bond_type_exist(bond_type);
 
-  bonded_ia_params[bond_type].p.volume_force.kv = kv;
-  bonded_ia_params[bond_type].p.volume_force.V0 = V0;
+  bonded_ia_params[bond_type].p.area_force_local.ka_l = ka_l;
+  bonded_ia_params[bond_type].p.area_force_local.A0_l = A0_l;
 
-  bonded_ia_params[bond_type].type = BONDED_IA_VOLUME_FORCE;
+  bonded_ia_params[bond_type].type = BONDED_IA_AREA_FORCE_LOCAL;
   bonded_ia_params[bond_type].num = 2;				
  
   /* broadcast interaction parameters */
@@ -47,5 +49,3 @@ int volume_force_set_params(int bond_type, double V0, double kv)
 
   return ES_OK;
 }
-
-
