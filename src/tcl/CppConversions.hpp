296222f302e04116771b4b4cd73e40f33e84cbad
Stefan Kesselheim <kessel@icp.uni-stuttgart.de>
2013-09-12 12:34:20 +0200

    small fixes to the generic Tcl_Append

--- src/tcl/CppConversions.hpp
+++ src/tcl/CppConversions.hpp
@@ -1,11 +1,32 @@
+/*
+  Copyright (C) 2010,2011,2012,2013 The ESPResSo project
+  Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010 
+    Max-Planck-Institute for Polymer Research, Theory Group
+  
+  This file is part of ESPResSo.
+  
+  ESPResSo is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  ESPResSo is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
 
-#ifdef CPP_CONVERSIONS_H
+#ifndef CPP_CONVERSIONS_H
 #define CPP_CONVERSIONS_H
 
 #include "tcl.h"
+#include <iostream>
 #include "Eigen/Core"
 
-/* *
+/**
  * In this file we define a few generic functions to make returning 
  * complex c++ data easy. The idea is as follows: 
  *
@@ -35,6 +56,12 @@
  * is a list.
  */
 
+/**
+ * Generic implementation for types that support << (ostream)
+ *
+ * The is_in_list flag can not be used, as we can not know if
+ * the object passed is actually a list-type object or a simple object.
+ */
 template <typename T>
 void Tcl_Append(Tcl_Interp* interp, const T& what, bool is_in_list=0) {
   std::stringstream ss;
@@ -42,17 +69,32 @@ void Tcl_Append(Tcl_Interp* interp, const T& what, bool is_in_list=0) {
   Tcl_AppendResult(interp, ss.str().c_str(), (char *)NULL);
 }
 
+/**
+ * Implementation for double, using TCL_PrintDouble, to make sure 
+ * TCL is happy with the float representation.
+ */
 void Tcl_Append(Tcl_Interp* interp, const double& what, bool is_in_list=0) {
   char buffer[TCL_DOUBLE_SPACE];
   Tcl_PrintDouble(interp, what, buffer);
   Tcl_AppendResult(interp, buffer, (char *)NULL);
 }
 
+/**
+ * Implementation for fixed width char arrays:
+ * Usage Tcl_Append("Message for user")
+ * 
+ */
 template <size_t N>
 void Tcl_Append(Tcl_Interp* interp, const char (&what)[N], bool is_in_list=0) {
   Tcl_AppendResult(interp, what, (char *)NULL);
 }
 
+/**
+ * Implementation for Eigen::Vector3d
+ * 
+ * Appends a result that can be interpreted as TCL list.
+ * 
+ */
 void Tcl_Append(Tcl_Interp* interp, const Eigen::Vector3d &what, bool is_in_list=0) {
   if (is_in_list) 
     Tcl_Append(interp, "{");
@@ -67,13 +109,30 @@ void Tcl_Append(Tcl_Interp* interp, const Eigen::Vector3d &what, bool is_in_list
     Tcl_Append(interp, "}");
 }
 
+/**
+ * Implementation for std::vector<T>
+ * 
+ * Simply calls the generic function Tcl_Append_Iterable.
+ * This then calls Tcl_Append for every member of the vector.
+ * 
+ */
 template <typename T>
 void Tcl_Append(Tcl_Interp* interp, const std::vector<T> what, bool is_in_list=0) {
-  append_iterable_to_tcl_result(interp, what, is_in_list);
+  Tcl_Append_Iterable(interp, what, is_in_list);
 }
 
+/**
+ * Generic implemtation that supports everything which can be iterated over.
+ * This requires 
+ * typedef const_iterator
+ * const_reference begin()
+ * const_reference end()
+ *
+ * Calls the Tcl_Append for every member of the iterable object.
+ * 
+ */
 template<class C>
-void append_iterable_to_tcl_result(Tcl_Interp* interp, const C& what, bool is_in_list=0) {
+void Tcl_Append_Iterable(Tcl_Interp* interp, const C& what, bool is_in_list=0) {
 
   typename C::const_iterator it=what.begin();
   
@@ -84,6 +143,7 @@ void append_iterable_to_tcl_result(Tcl_Interp* interp, const C& what, bool is_in
     Tcl_Append(interp, "{");
   if (it!=what.end()) {
     Tcl_Append(interp, *it, true);
+    ++it;
   }
   while (it!=what.end()) {
     Tcl_Append(interp, " ");
