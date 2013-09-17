5a2995b7fa993810380f7a441e94e6d272ef5e84
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2013-09-17 13:39:02 +0200

    Moved main loop components into mpi_integrate instead of tclcommand_integrate.

--- src/tcl/integrate_tcl.cpp
+++ src/tcl/integrate_tcl.cpp
@@ -195,7 +195,6 @@ int tclcommand_integrate_set_npt_isotropic(Tcl_Interp *interp, int argc, char **
 int tclcommand_integrate(ClientData data, Tcl_Interp *interp, int argc, char **argv) 
 {
   int  n_steps;
-  int i;
   
   INTEG_TRACE(fprintf(stderr,"%d: integrate:\n",this_node));
 
@@ -222,16 +221,8 @@ int tclcommand_integrate(ClientData data, Tcl_Interp *interp, int argc, char **a
     return tclcommand_integrate_print_usage(interp);;
   }
   /* perform integration */
-  if (!correlations_autoupdate) {
-    if (mpi_integrate(n_steps))
-      return gather_runtime_errors(interp, TCL_OK);
-  } else  {
-    for (i=0; i<n_steps; i++) {
-      if (mpi_integrate(1))
-        return gather_runtime_errors(interp, TCL_OK);
-      autoupdate_correlations();
-    }
-  }
+  if (mpi_integrate(n_steps))
+    return gather_runtime_errors(interp, TCL_OK);
   return TCL_OK;
 }
 
