cd6a00d2955da66462fd9e27ab938a71a46fe11d
J. de Graaf <github.degraaf@gmail.com>
2013-11-19 15:55:30 +0100

    Removed erronous commenting.

--- src/lbgpu_cuda.cu
+++ src/lbgpu_cuda.cu
@@ -1221,12 +1221,11 @@ __device__ void apply_forces(unsigned int index, float *mode, LB_node_force_gpu 
         node_f.force[(1 + ii*3 ) * para.number_of_nodes + index] = para.ext_force[1]*force_factor;
         node_f.force[(2 + ii*3 ) * para.number_of_nodes + index] = para.ext_force[2]*force_factor;
       }
-      else{
-/* TODO : UNCOMMENT IF YOU ONLY HAVE LB AND NO EK
+      else
+      {
         node_f.force[(0 + ii*3 ) * para.number_of_nodes + index] = 0.0f;
         node_f.force[(1 + ii*3 ) * para.number_of_nodes + index] = 0.0f;
         node_f.force[(2 + ii*3 ) * para.number_of_nodes + index] = 0.0f;
-*/
       }
 #else
       /** reset force */
