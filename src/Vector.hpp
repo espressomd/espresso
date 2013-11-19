fa108f1ab6cada2848dafe5577e8ad14402bc9bb
Florian Weik <fweik@icp.uni-stuttgart.de>
2013-11-19 15:12:58 +0100

    Updated makefile.

--- src/Vector.hpp
+++ src/Vector.hpp
@@ -1,12 +1,17 @@
-template<int n, type Scalar>
+#include <algorithm>
+
+template<int n, typename Scalar>
 class Vector {
     private:
         Scalar *d;
     public:
         Vector()                   : d(new Scalar[n]) {}
         ~Vector()                    {delete[] d;}
-        Vector(const Vector& rhs)    : a(new Scalar[n]) {std::copy(rhs.d,rhs.d+n,d);}
+        Vector(const Vector& rhs)    : d(new Scalar[n]) {std::copy(rhs.d,rhs.d+n,d);}
         void swap(Vector& rhs)       {std::swap(d,rhs.d);}
         Vector& operator=(const Vector& rhs) 
                                    {Vector tmp(rhs); swap(rhs); return *this;}
 };
+
+typedef Vector<3, double> Vector3d;
+
