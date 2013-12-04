4cf8d6acd9bf6dc09982d24ea1fec5e0b79de816
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2013-12-04 12:53:22 +0100

    Python: Doc strings and type checking

--- src/python/utils.pyx
+++ src/python/utils.pyx
@@ -16,16 +16,21 @@ cdef IntList* create_IntList_from_python_object(obj):
 
   return il
 
-cdef checkTypeOrExcept(x,t,msg):
-  """Checks that x is of type t, otherwise throws ValueError with the message msg.
+cdef checkTypeOrExcept(x,n,t,msg):
+  """Checks that x is of type t and that n values are given, otherwise throws ValueError with the message msg.
      If x is an array/list/tuple, all elements are checked.
      """
   # Check whether x is an array/list/tuple or a single value
-  if hasattr(x, "__getitem__"): 
-    for i in range(len(x)):
-      if not isinstance(x[i], t):
-         raise ValueError(msg)
+  if n>1:
+    if hasattr(x, "__getitem__"): 
+      for i in range(len(x)):
+        if not isinstance(x[i], t):
+           raise ValueError(msg)
+    else:
+      # if n>1, but the user passed a single value, also throw exception
+      raise ValueError(msg)
   else:
+    # N=1 and a single value
     if not isinstance(x, t):
        raise ValueError(msg)
 
