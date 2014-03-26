b2be67123c74311921d2b465c218c4865f83d04b
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2014-03-26 17:40:21 +0100

    python: Allow integres as input when setting particle properties which are of type float

--- src/python/utils.pyx
+++ src/python/utils.pyx
@@ -18,19 +18,23 @@ cdef IntList* create_IntList_from_python_object(obj):
 
 cdef checkTypeOrExcept(x,n,t,msg):
   """Checks that x is of type t and that n values are given, otherwise throws ValueError with the message msg.
-     If x is an array/list/tuple, all elements are checked.
+     If x is an array/list/tuple, the type checking is done on the elements, and
+     all elements are checked.
+     Integers are accepted when a float was asked for.
      """
   # Check whether x is an array/list/tuple or a single value
   if n>1:
     if hasattr(x, "__getitem__"): 
       for i in range(len(x)):
         if not isinstance(x[i], t):
-           raise ValueError(msg + " -- Item "+str(i)+" was of type "+type(x[i]).__name__)
+          if not (t==float and isinstance(x[i],int)):
+             raise ValueError(msg + " -- Item "+str(i)+" was of type "+type(x[i]).__name__)
     else:
       # if n>1, but the user passed a single value, also throw exception
       raise ValueError(msg+" -- A single value was given but "+str(n)+" were expected.")
   else:
     # N=1 and a single value
     if not isinstance(x, t):
-       raise ValueError(msg+" -- Got an "+type(x).__name__)
+      if not (t==float and isinstance(x,int)):
+        raise ValueError(msg+" -- Got an "+type(x).__name__)
 
