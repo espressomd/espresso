070c1218a4c9fc29474848e81dad459040c4a468
mohamed-abdelkhalek <s1mokhal@stmail.uni-bayreuth.de>
2014-06-03 13:01:02 +0200

    new analyis scheme

--- triel_sim/live_analysis.R
+++ triel_sim/live_analysis.R
@@ -20,13 +20,13 @@ c2vx <- particle2[[4]][length(particle0[[4]])]
 
 #interpolatedp2x <- seq.int(2, 6.9999, 0.0001)
 par(mfrow=c(2,2))
-plot(data.frame(particle0[13], particle0[1]))
+plot(data.frame(particle0[13], particle0[1]), xlim=c(ct*zoomX, ct), ylim=c(c0posx*zoomY, c0posx+(c0posx*zoomY)))
 points(data.frame(particle1[13], particle1[1]), col=2)
 points(data.frame(particle2[13], particle2[1]), col=3)
 legend("topleft",c("p0","p1","p2"), col=c(1,2,3), pch=c(1,1,1))
 
 
-plot(data.frame(particle0[13], particle0[4]))
+plot(data.frame(particle0[13], particle0[4]), xlim=c(ct*zoomX, ct), ylim=c(c0vx*zoomY, c0vx + (c0vx*zoomY)))
 points(data.frame(particle1[13], particle1[4]), col=2)
 points(data.frame(particle2[13], particle2[4]), col=3)
 points(data.frame(particle2[13], particle2[10]/0.0010229858259037613), col=4)
@@ -41,8 +41,7 @@ lp0 <- sqrt(((particle2[[1]] - particle0[[1]])^2) + (particle2[[2]] - particle0[
 ## ## lp0 <- lp0[!is.na(lp0)]
 
 
-plot(particle0[[13]], l0, ylim=c(0,5))
+plot(particle0[[13]], l0, xlim=c(ct*zoomX, ct))
 points(particle0[[13]], lp0, col=2)
 points(data.frame(particle0[13], (0.5 * l0 * lp0)), col=3)
 legend("topleft",c("l(0)","l'(0)","A(0)"), col=c(1,2,3), pch=c(1,1,1))
-
