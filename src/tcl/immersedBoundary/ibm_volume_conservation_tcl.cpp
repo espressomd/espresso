7346bca81a4f792d8465044461c0388f1c9caeb7
StephanGekle <stephan.gekle@uni-bayreuth.de>
2014-10-15 08:03:17 +0200

    Fixed a bug in checkpoints for volcons and removed a few file headers

--- src/tcl/immersedBoundary/ibm_volume_conservation_tcl.cpp
+++ src/tcl/immersedBoundary/ibm_volume_conservation_tcl.cpp
@@ -1,23 +1,4 @@
-/*
-  Copyright (C) 2010,2011,2012,2013 The ESPResSo project
-  Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010 
-  Max-Planck-Institute for Polymer Research, Theory Group
-  
-  This file is part of ESPResSo.
-  
-  ESPResSo is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-  
-  ESPResSo is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-*/
+
 
 #include "config.hpp"
 
@@ -83,7 +64,7 @@ int tclcommand_inter_parse_ibm_volume_conservation(Tcl_Interp *interp, int bond_
   else
   {
     // ****** Continue from checkpoint *********
-    if ( argc != 2 )
+    if ( argc != 3 )
     {
       Tcl_AppendResult(interp, "Wrong number of arguments reading checkpoint for ibm_volcons ", (char *) NULL);
       return TCL_ERROR;
@@ -119,7 +100,7 @@ int tclprint_to_result_ibm_volume_conservation(Tcl_Interp *interp, Bonded_ia_par
   char buffer[TCL_DOUBLE_SPACE];
   
   Tcl_PrintDouble(interp, params->p.ibmVolConsParameters.volRef, buffer);
-  Tcl_AppendResult(interp, "ibm_volcons checkpoint ", buffer," ", (char *) NULL);
+  Tcl_AppendResult(interp, "ibm_volcons cpt ", buffer," ", (char *) NULL);
   
   return TCL_OK;
 }
