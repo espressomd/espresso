91a64321756b3bf21a76b3a959105f545853cafe
Florian Weik <fweik@icp.uni-stuttgart.de>
2015-04-21 17:51:27 +0200

    Fix build w/o SD, hopefully for real this time. Missing (c)-headers.

--- src/core/integrate_sd_cuda_kernel.hpp
+++ src/core/integrate_sd_cuda_kernel.hpp
@@ -1,5 +1,26 @@
-#pragma once
+/*
+  Copyright (C) 2010,2012,2013,2014,2015 The ESPResSo project
+  Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010 
+    Max-Planck-Institute for Polymer Research, Theory Group
+  
+  This file is part of ESPResSo.
+  
+  ESPResSo is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  ESPResSo is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
 
+#ifndef __INTEGRATE_SD_CUDA_KERNEL_HPP
+#define __INTEGRATE_SD_CUDA_KERNEL_HPP
 
 // This computes the farfield contribution.
 // r is the vector of [x_1, y_1, z_1, x_2, y_2, z_2, ...]
@@ -160,3 +181,5 @@ __global__ void sd_nrm_inf(const int size, const int * const vec, int * res);
 
 
 __global__ void sd_nrm1(const int size,const real * const vec, real * erg);
+
+#endif
