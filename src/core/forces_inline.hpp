8c00b78e3c8c26f43c9202c1bb4c1ccffcc82f77
Florian Weik <fweik@icp.uni-stuttgart.de>
2015-05-18 14:11:40 +0200

    Always have 4 force stack variables in add_bonded_forces.

--- src/core/forces_inline.hpp
+++ src/core/forces_inline.hpp
@@ -598,8 +598,8 @@ inline void add_bonded_force(Particle *p1)
   double force[3]  = { 0., 0., 0. };
   double force2[3] = { 0., 0., 0. };
   double force3[3] = { 0., 0., 0. };
-#ifdef TWIST_STACK
   double force4[3] = { 0., 0., 0. };
+#if TWIST_STACK
   double force5[3] = { 0., 0., 0. };
   double force6[3] = { 0., 0., 0. };
   double force7[3] = { 0., 0., 0. };
