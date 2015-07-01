ad84b29601806a152a7d55f5b1d3e085fc9af28d
Tobias Rau <phy79182@stud.uni-stuttgart.de>
2015-07-01 13:08:15 +0200

    implementation seems to work

--- src/core/statistics_observable.cpp
+++ src/core/statistics_observable.cpp
@@ -573,80 +573,88 @@ int observable_calc_force_density_profile(observable* self) {
 #ifdef LB
 int observable_calc_lb_velocity_profile(observable* self) {
   double* A = self->last_value;
+  void* pdata_ = self->container;
+  unsigned int n_A = self->n;
   unsigned int i, j, k;
   unsigned int maxi, maxj, maxk;
   double xoffset, yoffset, zoffset;
   double x_incr, y_incr, z_incr;
   double p[3], v[3];
+  int linear_index;
   profile_data* pdata;
   pdata=(profile_data*) self->container;
-  int linear_index;
 
-    
-  for ( int i = 0; i<self->n; i++ ) {
-    A[i]=0;
-  }
-  double normalization_factor = 1.;
-  if ( pdata->xbins == 1 ) {
-    maxi = (int) floor(box_l[0]/lbpar.agrid);
-    normalization_factor/=maxi;
-    xoffset=0;
-    x_incr=lbpar.agrid;
-  } else {
-    maxi = pdata->xbins;
-    xoffset=pdata->minx;
-    x_incr=(pdata->maxx-pdata->minx)/(pdata->xbins-1);
-  }
-  if ( pdata->ybins == 1 ) {
-    maxj = (int) floor(box_l[1]/lbpar.agrid);
-    normalization_factor/=maxj;
-    yoffset=0;
-    y_incr=lbpar.agrid;
-  } else {
-    maxj = pdata->ybins;
-    yoffset=pdata->miny;
-    y_incr=(pdata->maxy-pdata->miny)/(pdata->ybins-1);
-  }
-  if ( pdata->zbins == 1 ) {
-    maxk = (int) floor(box_l[2]/lbpar.agrid);
-    normalization_factor/=maxk;
-    zoffset=0;
-    z_incr=lbpar.agrid;
-  } else {
-    maxk = pdata->zbins;
-    zoffset=pdata->minz;
-    z_incr=(pdata->maxz-pdata->minz)/(pdata->zbins-1);
-  }
 
-  for ( i = 0; i < maxi; i++ ) {
-    for ( j = 0; j < maxj; j++ ) {
-      for ( k = 0; k < maxk; k++ ) {
-        p[0]=xoffset + i*x_incr;
-        p[1]=yoffset + j*y_incr;
-        p[2]=zoffset + k*z_incr;
-        if (lb_lbfluid_get_interpolated_velocity(p, v)!=0)
-          return 1;
-        linear_index = 0;
-        if (pdata->xbins > 1)
-          linear_index += i*pdata->ybins*pdata->zbins;
-        if (pdata->ybins > 1)
-          linear_index += j*pdata->zbins;
-        if (pdata->zbins > 1)
-          linear_index +=k;
+#ifdef LB_GPU
+  if (lattice_switch & LATTICE_LB_GPU)
+    return statistics_observable_lbgpu_velocity_profile((profile_data*) pdata_, A, n_A);
+#endif
+  if (!(lattice_switch & LATTICE_LB)) {
+    for ( int i = 0; i<self->n; i++ ) {
+      A[i]=0;
+    }
+    double normalization_factor = 1.;
+    if ( pdata->xbins == 1 ) {
+      maxi = (int) floor(box_l[0]/lbpar.agrid);
+      normalization_factor/=maxi;
+      xoffset=0;
+      x_incr=lbpar.agrid;
+    } else {
+      maxi = pdata->xbins;
+      xoffset=pdata->minx;
+      x_incr=(pdata->maxx-pdata->minx)/(pdata->xbins-1);
+    }
+    if ( pdata->ybins == 1 ) {
+      maxj = (int) floor(box_l[1]/lbpar.agrid);
+      normalization_factor/=maxj;
+      yoffset=0;
+      y_incr=lbpar.agrid;
+    } else {
+      maxj = pdata->ybins;
+      yoffset=pdata->miny;
+      y_incr=(pdata->maxy-pdata->miny)/(pdata->ybins-1);
+    }
+    if ( pdata->zbins == 1 ) {
+      maxk = (int) floor(box_l[2]/lbpar.agrid);
+      normalization_factor/=maxk;
+      zoffset=0;
+      z_incr=lbpar.agrid;
+    } else {
+      maxk = pdata->zbins;
+      zoffset=pdata->minz;
+      z_incr=(pdata->maxz-pdata->minz)/(pdata->zbins-1);
+    }
 
-        A[3*linear_index+0]+=v[0];
-        A[3*linear_index+1]+=v[1];
-        A[3*linear_index+2]+=v[2];
+    for ( i = 0; i < maxi; i++ ) {
+      for ( j = 0; j < maxj; j++ ) {
+	for ( k = 0; k < maxk; k++ ) {
+	  p[0]=xoffset + i*x_incr;
+	  p[1]=yoffset + j*y_incr;
+	  p[2]=zoffset + k*z_incr;
+	  if (lb_lbfluid_get_interpolated_velocity(p, v)!=0)
+	    return 1;
+	  linear_index = 0;
+	  if (pdata->xbins > 1)
+	    linear_index += i*pdata->ybins*pdata->zbins;
+	  if (pdata->ybins > 1)
+	    linear_index += j*pdata->zbins;
+	  if (pdata->zbins > 1)
+	    linear_index +=k;
+
+	  A[3*linear_index+0]+=v[0];
+	  A[3*linear_index+1]+=v[1];
+	  A[3*linear_index+2]+=v[2];
+	}
       }
     }
-  }
   
-  for ( int i = 0; i<self->n; i++ ) {
-    A[i]*=normalization_factor;
-  }
+    for ( int i = 0; i<self->n; i++ ) {
+      A[i]*=normalization_factor;
+    }
 
   
-  return 0;
+    return 0;
+  }
 }
 #endif
 
