456a4add69823435e1c941e39bd424b78bb61a1f
Florian Weik <florianweik@gmail.com>
2015-08-12 19:56:01 +0200

    Class for handling generic parameters.

---
+++ src/core/Parameters.hpp
@@ -0,0 +1,32 @@
+#include "Variant.hpp"
+#include <map>
+#include <string>
+
+struct Parameter {
+  Parameter() : type(Variant::NONE), n_elements(0), set(false), required(false) {}
+  Parameter(Variant::Type _type, Variant _value, int _n_elements, bool _set, bool _required) :
+    type(_type), value(_value), n_elements(_n_elements), set(_set), required(_required) {}
+  Parameter(Variant::Type _type, bool _req) : type(_type), value(), n_elements(0), set(false), required(_req) {}
+  Parameter(Variant::Type _type, int _n, bool _req) : type(_type), value(), n_elements(_n), set(false), required(_req) {}
+  
+  Parameter &operator=(const Variant& rhs)  {
+    value = rhs; set = true;
+    return *this;
+  }
+  
+  Variant::Type type;
+  Variant value;
+  int n_elements;
+  bool set, required;
+};
+
+class Parameters : public std::map<std::string, Parameter> {
+public:
+  virtual bool check() {
+    bool ret = true;
+    for(iterator it = begin(); it != end(); ++it) {
+      ret &= (it->second.set | !it->second.required);
+    }
+    return ret;
+  }  
+};
