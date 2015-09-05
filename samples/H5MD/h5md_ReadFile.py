9707f388684c36e14e0e20ba1d69b7aac1b0c866
sehrhardt <sascha.ehrhardt@gmail.com>
2015-09-05 21:29:31 +0200

    Read h5-file

--- samples/H5MD/h5md_ReadFile.py
+++ samples/H5MD/h5md_ReadFile.py
@@ -20,8 +20,11 @@
 from __future__ import print_function
 import espressomd._system as es
 import espressomd
-import h5md
+from espressomd import integrate
+from espressomd import h5md
+from espressomd.interactions import HarmonicBond
 import numpy
+from __builtin__ import tuple
 
 # H5MD: Create/Read File dataset
 #############################################################
@@ -30,26 +33,92 @@ h5_filename="h5md_WriteReadFile.h5"
 h5=h5md.h5md("h5md_WriteReadFile.h5",system)
 
 n_part=10
-n_time=10
+n_time=5
 int_steps=10
 
 #h5.h5_dataset_size(h5.h5_file['particles/position/step'])
-h5.h5_read_particles.pos(n_time-1)
-#h5.h5_read_particles.image(n_time-1)
-h5.h5_read_particles.v(n_time-1)
-h5.h5_read_particles.f(n_time-1)
-h5.h5_read_particles.type()
-h5.h5_read_particles.mass()
-h5.h5_read_particles.id()
-#h5.h5_read_particles.box(n_time-1)
-#h5.h5_read_observable(n_time-1,[2,3,4],"energies","Obs1")
 
 for i in range(n_part):
-	print(system.part[i].pos)
+	system.part[i].pos=numpy.array([0,0,0])
+	
+
+
+
+h5.read_from_h5.time(n_time-1,"particles/atoms/position/","time")
+h5.read_from_h5.time_step(n_time-1,"particles/atoms/position/","step")
+h5.read_from_h5.type(n_time-1)
+h5.read_from_h5.pos(n_time-1)
+h5.read_from_h5.v(n_time-1)
+h5.read_from_h5.f(n_time-1)
+#h5.read_from_h5.bonds(n_time-1)
+h5.read_from_h5.mass(n_time-1)
+h5.read_from_h5.omega_lab(n_time-1)
+h5.read_from_h5.rinertia(n_time-1)
+h5.read_from_h5.omega_body(n_time-1)
+h5.read_from_h5.torque_lab(n_time-1)
+h5.read_from_h5.quat(n_time-1)
+####h5.read_from_h5.director(n_time-1) #Not implemented yet
+h5.read_from_h5.q(n_time-1)
+h5.read_from_h5.virtual(n_time-1)      #Only virtual or vs_relative in myconfig
+#h5.read_from_h5.vs_relative(n_time-1) #Only virtual or vs_relative in myconfig #ERROR in python code
+h5.read_from_h5.dip(n_time-1)
+h5.read_from_h5.dipm(n_time-1)
+h5.read_from_h5.ext_force(n_time-1)
+h5.read_from_h5.fix(n_time-1)
+h5.read_from_h5.ext_torque(n_time-1)
+h5.read_from_h5.gamma(n_time-1)
+h5.read_from_h5.temp(n_time-1)
+h5.read_from_h5.rotation(n_time-1)
+####h5.read_from_h5.exclude(n_time-1) #TODO
+####h5.read_from_h5.swimming(n_time-1) #TODO
+#h5.read_from_h5.box_edges(n_time-1)
+#h5.read_from_h5.box_boundary(n_time-1)print(
+#h5.read_from_h5.box_dimension(n_time-1)
+h5.read_from_h5.id(n_time-1)
+#h5.read_from_h5.image(n_time-1) #TOASK
+result_user = h5.read_from_h5.userdefined(n_time-1,"User/user1/","value")
 
 
 
 
+
+
+
+
+for i in range(n_part):
+# 	print(system.time)
+# 	print("timestep")
+# 	print(system.part[i].type)
+# 	print(system.part[i].pos)
+# 	print(system.part[i].v)
+# 	print(system.part[i].f)
+# 	#print(system.part[i].bonds)
+# 	print(system.part[i].mass)
+# 	print(system.part[i].omega_lab)
+# 	print(system.part[i].rinertia)
+# 	print(system.part[i].omega_body)
+# 	print(system.part[i].torque_lab)
+# 	print(system.part[i].quat)
+# 	####print(system.part[i].director)
+# 	print(system.part[i].q)
+# 	print(system.part[i].virtual)         															#Only virtual or vs_relative in myconfig
+# 	#print(system.part[i].vs_relative)																  #Only virtual or vs_relative in myconfig #ERROR in python code
+# 	print(system.part[i].dip)
+# 	print(system.part[i].dipm)
+# 	
+# 	print(system.part[i].ext_force)
+# 	print(system.part[i].fix)
+# 	print(system.part[i].ext_torque)
+# 	print(system.part[i].gamma)
+# 	print(system.part[i].temp)
+# 	print(system.part[i].rotation)
+# 	####print(system.part[i].exclude) #TODO
+# 	####print(system.part[i].swimming) #TODO
+# 	
+# 	print(system.box_l)
+# 	#####print(system.part[i].image) #TOASK
+# 	print(system.part[i].id)	
+	print(result_user)	
 
 
 
