f29ea96303c539b33a8524adde39e99ce6c87434
sehrhardt <sascha.ehrhardt@gmail.com>
2015-10-10 21:38:01 +0200

    Python Indentions

--- samples/H5MD/h5md_ReadFile.py
+++ samples/H5MD/h5md_ReadFile.py
@@ -17,14 +17,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 #  
 
-from __future__ import print_function
 import espressomd._system as es
 import espressomd
-from espressomd import integrate
 from espressomd import h5md
 from espressomd.interactions import HarmonicBond
-import numpy
-from __builtin__ import tuple
+import numpy as np
 
 # H5MD: Create/Read File dataset
 #############################################################
@@ -39,40 +36,37 @@ int_steps=10
 #h5.h5_dataset_size(h5.h5_file['particles/position/step'])
 
 for i in range(n_part):
-	system.part[i].pos=numpy.array([0,0,0])
-	
-
-
+    system.part[i].pos=np.array([0,0,0])
 
 h5.read_from_h5.time(n_time-1,"particles/atoms/position/","time")
-h5.read_from_h5.type()
-h5.read_from_h5.pos()
-h5.read_from_h5.v()
-h5.read_from_h5.f()
-#h5.read_from_h5.bonds()
-h5.read_from_h5.mass()
-h5.read_from_h5.omega_lab()
-h5.read_from_h5.rinertia()
-h5.read_from_h5.omega_body()
-h5.read_from_h5.torque_lab()
-h5.read_from_h5.quat()
-####h5.read_from_h5.director() #Not implemented yet
-h5.read_from_h5.q()
-h5.read_from_h5.virtual()      #Only virtual or vs_relative in myconfig
-#h5.read_from_h5.vs_relative() #Only virtual or vs_relative in myconfig #ERROR in python code
-h5.read_from_h5.dip()
-h5.read_from_h5.dipm()
-h5.read_from_h5.ext_force()
-h5.read_from_h5.fix()
-h5.read_from_h5.ext_torque()
-h5.read_from_h5.gamma()
-h5.read_from_h5.temp()
-h5.read_from_h5.rotation()
-####h5.read_from_h5.exclude() #TODO
-####h5.read_from_h5.swimming() #TODO
-h5.read_from_h5.box_edges()
-h5.read_from_h5.id()
-#h5.read_from_h5.image() #TOASK
+h5.read_from_h5.type(n_time-1)
+h5.read_from_h5.pos(n_time-1)
+h5.read_from_h5.v(n_time-1)
+h5.read_from_h5.f(n_time-1)
+#h5.read_from_h5.bonds(n_time-1)
+h5.read_from_h5.mass(n_time-1)
+h5.read_from_h5.omega_lab(n_time-1)
+h5.read_from_h5.rinertia(n_time-1)
+h5.read_from_h5.omega_body(n_time-1)
+h5.read_from_h5.torque_lab(n_time-1)
+h5.read_from_h5.quat(n_time-1)
+####h5.read_from_h5.director(n_time-1) #Not implemented yet
+h5.read_from_h5.q(n_time-1)
+h5.read_from_h5.virtual(n_time-1)      #Only virtual or vs_relative in myconfig
+#h5.read_from_h5.vs_relative(n_time-1) #Only virtual or vs_relative in myconfig #ERROR in python code
+h5.read_from_h5.dip(n_time-1)
+h5.read_from_h5.dipm(n_time-1)
+h5.read_from_h5.ext_force(n_time-1)
+h5.read_from_h5.fix(n_time-1)
+h5.read_from_h5.ext_torque(n_time-1)
+h5.read_from_h5.gamma(n_time-1)
+h5.read_from_h5.temp(n_time-1)
+h5.read_from_h5.rotation(n_time-1)
+####h5.read_from_h5.exclude(n_time-1) #TODO
+####h5.read_from_h5.swimming(n_time-1) #TODO
+h5.read_from_h5.box_edges(n_time-1)
+h5.read_from_h5.id(n_time-1)
+#h5.read_from_h5.image(n_time-1) #TOASK
 result_user = h5.read_from_h5.userdefined(n_time-1,"User/user1/","value")
 
 
@@ -83,72 +77,38 @@ result_user = h5.read_from_h5.userdefined(n_time-1,"User/user1/","value")
 
 
 for i in range(n_part):
-# 	print(system.time)
-# 	print(system.part[i].type)
-# 	print(system.part[i].pos)
-# 	print(system.part[i].v)
-# 	print(system.part[i].f)
-# 	#print(system.part[i].bonds)
-# 	print(system.part[i].mass)
-# 	print(system.part[i].omega_lab)
-# 	print(system.part[i].rinertia)
-# 	print(system.part[i].omega_body)
-# 	print(system.part[i].torque_lab)
-# 	print(system.part[i].quat)
-# 	####print(system.part[i].director)
-# 	print(system.part[i].q)
-# 	print(system.part[i].virtual)         															#Only virtual or vs_relative in myconfig
-# 	#print(system.part[i].vs_relative)																  #Only virtual or vs_relative in myconfig #ERROR in python code
-# 	print(system.part[i].dip)
-# 	print(system.part[i].dipm)
-#  	
-# 	print(system.part[i].ext_force)
-# 	print(system.part[i].fix)
-# 	print(system.part[i].ext_torque)
-# 	print(system.part[i].gamma)
-# 	print(system.part[i].temp)
-# 	print(system.part[i].rotation)
-# 	####print(system.part[i].exclude) #TODO
-# 	####print(system.part[i].swimming) #TODO
-#  	
-# 	print(system.box_l)
-# 	#####print(system.part[i].image) #TOASK
-# 	print(system.part[i].id)	
-	print(result_user)
+#     print(system.time)
+#     print(system.part[i].type)
+#     print(system.part[i].pos)
+    print(system.part[i].v)
+#     print(system.part[i].f)
+#     #print(system.part[i].bonds)
+#     print(system.part[i].mass)
+#     print(system.part[i].omega_lab)
+#     print(system.part[i].rinertia)
+#     print(system.part[i].omega_body)
+#     print(system.part[i].torque_lab)
+#     print(system.part[i].quat)
+#     ####print(system.part[i].director)
+#     print(system.part[i].q)
+# 	  print(system.part[i].virtual)         															#Onl     virtual or vs_relative in myconfig
+#     #print(system.part[i].vs_relative)																  #Only virtual or vs_relative in myconfig #ERROR in python code
+#     print(system.part[i].dip)
+#     print(system.part[i].dipm)
+#      
+#     print(system.part[i].ext_force)
+#     print(system.part[i].fix)
+#     print(system.part[i].ext_torque)
+#     print(system.part[i].gamma)
+#     print(system.part[i].temp)
+#     print(system.part[i].rotation)
+#     ####print(system.part[i].exclude) #TODO
+#     ####print(system.part[i].swimming) #TODO
+#      
+#     print(system.box_l)
+#     #####print(system.part[i].image) #TOASK
+#     print(system.part[i].id)    
+#     print(result_user)
 
 
 
-
-
-
-
-
-
-
-
-
-
-# #H5MD: Write VMD parameters
-# h5.h5_write_vmd_parameters()
-# h5.h5_write_vmd_parameters_extra.chain(["A", "B", "C"])
-# h5.h5_write_vmd_parameters_extra.name(["A", "B", "C"])
-# h5.h5_write_vmd_parameters_extra.resid([1,2,3,4,5])
-# h5.h5_write_vmd_parameters_extra.resname(["A", "B", "C"])
-# h5.h5_write_vmd_parameters_extra.segid(["A", "B", "C"])
-# h5.h5_write_vmd_parameters_extra.type(["A", "B", "C"])
-
-
-# h5.h5_read_vmd_parameters("A")#xxxxxxxxxxxxxxxx
-# h5.h5_read_vmd_parameters_extra.chain("A")#xxxxxxxxxxxxxxxx
-# h5.h5_read_vmd_parameters_extra.name("A")#xxxxxxxxxxxxxxxx
-# h5.h5_read_vmd_parameters_extra.resid("A")#xxxxxxxxxxxxxxxx
-# h5.h5_read_vmd_parameters_extra.resname("A")#xxxxxxxxxxxxxxxx
-# h5.h5_read_vmd_parameters_extra.segid("A")#xxxxxxxxxxxxxxxx
-# h5.h5_read_vmd_parameters_extra.type("A")#xxxxxxxxxxxxxxxx
-
-
-#TODO def MASS  ,  remove es aus constructor  ,  remove reload file 
-#TODO enegrie string to values
-#TODO image
-#attributes
-#box_l nur [10,0,0]
--- samples/H5MD/h5md_WriteFile.py
+++ samples/H5MD/h5md_WriteFile.py
@@ -17,14 +17,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 #  
 
-from __future__ import print_function
 import espressomd._system as es
 import espressomd
-from espressomd import integrate
 from espressomd import h5md
 from espressomd.interactions import HarmonicBond
-import numpy
-from __builtin__ import tuple
+import numpy as np
 
 # H5MD: Create/Read File dataset
 #############################################################
@@ -36,13 +33,11 @@ n_time=5
 int_steps=10
 
 h5=h5md.h5md("h5md_WriteReadFile.h5",system)
-
-
 ###############################################
 bondId=0
 bondClass=HarmonicBond
 params={"r_0":1.1, "k":5.2}
-		
+
 system.bondedInter[bondId]=bondClass(**params)
 outBond=system.bondedInter[bondId]
 tnIn=bondClass(**params).typeNumber()
@@ -53,103 +48,89 @@ outParams=outBond.params
 ################################################
 
 for i in range(n_part):
-	#PARTICLE
-	system.part[i].pos=numpy.array([0.1*i,0.2*i,0.3*i])
-	system.part[i].v=numpy.array([0.2*i,0.3*i,0.4*i])
-	system.part[i].f=numpy.array([0.3*i,0.4*i,0.5*i])
-	system.part[i].type=i%3
-	#system.part[i].bonds=[[outBond,(i+1)%n_part],[outBond,(i+2)%n_part]] #TODO
-	system.part[i].mass=0.4*i
-	system.part[i].omega_lab=numpy.array([0.5*i,0.6*i,0.7*i])
-	system.part[i].rinertia=numpy.array([0.6*i,0.7*i,0.8*i])
-	system.part[i].omega_body=numpy.array([0.7*i,0.8*i,0.9*i])
-	system.part[i].torque_lab=numpy.array([0.8*i,0.9*i,1.0*i])
-	system.part[i].quat=numpy.array([0.9*i,0.9*i,1.0*i,1.1*i])	
-	####system.part[i].director=numpy.array([0.1,0.1,0.1,0.1])*[i*3,i*3,i*3,i*3]	#Not implemented yet
-	system.part[i].q=1.0*i
-	system.part[i].virtual=11*i          															#Only virtual or vs_relative in myconfig
-	#system.part[i].vs_relative=numpy.array([1.2*i,1.3*i,1.4*i])	  #Only virtual or vs_relative in myconfig #ERROR in python code
-	system.part[i].dip=numpy.array([1.3*i,1.4*i,1.5*i])
-	system.part[i].dipm=14*i	
-	system.part[i].ext_force=numpy.array([1.5*i,1.6*i,1.7*i])
-	system.part[i].fix=numpy.array([i%2,i%2,i%2])
-	system.part[i].ext_torque=numpy.array([1.6*i,1.7*i,1.8*i])
-	system.part[i].gamma=1.7*i
-	system.part[i].temp=1.8*i
-# 	system.part[i].rotation=i%2	#Use only without analyze pressure
-	####system.part[i].exclude=-1 #TODO
-	####system.part[i].swimming=-1 #TODO
-	system.box_l = numpy.array([100,200,300])
-	#####system.part[i].image=numpy.array([0.1,0.1,0.1])*[i*2,i*2,i*2] #TOASK
-	system.part[i].id=19*i	# Will be overwritten from Espresso
-
-
+    #PARTICLE
+    system.part[i].pos=np.array([0.1*i,0.2*i,0.3*i])
+    system.part[i].v=np.array([0.2*i,0.3*i,0.4*i])
+    system.part[i].f=np.array([0.3*i,0.4*i,0.5*i])
+    system.part[i].type=i%3
+    #system.part[i].bonds=[[outBond,(i+1)%n_part],[outBond,(i+2)%n_part]] #TODO
+    system.part[i].mass=0.4*i
+    system.part[i].omega_lab=np.array([0.5*i,0.6*i,0.7*i])
+    system.part[i].rinertia=np.array([0.6*i,0.7*i,0.8*i])
+    system.part[i].omega_body=np.array([0.7*i,0.8*i,0.9*i])
+    system.part[i].torque_lab=np.array([0.8*i,0.9*i,1.0*i])
+    system.part[i].quat=np.array([0.9*i,0.9*i,1.0*i,1.1*i])
+    ####system.part[i].director=np.array([0.1,0.1,0.1,0.1])*[i*3,i*3,i*3,i*3]    #Not implemented yet
+    system.part[i].q=1.0*i
+    system.part[i].virtual=11*i          															#Only virtual or vs_relative in myconfig
+    #system.part[i].vs_relative=np.array([1.2*i,1.3*i,1.4*i])	  #Only virtual or vs_relative in myconfig #ERROR in python code
+    system.part[i].dip=np.array([1.3*i,1.4*i,1.5*i])
+    system.part[i].dipm=14*i    
+    system.part[i].ext_force=np.array([1.5*i,1.6*i,1.7*i])
+    system.part[i].fix=np.array([i%2,i%2,i%2])
+    system.part[i].ext_torque=np.array([1.6*i,1.7*i,1.8*i])
+    system.part[i].gamma=1.7*i
+    system.part[i].temp=1.8*i
+#     system.part[i].rotation=i%2	#Use only without analyze pressure
+    ####system.part[i].exclude=-1 #TODO
+    ####system.part[i].swimming=-1 #TODO
+    system.box_l = np.array([100,200,300])
+    #####system.part[i].image=np.array([0.1,0.1,0.1])*[i*2,i*2,i*2] #TOASK
+    system.part[i].id=19*i    # Will be overwritten from Espresso
+  
 for i in range(n_time):
-	system.time=0.1*(i+1)
-	h5.write_to_h5.time(i,"particles/atoms/position/","time")
-	h5.write_to_h5.time_step(i,"particles/atoms/position/","step")
-	h5.write_to_h5.type(-1)
-	h5.write_to_h5.pos(-1)
-	h5.write_to_h5.v(-1)
-	h5.write_to_h5.f(-1)
-	#h5.write_to_h5.bonds(-1)
-	h5.write_to_h5.mass(-1)
-	h5.write_to_h5.omega_lab(-1)
-	h5.write_to_h5.rinertia(-1)
-	h5.write_to_h5.omega_body(-1)
-	h5.write_to_h5.torque_lab(-1)
-	h5.write_to_h5.quat(-1)
-	####h5.write_to_h5.director(-1) #Not implemented yet
-	h5.write_to_h5.q(-1)
-	h5.write_to_h5.virtual(-1)      #Only virtual or vs_relative in myconfig
-	#h5.write_to_h5.vs_relative(-1) #Only virtual or vs_relative in myconfig #ERROR in python code
-	h5.write_to_h5.dip(-1)
-	h5.write_to_h5.dipm(-1)
-	h5.write_to_h5.ext_force(-1)
-	h5.write_to_h5.fix(-1)
-	h5.write_to_h5.ext_torque(-1)
-	h5.write_to_h5.gamma(-1)
-	h5.write_to_h5.temp(-1)
-	h5.write_to_h5.rotation(-1)		#Use only without analyze pressure
-	####h5.write_to_h5.exclude(-1) #TODO
-	####h5.write_to_h5.swimming(-1) #TODO
-	h5.write_to_h5.box_edges(-1)
-	h5.write_to_h5.box_boundary(-1)
-	h5.write_to_h5.box_dimension(-1)
-	h5.write_to_h5.id(-1)
-	#h5.write_to_h5.image(-1) #TOASK	
-	h5.write_to_h5.userdefined(-1,[1,2,3],"User/user1/","value",'f8')
-	
-	#ANALYZE
-	h5.write_to_h5.mindist(-1,[0],[1])
-	h5.write_to_h5.distto(-1,None,[1,1,1])	
-	h5.write_to_h5.analyze_linear_momentum(-1,True,True)
-	h5.write_to_h5.nbhood(-1,[1,1,1],1.0,'3d')
-	h5.write_to_h5.cylindrical_average(-1,[1,1,1],[1,1,1],1,1,1,1,[-1])
-	h5.write_to_h5.pressure(-1,False)
-	h5.write_to_h5.stress_tensor(-1,False)
-	h5.write_to_h5.analyze_energy(-1,'all','default','default')
-	h5.write_to_h5.calc_re(-1,1,1,1)
-	h5.write_to_h5.calc_rg(-1,1,1,1)
-	h5.write_to_h5.calc_rh(-1,1,1,1)
-	h5.write_to_h5.structure_factor(-1,1,1)
-
-
+    system.time=0.1*(i+1)
+    h5.write_to_h5.time(i,"particles/atoms/position/","time")
+    h5.write_to_h5.time_step(i,"particles/atoms/position/","step")
+    h5.write_to_h5.type(i)
+    h5.write_to_h5.pos(i)
+    h5.write_to_h5.v(i)
+    h5.write_to_h5.f(i)
+    #h5.write_to_h5.bonds(i)
+    h5.write_to_h5.mass(i)
+    h5.write_to_h5.omega_lab(i)
+    h5.write_to_h5.rinertia(i)
+    h5.write_to_h5.omega_body(i)
+    h5.write_to_h5.torque_lab(i)
+    h5.write_to_h5.quat(i)
+    ####h5.write_to_h5.director(i) #Not implemented yet
+    h5.write_to_h5.q(i)
+    h5.write_to_h5.virtual(i)      #Only virtual or vs_relative in myconfig
+    #h5.write_to_h5.vs_relative(i) #Only virtual or vs_relative in myconfig #ERROR in python code
+    h5.write_to_h5.dip(i)
+    h5.write_to_h5.dipm(i)
+    h5.write_to_h5.ext_force(i)
+    h5.write_to_h5.fix(i)
+    h5.write_to_h5.ext_torque(i)
+    h5.write_to_h5.gamma(i)
+    h5.write_to_h5.temp(i)
+    h5.write_to_h5.rotation(i)        #Use only without analyze pressure
+    ####h5.write_to_h5.exclude(i) #TODO
+    ####h5.write_to_h5.swimming(i) #TODO
+    h5.write_to_h5.box_edges(i)
+    h5.write_to_h5.box_boundary(i)
+    h5.write_to_h5.box_dimension(i)
+    h5.write_to_h5.id(i)
+    #h5.write_to_h5.image(i) #TOASK    
+    h5.write_to_h5.userdefined(i,[1,2,3],"User/user1/","value",'f8')
+    
+    #ANALYZE
+    h5.write_to_h5.mindist(i,[0],[1])
+    h5.write_to_h5.distto(i,None,[1,1,1])    
+    h5.write_to_h5.analyze_linear_momentum(i,True,True)
+    h5.write_to_h5.nbhood(i,[1,1,1],1.0,'3d')
+    h5.write_to_h5.cylindrical_average(i,[1,1,1],[1,1,1],1,1,1,1,[-1])
+    h5.write_to_h5.pressure(i,False)
+    h5.write_to_h5.stress_tensor(i,False)
+    h5.write_to_h5.analyze_energy(i,'all','default','default')
+    h5.write_to_h5.calc_re(i,1,1,1)
+    h5.write_to_h5.calc_rg(i,1,1,1)
+    h5.write_to_h5.calc_rh(i,1,1,1)
+    h5.write_to_h5.structure_factor(i,1,1)
 h5.close()
 
 
-# #H5MD: Write VMD parameters
-# h5.h5_write_vmd_parameters()
-# h5.h5_write_vmd_parameters_extra.chain(["A", "B", "C"])
-# h5.h5_write_vmd_parameters_extra.name(["A", "B", "C"])
-# h5.h5_write_vmd_parameters_extra.resid([1,2,3,4,5])
-# h5.h5_write_vmd_parameters_extra.resname(["A", "B", "C"])
-# h5.h5_write_vmd_parameters_extra.segid(["A", "B", "C"])
-# h5.h5_write_vmd_parameters_extra.type(["A", "B", "C"])
-
-
 #vmd
-#Tab 4er
 # image,bonds
 # todo xxx 
 #Manual
@@ -163,7 +144,5 @@ h5.close()
 
 
 ####TOASK
-#mass time dependent
-#energy etc only numbers
 #box tensor
 #bleibt analyzeargs.energy as usual
