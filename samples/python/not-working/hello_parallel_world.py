9afc629574c27114a39d69268b6419d92d819dfa
Michael Kuron <mkuron@icp.uni-stuttgart.de>
2015-12-16 16:51:23 +0100

    Only make OpenMPI symbols global

--- samples/python/not-working/hello_parallel_world.py
+++ samples/python/not-working/hello_parallel_world.py
@@ -17,9 +17,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 #  
 from __future__ import print_function
-import ctypes
-import sys
-sys.setdlopenflags((sys.getdlopenflags() | ctypes.RTLD_GLOBAL ))
 
 import espresso as es
 import numpy
