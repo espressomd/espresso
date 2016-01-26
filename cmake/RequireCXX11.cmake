3db7deabef73fcced58d8412c149c001dd6602f1
Florian Weik <fweik@icp.uni-stuttgart.de>
2016-01-26 16:51:40 +0100

    cmake: c++11 detection for cray and intel compilers and notes by @mkuron.

--- cmake/RequireCXX11.cmake
+++ cmake/RequireCXX11.cmake
@@ -6,37 +6,49 @@
 # all the logic below.
 
 if ( DEFINED HAVE_CXX11 )
-return()
+  return()
 endif ()
 
 set(required_gcc_version 4.8)
 set(required_clang_version 3.3)
+set(required_icc_version 11.0)
 
-# CMAKE_CXX_COMPILER_VERSION may not always be available
-(e.g. particularly
+# CMAKE_CXX_COMPILER_VERSION may not always be available (e.g. particularly
 # for CMakes older than 2.8.10, but use it if it exists.
 if ( DEFINED CMAKE_CXX_COMPILER_VERSION )
-if ( CMAKE_CXX_COMPILER_ID STREQUAL "GNU" )
-if ( CMAKE_CXX_COMPILER_VERSION VERSION_LESS
-${required_gcc_version} )
-message(FATAL_ERROR "GCC version must be at least "
-"${required_gcc_version} for C++11
-support, detected: "
-"${CMAKE_CXX_COMPILER_VERSION}")
-endif ()
-elseif ( CMAKE_CXX_COMPILER_ID STREQUAL
-"Clang" )
-if ( CMAKE_CXX_COMPILER_VERSION VERSION_LESS
-${required_clang_version} )
-message(FATAL_ERROR "Clang version must be at least "
-"${required_clang_version} for C++11
-support, detected: "
-"${CMAKE_CXX_COMPILER_VERSION}")
-endif ()
-endif ()
+  if ( CMAKE_CXX_COMPILER_ID STREQUAL "GNU" )
+    if ( CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${required_gcc_version} )
+      message(FATAL_ERROR "GCC version must be at least "
+	"${required_gcc_version} for C++11 support, detected: "
+	"${CMAKE_CXX_COMPILER_VERSION}")
+    endif ()
+  elseif ( CMAKE_CXX_COMPILER_ID STREQUAL
+      "Clang" )
+    if ( CMAKE_CXX_COMPILER_VERSION VERSION_LESS
+	${required_clang_version} )
+      message(FATAL_ERROR "Clang version must be at least "
+	"${required_clang_version} for C++11 support, detected: "
+	"${CMAKE_CXX_COMPILER_VERSION}")
+    endif ()
+  endif ()
 
-set(HAVE_CXX11 true)
-set(CMAKE_CXX_FLAGS
-"${CMAKE_CXX_FLAGS} -std=c++11")
-return()
+  if( ${CMAKE_CXX_COMPILER} MATCHES "icpc" OR "${CMAKE_CXX_COMPILER_ARG1}" MATCHES "icpc" )
+    if( CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${required_icc_version} )
+      message(FATAL_ERROR "icpc version must be at least "
+	"${required_icc_version} for C++11 support, detected: "
+	"${CMAKE_CXX_COMPILER_VERSION}")     
+    endif()
+  endif()
+
+  if( ${CMAKE_CXX_COMPILER_ID} MATCHES Cray )
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 8.4)
+      message( FATAL_ERROR "Insufficient Cray Compiler Environment version" )
+    endif()
+    add_flag(CMAKE_CXX_FLAGS "-hstd=c++11")
+    return()
+  endif()
+
+  set(HAVE_CXX11 true)
+  add_flag(CMAKE_CXX_FLAGS "-std=c++11")
+  return()
 endif ()
