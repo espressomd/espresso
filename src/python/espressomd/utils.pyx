1146e44d8fa538a5de7f902fc594b01eff20e533
KonradBreitsprecher <konradbreitsprecher@gmail.com>
2016-02-15 14:57:39 +0100

    Included parameter name in invalid range in utils.pyx check_range_or_except()

--- src/python/espressomd/utils.pyx
+++ src/python/espressomd/utils.pyx
@@ -92,7 +92,7 @@ cdef check_range_or_except(D, name, v_min, incl_min, v_max, incl_max):
                              ("[" if incl_min else "]") + str(v_min) + "," + str(v_max) + ("]" if incl_max else "["))
     # Single Value
     else:
-        if (v_min != "inf" and ((incl_min and x < v_min) or (not incl_min and x <= v_min)) or \
-            v_max != "inf" and ((incl_max and x > v_max) or (not incl_max and x >= v_max))):
-            raise ValueError("In " + name +  ": Value " + str(x) + " is out of range " + ("[" if incl_min else "]") + 
+        if (v_min != "inf" and ((incl_min and x < v_min) or (not incl_min and x <= v_min)) or
+                v_max != "inf" and ((incl_max and x > v_max) or (not incl_max and x >= v_max))):
+            raise ValueError("In " + name + ": Value " + str(x) + " is out of range " + ("[" if incl_min else "]") +
                              str(v_min) + "," + str(v_max) + ("]" if incl_max else "["))
