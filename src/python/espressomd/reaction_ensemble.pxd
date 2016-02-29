fc441e746ef5a826cbcd05fb8455d01c7acaaf83
Jonas Landsgesell <jonaslandsgesell@users.noreply.github.com>
2016-02-29 16:01:07 +0100

    added python interface for Wang-Landau reaction ensemble

---
+++ src/python/espressomd/reaction_ensemble.pxd
@@ -0,0 +1,94 @@
+include "myconfig.pxi"
+
+cdef extern from "reaction_ensemble.hpp":
+    ctypedef struct single_reaction:
+        int* educt_types
+        int len_educt_types
+        int* educt_coefficients
+        int* product_types
+        int len_product_types
+        int* product_coefficients
+        double equilibrium_constant
+        int nu_bar
+
+
+    ctypedef struct reaction_system:
+        int nr_single_reactions
+        single_reaction** reactions
+        int* type_index
+        int nr_different_types
+        double* charges_of_types
+        int water_type
+        double standard_pressure_in_simulation_units
+        double given_length_in_SI_units
+        double given_length_in_simulation_units
+        double temperature_reaction_ensemble
+        double exclusion_radius
+
+    cdef extern reaction_system current_reaction_system
+    int do_reaction()
+    int find_index_of_type(int type)
+    int calculate_nu_bar(int* educt_coefficients, int len_educt_types,  int* product_coefficients, int len_product_types)
+    int update_type_index(int* educt_types, int len_educt_types , int* product_types, int len_product_types)
+
+
+
+
+#///////////////////////////////////////////// Wang-Landau algorithm
+
+    ctypedef struct collective_variable:
+	    double CV_minimum
+	    double CV_maximum
+	    double delta_CV
+	    double (*determine_current_state_in_collective_variable_with_index) (int)
+	    int* corresponding_acid_types
+	    int nr_corresponding_acid_types
+	    int associated_type
+	    char* energy_boundaries_filename
+
+    ctypedef struct wang_landau_system:
+	    int* histogram
+	    int len_histogram
+	    double* wang_landau_potential
+	    int nr_collective_variables
+	    collective_variable** collective_variables
+	    int* nr_subindices_of_collective_variable
+	    double wang_landau_parameter
+	    double initial_wang_landau_parameter
+	    int int_fill_value
+	    double double_fill_value
+	    int number_of_monte_carlo_moves_between_check_of_convergence
+	    double final_wang_landau_parameter
+	    int used_bins
+	    int monte_carlo_trial_moves
+	    int wang_landau_steps
+	    char* output_filename
+	    double* minimum_energies_at_flat_index
+	    double* maximum_energies_at_flat_index
+	    bool do_energy_reweighting
+	    int counter_ion_type
+	    int polymer_start_id
+	    int polymer_end_id
+	    bool fix_polymer
+	    bool do_not_sample_reaction_partition_function
+	    bool use_hybrid_monte_carlo
+
+    cdef extern wang_landau_system current_wang_landau_system
+    int initialize_wang_landau()
+    int do_reaction_wang_landau()
+    void free_wang_landau()
+    int update_maximum_and_minimum_energies_at_current_state()
+    void write_out_preliminary_energy_run_results(char* filename)
+
+
+    int write_wang_landau_checkpoint(char* identifier)
+    int load_wang_landau_checkpoint(char* identifier)
+
+
+cdef class ReactionEnsemble:
+    cdef _params
+
+
+
+
+
---
+++ src/python/espressomd/reaction_ensemble.pyx
@@ -0,0 +1,224 @@
+include "myconfig.pxi"
+from libc.stdlib cimport malloc, realloc
+
+IF REACTION_ENSEMBLE:
+
+    cdef class ReactionEnsemble:
+        def __init__(self,*args,**kwargs):
+            self._params=self.default_params()
+
+            for k in self.required_keys():
+                if k not in kwargs:
+                    raise ValueError(
+                        "At least the following keys have to be given as keyword arguments: " + self.required_keys().__str__() + " got " + kwargs.__str__())
+                self._params[k] = kwargs[k]
+
+            for k in kwargs:
+                if k in self.valid_keys():
+                    self._params[k] = kwargs[k]
+                else:
+                    raise KeyError("%s is not a vaild key" % k)
+                
+            self.validate_params()
+            self._set_params_in_es_core()
+
+        def default_params(self):
+            return {"standard_pressure":0,
+                    "temperature":1,
+                    "exclusion_radius":0}
+
+        def validate_params(self):
+            return -1       
+
+        def validate_params_add(self):
+            return -1
+
+        def valid_keys(self):
+            return "standard_pressure", "temperature", "exclusion_radius"
+
+        def required_keys(self):
+            return "standard_pressure", "temperature", "exclusion_radius"
+
+        def _get_params_from_es_core(self):
+            params = {"temperature":current_reaction_system.temperature_reaction_ensemble,
+                      "standard_pressure":current_reaction_system.standard_pressure_in_simulation_units,
+                      "exclusion_radius":current_reaction_system.exclusion_radius}
+            return params
+
+        def _set_params_in_es_core(self):
+            current_reaction_system.temperature_reaction_ensemble = self._params["temperature"]
+            current_reaction_system.standard_pressure_in_simulation_units = self._params["standard_pressure"]
+            current_reaction_system.exclusion_radius = self._params["exclusion_radius"]
+
+
+
+        def add(self,*args,**kwargs):
+             for k in self.required_keys_add():
+                if k not in kwargs:
+                    raise ValueError(
+                        "At least the following keys have to be given as keyword arguments: " + self.required_keys_add().__str__() + " got " + kwargs.__str__())
+                self._params[k] = kwargs[k]
+             self._set_params_in_es_core_add()
+
+        def valid_keys_add(self):
+            return "equilibrium_constant", "educt_types", "educt_coefficients", "product_types", "product_coefficients"
+
+        def required_keys_add(self):
+            return ["equilibrium_constant", "educt_types", "educt_coefficients", "product_types", "product_coefficients"]
+
+        def default_params_add(self):
+            
+            return {}
+
+        def _set_params_in_es_core_add(self):
+            
+            cdef single_reaction* new_reaction =<single_reaction *>malloc(sizeof(single_reaction))
+            
+            new_reaction.equilibrium_constant=self._params["equilibrium_constant"]
+            print self._params["educt_types"]
+            cdef int *educt_types = <int *>malloc(len(self._params["educt_types"]) * sizeof(int))
+            for i in range(len(self._params["educt_types"])):
+                educt_types[i]=self._params["educt_types"][i]
+            new_reaction.educt_types=educt_types
+            new_reaction.len_educt_types=len(self._params["educt_types"]);
+            
+            cdef int *educt_coefficients = <int *>malloc(len(self._params["educt_coefficients"]) * sizeof(int))
+            for i in range(len(self._params["educt_coefficients"])):
+                educt_coefficients[i]=self._params["educt_coefficients"][i]
+            new_reaction.educt_coefficients=educt_coefficients
+            
+            cdef int *product_types = <int *>malloc(len(self._params["product_types"]) * sizeof(int))
+            for i in range(len(self._params["product_types"])):
+                product_types[i]=self._params["product_types"][i]
+            new_reaction.product_types=product_types
+            new_reaction.len_product_types=len(self._params["product_types"]);
+            
+            cdef int *product_coefficients = <int *>malloc(len(self._params["product_coefficients"]) * sizeof(int))
+            for i in range(len(self._params["product_coefficients"])):
+                product_coefficients[i]=self._params["product_coefficients"][i]
+            new_reaction.product_coefficients=product_coefficients
+            
+            new_reaction.nu_bar=calculate_nu_bar(new_reaction.educt_coefficients, new_reaction. len_educt_types,  new_reaction.product_coefficients, new_reaction.len_product_types);
+            
+            #if everything is fine:
+            current_reaction_system.reactions=<single_reaction**> realloc(current_reaction_system.reactions,sizeof(single_reaction*)*(current_reaction_system.nr_single_reactions+1)); #enlarge current_reaction_system
+            current_reaction_system.reactions[current_reaction_system.nr_single_reactions]=new_reaction;
+            current_reaction_system.nr_single_reactions+=1;
+	
+            #assign different types an index in a growing list that starts at and is incremented by 1 for each new type
+            update_type_index(new_reaction.educt_types, new_reaction.len_educt_types, new_reaction.product_types, new_reaction.len_product_types); 
+            
+            
+
+        def default_charges(self,*args,**kwargs):
+                for k in kwargs:
+                    if k in self.valid_keys_default_charge():
+                        self._params[k] = kwargs[k]
+                    else:
+                        raise KeyError("%s is not a vaild key" % k)
+        
+                self.validate_params_default_charge()
+                
+                for key in self._params["dictionary"]:
+                        current_reaction_system.charges_of_types[find_index_of_type(int(key))] = self._params["dictionary"][key]
+                
+        def valid_keys_default_charge(self):
+            return "dictionary"
+        
+        def validate_params_default_charge(self):
+            if(isinstance(self._params["dictionary"],dict)==False):
+                raise ValueError("No dictionary for relation between types and default charges provided.")
+        
+        def reaction(self):
+            do_reaction()
+
+
+
+	#//////////////////////////Wang-Landau algorithm
+	def add_collective_variable_degree_of_association(self,*args,**kwargs):
+		for k in kwargs:
+			if k in self.valid_keys_add_collective_variable_degree_of_association():
+				self._params[k]=kwargs[k]
+			else: KeyError("%s is not a valid key" %k)
+		cdef collective_variable* new_collective_variable=<collective_variable*> malloc(sizeof(collective_variable))
+
+		new_collective_variable.associated_type=self.params["associated_type"]
+		new_collective_variable.CV_minimum=self.params["min"]
+		new_collective_variable.CV_maximum=self.params["max"]
+
+            	cdef int *corresponding_acid_types = <int *>malloc(len(self._params["corresponding_acid_types"]) * sizeof(int))
+            	for i in range(len(self._params["corresponding_acid_types"])):
+                	corresponding_acid_types[i]=self._params["corresponding_acid_types"][i]
+            	new_collective_variable.corresponding_acid_types=corresponding_acid_types
+		new_collective_variable.nr_corresponding_acid_types=len(self._params["corresponding_acid_types"])
+
+
+		current_wang_landau_system.collective_variables=<collective_variable**> realloc(current_wang_landau_system.collective_variables,sizeof(collective_variable*)*(current_wang_landau_system.nr_collective_variables+1))
+		current_wang_landau_system.collective_variables.collective_variables[current_wang_landau_system.nr_collective_variables]=new_collective_variable
+		current_wang_landau_system.nr_collective_variables+=1
+		
+		initialize_wang_landau()
+
+	def valid_keys_add_collective_variable_degree_of_association(self):
+		return "associated_type", "min", "max", "corresponding_acid_types"
+
+	def add_collective_variable_potential_energy(self,*args,**kwargs):
+		for k in kwargs:
+			if k in self.valid_keys_add_collective_variable_potential_energy():
+				self._params[k]=kwargs[k]
+			else: KeyError("%s is not a valid key" %k)
+		cdef collective_variable* new_collective_variable=<collective_variable*> realloc(sizeof(collective_variable)*(current_wang_landau_system.nr_collective_variables+1))
+
+		new_collective_variable.energy_boundaries_filename=self._params[filename]
+		new_collective_variable.delta_CV=self._params[delta]
+		
+		current_wang_landau_system.collective_variables=<collective_variable**> realloc(current_wang_landau_system.collective_variables,sizeof(collective_variable*)*(current_wang_landau_system.nr_collective_variables+1));
+		current_wang_landau_system.collective_variables.collective_variables[current_wang_landau_system.nr_collective_variables]=new_collective_variable
+		current_wang_landau_system.nr_collective_variables+=1;
+		
+		initialize_wang_landau()
+
+
+	def valid_keys_add_collective_variable_potential_energy():
+		return "filename","delta"
+
+	def set_wang_landau_parameters(self):
+		for k in kwargs:
+			if k in self.valid_keys_set_wang_landau_parameters():
+				self._params[k]=kwargs[k]
+			else: KeyError("%s is not a valid key" %k)
+		
+		current_wang_landau_system.final_wang_landau_parameter=self._params[final_wang_landau_parameter]
+		current_wang_landau_system.wang_landau_steps=self._params[wang_landau_steps]
+		current_wang_landau_system.output_filename=self._params[full_path_to_output_filename]
+		current_wang_landau_system.do_not_sample_reaction_partition_function=self._params[do_not_sample_reaction_partition_function]
+		current_wang_landau_system.use_hybrid_monte_carlo=self._params[use_hybrid_monte_carlo]
+
+	def valid_keys_set_wang_landau_parameters():
+		return "final_wang_landau_parameter", "wang_landau_steps", "full_path_to_output_filename", "do_not_sample_reaction_partition_function", "use_hybrid_monte_carlo"
+		
+	def load_wang_landau_checkpoint(self):
+		load_wang_landau_checkpoint("checkpoint")
+	def write_wang_landau_checkpoint(self):
+		write_wang_landau_checkpoint("checkpoint")
+		
+	def update_maximum_and_minimum_energies_at_current_state(self):
+		update_maximum_and_minimum_energies_at_current_state()
+	
+	def write_out_preliminary_energy_run_results(self):
+		write_out_preliminary_energy_run_results("preliminary_energy_run_results")
+	
+	##TODO specify monte carlo move
+	def counter_ion_type(self):
+		return
+		
+	def polymer_start_id(self):
+		return
+		
+	def polymer_end_id(self):
+		return
+	def fix_polymer_monomers(self):
+		return
+	
+	
+	
