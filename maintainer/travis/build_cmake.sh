15d97aaa64bce590d04560ec8400871abb217346
Florian Weik <fweik@icp.uni-stuttgart.de>
2016-03-16 19:46:06 +0100

    travis: Explicitly set path for libpython.

--- maintainer/travis/build_cmake.sh
+++ maintainer/travis/build_cmake.sh
@@ -89,7 +89,7 @@ fi
 # CONFIGURE
 start "CONFIGURE"
 if $with_coverage ; then
-    cmake_params="-DCPPFLAGS=\"-coverage -O0\" -DCXXFLAGS=\"-coverage -O0\" $cmake_params"
+    cmake_params="-DCPPFLAGS=\"-coverage -O0\" -DCXXFLAGS=\"-coverage -O0\" PYTHON_LIBRARY=/home/travis/miniconda/envs/test/lib/libpython2.7.so.1.0 $cmake_params"
 fi
 
 if $with_mpi; then
@@ -142,7 +142,7 @@ cmd "make" || exit $?
 
 end "BUILD"
 
-# CHEKC	cat $srcdir/testsuite/python/Testing/Temporary/LastTest.log
+# CHECK	cat $srcdir/testsuite/python/Testing/Temporary/LastTest.log
 if $make_check; then
     start "TEST"
 
