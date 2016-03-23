061e84062ee9eeedb9b3ec4a923a0c7276e338b8
Robin Hobil Bardakcioglu <robin.hobil@gmail.com>
2016-03-23 18:24:30 +0100

    Closes #569: require declaration in _system.System

--- src/python/espressomd/system.py
+++ src/python/espressomd/system.py
@@ -7,4 +7,10 @@ from espressomd import _system
 class System(_system.System):
 
     def __init__(self):
-        _system.System.__init__(self)
+       _system.System.__init__(self)
+
+    def __setattr__(self, name, value):
+        try: self.__getattribute__(name)
+        except AttributeError:
+            raise AttributeError("System does not have the attribute", name)
+        super(System, self).__setattr__(name, value)
