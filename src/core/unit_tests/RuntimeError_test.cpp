6cb843b614060c518b6b4b7cde326ab165f2bda0
Florian Weik <fweik@icp.uni-stuttgart.de>
2016-05-25 19:52:28 +0200

    unit_tests: Replace header only boost test by linked version to improve build time.

--- src/core/unit_tests/RuntimeError_test.cpp
+++ src/core/unit_tests/RuntimeError_test.cpp
@@ -27,7 +27,8 @@
 #include <sstream>
 
 #define BOOST_TEST_MODULE RuntimeError test
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
 
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
