0184fc4c373eac236133f6dd11e3a5ff7c6daaad
Florian Weik <fweik@icp.uni-stuttgart.de>
2016-06-20 13:26:37 +0200

    utils: Destructor for InstanceCallback.

--- src/core/utils/parallel/InstanceCallback.hpp
+++ src/core/utils/parallel/InstanceCallback.hpp
@@ -1,22 +1,22 @@
 /*
   Copyright (C) 2010,2011,2012,2013,2014,2015,2016 The ESPResSo project
-  Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010 
+  Copyright (C) 2002,2003,2004,2005,2006,2007,2008,2009,2010
   Max-Planck-Institute for Polymer Research, Theory Group
-  
+
   This file is part of ESPResSo.
-  
+
   ESPResSo is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   ESPResSo is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef COMMUNICATION_INSTANCE_CALLBACK_HPP
@@ -37,10 +37,10 @@ namespace Communication {
  * the instance: It is created in the constructor
  * and removed in the destructor.
  * Clients have to implement the mpi_slave function
- * which is the actual callback. 
+ * which is the actual callback.
  */
 class InstanceCallback {
- public:
+public:
   InstanceCallback() {
     using std::placeholders::_1;
     using std::placeholders::_2;
@@ -51,6 +51,8 @@ class InstanceCallback {
 
     m_callback_id = Communication::mpiCallbacks().add(cb);
   }
+
+  ~InstanceCallback() { Communication::mpiCallbacks().remove(m_callback_id); }
 
   /**
    * @brief Run the callback function on the slave.
@@ -65,8 +67,8 @@ class InstanceCallback {
 
     Communication::mpiCallbacks().call(m_callback_id, a, b);
   }
-  
- private:
+
+private:
   /* mpi_slave should be private because it is virtual,
    * but the callback system has to call it. */
   friend Communication::MpiCallbacks;
