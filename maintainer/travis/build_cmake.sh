bc9473dd36010a14c385ea7e4d4e592d858ec2c9
Tilak Waelde <tiwa@hippo.icp.uni-stuttgart.de>
2016-06-23 20:04:25 +0200

    junest?

--- maintainer/travis/build_cmake.sh
+++ maintainer/travis/build_cmake.sh
@@ -114,7 +114,7 @@ else
     cmake_params="-DWITH_PYTHON=OFF $cmake_params"
 fi
 
-cmake_params="-DCMAKE_CXX_COMPILER=/usr/bin/g++ -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_MAKE_PROGRAM=/usr/bin/make -DNUMPY_INCLUDE_DIR=/usr/lib64/python2.7/site-packages/numpy/core/include/numpy/ -DNUMPY_VERSION=1.11.0-1 $cmake_params"
+cmake_params="-DCMAKE_CXX_COMPILER=/usr/bin/g++  -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_MAKE_PROGRAM=/usr/bin/make -DNUMPY_INCLUDE_DIR=/usr/lib64/python2.7/site-packages/numpy/core/include/numpy/ -DNUMPY_VERSION=1.11.0-1 $cmake_params"
 
 MYCONFIG_DIR=$srcdir/maintainer/jenkins/configs
 if [ "$myconfig" = "default" ]; then
