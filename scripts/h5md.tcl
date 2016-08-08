551ebdcf784b1bd1e4632fb9aea52e3dba8c82fc
Kai Szuttor <kai@icp.uni-stuttgart.de>
2016-08-08 15:13:46 +0200

    h5mdtcl: fixed bug in writing user defined variables to h5 and adjusted to new filename behaviour.

--- scripts/h5md.tcl
+++ scripts/h5md.tcl
@@ -23,15 +23,19 @@ proc h5md_init { data_path {_h5md_p_ids ""} } {
     set filenamebasepath [file rootname $data_path]
     set filenamebase [file tail $filenamebasepath]
     set datadirfiles [glob -type f -nocomplain -dir $datadir *]
-    if { [expr [llength [glob -type f -nocomplain -dir $datadir "${filenamebase}.h5"]] != 0] } {
+    if { [expr [file exists "${filenamebasepath}.h5"] && [file exists "${filenamebasepath}.h5_tmp"]] } {
+        puts "It seems like either your simulation crashed before or you didnt call the close function."
+        exit 111
+    } elseif { [file exists "${filenamebasepath}.h5"] } {
         puts "Found existing h5 file matching given pattern."; flush stdout
-        set __h5md_filename_tmp "${filenamebasepath}_tmp.h5"
+        set __h5md_filename_tmp "${filenamebasepath}.h5_tmp"
         file copy "${__h5md_filename}" "${__h5md_filename_tmp}"
         h5mdfile H5Fopen "${__h5md_filename_tmp}"
         return
     } else {
+        set __h5md_filename_tmp "${filenamebasepath}.h5_tmp"
         # Create hdf5 file
-        h5mdfile H5Fcreate "${__h5md_filename}"
+        h5mdfile H5Fcreate "${__h5md_filename_tmp}"
     }
     # Create data groups
     h5mdfile H5Gcreate2 "particles"
@@ -250,7 +254,6 @@ proc h5md_write_positions { args } {
     # Write positions of all particles
     h5mdfile H5Dextend dims [expr [lindex $offset 0]+1 ] $h5md_num_part 3
     h5mdfile H5Sselect_hyperslab offset [expr [lindex $offset 0]] 0 0
-    h5mdfile H5_Tclose
     h5mdfile H5Screate_simple type double dims 1 $h5md_num_part 3
     set i 0
     foreach p_id $h5md_p_ids {
@@ -414,7 +417,7 @@ proc h5md_observable1D_write { args } {
     h5mdfile H5Dopen2 "observables/[lindex $args 0]/time"
     set offset [h5mdfile get_dataset_dims]
     h5mdfile H5Dextend dims [expr [lindex $offset 0]+1] 
-    h5mdfile H5Sselect_hyperslab offset [lindex $offset 0] 
+    h5mdfile H5Sselect_hyperslab offset [lindex $offset 0]
     h5mdfile H5Screate_simple type double dims 1 
     h5mdfile H5_write_value value [setmd time] index 0 
     h5mdfile H5Dwrite
@@ -503,9 +506,11 @@ proc h5md_observable2D_write { name } {
 
 # Close all h5md groups and datasets and free memory at the end
 proc h5md_close {} {
+    global __h5md_filename __h5md_filename_tmp
     h5mdfile H5_free_memory
     h5mdfile H5Fclose
-    file copy ${__h5md_filename}
+    file delete ${__h5md_filename}
+    file copy ${__h5md_filename_tmp} ${__h5md_filename}
     file delete ${__h5md_filename_tmp}
 }
 
