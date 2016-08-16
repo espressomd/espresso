a066746810d09f8eb4a0e4021548792fefcea842
psci <b.m.tanygin@gmail.com>
2016-08-16 23:24:21 +0300

    Cleanup of the mandatory HAVE_CXX11

--- src/core/config.hpp
+++ src/core/config.hpp
@@ -36,11 +36,6 @@
 #include <cmake_config.hpp>
 #endif
 
-/* If nvcc is used we disable c++11 support. Not the case starting from the version 7.0 of NVCC! */
-/*#if defined(__CUDACC__) && defined(HAVE_CXX11)
-#undef HAVE_CXX11
-#endif*/
-
 /* Prevent C++ bindings in MPI (there is a DataType called LB in there) */
 #define OMPI_SKIP_MPICXX
 #define MPICH_SKIP_MPICXX
