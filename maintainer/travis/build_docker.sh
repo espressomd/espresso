35df49d93ef1e9156628de3ae0a902f7862598e4
Florian Weik <fweik@icp.uni-stuttgart.de>
2016-08-18 16:25:25 -0600

    travis: Fix path for travis

--- maintainer/travis/build_docker.sh
+++ maintainer/travis/build_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -u espresso -e insource=$insource -e configure_params=$configure_params -e with_fftw=$with_fftw -e with_tcl=$with_tcl -e with_python_interface=$with_python_interface -e myconfig=$myconfig -e check_procs=$check_procs -v /home/florian/Source/espresso:/travis -it espressomd/travis-build /bin/bash -c "git clone /travis && cd travis && maintainer/travis/build_cmake.sh"
+docker run -u espresso -e insource=$insource -e configure_params=$configure_params -e with_fftw=$with_fftw -e with_tcl=$with_tcl -e with_python_interface=$with_python_interface -e myconfig=$myconfig -e check_procs=$check_procs -v /:/travis -it espressomd/travis-build /bin/bash -c "git clone /travis && cd travis && maintainer/travis/build_cmake.sh"
 
 
