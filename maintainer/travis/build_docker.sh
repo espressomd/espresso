378c46a964a76e570efc3a165775771bc2d29ebd
Florian Weik <fweik@icp.uni-stuttgart.de>
2016-08-18 16:31:56 -0600

    travis show pwd

--- maintainer/travis/build_docker.sh
+++ maintainer/travis/build_docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo ${PWD}
+ls
+
 docker run -u espresso -e insource=$insource -e configure_params=$configure_params -e with_fftw=$with_fftw -e with_tcl=$with_tcl -e with_python_interface=$with_python_interface -e myconfig=$myconfig -e check_procs=$check_procs -v /:/travis -it espressomd/travis-build /bin/bash -c "git clone /travis && cd travis && maintainer/travis/build_cmake.sh"
 
 
