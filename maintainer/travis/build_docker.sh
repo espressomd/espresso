9b0af6fc083775b6f1c3ff4e20d1d7c0486a0dbd
Florian Weik <fweik@icp.uni-stuttgart.de>
2016-08-19 12:47:34 -0600

    travis: Better handling of env vars.

--- maintainer/travis/build_docker.sh
+++ maintainer/travis/build_docker.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-docker run -u espresso -e insource=$insource -e configure_params=$configure_params -e with_fftw=$with_fftw -e with_tcl=$with_tcl -e with_python_interface=$with_python_interface -e myconfig=$myconfig -e check_procs=$check_procs -v ${PWD}:/travis -it espressomd/travis-build /bin/bash -c "git clone /travis && cd travis && maintainer/travis/build_cmake.sh"
+ENV_FILE=$(mktemp esXXXXXXX.env)
 
+cat > $ENV_FILE <<EOF
+insource=$insource
+configure_params=$configure_params
+with_fftw=$with_fftw
+with_tcl=$with_tcl
+with_python_interface=$with_python_interface
+myconfig=$myconfig
+check_procs=$check_procs
+EOF
 
+docker run -u espresso --env-file $ENV_FILE -v ${PWD}:/travis -it espressomd/travis-build /bin/bash -c "git clone /travis && cd travis && maintainer/travis/build_cmake.sh"
