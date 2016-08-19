28d21f685ad03730b2dce6fc4e9c8e1bc8f28a77
Florian Weik <fweik@icp.uni-stuttgart.de>
2016-08-19 15:45:41 -0600

    testsuite: Don't fail all python tests if vtk is not found.

--- testsuite/python/tests_common.py
+++ testsuite/python/tests_common.py
@@ -1,23 +1,25 @@
 import numpy as np
-import vtk
-from vtk.util.numpy_support import vtk_to_numpy
 
-def calculate_vtk_max_pointwise_difference(file1,file2,tol=1e-6):
-    arrays = [0]*2
+try:
+    import vtk
+    from vtk.util.numpy_support import vtk_to_numpy
 
-    reader = vtk.vtkStructuredPointsReader()
-    for i, fname in enumerate([file1, file2]):
-        reader.SetFileName(fname)
-        reader.Update()
-        data = reader.GetOutput().GetPointData()
-        arrays[i] = np.array([vtk_to_numpy(data.GetArray(n)) for n in range(data.GetNumberOfArrays())])
+    def calculate_vtk_max_pointwise_difference(file1,file2,tol=1e-6):
+        arrays = [0]*2
 
-    try:
-        return np.allclose(arrays[0],arrays[1],rtol=0,atol=tol), np.max(np.abs(arrays[0]-arrays[1]))
-    except:
-        return False, np.inf
+        reader = vtk.vtkStructuredPointsReader()
+        for i, fname in enumerate([file1, file2]):
+            reader.SetFileName(fname)
+            reader.Update()
+            data = reader.GetOutput().GetPointData()
+            arrays[i] = np.array([vtk_to_numpy(data.GetArray(n)) for n in range(data.GetNumberOfArrays())])
 
-
+        try:
+            return np.allclose(arrays[0],arrays[1],rtol=0,atol=tol), np.max(np.abs(arrays[0]-arrays[1]))
+        except:
+            return False, np.inf
+except:
+    pass
 
 # Tests particle property setters/getters
 import unittest as ut
