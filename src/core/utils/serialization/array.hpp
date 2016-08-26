32be78f840295fbff92bccef29d529901ca5d385
Florian Weik <fweik@icp.uni-stuttgart.de>
2016-08-26 23:04:27 +0200

    utils: Added boost version check.

--- src/core/utils/serialization/array.hpp
+++ src/core/utils/serialization/array.hpp
@@ -3,12 +3,20 @@
 
 #include <array>
 
+#include <boost/version.hpp>
+
+/* New versions of boost alrady containt this
+ * function
+ * */
+#if BOOST_VERSION < 1005600
+
 namespace boost {
 namespace serialization {
-	template<typename Archive, typename T, std::size_t N>
-		void serialize(Archive &ar, std::array<T, N> & a, const unsigned int) {
-			ar & *static_cast<T (*)[N]>(static_cast<void *>(a.data()));
-		}
+template <typename Archive, typename T, std::size_t N>
+void serialize(Archive &ar, std::array<T, N> &a, const unsigned int) {
+  ar &*static_cast<T(*)[N]>(static_cast<void *>(a.data()));
 }
 }
+}
+#endif
 #endif
