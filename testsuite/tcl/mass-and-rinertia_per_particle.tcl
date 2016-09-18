592b3e1174bcc46828473d3d7c9beeb4100b4982
Dr. Bogdan Tanygin <b.m.tanygin@gmail.com>
2016-09-18 21:56:17 +0300

    Intermittent test stabilization

--- testsuite/tcl/mass-and-rinertia_per_particle.tcl
+++ testsuite/tcl/mass-and-rinertia_per_particle.tcl
@@ -125,10 +125,10 @@ proc test_mass-and-rinertia_per_particle {test_case} {
     setmd time_step 0.008
 
     set n 200
-    set mass [expr [t_random] *20]
-    set j1 [expr [t_random] * 20]
-    set j2 [expr [t_random] * 20]
-    set j3 [expr [t_random] * 20]
+    set mass [expr (0.2 + [t_random]) *20]
+    set j1 [expr (0.2 + [t_random]) * 20]
+    set j2 [expr (0.2 + [t_random]) * 20]
+    set j3 [expr (0.2 + [t_random]) * 20]
 
     for {set i 0} {$i<$n} {incr i} {
         for {set k 0} {$k<2} {incr k} {
@@ -151,7 +151,7 @@ proc test_mass-and-rinertia_per_particle {test_case} {
     }
 
 
-    set loops 50
+    set loops 100
     puts "Thermalizing..."
     integrate 1200
     puts "Measuring..."
@@ -173,7 +173,7 @@ proc test_mass-and-rinertia_per_particle {test_case} {
         }
     }
 
-    set tolerance 0.12
+    set tolerance 0.15
     for {set k 0} {$k<2} {incr k} {
         set Evx($k) [expr 0.5 * $mass *$vx2($k)/$n/$loops]
         set Evy($k) [expr 0.5 * $mass *$vy2($k)/$n/$loops]
