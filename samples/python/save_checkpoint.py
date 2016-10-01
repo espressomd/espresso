df7662b53143900cefd6ae5ec51727f34a0339cd
Alexander Weyman <alexander.weyman@gmail.com>
2016-10-01 12:28:10 +0200

    extend testcases (user data, system, thermostat, non_bonded_inter, part, p3m)

--- samples/python/save_checkpoint.py
+++ samples/python/save_checkpoint.py
@@ -1,12 +1,18 @@
 import espressomd
+from espressomd import electrostatics
 from espressomd import checkpointing
 
-checkpoint = checkpointing.Checkpointing("mycheckpoint", "./checkpointing/")
+import numpy
 
+checkpoint = checkpointing.Checkpointing(checkpoint_id="mycheckpoint")
+
+# test for user data
 myvar = "some script variable"
 
 checkpoint.register("myvar")
 
+
+# test for "system"
 box_l = 10.7437
 
 system = espressomd.System()
@@ -17,6 +23,13 @@ system.box_l = [box_l, box_l, box_l]
 checkpoint.register("system")
 
 
+# test for "system.thermostat"
+system.thermostat.set_langevin(kT=1.0, gamma=1.0)
+
+checkpoint.register("system.thermostat")
+
+
+# test for "system.non_bonded_inter"
 lj_eps = 1.0
 lj_sig = 1.0
 lj_cut = 1.12246
@@ -28,6 +41,25 @@ system.non_bonded_inter[0, 0].lennard_jones.set_params(
 system.non_bonded_inter.set_force_cap(lj_cap)
 
 checkpoint.register("system.non_bonded_inter")
+
+
+# test for "system.part"
+n_part = 10
+for i in range(n_part):
+    system.part.add(id=i, pos=numpy.random.random(3) * system.box_l)
+
+checkpoint.register("system.part")
+
+
+# test for "p3m"
+for i in range(n_part / 2 - 1):
+    system.part[2 * i].q = -1.0
+    system.part[2 * i + 1].q = 1.0
+p3m = electrostatics.P3M(bjerrum_length=1.0, accuracy=1e-2)
+system.actors.add(p3m)
+
+checkpoint.register("p3m")
+
 
 
 checkpoint.save()
