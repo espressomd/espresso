f0b0580842039319264b31114a4b84955b2c4235
KonradBreitsprecher <konradbreitsprecher@gmail.com>
2016-10-07 11:38:42 +0200

    Added ifdef for CONSTRAINTS in script_interface/constraints/initialize.cpp

--- src/script_interface/constraints/initialize.cpp
+++ src/script_interface/constraints/initialize.cpp
@@ -17,6 +17,8 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#ifdef CONSTRAINTS
+
 #include "initialize.hpp"
 #include "ParallelScriptInterface.hpp"
 #include "utils/Factory.hpp"
@@ -36,3 +38,5 @@ void initialize() {
 }
 } /* namespace Constraints */
 } /* namespace ScriptInterface */
+
+#endif
