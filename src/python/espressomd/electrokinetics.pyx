7b285a7334204ec803c507125d361b110c108e8d
Tobias Rau <phy79182@stud.uni-stuttgart.de>
2016-10-07 14:07:25 +0200

    imported EK stuff from codingday

--- src/python/espressomd/electrokinetics.pyx
+++ src/python/espressomd/electrokinetics.pyx
@@ -1,198 +1,141 @@
-from __future__ import print_function, absolute_import
-from libcpp cimport bool
+include "myconfig.pxi"
+import lb
 import numpy as np
 
-include "myconfig.pxi"
+IF ELECTROKINETICS:
 
-IF ELECTROKINETICS == 1:
-    cdef extern from "electrokinetics.hpp":
-        void ek_print_parameters()
-        void ek_print_lbpar()
-        unsigned int ek_calculate_boundary_mass()
+    cdef class Electrokinetics(lb.HydrodynamicInteraction):
+        
+        def validate_params(self):
+            default_params = self.default_params()
+            
+            if not (self._params["stencil"] in ["linkcentered", "nonlinear", "nodecentered"]):
+                raise ValueError("stencil has to be 'linkcentered', 'nonlinear' or 'nodecentered'.")
 
-        int ek_print_vtk_density(int species, char * filename)
-        int ek_print_vtk_flux(int species, char * filename)
-        int ek_print_vtk_potential(char * filename)
+            if not (self._params["fluid_coupling"] in ["friction", "estatics"]):
+                raise ValueError("fluid_coupling has to be 'friction' or 'estatics'.")
+                
+        
+        def valid_keys(self):
+            return "agrid", "lb_density", "viscosity", "friction", "bulk_viscosity", "gamma_even", "gamma_odd", "T", "bjerrum_length", "stencil", "advection", "fluid_coupling"
 
-        int ek_print_vtk_lbforce(char * filename)
-        IF EK_REACTION == 1:
-            int ek_print_vtk_reaction_tags(char * filename)
-        int ek_lb_print_vtk_density(char * filename)
-        int ek_lb_print_vtk_velocity(char * filename)
+        def required_keys(self):
+            return ["agrid", "lb_density", "viscosity", "friction", "T", "bjerrum length"]
 
-        int ek_init()
+        def default_params(self):
+            return {"agrid": -1,
+                    "lb_density": -1,
+                    "viscosity": -1,
+                    "bulk_viscosity": -1,
+                    "gamma_odd": 0.0,
+                    "gamma_even": 0.0,
+                    "friction": 0.0,
+                    "T": -1,
+                    "bjerrum_length": -1,
+                    "stencil": 0,
+                    "advection": True,
+                    "fluid_coupling": True}
 
-        int ek_set_agrid(double agrid)
-        int ek_set_lb_density(double lb_density)
-        int ek_set_viscosity(double viscosity)
-        int ek_set_friction(double friction)
-        int ek_set_T(double T)
-        int ek_set_bjerrumlength(double bjerrumlength)
+        def _get_params_from_es_core(self):
+            if ek_parameters.stencil == 0:
+                stencil = "linkcentered"
+            elif ek_parameters.stencil == 1:
+                stencil = "nonlinear"
+            elif ek_parameters.stencil == 2:
+                stencil = "nodecentered"
+            else: 
+                raise Exception("Value of stencil could not be identified.")
+                
+            if ek_parameters.fluidcoupling_ideal_contribution == True:
+                fluid_coupling = "friction"
+            else:
+                fluid_coupling = "estatics"
 
-        IF EK_ELECTROSTATIC_COUPLING == 1:
-            int ek_print_vtk_particle_potential(char * filename)
-            int ek_set_electrostatics_coupling(bool electrostatics_coupling)
+            return {"agrid": ek_parameters.agrid,
+                    "lb_density": ek_parameters.lb_density,
+                    "viscosity": ek_parameters.viscosity,
+                    "bulk_viscosity": ek_parameters.bulk_viscosity,
+                    "gamma_odd": ek_parameters.gamma_odd,
+                    "gamma_even": ek_parameters.gamma_even,
+                    "friction": ek_parameters.friction,
+                    "T": ek_parameters.T,
+                    "bjerrum_length":ek_parameters.bjerrumlength,
+                    "stencil": stencil,
+                    "advection": ek_parameters.advection,
+                    "fluid_coupling": fluid_coupling}
 
-    #    int ek_set_bulk_viscosity(double bulk_viscosity)
-    #    int ek_set_gamma_odd(double gamma_odd)
-    #    int ek_set_gamma_even(double gamma_even)
-        int ek_set_lb_force(double * ext_force)
-        int ek_set_density(int species, double density)
-        int ek_set_D(int species, double D)
-        int ek_set_valency(int species, double valency)
-        int ek_set_ext_force(int species, double ext_force_x, double ext_force_y, double ext_force_z)
-        int ek_set_stencil(int stencil)
+            
+        def _set_params_in_es_core(self):
+            if self._params["stencil"] = "linkcentered":
+                ek_set_stencil(0)
+            elif self._params["stencil"] = "nonlinear":
+                ek_set_stencil(1)
+            elif self._params["stencil"] = "nodecentered":
+                ek_set_stencil(2)
 
-        int ek_node_print_velocity(int x, int y, int z, double * velocity)
-        int ek_node_print_density(int species, int x, int y, int z, double * density)
-        int ek_node_print_flux(int species, int x, int y, int z, double * flux)
-        int ek_node_set_density(int species, int x, int y, int z, double density)
+            if self._params["fluid_coupling"] = "friction":
+                ek_set_fluidcoupling(True)
+            elif self._params["fluid_coupling"] = "estatics":
+                ek_set_fluidcoupling(False)
 
-        float ek_calculate_net_charge()
+            ek_set_agrid(self._params["agrid"])
+            ek_set_lb_density(self._parms["lb_density"])
+            ek_set_viscosity(self._params["viscosity"])
+            ek_set_friction(self._params["friction"])
+            ek_set_T(self._params["T"])
+            ek_set_bjerrumlength(self._params["bjerrum_length"])
+            ek_set_bulk_viscosity(self._params["bulk_viscosity"])
+            ek_set_gamma_odd(self._params["gamma_odd"])
+            ek_set_gamma_even(self._params["gamma_even"])
+            ek_set_advection(self._params["advection"])
 
-        int ek_neutralize_system(int species)
-        int ek_save_checkpoint(char * filename)
-        int ek_load_checkpoint(char * filename)
+            
+        def set_density(self, species=None, density=None, node=None):
+            if spicies == None or density == None:
+                raise ValueError("species and density has to be set.")
+            if not isinstance(int, species):
+                raise ValueError("species needs to be an integer.")
+            if node == None:
+                ek_set_density(species, density)
+            else:
+                if not (isinstance(list, node) or isinstance(np.ndarray, node)):
+                    if len(coords) != 3:
+                        raise ValueError("coords has to be an array of length 3 of integers.")
+                ek_node_set_density(species, node[0], node[1], node[2], density)
 
-    def ek_init_wrapper():
-        err = ek_init()
+                    
+        def _activate_method(self):
+            self._set_params_in_es_core()
+            err = ek_init()
+            if err == 2:
+                raise Exception('EK init failed', 'agrid incompatible with box size')
+            elif err != 0:
+                raise Exception('EK init failed', 'unknown error')
 
-        if err == 2:
-            raise Exception(
-                'EK init failed', 'agrid incompatible with box size')
-        elif err != 0:
-            raise Exception('EK init failed', 'unknown error')
 
-    def init(agrid=1.0, lb_density=1.0, viscosity=1.0, friction=1.0, bjerrum_length=0.7095, T=1.0, stencil='linkcentered'):
-        if ek_set_agrid(agrid):
-            raise Exception('EK init error', 'could not set agrid')
 
-        if ek_set_lb_density(lb_density):
-            raise Exception('EK init error', 'could not set lb_density')
 
-        if ek_set_viscosity(viscosity):
-            raise Exception('EK init error', 'could not set viscosity')
+        # TODO:
+        def checkpoint(self):
+            raise Exception("Please implement this method in the pickle routine.")
 
-        if ek_set_friction(friction):
-            raise Exception('EK init error', 'could not set friction')
+        def add_reaction(self, shape):
+            raise Exception("This method is not implemented yet.")
 
-        if ek_set_bjerrumlength(bjerrum_length):
-            raise Exception('EK init error', 'could not set bjerrum_length')
+        def add_boundary(self, shape)
+            raise Exception("This method is not implemented yet.")
 
-        if ek_set_T(T):
-            raise Exception('EK init error', 'could not set T')
 
-        if stencil == 'linkcentered':
-            if ek_set_stencil(0):
-                raise Exception(
-                    'EK init error', 'could not set linkcentered stencil')
-        elif stencil == 'nonlinear':
-            if ek_set_stencil(1):
-                raise Exception(
-                    'EK init error', 'could not set nonlinear stencil')
-        elif stencil == 'nodecentered':
-            if ek_set_stencil(2):
-                raise Exception(
-                    'EK init error', 'could not set nodecentered stencil')
-        else:
-            raise Exception('EK init error', 'unknown stencil')
 
-        ek_init_wrapper()
+# Suggested interface usage
+# ek = Electrokinetics(params)
+# system.actor.add(ek)
 
-    def init_species(id, density, D, valency, ext_force):
-        if ek_set_density(id, density):
-            raise Exception('EK species init error', 'could not set density')
+# pos_ions = ek.Species(charge=+1, concentration=0.06, D=0.001)
+# neg_ions = ek.Species(charge=-1, concentration=0.07, D=0.001)
 
-        if ek_set_D(id, D):
-            raise Exception('EK species init error', 'could not set D')
+# ek.add_species(pos_ions, neg_ions)
 
-        if ek_set_valency(id, valency):
-            raise Exception('EK species init error', 'could not set valency')
+# pos_ion[5,6,1].density = 0.08     #nodegrid 5,6,1
 
-        if ek_set_ext_force(id, ext_force[0], ext_force[1], ext_force[2]):
-            raise Exception('EK species init error', 'could not set ext_force')
 
-        ek_init_wrapper()
-
-    def neutralize_system(species_id):
-        err = ek_neutralize_system(species_id)
-
-        if err == 1:
-            raise Exception(
-                'EK neutralize_system error', 'Species used for neutralization must exist')
-        elif err == 2:
-            raise Exception(
-                'EK neutralize_system error', 'Species used for neutralization must be charged')
-        elif err == 3:
-            raise Exception('EK neutralize_system error',
-                            'Neutralization with specified species would result in negative density')
-        elif err != 0:
-            raise Exception(
-                'EK neutralize_system error', 'Unknown error in EK neutralize_system')
-
-        ek_init_wrapper()
-
-    def print_parameters():
-        ek_print_parameters()
-
-    def print_lbpar():
-        ek_print_lbpar()
-
-    def set_electrostatics_coupling(state):
-        IF EK_ELECTROSTATIC_COUPLING == 1:
-            ek_set_electrostatics_coupling(state)
-            ek_init_wrapper()
-        ELSE:
-            raise Exception(
-                'missing feature', 'feature EK_ELECTROSTATICS_COUPLING needs to be enabled')
-
-    def set_lb_force(force):
-        cdef double tmp[3]
-        tmp[0] = force[0]
-        tmp[1] = force[1]
-        tmp[2] = force[2]
-        ek_set_lb_force(tmp)
-
-        ek_init_wrapper()
-
-    def net_charge():
-        return ek_calculate_net_charge()
-
-    def save_checkpoint(path):
-        if ek_save_checkpoint(path):
-            raise Exception(
-                'EK checkpointing error', 'could not save checkpoint')
-
-    def load_checkpoint(path):
-        if ek_load_checkpoint(path):
-            raise Exception(
-                'EK checkpointing error', 'could not load checkpoint')
-
-    def print_density_vtk(species_id, path):
-        if ek_print_vtk_density(species_id, path):
-            raise Exception('EK output error', 'could not save density VTK')
-
-    def print_flux_vtk(species_id, path):
-        if ek_print_vtk_flux(species_id, path):
-            raise Exception('EK output error', 'could not save flux VTK')
-
-    def print_potential_vtk(path):
-        if ek_print_vtk_potential(path):
-            raise Exception('EK output error', 'could not save potential VTK')
-
-    def print_lb_force_vtk(path):
-        if ek_print_vtk_lbforce(path):
-            raise Exception('EK output error', 'could not save lbforce VTK')
-    IF EK_REACTION == 1:
-        def print_reaction_tags_vtk(path):
-            if ek_print_vtk_reaction_tags(path):
-                raise Exception(
-                    'EK output error', 'could not save reaction tags VTK')
-
-    def print_lb_density_vtk(path):
-        if ek_lb_print_vtk_density(path):
-            raise Exception('EK output error', 'could not save lbdensity VTK')
-
-    def print_velocity_vtk(path):
-        if ek_lb_print_vtk_velocity(path):
-            raise Exception('EK output error', 'could not save velocity VTK')
