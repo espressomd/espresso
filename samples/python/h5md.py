c16bbc286a0e3858efbe07c5c03fee0dc96876ac
Kai Szuttor <kai@icp.uni-stuttgart.de>
2016-10-13 12:25:48 +0200

    Update of h5mdcore python interface.

---
+++ samples/python/h5md.py
@@ -0,0 +1,17 @@
+import espressomd
+from espressomd.io.writer import h5md 
+import numpy as np
+
+system = espressomd.System()
+system.box_l = [10.0, 10.0, 10.0]
+system.time_step = 0.01
+system.thermostat.set_langevin(kT=1.0, gamma=1.0)
+system.cell_system.skin = 0.4
+
+for i in range(1000):
+    system.part.add(id=i, pos=np.random.random(3) * system.box_l, type=23)
+system.integrator.run(steps=100)
+h5 = h5md.H5md(filename="bla.h5", write_pos=True, write_vel=True)
+print h5._print_params()
+h5.write()
+h5.close()
--- src/python/espressomd/io/writer/h5md.py
+++ src/python/espressomd/io/writer/h5md.py
@@ -1,6 +1,40 @@
-from ...script_interface import ScriptInterfaceHelper
+from ...script_interface import PScriptInterface
 import sys
 
-class H5md(ScriptInterfaceHelper):
-    _so_name = "ScriptInterface::Writer::H5mdScript"
-    _so_bind_methods = ["init_file", "write", "close"]
+
+class H5md():
+    def __init__(self, **kwargs):
+        self.valid_params = ['filename',]
+        if not ('filename' in kwargs):
+            raise ValueError("'filename' parameter missing.")
+        self.what = {'write_pos': 1<<0,
+                     'write_vel': 1<<1,
+                     'write_force': 1<<2,
+                     'write_type': 1<<3,
+                     'write_mass': 1<<4}
+        self.valid_params.append(self.what.keys())
+        self.what_bin = 0
+        for i,j in kwargs.iteritems():
+            if (i in self.what.keys()):
+                if (isinstance(j, bool)):
+                    if j:
+                        self.what_bin += self.what[i]
+                else:
+                    raise ValueError("{} has to be a bool value.".format(i))
+            elif (i not in self.valid_params):
+                raise ValueError("Unknown parameter {} for H5MD writer.".format(i))
+        self.h5md_instance = PScriptInterface("ScriptInterface::Writer::H5mdScript")
+        self.h5md_instance.set_params(filename=kwargs['filename'], what=self.what_bin, 
+                                      scriptname=sys.argv[0])
+        self.h5md_instance.call_method("init_file")
+
+    def _print_params(self):
+        return self.h5md_instance.get_params()
+
+
+    def write(self):
+        self.h5md_instance.call_method("write")
+    
+
+    def close(self):
+        self.h5md_instance.call_method("close")
