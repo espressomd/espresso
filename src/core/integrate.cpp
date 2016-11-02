8bb8c6df92d6d5bf33a542fe0436f40ddb3b684d
Bogdan Tanygin <b.m.tanygin@gmail.com>
2016-11-02 18:10:45 +0200

    Refactoring

--- src/core/integrate.cpp
+++ src/core/integrate.cpp
@@ -733,9 +733,9 @@ void rescale_forces_propagate_vel() {
       if (thermo_switch & THERMO_LANGEVIN)
       {
 #ifdef LANGEVIN_PER_PARTICLE
-          step4_factor = 1 / (1 - p->p.vv_langevin_pref1 * scale / (p[i]).p.mass);
+          step4_factor = 1.0 / (1.0 - p->p.vv_langevin_pref1 * scale / (p[i]).p.mass);
 #else
-          step4_factor = 1 / (1 - vv_langevin_pref1 * scale / (p[i]).p.mass);
+          step4_factor = 1.0 / (1.0 - vv_langevin_pref1 * scale / (p[i]).p.mass);
 #endif // LANGEVIN_PER_PARTICLE
       }
 #endif
