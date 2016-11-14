b022c21a0e3c46a1be482c50b25d54da56e9aa53
Kai Szuttor <kai@icp.uni-stuttgart.de>
2016-11-14 21:51:41 +0100

    Fixing indentation in python interface.

--- src/python/espressomd/correlators.py
+++ src/python/espressomd/correlators.py
@@ -8,9 +8,9 @@ class Correlator(ScriptInterfaceHelper):
     _so_bind_methods = ("update","auto_update","finalize", "dim_corr","n_results","hierarchy_depth")
     
     def result(self):
-       res=np.array(self.call_method("get_correlation"))
-       res=res.reshape((self.n_results(),2+self.dim_corr()))
-       return res
+        res=np.array(self.call_method("get_correlation"))
+        res=res.reshape((self.n_results(),2+self.dim_corr()))
+        return res
 
 
 
