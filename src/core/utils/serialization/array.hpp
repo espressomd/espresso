0fabf28647d17acb83527911548edae7f96de0ba
Florian Weik <fweik@icp.uni-stuttgart.de>
2016-11-15 10:11:06 +0100

    utils: Enable mpi optimization for std::array

--- src/core/utils/serialization/array.hpp
+++ src/core/utils/serialization/array.hpp
@@ -3,12 +3,12 @@
 
 #include <boost/version.hpp>
 
-/* New versions of boost alrady containt this
+/* New versions of boost already containt this
  * function
  * */
 #if BOOST_VERSION < 105600
-#include <boost/serialization/serialization.hpp>
 #include <array>
+#include <boost/serialization/serialization.hpp>
 
 namespace boost {
 namespace serialization {
@@ -18,9 +18,19 @@ void serialize(Archive &ar, std::array<T, N> &a, const unsigned int) {
 }
 }
 }
-
 #else
 #include <array>
 #include <boost/serialization/serialization.hpp>
+#endif
+
+/* Will be included in boost from verison 1.63 */
+#if BOOST_VERSION <= 106200
+#include <boost/mpi/datatype_fwd.hpp>
+namespace boost {
+namespace mpi {
+template <class T, std::size_t N>
+struct is_mpi_datatype<std::array<T, N>> : public is_mpi_datatype<T> {};
+} /* namespace mpi */
+} /* namespace boost */
 #endif
 #endif
