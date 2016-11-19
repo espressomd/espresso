cb5cbf7428c9da59cd0ee4fec01fa4b32d1396b9
Kai Szuttor <kaiszuttor@hotmail.com>
2016-11-19 19:21:24 +0100

    h5mdcore: pylint score for python sample 10.0/10.0.

--- samples/python/h5md.py
+++ samples/python/h5md.py
@@ -1,17 +1,23 @@
-import espressomd
-from espressomd.io.writer import h5md 
-import numpy as np
+"""Sample for the usage of H5MD trajectory writing
+in ESPResSo.
+"""
 
-system = espressomd.System()
-system.box_l = [10.0, 10.0, 10.0]
-system.time_step = 0.01
-system.thermostat.set_langevin(kT=1.0, gamma=1.0)
-system.cell_system.skin = 0.4
+
+import numpy as np
+import espressomd # pylint: disable=import-error
+from espressomd.io.writer import h5md # pylint: disable=import-error
+
+
+SYS = espressomd.System()
+SYS.box_l = [10.0, 10.0, 10.0]
+SYS.time_step = 0.01
+SYS.thermostat.set_langevin(kT=1.0, gamma=1.0)
+SYS.cell_system.skin = 0.4
 
 for i in range(1000):
-    system.part.add(id=i, pos=np.random.random(3) * system.box_l, type=23)
-system.integrator.run(steps=10)
-h5 = h5md.H5md(filename="bla.h5", write_pos=True, write_vel=True,
-               write_force=True, write_type=True, write_mass=False)
-h5.write()
-h5.close()
+    SYS.part.add(id=i, pos=np.random.random(3) * SYS.box_l, type=23)
+SYS.integrator.run(steps=10)
+H5_FILE = h5md.H5md(filename="bla.h5", write_pos=True, write_vel=True,
+                    write_force=True, write_type=True, write_mass=False)
+H5_FILE.write()
+H5_FILE.close()
