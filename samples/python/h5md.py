c7c11b89b2841544ace496abba945e71333a3126
Kai Szuttor <kai@icp.uni-stuttgart.de>
2016-11-23 14:19:35 +0100

    h5mdcore: more appropriate filename of sample h5 file.

--- samples/python/h5md.py
+++ samples/python/h5md.py
@@ -17,7 +17,7 @@ sys.cell_system.skin = 0.4
 for i in range(1000):
     sys.part.add(id=i, pos=np.random.random(3) * sys.box_l, type=23)
 sys.integrator.run(steps=10)
-h5_file = h5md.H5md(filename="bla.h5", write_pos=True, write_vel=True,
+h5_file = h5md.H5md(filename="sample.h5", write_pos=True, write_vel=True,
                     write_force=True, write_type=True, write_mass=False)
 h5_file.write()
 h5_file.close()
