6f9c94767abba6e1db8a2db61096ff8520100b9e
Florian Weik <fweik@icp.uni-stuttgart.de>
2017-02-20 13:37:38 +0100

    random: Initialize PRNG with random number from system.

--- src/core/random.cpp
+++ src/core/random.cpp
@@ -127,7 +127,7 @@ string mpi_random_get_stat() {
 
 void init_random(void) {
   /** Set the initial seed */
-  init_random_seed(1 + this_node);
+  init_random_seed(std::random_device()());
 
   /** Register callbacks */
   mpiCallbacks().add(mpi_random_seed_slave);
@@ -135,11 +135,13 @@ void init_random(void) {
   mpiCallbacks().add(mpi_random_get_stat_slave);
 }
 
-void init_random_seed(int seed)
-{
-  std::seed_seq seeder{seed}; //come up with "sane" initialization to avoid too many zeros in the internal state of the Mersenne twister
+void init_random_seed(int seed) {
+  std::seed_seq seeder{seed}; // come up with "sane" initialization to avoid too
+                              // many zeros in the internal state of the
+                              // Mersenne twister
   generator.seed(seeder);
-  generator.discard(1e6); //discard the first 1e6 random numbers to warm up the Mersenne-Twister PRNG
+  generator.discard(1e6); // discard the first 1e6 random numbers to warm up the
+                          // Mersenne-Twister PRNG
 }
 
 } /* Random */
