12ec3dbbc6cbd17f83782f7e65a3915ebbf673a7
Henri Menke <henri@icp.uni-stuttgart.de>
2017-03-01 10:53:33 +0100

    [lbgpu] LB_GPU timestep is float and can only be compared within float precision

--- src/python/espressomd/_system.pyx
+++ src/python/espressomd/_system.pyx
@@ -186,7 +186,7 @@ cdef class System:
                     raise ValueError(
                         "Time Step (" + str(time_step) + ") must be > LB_time_step (" + str(lbpar.tau) + ")")
             IF LB_GPU:
-                if lbpar_gpu.tau >= 0.0 and _time_step < lbpar_gpu.tau:
+                if lbpar_gpu.tau >= 0.0 and lbpar_gpu.tau - _time_step > 1e-8*abs(lbpar_gpu.tau+_time_step):
                     raise ValueError(
                         "Time Step (" + str(time_step) + ") must be > LB_time_step (" + str(lbpar_gpu.tau) + ")")
             mpi_set_time_step(_time_step)
