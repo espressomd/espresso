385db4b1cff0899c7bfdf2092bb5a444c57a337e
Florian Weik <fweik@icp.uni-stuttgart.de>
2017-04-11 09:32:18 +0200

    utils: More comments.

--- src/core/utils/make_function.hpp
+++ src/core/utils/make_function.hpp
@@ -10,16 +10,36 @@ namespace Utils {
 namespace detail {
 template <class FPtr> struct function_traits;
 
+/**
+ * @brief Determine the signature from a pointer to member function (C::*).
+ */
 template <class T, class C> struct function_traits<T(C::*)> { typedef T type; };
 }
 
-/* Identity for std::function */
+/* make_function deduces the signature of a class with not-overloaded operator()
+   member , a lambda or std::function,
+   and creates a corresponding std::function. This is needed to solve some
+   issues with
+   template parameer deduction if the type is not now beforehand. */
+
+/**
+ * @brief Given a std::function, return a copy.
+ *
+ * For a std::function argument nothing has to be done, just a copy is returned.
+ */
 template <typename T, typename... Args>
 std::function<T(Args...)> make_function(std::function<T(Args...)> const &f) {
   return f;
 }
 
-/* For lambdas and functors */
+/**
+ * @brief Given a Functor or a Closure, return a std::function with the same
+ * signature that contains a copy of the argument.
+ *
+ * This inspects the operator() on the given type, deduces the signature,
+ * if operator() is const, as in non-mutable lambdas, the const is removed,
+ * and a std::function with the correct type, containing a copy, is returned.
+ */
 template <
     typename F,
     typename Signature = typename Utils::function_remove_const<
