c3a74566946f1e64ec535db7ed3f08eeb7a8c0b9
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2017-05-02 14:32:07 +0200

    Initial refactor and doc of cluster_analysis.hpp

--- src/core/cluster_analysis.hpp
+++ src/core/cluster_analysis.hpp
@@ -23,16 +23,14 @@ class NeighborCriterion {
 
 
 
-
+/** @brief Represents a single cluster of particles */
 class Cluster {
   public: 
-    /** Particles in the cluster */
+    /** @brief Ids of the particles in the cluster */
     std::vector<int> particles;
-    // add a particle (Makes a copy of the original)
+    /** @brief add a particle to the cluster */
     void add_particle(const Particle& p);
  };
-
-// add a particle (Makes a copy of the original)
 
 void Cluster::add_particle(const Particle& p) {
  particles.push_back(p.p.identity);
@@ -40,37 +38,47 @@ void Cluster::add_particle(const Particle& p) {
 
 
 
+/** @brief Holds the result and parameters of a cluster analysis */
 class ClusterStructure {
  public:
-  // Container to hold the clusters
+  /** @brief Map holding the individual clusters. The key is an interger cluster id */
   std::map<int,Cluster> clusters;
-  // ID of cluster, a particle belongs to
+  /** @brief Map between particle ids and corresponding cluster ids */
   std::map<int, int> cluster_id;
-  // Clusters that turn out to be the same (i.e., if two particles are
-  // neighbors that already belong to different clusters)
-  std::map<int,int> cluster_identities;
-  // Clear data strucutres
+  /** @brief Clear data strucutres */
   void clear();
-  // Analyze the cluster structure for energy and sistance-based
-  // criteria
-  void analyze_pair();
-  // Analyze cluster strcutre based on the presence of a bond as criterion
-  void analyze_bonds();
-  // Count a pair of particles
-  void add_pair(Particle& p1, Particle& p2);
-  // Merge clusters which turned out to be one and the same
-  void merge_clusters();
-  // Neighbor criterion
-  void set_criterion(NeighborCriterion* c);
+  /** @brief Run cluster analysis, consider all aprticle pairs */
+  void run_for_all_pairs();
+  /** @brief Run cluster analysis, consider pairs of particles connected by a bonded interaction */
+  void run_for_bonded_particles();
+  /** Is particle p part of a cluster */
   bool part_of_cluster(const Particle& p);
-  NeighborCriterion* get_criterion() {return nc;};
- private:
+  /** Sets the pair criterion which decides if two particles are neighbors */
+  void set_pair_criterion(std::shared_ptr<PairCriterion> const &c) {
+    m_pair_criterion = c;
+  }
+
+  PairCriterion const &pair_criterion() const { return *m_pair_criterion; }
+
+ Private:
+  /** @brief Clusters that turn out to be the same during the analysis process
+  * (i.e., if two particles are neighbors that already belong to different clusters
+  */
+  std::map<int,int> m_cluster_identities;
+
+  /** @brief pari criterion which decides whether two particles are neighbors */
+  std::shared_ptr<PairCriterion> m_pair_criterion;
+  
+  /** @brief Consider an individual pair of particles during cluster analysis */
+  void add_pair(Particle& p1, Particle& p2);
+  /** Merge clusters and populate their structures */
+  void merge_clusters();
   NeighborCriterion* nc;
-  // Follow a chain of cluster identities
+  /** @brief Follow a chain of cluster identities during analysis */
   inline int find_id_for(int x);
-  //
+  /** @brief Get next free lucster id */
   inline int get_next_free_cluster_id();
- 
+  
 };
 
 
