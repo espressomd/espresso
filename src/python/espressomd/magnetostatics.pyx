32c1d5d59ba5d6f8ae4806847ef1b5777dfd1555
Dr. Bogdan Tanygin <b.m.tanygin@gmail.com>
2017-05-04 21:52:57 +0300

    Correct BH deactivation approach

--- src/python/espressomd/magnetostatics.pyx
+++ src/python/espressomd/magnetostatics.pyx
@@ -319,6 +319,10 @@ IF DIPOLES == 1:
     
             def _activate_method(self):
                 self._set_params_in_es_core()
+            
+            def _deactivate_method(self):
+                super(type(self),self)._deactivate_method()
+                deactivate_dipolar_barnes_hut()
     
             def _set_params_in_es_core(self):
                 self.set_magnetostatics_prefactor()
