6f206a4e022f56356606630413bcfa4863941dd1
Florian Weik <fweik@icp.uni-stuttgart.de>
2017-05-11 15:25:52 +0200

    core:...

--- src/core/unit_tests/mock/Particle.hpp
+++ src/core/unit_tests/mock/Particle.hpp
@@ -1,14 +1,19 @@
 #ifndef CORE_UNIT_TESTS_MOCK_PARTICLE_HPP
 #define CORE_UNIT_TESTS_MOCK_PARTICLE_HPP
 
+#include <vector>
+
 namespace Testing {
 class Particle {
   unsigned m_id;
 
 public:
+  Particle() : m_id(-1) {}
   explicit Particle(int id) : m_id(id) {}
 
   int identity() const { return m_id; }
+
+  std::vector<int> bl;
 };
 }
 
