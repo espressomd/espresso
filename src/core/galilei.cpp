9656f4f8093032ace96934facafdc04acbfc6b30
Florian Weik <fweik@icp.uni-stuttgart.de>
2017-05-29 17:24:14 +0200

    galilei: ParticleRange

--- src/core/galilei.cpp
+++ src/core/galilei.cpp
@@ -34,27 +34,17 @@ galilei_struct gal;
 /* Stop the particle motion by setting the
    velocity of each particle to zero */
 void local_kill_particle_motion(int omega) {
-  int c, np, i;
-  Particle *part;
-  Cell *cell;
+  for (auto &p : local_cells.particles()) {
+    p.m.v[0] = 0.0;
+    p.m.v[1] = 0.0;
+    p.m.v[2] = 0.0;
 
-  for (c = 0; c < local_cells.n; c++) {
-    cell = local_cells.cell[c];
-    part = cell->part;
-    np = cell->n;
-
-    for (i = 0; i < np; i++) {
-      part[i].m.v[0] = 0.0;
-      part[i].m.v[1] = 0.0;
-      part[i].m.v[2] = 0.0;
-
-      if (omega != 0) {
+    if (omega != 0) {
 #ifdef ROTATION
-        part[i].m.omega[0] = 0.0;
-        part[i].m.omega[1] = 0.0;
-        part[i].m.omega[2] = 0.0;
+      p.m.omega[0] = 0.0;
+      p.m.omega[1] = 0.0;
+      p.m.omega[2] = 0.0;
 #endif
-      }
     }
   }
 }
@@ -62,172 +52,76 @@ void local_kill_particle_motion(int omega) {
 /* Set all the forces acting on the particles
    to zero */
 void local_kill_particle_forces(int torque) {
-  int c, np, i;
-  Particle *part;
-  Cell *cell;
+  for (auto &p : local_cells.particles()) {
+    p.f.f[0] = 0.0;
+    p.f.f[1] = 0.0;
+    p.f.f[2] = 0.0;
 
-  for (c = 0; c < local_cells.n; c++) {
-    cell = local_cells.cell[c];
-    part = cell->part;
-    np = cell->n;
-
-    for (i = 0; i < np; i++) {
-      part[i].f.f[0] = 0.0;
-      part[i].f.f[1] = 0.0;
-      part[i].f.f[2] = 0.0;
-
-      if (torque != 0) {
+    if (torque != 0) {
 #ifdef ROTATION
-        part[i].f.torque[0] = 0.0;
-        part[i].f.torque[1] = 0.0;
-        part[i].f.torque[2] = 0.0;
+      p.f.torque[0] = 0.0;
+      p.f.torque[1] = 0.0;
+      p.f.torque[2] = 0.0;
 #endif
-      }
     }
   }
 }
 
 /* Calculate the CMS of the system */
 void local_system_CMS(double *sdata) {
-  int c, np, i;
-  Particle *part;
-  Cell *cell;
   double x = 0.0, y = 0.0, z = 0.0;
   double ppos[3];
   int img[3];
 
-#ifdef MASS
-
   double mass = 0.0;
   double M;
 
-  for (c = 0; c < local_cells.n; c++) {
-    cell = local_cells.cell[c];
-    part = cell->part;
-    np = cell->n;
+  for (auto const &p : local_cells.particles()) {
+    M = p.p.mass;
+    mass += M;
 
-    for (i = 0; i < np; i++) {
-      M = part[i].p.mass;
-      mass += M;
+    memmove(ppos, p.r.p, 3 * sizeof(double));
+    memmove(img, p.l.i, 3 * sizeof(int));
+    unfold_position(ppos, img);
 
-      memmove(ppos, part[i].r.p, 3 * sizeof(double));
-      memmove(img, part[i].l.i, 3 * sizeof(int));
-      unfold_position(ppos, img);
-
-      x += M * ppos[0];
-      y += M * ppos[1];
-      z += M * ppos[2];
-    }
+    x += M * ppos[0];
+    y += M * ppos[1];
+    z += M * ppos[2];
   }
 
   sdata[0] = x;
   sdata[1] = y;
   sdata[2] = z;
   sdata[3] = mass;
-
-#else
-
-  int npart = 0;
-
-  for (c = 0; c < local_cells.n; c++) {
-    cell = local_cells.cell[c];
-    part = cell->part;
-    np = cell->n;
-
-    for (i = 0; i < np; i++) {
-      npart++;
-
-      memmove(ppos, part[i].r.p, 3 * sizeof(double));
-      memmove(img, part[i].l.i, 3 * sizeof(int));
-      unfold_position(ppos, img);
-
-      x += ppos[0];
-      y += ppos[1];
-      z += ppos[2];
-    }
-  }
-
-  sdata[0] = x;
-  sdata[1] = y;
-  sdata[2] = z;
-  sdata[3] = (double)npart;
-
-#endif
 }
 
 /* Calculate the CMS velocity of the system */
 void local_system_CMS_velocity(double *sdata) {
-  int c, np, i;
-  Particle *part;
-  Cell *cell;
   double x = 0.0, y = 0.0, z = 0.0;
-
-#ifdef MASS
 
   double mass = 0.0;
   double M;
 
-  for (c = 0; c < local_cells.n; c++) {
-    cell = local_cells.cell[c];
-    part = cell->part;
-    np = cell->n;
+  for (auto const &p : local_cells.particles()) {
+    M = p.p.mass;
+    mass += M;
 
-    for (i = 0; i < np; i++) {
-      M = part[i].p.mass;
-      mass += M;
-
-      x += M * part[i].m.v[0];
-      y += M * part[i].m.v[1];
-      z += M * part[i].m.v[2];
-    }
+    x += M * p.m.v[0];
+    y += M * p.m.v[1];
+    z += M * p.m.v[2];
   }
 
   sdata[0] = x;
   sdata[1] = y;
   sdata[2] = z;
   sdata[3] = mass;
-
-#else
-
-  int npart = 0;
-
-  for (c = 0; c < local_cells.n; c++) {
-    cell = local_cells.cell[c];
-    part = cell->part;
-    np = cell->n;
-
-    for (i = 0; i < np; i++) {
-      npart++;
-
-      x += part[i].m.v[0];
-      y += part[i].m.v[1];
-      z += part[i].m.v[2];
-    }
-  }
-
-  sdata[0] = x;
-  sdata[1] = y;
-  sdata[2] = z;
-  sdata[3] = (double)npart;
-
-#endif
 }
 
 /* Remove the CMS velocity */
 void local_galilei_transform(double *sdata) {
-  int c, np, i;
-  Particle *part;
-  Cell *cell;
-
-  for (c = 0; c < local_cells.n; c++) {
-    cell = local_cells.cell[c];
-    part = cell->part;
-    np = cell->n;
-
-    for (i = 0; i < np; i++) {
-      part[i].m.v[0] -= sdata[0];
-      part[i].m.v[1] -= sdata[1];
-      part[i].m.v[2] -= sdata[2];
-    }
+  for (auto &p : local_cells.particles()) {
+    p.m.v[0] -= sdata[0];
+    p.m.v[1] -= sdata[1];
+    p.m.v[2] -= sdata[2];
   }
 }
