798641917f1e084670e752bfa45b65250333f562
Patrick Kreissl <patrick.kreissl@pa-le.de>
2017-05-29 15:50:42 +0200

    Implement add_energy in HomogeneousMagneticField constraint.

--- src/core/constraints/HomogeneousMagneticField.cpp
+++ src/core/constraints/HomogeneousMagneticField.cpp
@@ -6,28 +6,21 @@ namespace Constraints {
 void HomogeneousMagneticField::add_force(Particle *p, double *folded_pos) {
 #ifdef ROTATION
 #ifdef DIPOLES
-    p->f.torque[0] += p->r.dip[1]*m_field[2]-p->r.dip[2]*m_field[1];
-    p->f.torque[1] += p->r.dip[2]*m_field[0]-p->r.dip[0]*m_field[2];
-    p->f.torque[2] += p->r.dip[0]*m_field[1]-p->r.dip[1]*m_field[0];
+    double c[3];
+    utils::cross_product(p->r.dip, &m_field.front(), c);
+    for (int i=0; i<3; ++i) {
+        p->f.torque[i] += c[i];
+        std::cout << i << std::endl;
+    }
 #endif
 #endif
 }
 
 void HomogeneousMagneticField::add_energy(Particle *p, double *folded_pos, Observable_stat &energy) const {
-#ifdef ROTATION
 #ifdef DIPOLES
-  /* TODO:
-   * see TCL:
-   * double ext_magn_field_energy(Particle *p1, Constraint_ext_magn_field *c)
-   * {
-   * #ifdef DIPOLES
-   *   return -1.0 * scalar(c->ext_magn_field,p1->r.dip);
-   * #endif
-   *   return 0;
-   * }
-   */
+    double nonbonded_en = -1.0 * utils::dot_product(&m_field.front(), p->r.dip);
+    *obsstat_dipolar(&energy, p->p.type) += nonbonded_en;
 #endif
-#endif    
 }
 
 }
--- src/core/statistics.hpp
+++ src/core/statistics.hpp
@@ -439,6 +439,11 @@ inline double *obsstat_nonbonded_inter(Observable_stat_non_bonded *stat, int p1,
   return stat->non_bonded_inter + stat->chunk_size_nb*(((2 * n_particle_types - 1 - p1) * p1) / 2  +  p2);
 }
 
+inline double *obsstat_dipolar(Observable_stat *stat, int j)
+{
+  return stat->dipolar + stat->chunk_size*j;
+}
+
 void invalidate_obs();
 
 /** Docs missing
