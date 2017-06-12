bd527eae7b4664bbac169c52e281030006a9f124
Florian Weik <fweik@icp.uni-stuttgart.de>
2017-06-12 13:19:25 +0200

    core: Store neighbors and VL in cells.

--- src/core/Cell.hpp
+++ src/core/Cell.hpp
@@ -1,15 +1,38 @@
 #ifndef CORE_CELL_HPP
 #define CORE_CELL_HPP
 
-#include "particle_data.hpp"
+#include <functional>
+#include <vector>
 
-/** A cell is a \ref ParticleList representing a particle group with
-    respect to the integration algorithm.
-*/
-// typedef ParticleList Cell;
+#include <boost/iterator/transform_iterator.hpp>
+
+#include "particle_data.hpp"
+#include "utils/Range.hpp"
 
 class Cell : public ParticleList {
+  struct GetReference {
+    Cell &operator()(std::reference_wrapper<Cell> &cell_ref) const {
+      return cell_ref.get();
+    }
+  };
+
+  using neighbors_t = std::vector<std::reference_wrapper<Cell>>;
+
 public:
+  using neighbor_iterator =
+      boost::transform_iterator<GetReference, neighbors_t::iterator>;
+
+  /** Topological neighbors of the cell */
+  std::vector<std::reference_wrapper<Cell>> m_neighbors;
+
+  /** Interaction pairs */
+  std::vector<std::pair<Particle *, Particle *>> m_verlet_list;
+
+  Utils::Range<neighbor_iterator> neighbors() {
+    return Utils::make_range(neighbor_iterator(m_neighbors.begin()),
+                             neighbor_iterator(m_neighbors.end()));
+  }
+
   void resize(size_t size) {
     realloc_particlelist(static_cast<ParticleList *>(this), this->n = size);
   }
