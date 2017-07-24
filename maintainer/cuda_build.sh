8e2c2558394fd149b259567bd8846fa6132e2bdc
Kai Szuttor <kai@icp.uni-stuttgart.de>
2017-07-24 13:58:29 +0200

    Update of CUDA build script.

--- maintainer/cuda_build.sh
+++ maintainer/cuda_build.sh
@@ -4,8 +4,16 @@ cd ${CI_PROJECT_DIR};
 cp maintainer/configs/maxset.hpp myconfig.hpp;
 mkdir build && cd build;
 cmake .. -DCUDA_NVCC_FLAGS="-D_FORCE_INLINES";
-make -j 4 && make check_python; cat testsuite/python/Testing/Temporary/LastTest.log
+make -j 4 && make check_python;
+result=$?;
+if (( result == 0 )); then
+    STATUS="success"
+else
+    STATUS="failure"
+    cat testsuite/python/Testing/Temporary/LastTest.log
+fi
 GIT_COMMIT=$(git rev-parse HEAD)
+URL=$(echo "https://gitlab.icp.uni-stuttgart.de/kai/espresso-build/pipelines/${CI_JOB_ID}"
 curl "https://api.github.com/repos/kaiszuttor/espresso/statuses/$GIT_COMMIT?access_token=$GITHUB_TOKEN" \
       -H "Content-Type: application/json" \
       -X POST \
