383152da237781978281a481f9264c6c3c8f554c
Kai Szuttor <kai@icp.uni-stuttgart.de>
2017-07-28 20:25:50 +0200

    mkuron's comments.

--- maintainer/cuda_build.sh
+++ maintainer/cuda_build.sh
@@ -1,19 +1,20 @@
+#!/usr/bin/env bash
+
 GIT_COMMIT=$(git rev-parse HEAD)
-URL=$(echo "https://gitlab.icp.uni-stuttgart.de/kai/espresso-build/-/jobs/${CI_JOB_ID}")
+URL=$(echo "https://gitlab.icp.uni-stuttgart.de/espressomd/espresso/-/jobs/${CI_JOB_ID}")
 STATUS="pending"
-curl "https://api.github.com/repos/kaiszuttor/espresso/statuses/$GIT_COMMIT?access_token=$GITHUB_TOKEN" \
+curl "https://api.github.com/repos/espressomd/espresso/statuses/$GIT_COMMIT?access_token=$GITHUB_TOKEN" \
       -H "Content-Type: application/json" \
       -X POST \
       -d "{\"state\": \"$STATUS\", \"context\": \"ICP CUDA build\", \"target_url\": \"$URL\"}"
 
-whoami;
-nvidia-smi;
-cd ${CI_PROJECT_DIR};
-cp maintainer/configs/maxset.hpp myconfig.hpp;
-mkdir build && cd build;
-cmake .. -DCUDA_NVCC_FLAGS="-D_FORCE_INLINES";
-make -j 4 && make check_python;
-result=$?;
+nvidia-smi
+cd ${CI_PROJECT_DIR}
+cp maintainer/configs/maxset.hpp myconfig.hpp
+mkdir build && cd build
+cmake .. -DCUDA_NVCC_FLAGS="-D_FORCE_INLINES"
+make -j 4 && make check_python
+result=$?
 echo $result
 if (( $result == 0 )); then
     STATUS="success"
