79309331eed8af6910123a655d322be151a50464
Marcello Sega <marcello.sega@gmail.com>
2017-07-28 11:48:07 +0200

    ported to MDAnalysis 0.16

--- samples/python/MDAnalysisIntegration.py
+++ samples/python/MDAnalysisIntegration.py
@@ -1,57 +1,107 @@
-## This example shows how to integrate the functionalities of MDAnalysis in ESPResSo
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+"""
+    This example shows how to integrate MDAnalysis in ESPResSo
+"""
 import espressomd
-from espressomd import MDA_ESP 
+from espressomd import MDA_ESP
 import numpy as np
 import MDAnalysis as mda
 
-### set up a minimal sample system 
+### set up a minimal sample system
 
 system = espressomd.System()
-system.time_step   		= 0.001
-system.cell_system.skin         = 0.1
+system.time_step           = 0.001
+system.cell_system.skin    = 0.1
+system.box_l               = [10,10,10]
 
-system.box_l 			= [10,10,10]
 for i in range(10):
-	system.part.add(id=i, pos=np.random.random(3) * system.box_l)
-for i in range(5):
-	system.part[i].q = 1.0
+    system.part.add(id=i,
+                    pos=np.random.random(3) * system.box_l,
+                    v=np.random.random(3)
+                    )
+for i in range(5,10):
+    system.part[i].q = 1.0
+    system.part[i].type =  1
 
-### prepare the stream
+#
+# ========================================================="
+# Example #1: prepare the stream and access various        "
+# quantities from MDAnalysis                               "
+# ========================================================="
+#
 
-eos = MDA_ESP.ESPStream()
+eos = MDA_ESP.Stream(system)
 
-### MDAnalysis Universe:  we must pass the additional 'espresso' keyword
-u =  mda.Universe( eos.stream(), espresso=eos.system() )
+u =  mda.Universe( eos.topology, eos.trajectory )
 
-### Write the config as a pdb file
-print "\n *** Example #1: write configuration on system.pdb"
+
+### let's have a look at the universe
+print u
+
+### Inspect atoms
+print u.atoms
+
+print "Positions:"
+print u.atoms.positions
+print "Velocities:"
+print u.atoms.velocities
+print "Forces:"
+print u.atoms.forces
+print "Names:"
+print u.atoms.names
+print "IDs:"
+print u.atoms.ids
+print "Types:"
+print u.atoms.types
+print "Charges:"
+print u.atoms.charges
+
+#
+# ========================================================="
+# Example #2: Write the configuration on a PDB file        "
+# ========================================================="
+#
+
+
 u.atoms.write("system.pdb")
+print "===> The initial configuration has been written on system.pdb "
 
-### Example of a selection + analysis done in MDAnalysis
-### RDF of all charged particles
+
+
+#
+# ========================================================="
+# Example #3: Calculate a radial distribution function     "
+# ========================================================="
+#
 from MDAnalysis.analysis.rdf import  InterRDF
 
 charged = u.select_atoms("prop charge  > 0")
-
 rdf = InterRDF(charged, charged,nbins=7,range=(0,10))
 
+# This runs so far only over the single frame we have loaded.
+# Multiframe averaging must be done by hand
 rdf.run()
-print "\n *** Example #2: rdf of charged particles:", rdf.rdf 
 
+#
+# ========================================================="
+# Example #4: Saving frames to a GROMACS's TRR trajectory
+# ========================================================="
+#
 
-### Let's add more frames...
-### Want to use your favourite gromacs analysis tool? No prob, let's save a .trr
-print "\n *** Example #3: write gromacs binary trajectory traj.trr"
 from MDAnalysis.coordinates.TRR import TRRWriter
+
 W = TRRWriter("traj.trr",n_atoms=len(system.part))
-for i in range(10):
-	# integrate (nothing, in this example)
-	system.integrator.run(1)
-	# replace last frame
-	u.load_new( eos.stream(), espresso=eos.system())
-	# append it to the .trr trajectory
-	W.write_next_timestep(u.trajectory.ts)
- 
 
-print ""
+for i in range(100):
+    # integrate
+    system.integrator.run(1)
+    # replace last frame
+    # TODO loading new frames will be automated in future versions
+    u.load_new(eos.trajectory)
+    # append it to the .trr trajectory
+    W.write_next_timestep(u.trajectory.ts)
 
+print "===> The trajectory has been saved in the traj.trr file"
+
+#
