982d63872e479a2336bdefd91692e37dd21483aa
Kai Szuttor <kai@icp.uni-stuttgart.de>
2017-08-08 10:46:28 +0200

    Cleanup.

--- src/python/espressomd/system.pyx
+++ src/python/espressomd/system.pyx
@@ -106,41 +106,6 @@ cdef class System(object):
         else:
             raise RuntimeError("You can only have one instance of the system class at a time.")
 
-#    def __setattr__(self, name, value):
-#        if hasattr(self, name):
-#            self.name = value
-#            #__setattr__(name, value)
-#        else:
-#            raise AttributeError(
-#                "System does not have the attribute " + name + "."
-#                + "\nIf you know what you're doing, use "
-#                + "system.create_attr()"
-#            )
-#
-#    def create_attr(self, *args, **kwargs):
-#        """
-#            Circumvents the ``__setattr__`` lock. Allows to create and set new
-#            attributes to System instances.
-#            For ``*args``, it initializes an attribute with None value, if the
-#            attribute does not already exist.
-#            For ``**kwargs``, it simply calls ``super().__setattr__``.
-#        """
-#        for arg in args:
-#            try: name = str(arg)
-#            except ValueError:
-#                print("Please pass either **kwargs or string *args to"
-#                      + "create_attr()"
-#                      )
-#                continue
-#
-#            if hasattr(self, name):
-#                print("Attribute " + name + " already exists.")
-#            else:
-#                __setattr__(name, None)
-#
-#        for name, value in list(kwargs.items()):
-#            __setattr__(name, value)
-
     # __getstate__ and __setstate__ define the pickle interaction
     def __getstate__(self):
         odict = {}
