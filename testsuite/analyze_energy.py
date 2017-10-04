8aa71b7676e739b8055fd1b1fbb0e52e9a7aa2ae
David Sean <david.sean@gmail.com>
2017-10-04 13:18:49 +0200

    more than one bonded and non-bonded interaction

--- testsuite/analyze_energy.py
+++ testsuite/analyze_energy.py
@@ -18,25 +18,35 @@ class test_analyze_energy(ut.TestCase):
     system.non_bonded_inter[0, 0].lennard_jones.set_params(
         epsilon=1.0, sigma=1.0,
         cutoff=2**(1./6.), shift="auto")
+    system.non_bonded_inter[1, 1].lennard_jones.set_params(
+        epsilon=1.0, sigma=1.0,
+        cutoff=2**(1./6.), shift="auto")
+
     system.part.add(id = 0, pos = [1, 2, 2], type=0)
     system.part.add(id = 1, pos = [5, 2, 2], type=0)
     system.thermostat.set_langevin(kT = 0., gamma = 1.)
+    harmonic = HarmonicBond(r_0 = 0.0, k = 3)
+    system.bonded_inter.add(harmonic)
         
     def test_kinetic(self):
         self.system.part[0].pos = [1, 2, 2]
         self.system.part[1].pos = [5, 2, 2]
         self.system.part[0].v = [3, 4, 5]
+        self.system.part[1].v = [0, 0, 0]
+        # initial, single moving particle
+        energy = self.system.analysis.energy()
+        self.assertTrue( np.allclose(energy["total"], 25.))
+        self.assertTrue( np.allclose(energy["kinetic"], 25.))
+        self.assertTrue( np.allclose(energy["bonded"], 0.))
+        self.assertTrue( np.allclose(energy["non_bonded"], 0.))
+        # initial, two moving particles
         self.system.part[1].v = [3, 4, 5]
-
-        # initial 
         energy = self.system.analysis.energy()
         self.assertTrue( np.allclose(energy["total"], 50.))
         self.assertTrue( np.allclose(energy["kinetic"], 50.))
         self.assertTrue( np.allclose(energy["bonded"], 0.))
         self.assertTrue( np.allclose(energy["non_bonded"], 0.))
-
-
-        # rest        
+        # rest
         self.system.integrator.run(10000)
         energy = self.system.analysis.energy()
         self.assertTrue( np.allclose(energy["total"], 0.))
@@ -46,67 +56,89 @@ class test_analyze_energy(ut.TestCase):
 
     def test_non_bonded(self):
         self.system.part[0].pos = [1, 2, 2]
-        self.system.part[1].pos = [1+2**(1./6.), 2, 2]
-        self.system.part[0].v = [0, 0, 0]
-        self.system.part[1].v = [0, 0, 0]
-
-        energy = self.system.analysis.energy()
-        self.assertTrue( np.allclose(energy["total"], 0.))
-        self.assertTrue( np.allclose(energy["kinetic"], 0.))
-        self.assertTrue( np.allclose(energy["bonded"], 0.))
-        self.assertTrue( np.allclose(energy["non_bonded"], 0.))
-
         self.system.part[1].pos = [2, 2, 2]
         energy = self.system.analysis.energy()
         self.assertTrue( np.allclose(energy["total"], 1.))
         self.assertTrue( np.allclose(energy["kinetic"], 0.))
         self.assertTrue( np.allclose(energy["bonded"], 0.))
         self.assertTrue( np.allclose(energy["non_bonded"], 1.))
+        # add another pair of particles
+        self.system.part.add(id = 3, pos = [1, 5, 5], type=1)
+        self.system.part.add(id = 4, pos = [2, 5, 5], type=1)
+        energy = self.system.analysis.energy()
+        self.assertTrue( np.allclose(energy["total"], 2.))
+        self.assertTrue( np.allclose(energy["kinetic"], 0.))
+        self.assertTrue( np.allclose(energy["bonded"], 0.))
+        self.assertTrue( np.allclose(energy["non_bonded"], 2.))
+        self.system.part[3].remove()
+        self.system.part[4].remove()
+
 
     def test_bonded(self):
         self.system.part[0].pos = [1, 2, 2]
         self.system.part[1].pos = [3, 2, 2]
         self.system.part[0].v = [0, 0, 0]
-        self.system.part[1].v = [0, 0, 0]
-
-        harmonic = HarmonicBond(r_0 = 0.0, k = 3)
-        self.system.bonded_inter.add(harmonic)
-        self.system.part[0].add_bond((harmonic, 1))
-        
+        self.system.part[1].v = [0, 0, 0]       
+        # single bond
+        self.system.part[0].add_bond((self.harmonic, 1))
         energy = self.system.analysis.energy()
         self.assertTrue( np.allclose(energy["total"], 6))
         self.assertTrue( np.allclose(energy["kinetic"], 0.))
         self.assertTrue( np.allclose(energy["bonded"], 6))
         self.assertTrue( np.allclose(energy["non_bonded"], 0.))
-        self.system.part[0].delete_bond((harmonic, 1))
-        
+        # two bonds
+        self.system.part[0].add_bond((self.harmonic, 1))
+        energy = self.system.analysis.energy()
+        self.assertTrue( np.allclose(energy["total"], 12))
+        self.assertTrue( np.allclose(energy["kinetic"], 0.))
+        self.assertTrue( np.allclose(energy["bonded"], 12))
+        self.assertTrue( np.allclose(energy["non_bonded"], 0.))
+        # bonds deleted
+        self.system.part[0].delete_all_bonds()
         energy = self.system.analysis.energy()
         self.assertTrue( np.allclose(energy["total"], 0.))
         self.assertTrue( np.allclose(energy["kinetic"], 0.))
         self.assertTrue( np.allclose(energy["bonded"], 0))
         self.assertTrue( np.allclose(energy["non_bonded"], 0.))
-        
+
+
     def test_all(self):
         self.system.part[0].pos = [1, 2, 2]
         self.system.part[1].pos = [2, 2, 2]
         self.system.part[0].v = [3, 4, 5]
         self.system.part[1].v = [3, 4, 5]
-        harmonic = HarmonicBond(r_0 = 0.0, k = 3)
-        self.system.bonded_inter.add(harmonic)
-        self.system.part[0].add_bond((harmonic, 1))
-
+        #single bond
+        self.system.part[0].add_bond((self.harmonic, 1))
         energy = self.system.analysis.energy()
         self.assertTrue( np.allclose(energy["total"], 50. + 3./2. + 1.))
         self.assertTrue( np.allclose(energy["kinetic"], 50.))
         self.assertTrue( np.allclose(energy["bonded"], 3./2.))
         self.assertTrue( np.allclose(energy["non_bonded"], 1.))
-        self.system.part[0].delete_bond((harmonic, 1))
-        
-
-
+        #two bonds
+        self.system.part[0].add_bond((self.harmonic, 1))
+        energy = self.system.analysis.energy()
+        self.assertTrue( np.allclose(energy["total"], 50. + 3 + 1.))
+        self.assertTrue( np.allclose(energy["kinetic"], 50.))
+        self.assertTrue( np.allclose(energy["bonded"], 3.))
+        self.assertTrue( np.allclose(energy["non_bonded"], 1.))
+        # add another pair of particles
+        self.system.part.add(id = 3, pos = [1, 5, 5], type=1)
+        self.system.part.add(id = 4, pos = [2, 5, 5], type=1)
+        energy = self.system.analysis.energy()
+        self.assertTrue( np.allclose(energy["total"], 50. + 3 + (1. + 1.)))
+        self.assertTrue( np.allclose(energy["kinetic"], 50.))
+        self.assertTrue( np.allclose(energy["bonded"], 3.))
+        self.assertTrue( np.allclose(energy["non_bonded"], 1. + 1.))
+        self.system.part[3].remove()
+        self.system.part[4].remove()
+        self.system.part[0].delete_all_bonds()
 
 
 
 if __name__ == "__main__":
     print("Features: ", espressomd.features())
-    ut.main()
+    
+    suite = ut.TestLoader().loadTestsFromTestCase(test_analyze_energy)
+    ut.TextTestRunner(verbosity=2).run(suite)
+
+    #ut.main()
