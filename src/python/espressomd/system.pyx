44ea047a1c13820b24631b6070f9b7e9258970eb
Kai Szuttor <kai@icp.uni-stuttgart.de>
2017-10-06 11:15:16 +0200

    Switching from deprecated property syntax to decorator based.

--- src/python/espressomd/system.pyx
+++ src/python/espressomd/system.pyx
@@ -57,7 +57,7 @@ IF LEES_EDWARDS == 1:
 cdef bool _system_created = False
 
 cdef class System(object):
-    """ The base class for espressomd.system.System().
+    """ The central class for the simulation system.
 
     .. note:: every attribute has to be declared at the class level.
               This means that methods cannot define an attribute by using
@@ -122,148 +122,157 @@ cdef class System(object):
         for property_ in params.keys():
             System.__setattr__(self, property_, params[property_])
 
-    property box_l:
+    @property
+    def box_l(self):
         """
         Array like, list of three floats
         """
+        return np.array([box_l[0], box_l[1], box_l[2]])
 
-        def __set__(self, _box_l):
-            if len(_box_l) != 3:
-                raise ValueError("Box length must be of length 3")
-            for i in range(3):
-                if _box_l[i] <= 0:
-                    raise ValueError(
-                        "Box length must be > 0 in all directions")
-                box_l[i] = _box_l[i]
+    @box_l.setter
+    def box_l(self, _box_l):
+        if len(_box_l) != 3:
+            raise ValueError("Box length must be of length 3")
+        for i in range(3):
+            if _box_l[i] <= 0:
+                raise ValueError(
+                    "Box length must be > 0 in all directions")
+            box_l[i] = _box_l[i]
+        mpi_bcast_parameter(FIELD_BOXL)
 
-            mpi_bcast_parameter(FIELD_BOXL)
+    @property
+    def integ_switch(self):
+        return integ_switch
 
-        def __get__(self):
-            return np.array([box_l[0], box_l[1], box_l[2]])
-
-    property integ_switch:
-        def __get__(self):
-            return integ_switch
-
-    property periodicity:
+    @property
+    def periodicity(self):
         """
         list of three integers
         [x, y, z]
         zero for no periodicity in this direction
         one for periodicity
         """
+        periodicity = np.zeros(3)
+        periodicity[0] = periodic % 2
+        periodicity[1] = int(periodic / 2) % 2
+        periodicity[2] = int(periodic / 4) % 2
+        return periodicity
 
-        def __set__(self, _periodic):
-            global periodic
-            if len(_periodic) != 3:
-                raise ValueError(
-                    "periodicity must be of length 3, got length " + str(len(_periodic)))
-            periodicity = np.zeros(3)
-            for i in range(3):
-                if _periodic[i] != 1:
-                    IF PARTIAL_PERIODIC:
-                        pass
-                    ELSE:
-                        raise ValueError(
-                            "The feature PARTIAL_PERIODIC needs to be activated in myconfig.hpp")
-            for i in range(3):
-                periodicity[i] = _periodic[i]
-            periodic = 4 * _periodic[2] + 2 * _periodic[1] + _periodic[0]
-            # first 3 bits of periodic determine the periodicity
-            mpi_bcast_parameter(FIELD_PERIODIC)
+    @periodicity.setter
+    def periodicity(self, _periodic):
+        global periodic
+        if len(_periodic) != 3:
+            raise ValueError(
+                "periodicity must be of length 3, got length " + str(len(_periodic)))
+        periodicity = np.zeros(3)
+        for i in range(3):
+            if _periodic[i] != 1:
+                IF PARTIAL_PERIODIC:
+                    pass
+                ELSE:
+                    raise ValueError(
+                        "The feature PARTIAL_PERIODIC needs to be activated in myconfig.hpp")
+        for i in range(3):
+            periodicity[i] = _periodic[i]
+        periodic = 4 * _periodic[2] + 2 * _periodic[1] + _periodic[0]
+        # first 3 bits of periodic determine the periodicity
+        mpi_bcast_parameter(FIELD_PERIODIC)
 
-        def __get__(self):
-            periodicity = np.zeros(3)
-            periodicity[0] = periodic % 2
-            periodicity[1] = int(periodic / 2) % 2
-            periodicity[2] = int(periodic / 4) % 2
-            return periodicity
-
-    property time:
+    @property
+    def time(self):
         """
         Set the time in the simulation 
         """
-        def __set__(self, double _time):
-            if _time < 0:
-                raise ValueError("Simulation time must be >= 0")
-            global sim_time
-            sim_time = _time
-            mpi_bcast_parameter(FIELD_SIMTIME)
+        global sim_time
+        return sim_time
 
-        def __get__(self):
-            global sim_time
-            return sim_time
+    @time.setter
+    def time(self, double _time):
+        if _time < 0:
+            raise ValueError("Simulation time must be >= 0")
+        global sim_time
+        sim_time = _time
+        mpi_bcast_parameter(FIELD_SIMTIME)
 
-    property smaller_time_step:
+    @property
+    def smaller_time_step(self):
         """
-        Setting this property to a positive integer value turns on the multi-timestepping algorithm. The ratio :attr:`espressomd.system.System.time_step`/:attr:`espressomd.system.System.smaller_time_step` must be an integer.
+        Setting this property to a positive integer value turns on the
+        multi-timestepping algorithm. The ratio
+        :attr:`espressomd.system.System.time_step`/:attr:`espressomd.system.System.smaller_time_step`
+        must be an integer.
         """
-        def __set__(self, double _smaller_time_step):
-            IF MULTI_TIMESTEP:
-                global smaller_time_step
-                if _smaller_time_step <= 0:
-                    raise ValueError("Smaller time step must be positive")
-                mpi_set_smaller_time_step(_smaller_time_step)
+        return smaller_time_step
 
-        def __get__(self):
-            return smaller_time_step
+    @smaller_time_step.setter
+    def smaller_time_step(self, double _smaller_time_step):
+        IF MULTI_TIMESTEP:
+            global smaller_time_step
+            if _smaller_time_step <= 0:
+                raise ValueError("Smaller time step must be positive")
+            mpi_set_smaller_time_step(_smaller_time_step)
 
-    property time_step:
+    @property
+    def time_step(self):
         """
         Sets the time step for the integrator. 
         """
-        def __set__(self, double _time_step):
-            IF LB:
-                global lbpar
-            IF LB_GPU:
-                global lbpar_gpu
-            if _time_step <= 0:
-                raise ValueError("Time Step must be positive")
-            IF LB:
-                if lbpar.tau >= 0.0 and _time_step < lbpar.tau:
-                    raise ValueError(
-                        "Time Step (" + str(time_step) + ") must be > LB_time_step (" + str(lbpar.tau) + ")")
-            IF LB_GPU:
-                if (lbpar_gpu.tau >= 0.0 and
-                        lbpar_gpu.tau - _time_step > numeric_limits[float].epsilon() * abs(lbpar_gpu.tau + _time_step)):
-                    raise ValueError(
-                        "Time Step (" + str(time_step) + ") must be > LB_time_step (" + str(lbpar_gpu.tau) + ")")
-            mpi_set_time_step(_time_step)
+        return time_step
 
-        def __get__(self):
-            return time_step
+    @time_step.setter
+    def time_step(self, double _time_step):
+        IF LB:
+            global lbpar
+        IF LB_GPU:
+            global lbpar_gpu
+        if _time_step <= 0:
+            raise ValueError("Time Step must be positive")
+        IF LB:
+            if lbpar.tau >= 0.0 and _time_step < lbpar.tau:
+                raise ValueError(
+                    "Time Step (" + str(time_step) + ") must be > LB_time_step (" + str(lbpar.tau) + ")")
+        IF LB_GPU:
+            if (lbpar_gpu.tau >= 0.0 and
+                    lbpar_gpu.tau - _time_step > numeric_limits[float].epsilon() * abs(lbpar_gpu.tau + _time_step)):
+                raise ValueError(
+                    "Time Step (" + str(time_step) + ") must be > LB_time_step (" + str(lbpar_gpu.tau) + ")")
+        mpi_set_time_step(_time_step)
 
-    property timings:
-        def __set__(self, int _timings):
-            global timing_samples
-            if _timings <= 0:
-                timing_samples = 0
-            else:
-                timing_samples = _timings
 
-        def __get__(self):
-            return timing_samples
+    @property
+    def timings(self):
+        return timing_samples
 
-    property max_cut_nonbonded:
-        def __get__(self):
-            return max_cut_nonbonded
+    @timings.setter
+    def timings(self, int _timings):
+        global timing_samples
+        if _timings <= 0:
+            timing_samples = 0
+        else:
+            timing_samples = _timings
 
-    property lattice_switch:
-        def __get__(self):
-            return lattice_switch
+    @property
+    def max_cut_nonbonded(self):
+        return max_cut_nonbonded
 
-    property max_cut_bonded:
-        def __get__(self):
-            return max_cut_bonded
+    @property
+    def lattice_switch(self):
+        return lattice_switch
 
-    property min_global_cut:
-        def __set__(self, _min_global_cut):
-            global min_global_cut
-            min_global_cut = _min_global_cut
-            mpi_bcast_parameter(FIELD_MIN_GLOBAL_CUT)
+    @property
+    def max_cut_bonded(self):
+        return max_cut_bonded
 
-        def __get__(self):
-            return min_global_cut
+    @property
+    def min_global_cut(self):
+        return min_global_cut
+
+    @min_global_cut.setter
+    def min_global_cut(self, _min_global_cut):
+        global min_global_cut
+        min_global_cut = _min_global_cut
+        mpi_bcast_parameter(FIELD_MIN_GLOBAL_CUT)
+
 
     def _get_PRNG_state_size(self):
         """
@@ -288,69 +297,74 @@ cdef class System(object):
             states[i] = " ".join(map(str, states_on_node_i))
         mpi_random_set_stat(states)
 
-    property seed:
+    @property
+    def seed(self):
+        return self.__seed
+
+    @seed.setter
+    def seed(self, _seed):
         """
         Sets the seed of the pseudo random number with a list of seeds which is as long as the number of used nodes.
         """
-        
-        def __set__(self, _seed):
-            cdef vector[int] seed_array
-            self.__seed = _seed
-            if(isinstance(_seed, int) and n_nodes == 1):
-                seed_array.resize(1)
-                seed_array[0] = int(_seed)
-                mpi_random_seed(0, seed_array)
-            elif(hasattr(_seed, "__iter__")):
-                if(len(_seed) < n_nodes or len(_seed) > n_nodes):
-                    raise ValueError(
-                        "The list needs to contain one seed value per node")
-                seed_array.resize(len(_seed))
-                for i in range(len(_seed)):
-                    seed_array[i] = int(_seed[i])
-                mpi_random_seed(n_nodes, seed_array)
-            else:
+        cdef vector[int] seed_array
+        self.__seed = _seed
+        if(isinstance(_seed, int) and n_nodes == 1):
+            seed_array.resize(1)
+            seed_array[0] = int(_seed)
+            mpi_random_seed(0, seed_array)
+        elif(hasattr(_seed, "__iter__")):
+            if(len(_seed) < n_nodes or len(_seed) > n_nodes):
                 raise ValueError(
-                    "The seed has to be an integer or a list of integers with one integer per node")
+                    "The list needs to contain one seed value per node")
+            seed_array.resize(len(_seed))
+            for i in range(len(_seed)):
+                seed_array[i] = int(_seed[i])
+            mpi_random_seed(n_nodes, seed_array)
+        else:
+            raise ValueError(
+                "The seed has to be an integer or a list of integers with one integer per node")
 
-        def __get__(self):
-            return self.__seed
-
-    property random_number_generator_state:
-        """Sets the random number generator state in the core. this is of interest for deterministic checkpointing
+    @property
+    def random_number_generator_state(self):
         """
-        
-        def __set__(self, rng_state):
-            _state_size_plus_one = self._get_PRNG_state_size() + 1
-            if(len(rng_state) == n_nodes * _state_size_plus_one):
-                states = string_vec(n_nodes)
-                for i in range(n_nodes):
-                    states[i] = " ".join(
-                        map(str, rng_state[i * _state_size_plus_one:(i + 1) * _state_size_plus_one]))
-                mpi_random_set_stat(states)
-            else:
-                raise ValueError("Wrong # of args: Usage: 'random_number_generator_state \"<state(1)> ... <state(n_nodes*(state_size+1))>, where each <state(i)> is an integer. The state size of the PRNG can be obtained by calling _get_PRNG_state_size().")
+        Sets the random number generator state in the core. this is of
+        interest for deterministic checkpointing.
 
-        def __get__(self):
-            rng_state = map(int, (mpi_random_get_stat().c_str()).split())
-            return rng_state
+        """
+        rng_state = map(int, (mpi_random_get_stat().c_str()).split())
+        return rng_state
+
+    @random_number_generator_state.setter
+    def random_number_generator_state(self, rng_state):
+        _state_size_plus_one = self._get_PRNG_state_size() + 1
+        if(len(rng_state) == n_nodes * _state_size_plus_one):
+            states = string_vec(n_nodes)
+            for i in range(n_nodes):
+                states[i] = " ".join(
+                    map(str, rng_state[i * _state_size_plus_one:(i + 1) * _state_size_plus_one]))
+            mpi_random_set_stat(states)
+        else:
+            raise ValueError("Wrong # of args: Usage: 'random_number_generator_state \"<state(1)> ... <state(n_nodes*(state_size+1))>, where each <state(i)> is an integer. The state size of the PRNG can be obtained by calling _get_PRNG_state_size().")
 
     IF LEES_EDWARDS == 1:
-        property lees_edwards_offset:
-        # defines the lees edwards offset
-            def __set__(self, double _lees_edwards_offset):
+        @property
+        def lees_edwards_offset(self):
+            """Defines the lees edwards offset.
 
-                if isinstance(_lees_edwards_offset, float):
-                    global lees_edwards_offset
-                    lees_edwards_offset = _lees_edwards_offset
-                    #new_offset = _lees_edwards_offset
-                    mpi_bcast_parameter(FIELD_LEES_EDWARDS_OFFSET)
+            """
+            return lees_edwards_offset
 
-                else:
-                    raise ValueError("Wrong # of args! Usage: lees_edwards_offset { new_offset }")
+        @lees_edwards_offset.setter
+        def lees_edwards_offset(self, double _lees_edwards_offset):
 
-            def __get__(self):
-        # global lees_edwards_offset
-                return lees_edwards_offset
+            if isinstance(_lees_edwards_offset, float):
+                global lees_edwards_offset
+                lees_edwards_offset = _lees_edwards_offset
+                #new_offset = _lees_edwards_offset
+                mpi_bcast_parameter(FIELD_LEES_EDWARDS_OFFSET)
+
+            else:
+                raise ValueError("Wrong # of args! Usage: lees_edwards_offset { new_offset }")
 
     def change_volume_and_rescale_particles(self, d_new, dir="xyz"):
         """Change box size and rescale particle coordinates.
@@ -377,7 +391,8 @@ cdef class System(object):
         else:
             raise ValueError(
                 'Usage: change_volume_and_rescale_particles(<L_new>, [{ "x" | "y" | "z" | "xyz" }])')
-
+    
+    @property
     def volume(self):
         """
         Return box volume of the cuboid box
