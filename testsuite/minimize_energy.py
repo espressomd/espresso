fbc62f463b805cfa31ac495dc820ad398cbda0a8
Florian Weik <fweik@icp.uni-stuttgart.de>
2017-10-06 11:21:15 +0200

    testsuite: Don't equality-compare floats in minimize_energy

--- testsuite/minimize_energy.py
+++ testsuite/minimize_energy.py
@@ -37,7 +37,7 @@ class test_minimize_energy(ut.TestCase):
 
         energy = self.system.analysis.energy()
 
-        self.assertEqual(energy["total"], 0)
+        self.assertAlmostEqual(energy["total"], 0, places=10)
 
 if __name__ == "__main__":
     print("Features: ", espressomd.features())
