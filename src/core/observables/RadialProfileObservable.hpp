04ef564bd4ef7f4250182782f480a14dcd20917d
Kai Szuttor <kai@icp.uni-stuttgart.de>
2017-11-03 16:32:47 +0100

    observables: implemented base class for radial profile observables.

---
+++ src/core/observables/RadialProfileObservable.hpp
@@ -0,0 +1,35 @@
+#ifndef OBSERVABLES_RADIALPROFILEOBSERVABLE_HPP
+#define OBSERVABLES_RADIALPROFILEOBSERVABLE_HPP
+
+#include <cmath>
+
+#include "PidObservable.hpp"
+#include "particle_data.hpp"
+#include "Vector.hpp"
+#include "integrate.hpp"
+
+namespace Observables {
+
+class RadialProfileObservable : public PidObservable {
+public:
+  ::Vector<3, double> center;
+  double min_r, max_r;
+  double min_phi, max_phi;
+  double min_z, max_z;
+  // Number of bins for each coordinate.
+  int n_r_bins, n_phi_bins, n_z_bins;
+  double r_bin_size = (max_r - min_r) / n_r_bins;
+  double phi_bin_size = (max_phi - min_phi) / n_phi_bins;
+  double z_bin_size = (max_z - min_z) / n_z_bins;
+  virtual int n_values() const override { return n_r_bins * n_phi_bins * n_z_bins; };
+  ::Vector<3, double> transform_to_cylinder_coordinates(::Vector<3, double> const &pos, ::Vector<3, double> const &center) const {
+    double x = pos[0] - center[0];
+    double y = pos[1] - center[1];
+    double r = std::sqrt(x * x + y * y);
+    double phi = std::atan2(y, r);
+    return ::Vector<3, double> {r, phi, pos[2]};
+  }
+};
+
+} // Namespace Observables
+#endif
