0a74c788ef9760cd442979a6b3215423cc9139d3
Kai Szuttor <kai@icp.uni-stuttgart.de>
2017-11-04 18:04:16 +0100

    observables: remove center argument of coordinate transformation (shift has to be performed by the obs implementation) in radial profile observable.

--- src/core/observables/RadialProfileObservable.hpp
+++ src/core/observables/RadialProfileObservable.hpp
@@ -18,15 +18,19 @@ public:
   double min_z, max_z;
   // Number of bins for each coordinate.
   int n_r_bins, n_phi_bins, n_z_bins;
-  double r_bin_size = (max_r - min_r) / n_r_bins;
-  double phi_bin_size = (max_phi - min_phi) / n_phi_bins;
-  double z_bin_size = (max_z - min_z) / n_z_bins;
+  double r_bin_size() {
+      return (max_r - min_r) / n_r_bins;
+  }
+  double phi_bin_size() {
+      return (max_phi - min_phi) / n_phi_bins;
+  }
+  double z_bin_size() {
+      return (max_z - min_z) / n_z_bins;
+  }
   virtual int n_values() const override { return n_r_bins * n_phi_bins * n_z_bins; };
-  ::Vector<3, double> transform_to_cylinder_coordinates(::Vector<3, double> const &pos, ::Vector<3, double> const &center) const {
-    double x = pos[0] - center[0];
-    double y = pos[1] - center[1];
-    double r = std::sqrt(x * x + y * y);
-    double phi = std::atan2(y, r);
+  ::Vector<3, double> transform_to_cylinder_coordinates(::Vector<3, double> const &pos) const {
+    double r = std::sqrt(pos[0] * pos[0] + pos[1] * pos[1]);
+    double phi = std::atan2(pos[1], pos[0]);
     return ::Vector<3, double> {r, phi, pos[2]};
   }
 };
