589d96d7d2394961fefa4b16954ca205892a736b
Kai Szuttor <kai@icp.uni-stuttgart.de>
2017-11-09 15:42:04 +0100

    testsuite: improved observable test.

--- testsuite/observable_cylindricalfluxdensity.py
+++ testsuite/observable_cylindricalfluxdensity.py
@@ -19,8 +19,8 @@ class TestCylindricalFluxDensityObservable(ut.TestCase):
     params = {
         'center': [5.0, 5.0, 0.0],  # center of the histogram
         'n_r_bins': 10,  # number of bins in r
-        'n_phi_bins': 1,  # -*- in phi
-        'n_z_bins': 1,  # -*- in z
+        'n_phi_bins': 10,  # -*- in phi
+        'n_z_bins': 10,  # -*- in z
         'min_r': 0.0,
         'min_phi': -np.pi,
         'min_z': 0.0,
@@ -34,16 +34,70 @@ class TestCylindricalFluxDensityObservable(ut.TestCase):
         return utils.get_unravelled_index(
             [n_r_bins, n_phi_bins, n_z_bins, 3], 4, ind)
 
-    def test_r(self):
-        """Test binning in r.
+    def pol_coords(self):
+        positions = np.zeros((self.params['N'], 3))
+        velocities = np.zeros((self.params['N'], 3))
+        for i, p in enumerate(self.system.part):
+            tmp = p.pos - np.array(self.params['center'])
+            positions[i, 0] = np.sqrt(tmp[0]**2.0 + tmp[1]**2.0)
+            positions[i, 1] = np.arctan2(tmp[1], tmp[0])
+            positions[i, 2] = tmp[2]
+            # v_r
+            velocities[i, 0] = (tmp[0] * p.v[0] + tmp[1] *
+                                p.v[1]) / np.sqrt(tmp[0]**2.0 + tmp[1]**2.0)
+            # v_phi
+            velocities[i, 1] = (tmp[0] * p.v[1] - tmp[1] *
+                                p.v[0]) / (tmp[0]**2.0 + tmp[1]**2.0)
+            # v_z
+            velocities[i, 2] = p.v[2]
+        return positions, velocities
 
-        """
+    def test_hist(self):
+        # Calculate the histogram normalization.
+        bin_volume = np.zeros(self.params['n_r_bins'])
+        r_bin_size = (
+            self.params['max_r'] - self.params['min_r']) / self.params['n_r_bins']
+        phi_bin_size = (
+            self.params['max_phi'] - self.params['min_phi']) / self.params['n_phi_bins']
+        z_bin_size = (
+            self.params['max_z'] - self.params['min_z']) / self.params['n_z_bins']
+        for i in range(self.params['n_r_bins']):
+            bin_volume[i] = np.pi * ((self.params['min_r'] + r_bin_size * (i + 1))**2.0 -
+                                     (self.params['min_r'] + r_bin_size * i)**2.0) * \
+                phi_bin_size / (2.0 * np.pi) * z_bin_size
         self.system.part.clear()
+        # Parameters for an ellipse.
+        a = 1.0  # semi minor-axis length
+        b = 2.0  # semi major-axis length
+        # Choose the cartesian velocities such that each particle gets the same
+        # v_r, v_phi and v_z, respectively.
+        v_r = .75
+        v_phi = 2.5
+        v_z = 1.5
         for i in range(self.params['N']):
-            self.system.part.add(id=i, pos=np.array(
-                self.params['center']) + np.array([i * 5.0 / self.params['N'], 0, 0]), v=[0.0, 0.0, 3.0])
+            position = np.array([a *
+                                 np.cos(i *
+                                        2.0 *
+                                        np.pi /
+                                        (self.params['N'] +
+                                         1)), b *
+                                 np.sin(i *
+                                        2.0 *
+                                        np.pi /
+                                        (self.params['N'] +
+                                         1)), i *
+                                 10.0 /
+                                 (self.params['N'] +
+                                     1)])
+            v_y = (position[0] * np.sqrt(position[0]**2.0 + position[1]**2.0) * \
+                   v_phi + position[1] * v_r) / np.sqrt(position[0]**2.0 + position[1]**2.0)
+            v_x = (v_r * np.sqrt(position[0]**2.0 + position[1] **
+                                 2.0) - position[1] * v_y) / position[0]
+            position += np.array(self.params['center'])
+            self.system.part.add(id=i, pos=position, v=[v_x, v_y, v_z])
+        pol_positions, pol_velocities = self.pol_coords()
         # Set up the Observable.
-        self.p = espressomd.observables.CylindricalFluxDensityProfile(
+        p = espressomd.observables.CylindricalFluxDensityProfile(
             ids=range(
                 self.params['N']),
             center=self.params['center'],
@@ -56,110 +110,24 @@ class TestCylindricalFluxDensityObservable(ut.TestCase):
             max_r=self.params['max_r'],
             max_phi=self.params['max_phi'],
             max_z=self.params['max_z'])
-
-        self.r_bin_size = (
-            self.params['max_r'] - self.params['min_r']) / self.params['n_r_bins']
-        self.hist = np.array(self.p.calculate())
-        self.res = []
-        for ind, v_dens in enumerate(self.hist):
-            # Get the unflattened index.
-            _ind = self.get_ind(self.params['n_r_bins'], self.params['n_phi_bins'], self.params['n_z_bins'], ind)
-            # If we have the 'z' component add the velocity density to the
-            # result.
-            if _ind[3] == 2:
-                # Append the mid of the bin position and the z-component of the
-                # velocity density.
-                self.res.append([(_ind[0] + 0.5) * self.r_bin_size, v_dens])
-        self.res = np.array(self.res)
-        # The Riemann sum should yield the sum of all z-velocities
-        riemann_sum = np.sum(self.res[:, 0] * self.res[:, 1]) * 2.0 * np.pi * \
-            (self.params['max_z'] - self.params['min_z']) * self.r_bin_size
-        self.assertAlmostEqual(riemann_sum, self.params['N'] * 3.0, places=4)
-        # Despite the Jacobian (r) factor all values should be the same by construction.
-        tmp = self.res[:, 0] * self.res[:, 1]
-        self.assertTrue(all(x - tmp[0] < 1E-8 for x in tmp))
-
-    def test_phi(self):
-        """Test binning in phi.
-
-        """
-        self.system.part.clear()
-        # Place particles on a circle around the center.
-        angles = np.linspace(0.0, 2.0*np.pi, num=self.params['N'], endpoint=False)
-        for i in range(self.params['N']):
-            pos = np.array(self.params['center'])
-            pos += np.array([np.cos(angles[i]),
-                             np.sin(angles[i]), 0.0])
-            self.system.part.add(id=i, pos=pos, v=[0.0, 0.0, 3.0])
-        # Set up the Observable.
-        p = espressomd.observables.CylindricalFluxDensityProfile(
-            ids=range(
-                self.params['N']),
-            center=self.params['center'],
-            n_r_bins=1,
-            n_phi_bins=10,
-            n_z_bins=self.params['n_z_bins'],
-            min_r=self.params['min_r'],
-            min_phi=self.params['min_phi'],
-            min_z=self.params['min_z'],
-            max_r=self.params['max_r'],
-            max_phi=self.params['max_phi'],
-            max_z=self.params['max_z'])
-        self.phi_bin_size = (
-            self.params['max_phi'] - self.params['min_phi']) / 10.0
-        self.hist = np.array(p.calculate())
-        self.res = []
-        for ind, v_dens in enumerate(self.hist):
-            _ind = self.get_ind(1, 10, self.params['n_z_bins'], ind)
-            if _ind[3] == 2:
-                print _ind, v_dens
-                self.res.append([(_ind[1] + 0.5) * self.phi_bin_size, v_dens])
-        self.res = np.array(self.res)
-        # The Riemann sum should yield the sum of all z-velocities.
-        riemann_sum = np.sum(self.res[:, 1]) * \
-            0.5 * 5.0 * 5.0 * 10.0 * self.phi_bin_size
-        self.assertAlmostEqual(riemann_sum, self.params['N'] * 3.0, places=4)
-        # All values should be the same by construction.
-        #self.assertTrue(all(x - self.res[0, 1] < 1E-8 for x in self.res[:, 1]))
-
-    def test_z(self):
-        """Test binning in z.
-
-        """
-        self.system.part.clear()
-        for i in range(self.params['N']):
-            pos = np.array(self.params['center'])
-            pos += np.array([0.0, 0.0, i * self.params['max_z'] / self.params['N']])
-            self.system.part.add(id=i, pos=pos, v=[0.0, 0.0, 3.0])
-        # Set up the Observable.
-        p = espressomd.observables.CylindricalFluxDensityProfile(
-            ids=range(
-                self.params['N']),
-            center=self.params['center'],
-            n_r_bins=1,
-            n_phi_bins=1,
-            n_z_bins=10,
-            min_r=self.params['min_r'],
-            min_phi=self.params['min_phi'],
-            min_z=self.params['min_z'],
-            max_r=self.params['max_r'],
-            max_phi=self.params['max_phi'],
-            max_z=self.params['max_z'])
-        self.z_bin_size = (
-            self.params['max_z'] - self.params['min_z']) / 10.0
-        self.hist = np.array(p.calculate())
-        self.res = []
-        for ind, v_dens in enumerate(self.hist):
-            _ind = self.get_ind(1, 1, 10, ind)
-            if _ind[3] == 2:
-                self.res.append([(_ind[2] + 0.5) * self.z_bin_size, v_dens])
-        self.res = np.array(self.res)
-        # The Riemann sum should yield the sum of all z-velocities
-        riemann_sum = np.sum(self.res[:, 1]) * 2.0 * \
-            np.pi * 0.5 * 5.0 * 5.0 * self.z_bin_size
-        self.assertAlmostEqual(riemann_sum, self.params['N'] * 3.0, places=4)
-        # All values should be the same by construction.
-        self.assertTrue(all(x - self.res[0, 1] < 1E-8 for x in self.res[:, 1]))
+        core_hist = np.array(
+            p.calculate()).reshape(
+            self.params['n_r_bins'],
+            self.params['n_phi_bins'],
+            self.params['n_z_bins'],
+            3)
+        core_hist_v_r = core_hist[:, :, :, 0]
+        core_hist_v_phi = core_hist[:, :, :, 1]
+        core_hist_v_z = core_hist[:, :, :, 2]
+        np_hist, _ = np.histogramdd(pol_positions, bins=(self.params['n_r_bins'], self.params['n_phi_bins'], self.params['n_z_bins']),
+                                    range=[(self.params['min_r'], self.params['max_r']), (self.params['min_phi'], self.params['max_phi']),
+                                           (self.params['min_z'], self.params['max_z'])])
+        # Normalization
+        for i in range(self.params['n_r_bins']):
+            np_hist[i, :, :] /= bin_volume[i]
+        np.testing.assert_array_almost_equal(np_hist * v_r, core_hist_v_r)
+        np.testing.assert_array_almost_equal(np_hist * v_phi, core_hist_v_phi)
+        np.testing.assert_array_almost_equal(np_hist * v_z, core_hist_v_z)
 
 
 if __name__ == "__main__":
