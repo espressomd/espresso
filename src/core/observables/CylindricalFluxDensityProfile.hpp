0c6d8172fc2652c23b0a6b1e16d8d19ffe100965
Kai Szuttor <kai@icp.uni-stuttgart.de>
2017-11-30 15:09:33 +0100

    histogram: clang-format.

--- src/core/observables/CylindricalFluxDensityProfile.hpp
+++ src/core/observables/CylindricalFluxDensityProfile.hpp
@@ -9,10 +9,12 @@ namespace Observables {
 class CylindricalFluxDensityProfile : public CylindricalProfileObservable {
 public:
   virtual std::vector<double> operator()(PartCfg &partCfg) const override {
-    std::vector<size_t> n_bins{{static_cast<size_t>(n_r_bins), static_cast<size_t>(n_phi_bins), static_cast<size_t>(n_z_bins)}};
-    std::vector<std::pair<double, double>> limits{{std::make_pair(min_r, max_r),
-                                                  std::make_pair(min_phi, max_phi),
-                                                  std::make_pair(min_z, max_z)}};
+    std::vector<size_t> n_bins{{static_cast<size_t>(n_r_bins),
+                                static_cast<size_t>(n_phi_bins),
+                                static_cast<size_t>(n_z_bins)}};
+    std::vector<std::pair<double, double>> limits{
+        {std::make_pair(min_r, max_r), std::make_pair(min_phi, max_phi),
+         std::make_pair(min_z, max_z)}};
     Utils::CylindricalHistogram histogram(n_bins, 3, limits);
     double bin_volume;
     int r_bin, phi_bin, z_bin;
@@ -53,7 +55,7 @@ public:
       double v_z = vel[2] / time_step;
       // Write data to the histogram.
       histogram.update(ppos_cyl, std::vector<double>{{v_r, v_phi, v_z}});
-      }
+    }
     histogram.normalize();
     return histogram.get_histogram();
   }
@@ -67,8 +69,8 @@ private:
     // indices.
     static const int len_dims[4] = {n_r_bins, n_phi_bins, n_z_bins, 3};
     static const int n_dims = 4;
-    static const std::array<double, 3> bin_sizes = {{
-        r_bin_size(), phi_bin_size(), z_bin_size()}};
+    static const std::array<double, 3> bin_sizes = {
+        {r_bin_size(), phi_bin_size(), z_bin_size()}};
     std::array<double, 3> position;
     int index;
     int unravelled_index[4];
@@ -76,10 +78,10 @@ private:
     for (auto it = tmp.begin(); it != tmp.end(); it += 3) {
       index = std::distance(tmp.begin(), it);
       ::Utils::unravel_index(len_dims, n_dims, index, unravelled_index);
-      position = {{
-          (static_cast<double>(unravelled_index[0]) + 0.5) * bin_sizes[0],
-          (static_cast<double>(unravelled_index[1]) + 0.5) * bin_sizes[1],
-          (static_cast<double>(unravelled_index[2]) + 0.5) * bin_sizes[2]}};
+      position = {
+          {(static_cast<double>(unravelled_index[0]) + 0.5) * bin_sizes[0],
+           (static_cast<double>(unravelled_index[1]) + 0.5) * bin_sizes[1],
+           (static_cast<double>(unravelled_index[2]) + 0.5) * bin_sizes[2]}};
       m_ofile << position[0] << " " << position[1] << " " << position[2] << " "
               << *it << " " << *(it + 1) << " " << *(it + 2) << "\n";
     }
--- src/core/utils/Histogram.hpp
+++ src/core/utils/Histogram.hpp
@@ -270,3 +270,4 @@ private:
 } // Namespace Utils
 
 #endif
+
