c76b622dd2e7c4712cf752210f5e85989b1b259b
Timur Koyuk <tkoyuk@pudu.icp.uni-stuttgart.de>
2017-12-09 11:38:03 +0100

    Interface Correction due to new BondContainer and new get_n_bond_partners function

--- src/core/bond/Bond.hpp
+++ src/core/bond/Bond.hpp
@@ -1,11 +1,13 @@
 #ifndef ABSTRACT_BOND_CLASS_H
 #define ABSTRACT_BOND_CLASS_H
-#include "particle_data.hpp"
-#include <vector>
 #include "BondType.hpp"
+#include "particle_data.hpp"
+#include <vector> //for std::vector
+#include <array> //for std::array
+#include <boost/optional.hpp> // for boost::optional
 
 /*
-This is the basis class for bonded interactions.
+This is the base class for bonded interactions.
 It is abstract and all classes inheriting from it
 are concrete classes.
  */
@@ -14,9 +16,9 @@ class Bond {
 public:
 
   // constructor
-  Bond(int n_partners);
+  Bond(int n_partners){m_npartners = n_partners;}
   // virtual desctructor
-  virtual ~Bond();
+  virtual ~Bond()=default;
 
   //### FORCE AND ENERGY ###
   //no definition of these functions because this is an abstract class,
@@ -26,27 +28,42 @@ public:
   virtual int add_bonded_force(Particle *p1, int bl_id) = 0;
   //energy calculation
   // return value: 0: ok, 1: bond broken, 2: return from "add_bonded_force" in forces_inline.cpp
-  virtual int add_bonded_energy(Particle *p1, int bl_id, double *_energy) = 0;
-
-  //### PRESSURE CALCULATION ###
-  // for pressure calculation in 
-  // add_bonded_virials 
-  // in pressure.hpp
-  // return value: 0: ok, 1: bond broken, 2: return from "add_bonded_virials" in pressure.hpp
-  virtual int add_virial(Particle *p1, int bl_id);
-  //for add_three_body_bonded_stress
-  virtual int add_three_body_pressure(Particle *p1, int bl_id);
-  // for calculating stress tensor in pressure.cpp: int local_stress_tensor_calc()
-  virtual int calc_pair_force(Particle *p1, Particle *p2, int bl_id, double force[3]);
+  virtual int add_bonded_energy(Particle *p1, int bl_id) = 0;
 
   //### BOND PARTNER ###
   // get the total number of bond partners
   int get_number_of_bond_partners() const {return m_npartners;};
 
+  //!!!Definition of template function has to be in header file Bond.hpp!!!
   // get n bond partners
   // p1: Particle, which bond partners are determined
   // bl_id: index in p1->bl.e where the bond is
-  int get_n_bond_partners(Particle *p1, int bl_id);
+  template<size_t N>
+  boost::optional<std::array<Particle *, N> > get_n_bond_partners(Particle *p1, int bl_id) const
+  {
+
+    //definition and initialization of return value
+    //if there are null pointers, the function returns nothing
+    std::array<Particle *, N> bond_partners;
+    // fills entries with NULL pointer
+    bond_partners.fill(NULL);
+
+    for(int i=0;i<bond_partners.size();i++){
+      // get ith bond partner
+      bond_partners[i] = local_particles[p1->bl.e[bl_id+(i+1)]];
+      // error message if one partner doesnt exist
+      if(!bond_partners[i]){
+	runtimeErrorMsg() << "bond broken between particles " << p1->p.identity;
+	for(int j=1; j<i+1;j++){
+	  runtimeErrorMsg() << ", " << p1->bl.e[bl_id+j] << ", ";
+	};
+	runtimeErrorMsg() << " (particles not stored on the same node)";
+	return boost::none;
+      };
+
+    };
+    return bond_partners;
+  };
 
   //### BOND TYPE ###
   // get bond type
@@ -56,13 +73,7 @@ public:
   //bond type
   BondType m_bondtype{BondType::BONDED_IA_NONE};
   // number of bond partners
-  int m_npartners;
-  // bond partners
-  Particle** m_bond_partners;
-
-private:
-  // asign value of bond partners to NULL
-  void reset_bond_partners();
+  size_t m_npartners;
 
 };
 
