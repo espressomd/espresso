85affa8cafe912bf5463f85079eca93f1a6adef3
Henri Menke <henri@icp.uni-stuttgart.de>
2017-12-17 14:08:55 +1300

    Increase coverage

---
+++ tests/errors.cpp
@@ -0,0 +1,25 @@
+#define BOOST_TEST_MODULE variables
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include "matheval.hpp"
+
+BOOST_AUTO_TEST_CASE(parse_failure)                   \
+{                                                     \
+    std::string const s = "#";                        \
+    BOOST_CHECK_THROW(matheval::parse<double>(s, {}), \
+                      std::runtime_error);            \
+}
+
+BOOST_AUTO_TEST_CASE(expectation_failure)             \
+{                                                     \
+    std::string const s = "(";                        \
+    BOOST_CHECK_THROW(matheval::parse<double>(s, {}), \
+                      std::runtime_error);            \
+}
+
+BOOST_AUTO_TEST_CASE(unknown_variable)                \
+{                                                     \
+    std::string const s = "x";                        \
+    BOOST_CHECK_THROW(matheval::parse<double>(s, {}), \
+                      std::invalid_argument);         \
+}
--- tests/literals.cpp
+++ tests/literals.cpp
@@ -5,11 +5,11 @@
 
 using namespace boost::math::constants;
 
-EXPRTEST(literal1, "1.234",    1.234)
-EXPRTEST(literal2, "4.2e2",    420)
-EXPRTEST(literal3, "5e-01",    0.5)
-EXPRTEST(literal4, "-3",      -3)
-EXPRTEST(literal5, "pi",       pi<double>())
+EXPRTEST(literal1, "1.234"  ,  1.234)
+EXPRTEST(literal2, "4.2e2"  ,  420)
+EXPRTEST(literal3, "5e-01"  ,  0.5)
+EXPRTEST(literal4, "-3"     , -3)
+EXPRTEST(literal5, "pi"     ,  pi<double>())
 EXPRTEST(literal6, "epsilon",  std::numeric_limits<double>::epsilon())
-EXPRTEST(literal7, "phi",      phi<double>())
-EXPRTEST(literal8, "e",        e<double>())
+EXPRTEST(literal7, "phi"    ,  phi<double>())
+EXPRTEST(literal8, "e"      ,  e<double>())
