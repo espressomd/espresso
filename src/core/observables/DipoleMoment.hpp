2f9a063162859d147d47c0c4d8ec1fde3eeb7ce4
Kai Szuttor <kai@icp.uni-stuttgart.de>
2017-12-19 12:33:41 +0100

    observables: fixing dipolemoment.

--- src/core/observables/DipoleMoment.hpp
+++ src/core/observables/DipoleMoment.hpp
@@ -11,14 +11,14 @@ class DipoleMoment : public PidObservable {
 public:
   virtual int n_values() const override { return 3; };
   virtual std::vector<double> operator()(PartCfg &partCfg) const override {
-    std::vector<double> res(n_values());
+    std::vector<double> res(n_values(), 0.0);
     for (int i = 0; i < ids().size(); i++) {
 #ifdef ELECTROSTATICS
       double charge = partCfg[ids()[i]].p.q;
 
-      res[0] = charge * partCfg[ids()[i]].r.p[0];
-      res[1] = charge * partCfg[ids()[i]].r.p[1];
-      res[2] = charge * partCfg[ids()[i]].r.p[2];
+      res[0] += charge * partCfg[ids()[i]].r.p[0];
+      res[1] += charge * partCfg[ids()[i]].r.p[1];
+      res[2] += charge * partCfg[ids()[i]].r.p[2];
 #endif // ELECTROSTATICS
     }
     return res;
