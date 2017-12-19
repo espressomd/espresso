06fbf7fb65b2962a66286f8f81d371598bd16dee
Kai Szuttor <kai@icp.uni-stuttgart.de>
2017-12-19 12:07:21 +0100

    observables: fixing magneticdipolemoment.

--- src/core/observables/MagneticDipoleMoment.hpp
+++ src/core/observables/MagneticDipoleMoment.hpp
@@ -11,12 +11,12 @@ class MagneticDipoleMoment : public PidObservable {
 public:
   virtual int n_values() const override { return 3; };
   virtual std::vector<double> operator()(PartCfg &partCfg) const override {
-    std::vector<double> res(n_values());
+    std::vector<double> res(n_values(), 0.0);
     for (int i = 0; i < ids().size(); i++) {
 #ifdef DIPOLES
-      res[0] = partCfg[ids()[i]].r.dip[0];
-      res[1] = partCfg[ids()[i]].r.dip[1];
-      res[2] = partCfg[ids()[i]].r.dip[2];
+      res[0] += partCfg[ids()[i]].r.dip[0];
+      res[1] += partCfg[ids()[i]].r.dip[1];
+      res[2] += partCfg[ids()[i]].r.dip[2];
 #endif
     }
     return res;
