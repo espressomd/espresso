b0fb5a4e3f2967766b1693d54a6a941bdcdf232a
Florian Weik <fweik@icp.uni-stuttgart.de>
2017-12-22 18:01:31 +0100

    testsuite: Improvment of random_pairs check.

--- testsuite/random_pairs.py
+++ testsuite/random_pairs.py
@@ -3,6 +3,7 @@ import unittest as ut
 import espressomd
 import numpy as np
 import itertools
+import collections
 
 class RandomPairTest(ut.TestCase):
     s = espressomd.System()
@@ -14,6 +15,8 @@ class RandomPairTest(ut.TestCase):
         s.cell_system.skin=0.0
         s.min_global_cut=1.5
         n_part = 500
+
+        np.random.seed(2)
 
         s.part.add(pos=s.box_l * np.random.random((n_part, 3)))
 
@@ -29,25 +32,30 @@ class RandomPairTest(ut.TestCase):
                 if self.s.distance(parts[i], parts[j]) < dist:
                     pairs.append((i, j))
 
+        self.assertTrue(len(pairs))
         return set(pairs)
 
-    def check_pairs(self):
-        n2_pairs = self.pairs_n2(1.5)
-        cs_pairs = set(self.s.cell_system.get_pairs_(1.5))
-        self.assertTrue(len(n2_pairs))
+    def check_duplicates(self, l):
+        for e in collections.Counter(l).values():
+            self.assertEqual(e, 1)
+
+    def check_pairs(self, n2_pairs):
+        cs_pairs = self.s.cell_system.get_pairs_(1.5)
+        self.check_duplicates(cs_pairs)
         self.assertTrue(len(cs_pairs))
-        self.assertEqual(n2_pairs ^ cs_pairs, set())
+        self.assertEqual(n2_pairs ^ set(cs_pairs), set())
 
-    def check_dd(self):
+    def check_dd(self, n2_pairs):
         self.s.cell_system.set_domain_decomposition()
-        self.check_pairs()
+        self.check_pairs(n2_pairs)
 
-    def check_layered(self):
+    def check_layered(self, n2_pairs):
         self.s.cell_system.set_layered()
-        self.check_pairs()
+        self.check_pairs(n2_pairs)
 
-    def check_n_squared(self):
+    def check_n_squared(self, n2_pairs):
         self.s.cell_system.set_n_square()
+        self.check_pairs(n2_pairs)
 
     def test(self):
         if espressomd.has_features("PARTIAL_PERIODIC"):
@@ -57,9 +65,11 @@ class RandomPairTest(ut.TestCase):
 
         for periodicity in itertools.product(periods, periods, periods):
             self.s.periodicity = periodicity
-            self.check_dd()
-            self.check_layered()
-            self.check_n_squared()
+            n2_pairs = self.pairs_n2(1.5)
+
+            self.check_dd(n2_pairs)
+            self.check_layered(n2_pairs)
+            self.check_n_squared(n2_pairs)
 
 if __name__ == '__main__':
     print("Features: ", espressomd.features())
