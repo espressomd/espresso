fe30b4fc64242fff7df4bf45b5866d6f708e0b62
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2018-01-10 10:37:25 +0100

    Virtual sites: missing file

---
+++ src/core/virtual_sites.hpp
@@ -0,0 +1,20 @@
+#ifndef VIRTUAL_SITES_HPP
+#define VIRTUAL_SITES_HPP
+
+#include "config.hpp"
+
+#ifdef VIRTUAL_SITES
+#include "virtual_sites/VirtualSites.hpp" 
+
+/** @brief get active virtual sites implementation */
+const std::shared_ptr<VirtualSites>& virtual_sites();
+
+/** @brief Set active virtual sites implementation */
+void set_virtual_sites(std::shared_ptr<VirtualSites> v);
+
+#ifdef VIRTUAL_SITES_RELATIVE
+int vs_relate_to(int part_num, int relate_to);
+#endif
+
+#endif
+#endif
