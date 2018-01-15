7fab2a663eee94e34957c5347bb7b55f1d491707
Jonas Landsgesell <jonaslandsgesell@users.noreply.github.com>
2018-01-15 17:24:52 +0100

    remove code for polymer configuration changing moves

--- src/core/reaction_ensemble.hpp
+++ src/core/reaction_ensemble.hpp
@@ -174,8 +174,6 @@ private:
   };
 
   void add_types_to_index(std::vector<int> &type_list);
-  std::vector<double> add_random_vector(double const *vector, int len_vector,
-                         double length_of_displacement);
   std::vector<double> get_random_position_in_box();
   std::vector<double>
   get_random_position_in_box_enhanced_proposal_of_small_radii();
