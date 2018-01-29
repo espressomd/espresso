4ea2f5e8a90d19e872861ffed94416ed4c1afae4
Jonas Landsgesell <jonaslandsgesell@users.noreply.github.com>
2018-01-29 13:01:01 +0100

    test forces on constraint

--- testsuite/constraint_shape_based.py
+++ testsuite/constraint_shape_based.py
@@ -11,53 +11,54 @@ import unittest as ut
 import espressomd
 
 from espressomd import interactions
-from espressomd.shapes import Wall
+import espressomd.shapes as shapes
 
 
 @ut.skipIf(not espressomd.has_features(["CONSTRAINTS", "LENNARD_JONES"]),
            "Features not available, skipping test!")
 class ShapeBasedConstraintTest(ut.TestCase):
+    box_l=10.
 
-    def prepare(self, S):
-        S.box_l = [10., 10., 10.]
-        S.time_step = 0.01
-        S.cell_system.skin = 0.4
+    def prepare(self, system):
+        system.box_l = [self.box_l, self.box_l, self.box_l]
+        system.time_step = 0.01
+        system.cell_system.skin = 0.4
 
-        S.part.add(pos=[5., 1.21, 0.83], type=0)
+        system.part.add(pos=[5., 1.21, 0.83], type=0)
 
-        S.non_bonded_inter[0, 1].lennard_jones.set_params(
+        system.non_bonded_inter[0, 1].lennard_jones.set_params(
             epsilon=1.0, sigma=1.0, cutoff=2.0, shift=0)
-        S.non_bonded_inter[0, 2].lennard_jones.set_params(
+        system.non_bonded_inter[0, 2].lennard_jones.set_params(
             epsilon=1.5, sigma=1.0, cutoff=2.0, shift=0)
 
     def lj_force(self, eps, sig, r):
         f_lj = 24.0 * eps * (2.0 * sig**12 / r**13 - sig**6 / r**7)
         return f_lj
 
-    def test(self):
-        S = espressomd.System()
-        self.prepare(S)
+    def test_wall(self):
+        system = espressomd.System()
+        self.prepare(system)
 
-        wy = Wall(normal=[0., 1., 0.], dist=0.)
-        wz = Wall(normal=[0., 0., 1.], dist=0.)
+        wy = shapes.Wall(normal=[0., 1., 0.], dist=0.)
+        wz = shapes.Wall(normal=[0., 0., 1.], dist=0.)
 
         # (1)
         constraint_wy = espressomd.constraints.ShapeBasedConstraint(
             shape=wy, particle_type=1)
-        S.constraints.add(constraint_wy)
+        wall_xz=system.constraints.add(constraint_wy)
 
         # (3)
-        S.constraints.add(shape=wz, particle_type=2)
+        wall_xy=system.constraints.add(shape=wz, particle_type=2)
 
         # Check forces
-        f_part = S.part[0].f
+        f_part = system.part[0].f
 
         self.assertEqual(f_part[0], 0.)
         self.assertEqual(f_part[1], 0.)
         self.assertEqual(f_part[2], 0.)
 
-        S.integrator.run(0)
-        f_part = S.part[0].f
+        system.integrator.run(0) #update forces
+        f_part = system.part[0].f
 
         self.assertEqual(f_part[0], 0.)
         self.assertAlmostEqual(f_part[1], self.lj_force(
@@ -65,11 +66,18 @@ class ShapeBasedConstraintTest(ut.TestCase):
         self.assertAlmostEqual(f_part[2], self.lj_force(
             eps=1.5, sig=1.0, r=0.83), places=10)
 
-        # Check removal
-        for c in S.constraints:
-            S.constraints.remove(c)
+        #test forces on walls
+        self.assertAlmostEqual(-1.0*wall_xz.total_force()[1], self.lj_force(
+            eps=1.0, sig=1.0, r=1.21), places=10) #minus for newtons thrid law        
+        self.assertAlmostEqual(-1.0*wall_xy.total_force()[2], self.lj_force(
+            eps=1.5, sig=1.0, r=0.83), places=10)        
 
-        for c in S.constraints:
+
+        # Check removal
+        for c in system.constraints:
+            system.constraints.remove(c)
+
+        for c in system.constraints:
             self.assertTrue(False)
 
 if __name__ == "__main__":
