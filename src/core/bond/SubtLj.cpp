bd77daa5f02010edcc307fc2641c3caaa75fd6b7
Timur Koyuk <tkoyuk@pudu.icp.uni-stuttgart.de>
2018-02-10 15:39:34 +0100

    fixed Lj-Bond

--- src/core/bond/SubtLj.cpp
+++ src/core/bond/SubtLj.cpp
@@ -4,16 +4,11 @@
 int Bond::SubtLj::calc_bonded_pair_force(Particle *p1, Particle *p2,
                                          double dx[3], double force[3]) const {
 #ifdef LENNARD_JONES
-  double dist = sqrt(sqrlen(dx));
-  double lj_force[3] = {0., 0., 0.};
-
-  add_lj_pair_force(p1, p2, get_ia_param(p1->p.type, p2->p.type), dx, dist,
-                    lj_force);
-
   for (int i = 0; i < 3; i++) {
-    force[i] -= -lj_force[i];
-  }
-
+    dx[i] *= -1;
+  };
+  add_lj_pair_force(p1, p2, get_ia_param(p1->p.type, p2->p.type), dx,
+		    Utils::veclen(dx), force);
 #endif
   return 0;
 }
@@ -21,10 +16,7 @@ int Bond::SubtLj::calc_bonded_pair_force(Particle *p1, Particle *p2,
 int Bond::SubtLj::calc_bonded_pair_energy(Particle *p1, Particle *p2,
                                           double dx[3], double *_energy) const {
 #ifdef LENNARD_JONES
-  double dist = sqrt(sqrlen(dx));
-
-  *_energy =
-      -lj_pair_energy(p1, p2, get_ia_param(p1->p.type, p2->p.type), dx, dist);
+  *_energy = -lj_pair_energy(p1, p2, get_ia_param(p1->p.type, p2->p.type), dx, Utils::veclen(dx));
 #endif
   return 0;
 }
