b6ed13a798a7b6624fe358de80fb6ec043a7885d
davidsean <david.sean@gmail.com>
2018-02-11 18:42:06 +0100

    forgot one

--- src/script_interface/constraints/ShapeBasedConstraint.hpp
+++ src/script_interface/constraints/ShapeBasedConstraint.hpp
@@ -59,7 +59,7 @@ public:
     if (name == "total_force") {
       return shape_based_constraint()->total_force();
     }
-    if (name == "mindist") {
+    if (name == "min_dist") {
       return shape_based_constraint()->min_dist();
     }
 
