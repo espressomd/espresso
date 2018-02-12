87de7c93a7fba7c23212da051b65c057482e80ca
davidsean <david.sean@gmail.com>
2018-02-12 19:35:02 +0100

    minor corrections

--- samples/h5md.py
+++ samples/h5md.py
@@ -22,7 +22,7 @@ fene = interactions.FeneBond(k=10, d_r_max=2)
 system.bonded_inter.add(fene)
 polymer.create_polymer(N_P=5, bond_length=1.0, MPC=50, bond=fene)
 
-sys.integrator.run(steps=0)
+system.integrator.run(steps=0)
 h5_file = h5md.H5md(filename="sample.h5", write_pos=True, write_vel=True,
                     write_force=True, write_species=True, write_mass=False, write_charge=True, write_ordered=True)
 for i in range(1):
