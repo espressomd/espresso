832372c184885af8c44ddb63cf9e4549cc3597e5
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-03-01 12:14:00 +0100

    particle_data: particle_exists fix.

--- src/core/particle_data.cpp
+++ src/core/particle_data.cpp
@@ -1346,5 +1346,7 @@ void pointer_to_rotational_inertia(Particle const *p, double const *&res) {
 #endif
 
 bool particle_exists(int part_id) noexcept {
+  if (particle_node.empty())
+    build_particle_node();
   return particle_node.count(part_id);
 }
