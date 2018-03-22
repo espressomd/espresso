d97f175f77a4e28172f0096b3c7d86cef62d45b1
Florian Weik <fweik@icp.uni-stuttgart.de>
2018-03-22 19:48:12 +0100

    testsuite: Imrpoved speed and statistics of NPT test.

--- testsuite/npt.py
+++ testsuite/npt.py
@@ -26,18 +26,16 @@ import tests_common
 
 @ut.skipIf(not espressomd.has_features(["NPT", "LENNARD_JONES"]),
            "Features not available, skipping test!")
-
 class NPTintegrator(ut.TestCase):
     """This compares pressure and compressibility of a LJ system against expected values."""
     S = espressomd.System(box_l=[1.0, 1.0, 1.0])
     p_ext = 2.0
 
     def setUp(self):
-        
         box_l = 5.78072780098
         self.S.box_l = [box_l]*3
         self.S.time_step = 0.01
-        self.S.cell_system.skin = 0.4
+        self.S.cell_system.skin = 0.0
 
         data = np.genfromtxt(tests_common.abspath(
             "data/npt_lj_system.data"))
@@ -45,30 +43,33 @@ class NPTintegrator(ut.TestCase):
         # Input format: id pos f
         for particle in data:
             pos = particle[:3]
-            f = particle[3:]
-            self.S.part.add(pos=pos, f=f)
+            self.S.part.add(pos=pos)
 
         self.S.non_bonded_inter[0, 0].lennard_jones.set_params(
             epsilon=1, sigma=1,
             cutoff=1.12246, shift=0.25)
-        
+
         self.S.thermostat.set_npt(kT = 1.0, gamma0 = 0.5, gammav = 0.001)
-        self.S.integrator.set_isotropic_npt(ext_pressure = self.p_ext, piston = 0.001)
+        self.S.integrator.set_isotropic_npt(ext_pressure = self.p_ext, piston = 0.0001)
 
     def test_npt(self):
-        avp=0
+        self.S.integrator.run(500)
+
         n=1500
-        Vs=[]
-        for t in range(n): 
-            self.S.integrator.run(10)
-            avp += self.S.analysis.pressure()['total']
-            l0 = self.S.box_l[0]
+        ls=np.zeros((n,))
+        ps=np.zeros((n,))
+        steps=10
+        for t in range(n):
+            self.S.integrator.run(steps)
+            p_inst = self.S.analysis.pressure()['total']
+            ps[t] = p_inst
+            ls[t] = self.S.box_l[0]
 
-            #if t > 500:
-            Vs.append(pow(l0,3))
+        avp = np.average(ps)
 
-        avp/=n
-        compressibility = pow(np.std(Vs),2)/np.average(Vs)
+        Vs = np.power(ls, 3)
+
+        compressibility = np.power(np.std(Vs),2)/np.average(Vs)
 
         self.assertAlmostEqual(2.0, avp, places=2)
         self.assertAlmostEqual(0.2, compressibility, places=1)
