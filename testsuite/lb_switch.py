a54a60d86bf406fed6fc1753631025a56475e595
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-06-11 16:39:55 +0200

    testsuite: extend LB switch test to test GPU version.

--- testsuite/lb_switch.py
+++ testsuite/lb_switch.py
@@ -17,26 +17,33 @@ class LBSwitchActor(ut.TestCase):
     system.time_step = 0.01
     system.cell_system.skin = 0.1
 
-    def test(self):
+    def switch_test(self, GPU=False):
         system = self.system
-
-        system.part.add(pos=[5.,5.,5.], v=[1.,0,0])
+        system.actors.clear()
+        system.part.add(pos=[1.,1.,1.], v=[1.,0,0], fix=[1, 1, 1])
         gamma_1 = 1.0
         gamma_2 = 2.0
+        ext_force_density = [0.2, 0.3, 0.15]
 
         system.thermostat.set_lb(kT=0.0)
 
-        lb_fluid_1 = espressomd.lb.LBFluid(
-            agrid=2.0, dens=1.0, visc=1.0, fric=gamma_1, tau=0.03)
+        lb_fluid_1_params = {'agrid': 2.0, 'dens': 1.0, 'visc': 1.0, 'fric': gamma_1, 'tau': 0.03}
+        lb_fluid_2_params = {'agrid': 2.0, 'dens': 1.0, 'visc': 1.0, 'fric': gamma_2, 'tau': 0.03}
 
-        lb_fluid_2 = espressomd.lb.LBFluid(
-            agrid=2.0, dens=1.0, visc=1.0, fric=gamma_2, tau=0.03)
+        if GPU:
+            lb_fluid_1 = espressomd.lb.LBFluidGPU(**lb_fluid_1_params)
+            lb_fluid_2 = espressomd.lb.LBFluidGPU(**lb_fluid_2_params)
+        else:
+            lb_fluid_1 = espressomd.lb.LBFluid(**lb_fluid_1_params)
+            lb_fluid_2 = espressomd.lb.LBFluid(**lb_fluid_2_params)
 
         system.actors.add(lb_fluid_1)
 
         system.integrator.run(1)
 
-        np.testing.assert_allclose(np.copy(system.part[0].f), [-gamma_1, 0.0, 0.0])
+        force_on_part = -lb_fluid_1_params['fric'] * system.part[0].v
+
+        np.testing.assert_allclose(np.copy(system.part[0].f), force_on_part)
 
         system.integrator.run(100)
         self.assertNotAlmostEqual(lb_fluid_1[3,3,3].velocity[0], 0.0)
@@ -58,6 +65,15 @@ class LBSwitchActor(ut.TestCase):
         system.integrator.run(1)
 
         np.testing.assert_allclose(np.copy(system.part[0].f), [-gamma_2, 0.0, 0.0])
+
+    def test_CPU_LB(self):
+        self.switch_test()
+
+    @ut.skipIf(not espressomd.has_features(["LB_GPU"]),
+            "LB_GPU not available, skipping test.")
+    def test_GPU_LB(self):
+        self.switch_test(GPU=True)
+
 
 if __name__ == "__main__":
     ut.main()
