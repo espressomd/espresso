5a3d9fea7f20fd4ae74f88d18fca3c8d1e5c203f
Florian Weik <fweik@icp.uni-stuttgart.de>
2018-06-28 16:27:39 +0200

    utils: Removed duplication

--- src/core/utils.hpp
+++ src/core/utils.hpp
@@ -488,10 +488,6 @@ void vecsub(T const *const a, T const *const b, T *const c) {
     c[i] = a[i] - b[i];
 }
 
-template <typename T> int sign(T value) {
-  return (T(0) < value) - (value < T(0));
-}
-
 } // namespace Utils
 
 /*@}*/
