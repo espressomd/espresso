cc1db7bf9b1903272cc2b2655aa8ecf18855a7e4
Florian Weik <fweik@icp.uni-stuttgart.de>
2018-07-05 13:55:03 +0200

    utils: typos

--- src/core/utils/serialization/array.hpp
+++ src/core/utils/serialization/array.hpp
@@ -3,20 +3,17 @@
 
 #include <boost/version.hpp>
 
-/* New versions of boost alrady containt this
- * function
- * */
+/* Newer versions of boost already contain this function */
 #if BOOST_VERSION < 105600
 #include <array>
-
 namespace boost {
 namespace serialization {
 template <typename Archive, typename T, std::size_t N>
 void serialize(Archive &ar, std::array<T, N> &a, const unsigned int) {
   ar &*static_cast<T(*)[N]>(static_cast<void *>(a.data()));
 }
-}
-}
+} // namespace serialization
+} // namespace boost
 #else
 #include <boost/serialization/array.hpp>
 #endif
