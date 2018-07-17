66d8b0af2c994e87e0f1074153bfbed315c94328
Andrea Tagliabue <atagliabue@ibex.icp.uni-stuttgart.de>
2018-07-17 10:08:23 +0200

    autopep

--- doc/tutorials/10-reaction_ensemble/scripts/RE_vs_cpH.py
+++ doc/tutorials/10-reaction_ensemble/scripts/RE_vs_cpH.py
@@ -29,14 +29,14 @@ from espressomd import reaction_ensemble
 
 
 # System parameters
-#############################################################
+#
 box_l = 60
 
 # Integration parameters
-#############################################################
-system = espressomd.System(box_l=[box_l]*3)
+#
+system = espressomd.System(box_l=[box_l] * 3)
 system.set_random_state_PRNG()
-#system.seed = system.cell_system.get_state()['n_nodes'] * [1234]
+# system.seed = system.cell_system.get_state()['n_nodes'] * [1234]
 np.random.seed(seed=system.seed)
 
 system.time_step = 0.02
@@ -44,14 +44,14 @@ system.cell_system.skin = 0.4
 system.cell_system.max_num_cells = 2744
 
 
-#############################################################
-#  Setup System                                             #
-#############################################################
-mode="reaction_ensemble"
-#mode="constant_pH_ensemble"
+#
+# Setup System                                             #
+#
+mode = "reaction_ensemble"
+# mode="constant_pH_ensemble"
 
 # Particle setup
-#############################################################
+#
 # type 0 = HA
 # type 1 = A-
 # type 2 = H+
@@ -64,29 +64,29 @@ for i in range(N0):
 for i in range(N0, 2 * N0):
     system.part.add(id=i, pos=np.random.random(3) * system.box_l, type=2)
 
-RE=None
-if(mode=="reaction_ensemble"):
+RE = None
+if(mode == "reaction_ensemble"):
     RE = reaction_ensemble.ReactionEnsemble(temperature=1, exclusion_radius=1)
 elif(mode == "constant_pH_ensemble"):
-    RE = reaction_ensemble.ConstantpHEnsemble(temperature=1, exclusion_radius=1)
-    RE.constant_pH=3
+    RE = reaction_ensemble.ConstantpHEnsemble(
+        temperature=1, exclusion_radius=1)
+    RE.constant_pH = 3
 RE.add_reaction(gamma=K_diss, reactant_types=[0], reactant_coefficients=[
-       1], product_types=[1, 2], product_coefficients=[1, 1], default_charges={0: 0, 1: -1, 2: +1})
+    1], product_types=[1, 2], product_coefficients=[1, 1], default_charges={0: 0, 1: -1, 2: +1})
 print(RE.get_status())
 system.setup_type_map([0, 1, 2])
 
 
-alpha = [] 
+alpha = []
 
 
 for i in range(100):
     RE.reaction(100)
-    print("HA", system.number_of_particles(type=0), "A-",system.number_of_particles(type=1), "H+", system.number_of_particles(type=2))
-    alpha.append(system.number_of_particles(type=1)/N0) 
+    print("HA", system.number_of_particles(type=0), "A-",
+          system.number_of_particles(type=1), "H+", system.number_of_particles(type=2))
+    alpha.append(system.number_of_particles(type=1) / N0)
 
 alpha_av = np.mean(alpha)
-alpha_err = np.std(alpha)/np.sqrt(len(alpha))
+alpha_err = np.std(alpha) / np.sqrt(len(alpha))
 
 print("\n<alpha> = {} (err = {})".format(alpha_av, alpha_err))
-
-
