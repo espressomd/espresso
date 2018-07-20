3cc5b48426593f0634065cf391cc23069b7af192
Patrick Kreissl <patrick.kreissl@pa-le.de>
2018-07-20 11:42:17 +0200

    Get rid of GCC 8.1.1 compiler warnings.

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -3,9 +3,7 @@
 
 char *strcat_alloc(char *left, const char *right) {
   if (!left) {
-    char *res = (char *)Utils::malloc(strlen(right) + 1);
-    strncpy(res, right, strlen(right) + 1);
-    return res;
+    return strdup(right);
   } else {
     size_t newlen = strlen(left) + strlen(right) + 1;
     char *res = Utils::realloc(left, newlen);
