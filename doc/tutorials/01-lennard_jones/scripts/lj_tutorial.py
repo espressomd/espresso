10dc01535b81a6b3063a9b3270fba3fac183325a
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-07-24 09:43:39 +0200

    tutorials: corrected syntax.

--- doc/tutorials/01-lennard_jones/scripts/lj_tutorial.py
+++ doc/tutorials/01-lennard_jones/scripts/lj_tutorial.py
@@ -65,7 +65,7 @@ lj_cap = 5
 
 # System setup
 #############################################################
-system              = espressomd.Systembox_l=([1.0, 1.0, 1.0])
+system              = espressomd.System(box_l=[1.0, 1.0, 1.0])
 system.seed         = system.cell_system.get_state()['n_nodes'] * [1234]
 np.random.seed(system.seed)
 
