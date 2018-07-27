2e31c9acc1d9f34fadc99ac41a2a75ca7318114f
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-07-27 09:46:40 +0200

    testsuite: autopep8 plus docstring.

--- testsuite/lb_poiseuille.py
+++ testsuite/lb_poiseuille.py
@@ -5,14 +5,46 @@ import espressomd.lb
 import espressomd.lbboundaries
 import espressomd.shapes
 
+
+"""
+Check the Lattice Boltzmann 'pressure' driven flow in a slab system
+by comparing to the analytical solution.
+
+
+"""
+
+
+
+
 AGRID = .25
 EXT_FORCE = .2
 VISC = .7
 DENS = 1.7
 TIME_STEP = 0.1
+LB_PARAMS = {'agrid': AGRID,
+             'dens': DENS,
+             'visc': VISC,
+             'fric': 1.0,
+             'tau': TIME_STEP,
+             'ext_force_density': [0.0, 0.0, EXT_FORCE]}
 
 
 def poiseuille_flow(z, H, ext_force_density, dyn_visc):
+    """
+    Analytical solution for plane Poiseuille flow.
+
+    Parameters
+    ----------
+    z : :obj:`float`
+        Distance to the mid plane of the channel.
+    H : :obj:`float`
+        Distance between the boundaries.
+    ext_force_density : :obj:`float`
+        Force density on the fluid normal to the boundaries.
+    dyn_visc : :obj:`float`
+        Dynamic viscosity of the LB fluid.
+
+    """
     return ext_force_density * 1. / (2 * dyn_visc) * (H**2.0 / 4.0 - z**2.0)
 
 
@@ -34,8 +66,9 @@ class LBPoiseuilleCommon(object):
         self.system.lbboundaries.add(wall1)
         self.system.lbboundaries.add(wall2)
 
-        mid_indices = [int((self.system.box_l[0] / AGRID) / 2), int(
-            (self.system.box_l[1] / AGRID) / 2), int((self.system.box_l[2] / AGRID) / 2)]
+        mid_indices = [int((self.system.box_l[0] / AGRID) / 2),
+                       int((self.system.box_l[1] / AGRID) / 2),
+                       int((self.system.box_l[2] / AGRID) / 2)]
         diff = float("inf")
         old_val = self.lbf[mid_indices].velocity[2]
         while diff > 0.01:
@@ -54,26 +87,30 @@ class LBPoiseuilleCommon(object):
                 for z in range(int(self.system.box_l[2] + 1)):
                     v_tmp.append(self.lbf[x, y, z].velocity[2])
             velocities[x, 1] = np.mean(np.array(v_tmp))
-            velocities[x, 0] = x * AGRID
+            velocities[x, 0] = (x + 0.5) * AGRID
 
         v_measured = velocities[1:-1, 1]
-        v_expected = poiseuille_flow(velocities[1:-1, 0] + 0.5 * AGRID - 0.5 *
-                                     self.system.box_l[0], self.system.box_l[0] - 2.0 * AGRID, EXT_FORCE, VISC * DENS)
+        v_expected = poiseuille_flow(velocities[1:-1,
+                                                0] - 0.5 * self.system.box_l[0],
+                                     self.system.box_l[0] - 2.0 * AGRID,
+                                     EXT_FORCE,
+                                     VISC * DENS)
         rmsd = np.sqrt(np.sum(np.square(v_expected - v_measured)))
-        self.assertLess(rmsd, 0.02*AGRID/TIME_STEP)
+        self.assertLess(rmsd, 0.02 * AGRID / TIME_STEP)
 
-@ut.skipIf(not espressomd.has_features(['LB', 'LB_BOUNDARIES']), "Skipping test due to missing features.")
+
+@ut.skipIf(not espressomd.has_features(
+    ['LB', 'LB_BOUNDARIES']), "Skipping test due to missing features.")
 class LBCPUPoiseuille(ut.TestCase, LBPoiseuilleCommon):
     def setUp(self):
-        self.lbf = espressomd.lb.LBFluid(agrid=AGRID, dens=DENS, visc=VISC,
-                                fric=1.0, tau=TIME_STEP, ext_force_density=[0.0, 0.0, EXT_FORCE])
+        self.lbf = espressomd.lb.LBFluid(**LB_PARAMS)
 
 
-@ut.skipIf(not espressomd.has_features(['LB_GPU', 'LB_BOUNDARIES_GPU']), "Skipping test due to missing features.")
+@ut.skipIf(not espressomd.has_features(
+    ['LB_GPU', 'LB_BOUNDARIES_GPU']), "Skipping test due to missing features.")
 class LBGPUPoiseuille(ut.TestCase, LBPoiseuilleCommon):
     def setUp(self):
-        self.lbf = espressomd.lb.LBFluidGPU(agrid=AGRID, dens=DENS, visc=VISC,
-                                   fric=1.0, tau=TIME_STEP, ext_force_density=[0.0, 0.0, EXT_FORCE])
+        self.lbf = espressomd.lb.LBFluidGPU(**LB_PARAMS)
 
 
 if __name__ == '__main__':
