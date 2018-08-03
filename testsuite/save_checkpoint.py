ebd3b7283197c8c5e6910d86d22e771941133566
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-08-03 12:57:17 +0200

    checkpointing: fix LB plus test.

--- testsuite/save_checkpoint.py
+++ testsuite/save_checkpoint.py
@@ -7,14 +7,18 @@ import espressomd.interactions
 import espressomd.virtual_sites
 import espressomd.accumulators
 import espressomd.observables
+import espressomd.lb
 
 checkpoint = espressomd.checkpointing.Checkpoint(checkpoint_id="mycheckpoint", checkpoint_path="@CMAKE_CURRENT_BINARY_DIR@")
 
 system = espressomd.System(box_l=[10.0, 10.0, 10.0])
-system.cell_system.skin = 0.4
+system.cell_system.skin = 0.1
 system.seed = system.cell_system.get_state()["n_nodes"] *[1234]
 system.time_step = 0.01
 system.min_global_cut = 2.0
+if espressomd.has_features('LB'):
+    lbf = espressomd.lb.LBFluid(agrid=0.5, visc=1.3, dens=1.5, tau=0.01, fric=2.0)
+    system.actors.add(lbf)
 
 system.part.add(pos=[1.0]*3)
 system.part.add(pos=[1.0, 1.0, 2.0])
@@ -50,4 +54,7 @@ particle_force0 = np.copy(system.part[0].f)
 particle_force1 = np.copy(system.part[1].f)
 checkpoint.register("particle_force0")
 checkpoint.register("particle_force1")
+if espressomd.has_features('LB'):
+    lbf[1, 1, 1].velocity = [0.1, 0.2, 0.3]
+    lbf.save_checkpoint("@CMAKE_CURRENT_BINARY_DIR@/lb.cpt", 1)
 checkpoint.save(0)
