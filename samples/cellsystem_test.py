c09d92a65e7c04dffa37a08ed84cef2faf92f330
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2018-08-06 15:57:34 +0200

    Samples: Cell system - adapt syntax of create_polymer

--- samples/cellsystem_test.py
+++ samples/cellsystem_test.py
@@ -59,7 +59,7 @@ system.non_bonded_inter[0, 0].lennard_jones.set_params(
     cutoff=2**(1. / 6), shift="auto")
 fene = interactions.FeneBond(k=10, d_r_max=1.5)
 system.bonded_inter.add(fene)
-polymer.create_polymer(N_P=1, bond_length=0.97, MPC=100, bond=fene)
+polymer.create_polymer(N_P=1, bond_length=0.97, MPC=100, bond=fene, start_pos = [0,0,0])
 
 n_steps = 1000
 
