ea9faa85697baa7411bde9e4012500bc05b86310
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2018-08-06 15:43:56 +0200

    Samples: minimal charged particles: outdated use of thermostat

--- samples/minimal-charged-particles.py
+++ samples/minimal-charged-particles.py
@@ -26,7 +26,6 @@ import espressomd
 required_features = ["ELECTROSTATICS","LENNARD_JONES"]
 espressomd.assert_features(required_features)
 
-from espressomd import thermostat
 from espressomd import electrostatics
 import numpy as np
 
@@ -53,7 +52,7 @@ np.random.seed(seed=system.seed)
 
 system.time_step = 0.01
 system.cell_system.skin = 0.4
-thermostat.Thermostat().set_langevin(kT=1.0, gamma=1.0)
+system.thermostat.set_langevin(kT=1.0, gamma=1.0)
 
 # warmup integration (with capped LJ potential)
 warm_steps = 100
