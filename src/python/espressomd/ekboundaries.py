bd1f63a99cb4e1f789dfdb971984eb83d654ad89
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-08-06 13:50:25 +0200

    python: make ekboundaries what they actually are.

--- src/python/espressomd/ekboundaries.py
+++ src/python/espressomd/ekboundaries.py
@@ -1,53 +1,19 @@
 from __future__ import print_function, absolute_import
 from .script_interface import ScriptInterfaceHelper, script_interface_register
-
+import espressomd.lbboundaries
 
 @script_interface_register
-class EKBoundaries(ScriptInterfaceHelper):
+class EKBoundaries(espressomd.lbboundaries.LBBoundaries):
     """
     Creates a set of electrokinetics boundaries.
+
     """
-
-    _so_name = "LBBoundaries::LBBoundaries"
-
-    def add(self, *args, **kwargs):
-        """
-        Adds a boundary to the set.
-        Either a valid boundary is an argument,
-        or a valid set of parameters to create a boundary.
-        
-        """
-
-        if len(args) == 1:
-            if isinstance(args[0], EKBoundary):
-                ekboundary = args[0]
-            else:
-                raise TypeError(
-                    "Either a EKBoundary object or key-value pairs for the parameters of a EKBoundary object need to be passed.")
-        else:
-            ekboundary = EKBoundary(**kwargs)
-        self.call_method("add", object=ekboundary)
-        return ekboundary
-
-    def remove(self, ekboundary):
-        """
-        Removes a boundary from the set.
-        
-        Parameters
-        ----------
-        ekboundary : :obj:`EKBoundary`
-                     The boundary to be removed from the set.
-                     
-        """
-
-        self.call_method("remove", ekboundary=ekboundary)
-
+    pass
 
 @script_interface_register
-class EKBoundary(ScriptInterfaceHelper):
+class EKBoundary(espressomd.lbboundaries.LBBoundary):
     """
     Creates a EK boundary.
     
     """
-
-    _so_name = "LBBoundaries::LBBoundary"
+    pass
