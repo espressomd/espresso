0acfb235acdc295d4d0c7117c06be93a9322da31
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-08-08 09:51:24 +0200

    python: only import EKB if available.

--- src/python/espressomd/ekboundaries.py
+++ src/python/espressomd/ekboundaries.py
@@ -2,18 +2,19 @@ from __future__ import print_function, absolute_import
 from .script_interface import ScriptInterfaceHelper, script_interface_register
 import espressomd.lbboundaries
 
-@script_interface_register
-class EKBoundaries(espressomd.lbboundaries.LBBoundaries):
-    """
-    Creates a set of electrokinetics boundaries.
+if any(i in espressomd.code_info.features() for i in ["LB_BOUNDARIES", "LB_BOUNDARIES_GPU"]):
+    @script_interface_register
+    class EKBoundaries(espressomd.lbboundaries.LBBoundaries):
+        """
+        Creates a set of electrokinetics boundaries.
 
-    """
-    pass
+        """
+        pass
 
-@script_interface_register
-class EKBoundary(espressomd.lbboundaries.LBBoundary):
-    """
-    Creates a EK boundary.
-    
-    """
-    pass
+    @script_interface_register
+    class EKBoundary(espressomd.lbboundaries.LBBoundary):
+        """
+        Creates a EK boundary.
+
+        """
+        pass
