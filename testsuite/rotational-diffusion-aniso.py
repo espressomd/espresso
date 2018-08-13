78242c546a058c03c6c0068ac6d0fa61902e3a64
Dr. Bogdan Tanygin <b.m.tanygin@gmail.com>
2018-08-14 00:50:59 +0300

    Test refactoring according to the review

--- testsuite/rotational-diffusion-aniso.py
+++ testsuite/rotational-diffusion-aniso.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
-import unittest as ut
-import numpy as np
-from numpy.random import random, seed
-import espressomd
 import math
-import tests_common as tc
+import numpy as np
+from numpy.random import seed, uniform
+import unittest as ut
+import espressomd
+import tests_common
 
 @ut.skipIf(not espressomd.has_features(["ROTATION",
                                         "PARTICLE_ANISOTROPY",
@@ -14,9 +14,9 @@ import tests_common as tc
 class RotDiffAniso(ut.TestCase):
     longMessage = True
     # Handle for espresso system
-    es = espressomd.System(box_l=[1.0,1.0,1.0])
-    es.cell_system.skin = 5.0
-    es.seed = 5
+    system = espressomd.System(box_l=[1.0,1.0,1.0])
+    system.cell_system.skin = 5.0
+    system.seed = 5
     
     # The NVT thermostat parameters
     kT = 0.0
@@ -27,47 +27,11 @@ class RotDiffAniso(ut.TestCase):
     
     @classmethod
     def setUpClass(cls):
-        np.random.seed(4)
+        seed(4)
 
     def setUp(self):
-        self.es.time = 0.0
-        self.es.part.clear()
-        print("\n")
-
-    def generate_scalar_ranged_rnd(self, min_par, max_par):
-        """
-        Generate the scaled random scalar in the range between
-        min_par*max_par and (min_par+1.0)*max_par.
-    
-        Parameters
-        ----------
-        min_par : :obj:`int`
-                  Minimal value parameter.
-        max_par : :obj:`int`
-                  Maximal value parameter.
-
-        """
-
-        res = (min_par + np.random.random()) * max_par
-        return res
-
-    def generate_vec_ranged_rnd(self, min_par, max_par):
-        """
-        Generate the scaled random 3D vector with a magnitude
-        in the range between sqrt(3)*min_par*max_par and
-        sqrt(3)*(min_par+1.0)*max_par.
-    
-        Parameters
-        ----------
-        min_par : :obj:`int`
-                  Minimal value parameter.
-        max_par : :obj:`int`
-                  Maximal value parameter.
-
-        """
-
-        res = (min_par + np.random.random(3)) * max_par
-        return res
+        self.system.time = 0.0
+        self.system.part.clear()
 
     def rot_diffusion_param_setup(self,n):
         """
@@ -83,17 +47,17 @@ class RotDiffAniso(ut.TestCase):
 
         ## Time
         # The time step should be less than t0 ~ mass / gamma
-        self.es.time_step = 3E-3
+        self.system.time_step = 3E-3
         
         ## Space
         box = 10.0
-        self.es.box_l = box,box,box
+        self.system.box_l = box,box,box
         if espressomd.has_features(("PARTIAL_PERIODIC",)):
-            self.es.periodicity = 0,0,0
+            self.system.periodicity = 0,0,0
         
         ## NVT thermostat
         # Just some temperature range to cover by the test:
-        self.kT = self.generate_scalar_ranged_rnd(0.3,5)
+        self.kT = uniform(1.5, 6.5)
         # Note: here & hereinafter specific variations in the random parameter ranges are related to
         # the test execution duration to achieve the required statistical averages faster.
         # The friction gamma_global should be large enough in order to have the small enough D ~ kT / gamma and
@@ -104,7 +68,7 @@ class RotDiffAniso(ut.TestCase):
         # tests which are based on the partial differential equation (eq. (68), Perrin1934) leading only to the simple
         # classical Einstein-Smoluchowski equations of the diffusion
         # in a contrast of the eq. (10.2.26) [N. Pottier, https://doi.org/10.1007/s10955-010-0114-6 (2010)].
-        self.gamma_global = 1E2 * self.generate_vec_ranged_rnd(0.5,0.7)
+        self.gamma_global = 1E2 * uniform(0.35, 1.05, (3))
         
         ## Particles
         # As far as the problem characteristic time is t0 ~ J / gamma
@@ -114,13 +78,13 @@ class RotDiffAniso(ut.TestCase):
         # Also, it is expected to test the large enough J.
         # It should be not very large, otherwise the thermalization will require
         # too much of the CPU time: the in silico time should clock over the t0.
-        self.J = self.generate_vec_ranged_rnd(0.1,15.)
+        self.J = uniform(1.5, 16.5, (3))
         for ind in range(n):
             part_pos = np.random.random(3) * box
-            self.es.part.add(rotation=(1,1,1), id=ind, rinertia=self.J,
+            self.system.part.add(rotation=(1,1,1), id=ind, rinertia=self.J,
                                 pos=part_pos)
             if "ROTATION" in espressomd.features():
-                self.es.part[ind].omega_body = 0.0,0.0,0.0
+                self.system.part[ind].omega_body = 0.0,0.0,0.0
 
     def check_rot_diffusion(self,n):
         """
@@ -132,7 +96,7 @@ class RotDiffAniso(ut.TestCase):
         Parameters
         ----------
         n : :obj:`int`
-            Number of particles of the each type. There are 2 types.
+            Number of particles.
 
         """
         # Global diffusivity tensor in the body frame:
@@ -140,15 +104,15 @@ class RotDiffAniso(ut.TestCase):
         dt0 = self.J / self.gamma_global
 
         # Thermalizing...
-        therm_steps = int(100)
-        self.es.integrator.run(therm_steps)
+        therm_steps = 100
+        self.system.integrator.run(therm_steps)
 
         # Measuring...
         # Set the initial conditions according to the [Perrin1936], p.3.
         # The body angular velocity is rotated now, but there is
         # only the thermal velocity, hence, this does not impact the test and its physical context.
         for ind in range(n):
-            self.es.part[ind].quat = 1.0, 0.0, 0.0, 0.0
+            self.system.part[ind].quat = 1.0, 0.0, 0.0, 0.0
         ## Average direction cosines
         # Diagonal ones:
         dcosjj_validate = np.zeros((3))
@@ -165,11 +129,11 @@ class RotDiffAniso(ut.TestCase):
         dcosij2_validate = np.ones((3,3))
         dcosij2_dev = np.zeros((3,3))
         
-        self.es.time = 0.0
+        self.system.time = 0.0
         int_steps = 10
         loops = 100
         for step in range(loops):
-            self.es.integrator.run(steps = int_steps)
+            self.system.integrator.run(steps = int_steps)
             dcosjj = np.zeros((3))
             dcosjj2 = np.zeros((3))
             dcosijpp = np.zeros((3,3))
@@ -177,7 +141,7 @@ class RotDiffAniso(ut.TestCase):
             dcosij2 = np.zeros((3,3))
             for ind in range(n):
                 # Just a direction cosines functions averaging..
-                dir_cos = tc.rotation_matrix_quat(self.es, ind)
+                dir_cos = tests_common.rotation_matrix_quat(self.system, ind)
                 for j in range(3):
                     # the LHS of eq. (23) [Perrin1936].
                     dcosjj[j] += dir_cos[j,j]
@@ -204,21 +168,21 @@ class RotDiffAniso(ut.TestCase):
             min_value = 0.14
             
             # Eq. (23) [Perrin1936].
-            dcosjj_validate[0] = np.exp(-(D[1] + D[2]) * self.es.time)
-            dcosjj_validate[1] = np.exp(-(D[0] + D[2]) * self.es.time)
-            dcosjj_validate[2] = np.exp(-(D[0] + D[1]) * self.es.time)
+            dcosjj_validate[0] = np.exp(-(D[1] + D[2]) * self.system.time)
+            dcosjj_validate[1] = np.exp(-(D[0] + D[2]) * self.system.time)
+            dcosjj_validate[2] = np.exp(-(D[0] + D[1]) * self.system.time)
             dcosjj_dev = np.absolute(dcosjj - dcosjj_validate) / dcosjj_validate
             for j in range(3):
                 if np.absolute(dcosjj_validate[j]) < min_value:
                     dcosjj_dev[j] = 0.0
             
             # Eq. (24) [Perrin1936].
-            dcosijpp_validate[0,1] = np.exp(-(4 * D[2] + D[1] + D[0]) * self.es.time)
-            dcosijpp_validate[1,0] = np.exp(-(4 * D[2] + D[1] + D[0]) * self.es.time)
-            dcosijpp_validate[0,2] = np.exp(-(4 * D[1] + D[2] + D[0]) * self.es.time)
-            dcosijpp_validate[2,0] = np.exp(-(4 * D[1] + D[2] + D[0]) * self.es.time)
-            dcosijpp_validate[1,2] = np.exp(-(4 * D[0] + D[2] + D[1]) * self.es.time)
-            dcosijpp_validate[2,1] = np.exp(-(4 * D[0] + D[2] + D[1]) * self.es.time)
+            dcosijpp_validate[0,1] = np.exp(-(4 * D[2] + D[1] + D[0]) * self.system.time)
+            dcosijpp_validate[1,0] = np.exp(-(4 * D[2] + D[1] + D[0]) * self.system.time)
+            dcosijpp_validate[0,2] = np.exp(-(4 * D[1] + D[2] + D[0]) * self.system.time)
+            dcosijpp_validate[2,0] = np.exp(-(4 * D[1] + D[2] + D[0]) * self.system.time)
+            dcosijpp_validate[1,2] = np.exp(-(4 * D[0] + D[2] + D[1]) * self.system.time)
+            dcosijpp_validate[2,1] = np.exp(-(4 * D[0] + D[2] + D[1]) * self.system.time)
             dcosijpp_dev = np.absolute(dcosijpp - dcosijpp_validate) / dcosijpp_validate
             for i in range(3):
                 for j in range(3):
@@ -226,12 +190,12 @@ class RotDiffAniso(ut.TestCase):
                         dcosijpp_dev[i,j] = 0.0
             
             # Eq. (25) [Perrin1936].
-            dcosijnn_validate[0,1] = np.exp(-(D[1] + D[0]) * self.es.time)
-            dcosijnn_validate[1,0] = np.exp(-(D[1] + D[0]) * self.es.time)
-            dcosijnn_validate[0,2] = np.exp(-(D[2] + D[0]) * self.es.time)
-            dcosijnn_validate[2,0] = np.exp(-(D[2] + D[0]) * self.es.time)
-            dcosijnn_validate[1,2] = np.exp(-(D[2] + D[1]) * self.es.time)
-            dcosijnn_validate[2,1] = np.exp(-(D[2] + D[1]) * self.es.time)
+            dcosijnn_validate[0,1] = np.exp(-(D[1] + D[0]) * self.system.time)
+            dcosijnn_validate[1,0] = np.exp(-(D[1] + D[0]) * self.system.time)
+            dcosijnn_validate[0,2] = np.exp(-(D[2] + D[0]) * self.system.time)
+            dcosijnn_validate[2,0] = np.exp(-(D[2] + D[0]) * self.system.time)
+            dcosijnn_validate[1,2] = np.exp(-(D[2] + D[1]) * self.system.time)
+            dcosijnn_validate[2,1] = np.exp(-(D[2] + D[1]) * self.system.time)
             dcosijnn_dev = np.absolute(dcosijnn - dcosijnn_validate) / dcosijnn_validate
             for i in range(3):
                 for j in range(3):
@@ -248,9 +212,9 @@ class RotDiffAniso(ut.TestCase):
             D1D1 /= 6.0
             # Eq. (32) [Perrin1936].
             dcosjj2_validate = 1./3. + (1./3.) * (1. + (D-D0)/(2.*np.sqrt(D0**2-D1D1))) \
-                               * np.exp(-6.*(D0-np.sqrt(D0**2-D1D1))*self.es.time) \
+                               * np.exp(-6.*(D0-np.sqrt(D0**2-D1D1))*self.system.time) \
                                      + (1./3.) * (1. - (D-D0)/(2.*np.sqrt(D0**2-D1D1))) \
-                               * np.exp(-6.*(D0+np.sqrt(D0**2-D1D1))*self.es.time)
+                               * np.exp(-6.*(D0+np.sqrt(D0**2-D1D1))*self.system.time)
             dcosjj2_dev = np.absolute(dcosjj2 - dcosjj2_validate) / dcosjj2_validate
             for j in range(3):
                 if np.absolute(dcosjj2_validate[j]) < min_value:
@@ -258,21 +222,21 @@ class RotDiffAniso(ut.TestCase):
             
             # Eq. (33) [Perrin1936].
             dcosij2_validate[0,1] = 1./3. - (1./6.) * (1. - (D[2]-D0)/(2.*np.sqrt(D0**2-D1D1))) \
-                               * np.exp(-6.*(D0-np.sqrt(D0**2-D1D1))*self.es.time) \
+                               * np.exp(-6.*(D0-np.sqrt(D0**2-D1D1))*self.system.time) \
                                           - (1./6.) * (1. + (D[2]-D0)/(2.*np.sqrt(D0**2-D1D1))) \
-                               * np.exp(-6.*(D0+np.sqrt(D0**2-D1D1))*self.es.time)
+                               * np.exp(-6.*(D0+np.sqrt(D0**2-D1D1))*self.system.time)
             dcosij2_validate[1,0] = dcosij2_validate[0,1]
             
             dcosij2_validate[0,2] = 1./3. - (1./6.) * (1. - (D[1]-D0)/(2.*np.sqrt(D0**2-D1D1))) \
-                               * np.exp(-6.*(D0-np.sqrt(D0**2-D1D1))*self.es.time) \
+                               * np.exp(-6.*(D0-np.sqrt(D0**2-D1D1))*self.system.time) \
                                           - (1./6.) * (1. + (D[1]-D0)/(2.*np.sqrt(D0**2-D1D1))) \
-                               * np.exp(-6.*(D0+np.sqrt(D0**2-D1D1))*self.es.time)
+                               * np.exp(-6.*(D0+np.sqrt(D0**2-D1D1))*self.system.time)
             dcosij2_validate[2,0] = dcosij2_validate[0,2]
             
             dcosij2_validate[1,2] = 1./3. - (1./6.) * (1. - (D[0]-D0)/(2.*np.sqrt(D0**2-D1D1))) \
-                               * np.exp(-6.*(D0-np.sqrt(D0**2-D1D1))*self.es.time) \
+                               * np.exp(-6.*(D0-np.sqrt(D0**2-D1D1))*self.system.time) \
                                           - (1./6.) * (1. + (D[0]-D0)/(2.*np.sqrt(D0**2-D1D1))) \
-                               * np.exp(-6.*(D0+np.sqrt(D0**2-D1D1))*self.es.time)
+                               * np.exp(-6.*(D0+np.sqrt(D0**2-D1D1))*self.system.time)
             dcosij2_validate[2,1] = dcosij2_validate[1,2]
             dcosij2_dev = np.absolute(dcosij2 - dcosij2_validate) / dcosij2_validate
             for i in range(3):
@@ -317,10 +281,9 @@ class RotDiffAniso(ut.TestCase):
     def test_case_00(self):
         n = 400
         self.rot_diffusion_param_setup(n)
-        self.es.thermostat.set_langevin(kT=self.kT, gamma=self.gamma_global)
+        self.system.thermostat.set_langevin(kT=self.kT, gamma=self.gamma_global)
         # Actual integration and validation run
         self.check_rot_diffusion(n)
 
 if __name__ == '__main__':
-    print("Features: ", espressomd.features())
     ut.main()
