d8d0176c796e018169c5ed0271e0d130326035f8
Florian Weik <fweik@icp.uni-stuttgart.de>
2018-08-16 14:44:46 +0200

    core: constraints: Some docstrings on Constraints::Contraint.

--- src/core/constraints/Constraint.hpp
+++ src/core/constraints/Constraint.hpp
@@ -9,18 +9,26 @@
 namespace Constraints {
 class Constraint {
 public:
+  /**
+   * @brief Add energy contribution of this constraints to energy.
+   */
   virtual void add_energy(const Particle &p, const Vector3d &folded_pos,
                           Observable_stat &energy) const = 0;
+  /**
+   * @brief Return constraint force on particle.
+   */
   virtual ParticleForce force(const Particle &p,
                               const Vector3d &folded_pos) = 0;
 
+  /**
+   * @brief Check if constraints if compatible with box size.
+   */
   virtual bool fits_in_box(Vector3d const &box) const = 0;
 
   virtual void reset_force(){};
 
-  virtual ~Constraint() {}
+  virtual ~Constraint() = default;
 };
-
 } /* namespace Constraints */
 
 #endif
