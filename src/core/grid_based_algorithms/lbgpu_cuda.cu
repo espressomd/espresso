1e5894eee62b8512261675d8d240c23d43e66386
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-09-07 14:05:22 +0200

    lb: call apply_force independent on particles exist.

--- src/core/grid_based_algorithms/lbgpu_cuda.cu
+++ src/core/grid_based_algorithms/lbgpu_cuda.cu
@@ -3559,8 +3559,7 @@ __global__ void integrate(LB_nodes_gpu n_a, LB_nodes_gpu n_b, LB_rho_v_gpu *d_v,
     if (para.fluct) {
       thermalize_modes(mode, index, &rng);
     }
-    /**if external force is used apply node force */
-    if (para.number_of_particles) apply_forces(index, mode, node_f, d_v);
+    apply_forces(index, mode, node_f, d_v);
     /**lb_calc_n_from_modes_push*/
     normalize_modes(mode);
     /**calc of velocity densities and streaming with pbc*/
