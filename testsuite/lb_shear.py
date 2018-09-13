af9b5d720f27f10d138a894d28d64703de35e837
Florian Weik <florianweik@gmail.com>
2018-09-13 22:34:52 +0200

    testsuite: Test LB shear flow against time dependent exact solution.

--- testsuite/lb_shear.py
+++ testsuite/lb_shear.py
@@ -21,19 +21,21 @@ import espressomd.lb
 import espressomd.lbboundaries
 import espressomd.shapes
 
-
 """
-Check the Lattice Boltzmann 'pressure' driven flow in a slab system
+Check the Lattice Boltzmann lid driven shear flow in a slab system
 by comparing to the analytical solution.
 
 
 """
 
 
-AGRID = .25
-VISC = .7
-DENS = 1.7
-TIME_STEP = 0.1
+AGRID = 0.5
+VISC = 1.4
+DENS = 2.3
+TIME_STEP = 0.01
+H = 30.
+SHEAR_RATE = 0.3
+
 LB_PARAMS = {'agrid': AGRID,
              'dens': DENS,
              'visc': VISC,
@@ -41,18 +43,46 @@ LB_PARAMS = {'agrid': AGRID,
              'tau': TIME_STEP}
 
 
+def shear_flow(x, t, nu, v, h, k_max):
+    """
+    Analytical solution for driven shear flow between two plates.
+
+    Parameters
+    ----------
+    x : :obj:`float`
+        Position from the left plane.
+    nu : :obj:`float`
+        Kinematic viscosity.
+    v : :obj:`float`
+        Shear rate.
+    h : :obj:`float`
+        Distance between the plates.
+    k_max: :obj:`int`
+        Maximum considered wave number.
+
+    """
+
+    u = x / h - 0.5
+    for k in np.arange(1, k_max + 1):
+        u += 1.0 / (np.pi * k) * np.exp(
+            -4 * np.pi ** 2 * nu * k ** 2 / h ** 2 * t) * np.sin(2 * np.pi / h * k * x)
+    return v * u
+
+
 class LBShearCommon(object):
 
     """Base class of the test that holds the test logic."""
     lbf = None
-    system = espressomd.System(box_l=[12.0, 3.0, 3.0])
+    system = espressomd.System(box_l=[H + 2. * AGRID,
+                                      3.0,
+                                      3.0])
     system.time_step = TIME_STEP
     system.cell_system.skin = 0.4 * AGRID
 
-    def prepare(self):
+    def test_profile(self):
         """
-        Integrate the LB fluid until steady state is reached within a certain
-        accuracy.
+        Integrate the LB fluid and regularly compare with
+        the exact solution.
 
         """
         self.system.actors.clear()
@@ -61,47 +91,33 @@ class LBShearCommon(object):
         wall_shape2 = espressomd.shapes.Wall(
             normal=[-1, 0, 0], dist=-(self.system.box_l[0] - AGRID))
         wall1 = espressomd.lbboundaries.LBBoundary(
-            shape=wall_shape1, velocity=[0, 0, +1])
+            shape=wall_shape1, velocity=[0, 0, -0.5 * SHEAR_RATE])
         wall2 = espressomd.lbboundaries.LBBoundary(
-            shape=wall_shape2, velocity=[0, 0, -1])
+            shape=wall_shape2, velocity=[0, 0, +0.5 * SHEAR_RATE])
 
         self.system.lbboundaries.add(wall1)
         self.system.lbboundaries.add(wall2)
 
-        quater_indices = [int((self.system.box_l[0] / AGRID) / 4),
-                          int((self.system.box_l[1] / AGRID) / 4),
-                          int((self.system.box_l[2] / AGRID) / 4)]
-        diff = float("inf")
-        old_val = self.lbf[quater_indices].velocity[2]
-        while diff > 0.01:
+        sample_points = int(H / AGRID)
+
+        for i in range(10):
             self.system.integrator.run(100)
-            new_val = self.lbf[quater_indices].velocity[2]
-            diff = abs(new_val - old_val)
-            old_val = new_val
 
-    def test_profile(self):
-        """
-        Compare against analytical function by calculating the RMSD.
+            v_measured = np.zeros(sample_points)
+            x = np.zeros(sample_points)
+            for j in range(1, sample_points + 1):
+                v_measured[j - 1] = self.lbf[j, 1, 1].velocity[2]
+                x[j - 1] = (j - 1 + 0.5) * AGRID
 
-        """
-        self.prepare()
-        velocities = np.zeros((int(self.system.box_l[0] / AGRID), 2))
+            v_expected = shear_flow(x=x,
+                                    t=self.system.time,
+                                    nu=VISC,
+                                    v=SHEAR_RATE,
+                                    h=H,
+                                    k_max=100)
 
-        for x in range(velocities.shape[0]):
-            v_tmp = []
-            for y in range(int(self.system.box_l[1] + 1)):
-                for z in range(int(self.system.box_l[2] + 1)):
-                    v_tmp.append(self.lbf[x, y, z].velocity[2])
-            velocities[x, 1] = np.mean(np.array(v_tmp))
-            velocities[x, 0] = (x + 0.5) * AGRID
-
-        x = velocities[1:-1, 0]
-        v_measured = velocities[1:-1, 1]
-        H = self.system.box_l[0] - 2.0 * AGRID
-        v_expected = (1. - 2. * (x - AGRID) / H)
-
-        rmsd = np.sqrt(np.sum(np.square(v_expected - v_measured)))
-        self.assertLess(rmsd, 0.02 * AGRID / TIME_STEP)
+            rmsd = np.sqrt(np.sum(np.square(v_expected - v_measured)))
+            self.assertLess(rmsd, 1.e-4 * AGRID / TIME_STEP)
 
 
 @ut.skipIf(not espressomd.has_features(
