49c7a498b69e00b27ffb345630556d9db2b21cfa
Sebastian Bindgen <info@bindgen.net>
2018-09-24 17:40:06 +0200

    Restructuring of the testcase and addition of equal criterium to the LE part of the integration loop. The latter still doesn't work properly

--- testsuite/lees_edwards.py
+++ testsuite/lees_edwards.py
@@ -17,7 +17,7 @@ class LeesEdwards(ut.TestCase):
   time_step = 1.0
   system.time_step = time_step
 
-  def test_Protocol(self):
+  def test_a_Protocol(self):
     """This test calculates if the offset and velocity of the Lees Edwards
     function are calculated correctly based on the input variables"""
 
@@ -40,158 +40,59 @@ class LeesEdwards(ut.TestCase):
     self.assertEqual(system.lees_edwards[1], 1.2)
     self.assertEqual(system.lees_edwards[2], 5.6)
 
-    system.time = 5.0
+    system.time = 0.0
 
     #Check if the offset is determined correctly
     frequency = 3.7
     omega = 2*np.pi*frequency
     amplitude = 1.6
-    system.lees_edwards = ["oscillatory_shear", frequency, amplitude]
+    system.lees_edwards = ["oscillatory_shear", omega, amplitude]
     self.assertEqual(system.lees_edwards[0], "oscillatory_shear")
-    for time in np.arange(0.0, 100.0, 10.0):
-      offset = amplitude * np.sin(frequency * time)
+    for time in np.arange(10., 100.0, 10.0):
+      system.integrator.run(10)
+      offset = amplitude * np.sin(omega * time)
       velocity = omega * amplitude * np.cos(omega * time)
 
-      np.testing.assert_equal(system.lees_edwards[3], velocity)
-      np.testing.assert_equal(system.lees_edwards[4], offset)
-      system.integrator.run(10)
+      np.testing.assert_almost_equal(system.lees_edwards[3], velocity)
+      np.testing.assert_almost_equal(system.lees_edwards[4], offset)
 
-  def test_BoundaryCrossing(self):
-    """The test calculates a particle's position for different lees_edwards_offsets
-    while crossing the upper and lower boundary in the y-direction. The
-    obtained positions as well as the velocity of the particle is checked via a 
-    comparison with the expected value."""
+  def test_b_BoundaryCrossing(self):
+    """A particle crosses the upper and lower boundary to test if position
+    and velocity are updated correctly."""
 
     system = self.system
     tol = self.tol
+    system.time = 0.0
 
-    # Check if coordinates are folded correctly
+    # Set up a one particle system and check the position offset after crossing the boundary
+    # Test for upper boundary
     system.lees_edwards = ["steady_shear", 0.1]
+    system.part.add(pos=[2.5, 4.9, 2.5], v=[0.0, 0.1, 0.0], id=0, type=0)
+
+    system.integrator.run(2)
+
+    expected_pos = [2.4, 5.1, 2.5]
+    expected_vel = [-0.1, 0.1, 0.0]
     
-    # Particle placement
-    system.part.add(pos=[2.5, 6.0, 2.5], v=[0.05, 0.1, 0.05], id=0, type=0)
-    
-    pos_difference_x = system.part[0].pos_folded[0] - 2.4
-    pos_difference_y = system.part[0].pos_folded[1] - 1.0
-    pos_difference_z = system.part[0].pos_folded[2] - 2.5
-    vel_difference_x = system.part[0].v[0] - 0.05
-    vel_difference_y = system.part[0].v[1] - 0.1
-    vel_difference_z = system.part[0].v[2] - 0.05
-    
-    self.assertLess(pos_difference_x, tol)
-    self.assertLess(pos_difference_y, tol)
-    self.assertLess(pos_difference_z, tol)
-    self.assertLess(vel_difference_x, tol)
-    self.assertLess(vel_difference_y, tol)
-    self.assertLess(vel_difference_z, tol)
+    np.testing.assert_almost_equal(system.part[0].pos, expected_pos)
+    np.testing.assert_almost_equal(system.part[0].v, expected_vel)
 
     system.part.clear()
-    system.lees_edwards_offset = 0.0
 
-    # Check position offset in x-direction while crossing upper boundary
-    # Particle placement
-    system.part.add(pos=[2.5, 4.8, 2.5], v=[0.0, 0.1, 0.0], id=0, type=0)
+    # Test for lower boundary
     
-    # Integration
-    for i in range(2):
-      system.integrator.run(1)
-      system.lees_edwards_offset = 0.1
-      pos_difference = system.part[0].pos_folded[0] - 2.5
-      vel_difference = system.part[0].v[1] - 0.1
+    system.time = 0.0
+    system.part.add(pos=[2.5, 0.1, 2.5], v=[0.0, -0.1, 0.0], id=0, type=0)
 
-      self.assertLess(pos_difference, tol)
-      self.assertLess(vel_difference, tol)
+    system.integrator.run()
 
-    for i in range(3):
-      system.integrator.run(1)
-      system.lees_edwards_offset = 0.1
-      pos_difference = system.part[0].pos_folded[0] - 2.4
-      vel_difference = system.part[0].v[1] - 0.1
-   
-      self.assertLess(pos_difference, tol)
-      self.assertLess(vel_difference, tol)
+    expected_pos = [2.6, -0.1, 2.5]
+    expected_vel = [0.1, -0.1, 0.0]
+    
+    print(system.part[0])
 
-    system.part.clear()
-    system.lees_edwards_offset = 0.0
-    
-    # Check position offset in x-direction while crossing lower boundary
-    # Particle placement
-    system.part.add(pos=[2.5, 0.2, 2.5], v=[0.0, -0.1, 0.0], id=0, type=0)
-    
-    # Integration
-    for i in range(2):
-      system.integrator.run(1)
-      system.lees_edwards_offset = 0.1
-      pos_difference = system.part[0].pos_folded[0] - 2.5
-      vel_difference = system.part[0].v[1] - -0.1
-      
-      self.assertLess(pos_difference, tol)
-      self.assertLess(vel_difference, tol)
-    
-    for i in range(3):
-      system.integrator.run(1)
-      system.lees_edwards_offset = 0.1
-      pos_difference = system.part[0].pos_folded[0] - 2.6
-      vel_difference = system.part[0].v[1] - -0.1
+    np.testing.assert_almost_equal(system.part[0].pos, expected_pos)
+    np.testing.assert_almost_equal(system.part[0].v, expected_vel)
 
-      self.assertLess(pos_difference, tol)
-      self.assertLess(vel_difference, tol)
-
-    system.part.clear()
-    system.lees_edwards_offset = 0.0
-    
-    # Check velocity offset with constant shear rate while crossing upper
-    # boundary
-    # Particle placement
-    system.part.add(pos=[2.5, 4.8, 2.5], v=[0.0, 0.1, 0.0], id=0, type=0)
-    
-    # Integration
-    for i in range(2):
-      system.integrator.run(1)
-      system.lees_edwards_offset += 0.1
-      pos_difference = system.part[0].pos_folded[0] - 2.5
-      vel_difference = system.part[0].v[0] + 0.0
-      
-      self.assertLess(pos_difference, tol)
-      self.assertLess(vel_difference, tol)
-    
-    for i in range(3, 6):
-      system.integrator.run(1)
-      system.lees_edwards_offset += 0.1
-      pos_difference = system.part[0].pos_folded[0] - 2.6 + i * 0.1
-      vel_difference = system.part[0].v[0] + 0.1
-      
-      self.assertLess(pos_difference, tol)
-      self.assertLess(vel_difference, tol)
-    
-    system.part.clear()
-    system.lees_edwards_offset = 0.0
-    
-    # Check position offset in x-direction while crossing lower boundary
-    # Particle placement
-    system.part.add(pos=[2.5, 0.2, 2.5], v=[0.0, -0.1, 0.0], id=0, type=0)
-    
-    # Integration
-    for i in range(2):
-      system.integrator.run(1)
-      system.lees_edwards_offset += 0.1
-      pos_difference = system.part[0].pos_folded[0] - 2.5
-      vel_difference = system.part[0].v[0] - 0.0
-      
-      self.assertLess(pos_difference, tol)
-      self.assertLess(vel_difference, tol)
-    
-    for i in range(3, 6):
-      system.integrator.run(1)
-      system.lees_edwards_offset += 0.1
-      pos_difference = system.part[0].pos_folded[0] - 2.4 - i * 0.1
-      vel_difference = system.part[0].v[0] - 0.1
-      
-      self.assertLess(pos_difference, tol)
-      self.assertLess(vel_difference, tol)
-    
-    system.part.clear()
-    system.lees_edwards_offset = 0.0
-    
 if __name__ == "__main__":
   ut.main()
