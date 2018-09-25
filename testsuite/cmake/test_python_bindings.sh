e1fb96d2a9a5de76452af603fb9348dbf5b1c181
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-09-25 15:35:03 +0200

    cmake: call check of installation with pypresso

--- testsuite/cmake/test_python_bindings.sh
+++ testsuite/cmake/test_python_bindings.sh
@@ -6,11 +6,11 @@ source BashUnitTests.sh
 # test if Python module can be imported
 function test_Python() {
   # test espresso installation via `make install DESTDIR=/some/dir`
-  assert_return_code "@PYTHON_EXECUTABLE@" -c "import sys;sys.path.insert(0, '@DESTDIR@/@CMAKE_INSTALL_PREFIX@/@Python_SITEARCH@');import espressomd"
+  assert_return_code "@CMAKE_BINARY_DIR@/pypresso" -c "import sys;sys.path.insert(0, '@DESTDIR@/@CMAKE_INSTALL_PREFIX@/@Python_SITEARCH@');import espressomd"
   # test espresso installation via `cmake -DCMAKE_INSTALL_PREFIX=/some/dir ..`
   if [ "@CMAKE_INSTALL_PREFIX@" = "/tmp/espresso-unit-tests" ]
   then
-    assert_return_code "@PYTHON_EXECUTABLE@" -c "import sys;sys.path.insert(0, '@CMAKE_INSTALL_PREFIX@/@Python_SITEARCH@');import espressomd"
+    assert_return_code "@CMAKE_BINARY_DIR@/pypresso" -c "import sys;sys.path.insert(0, '@CMAKE_INSTALL_PREFIX@/@Python_SITEARCH@');import espressomd"
   fi
 }
 
