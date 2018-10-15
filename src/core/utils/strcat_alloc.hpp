d088b94c45edafe1bb5564cc4e879d080a77fe76
Florian Weik <fweik@icp.uni-stuttgart.de>
2018-10-15 09:26:13 +0200

    utils: Fixed strcat_alloc

--- src/core/utils/strcat_alloc.hpp
+++ src/core/utils/strcat_alloc.hpp
@@ -38,8 +38,8 @@ inline char *strcat_alloc(char *left, const char *right) {
   if (!left) {
     return strdup(right);
   } else {
-    size_t newlen = strlen(left) + strlen(right) + 1;
-    char *res = Utils::realloc(left, newlen);
+    size_t newlen = strlen(left) + strlen(right);
+    char *res = Utils::realloc(left, newlen + 1);
     strncat(res, right, newlen);
     return res;
   }
