4320adc20c61b33716018e377617c4eb8896211f
Michael Kuron <mkuron@users.noreply.github.com>
2018-11-01 10:40:36 +0100

    curand_Philox4x32_10 for AMD

--- src/core/curand_wrapper.hpp
+++ src/core/curand_wrapper.hpp
@@ -1,13 +1,31 @@
 #ifndef CURAND_WRAPPER_HPP
 #define CURAND_WRAPPER_HPP
 
-#if defined(__HIPCC__) // AMD or Nvidia via HIP
-
-#include <hiprand/hiprand_kernel.h>
-
-#else
+#if defined(__CUDACC__)
 
 #include <curand_kernel.h>
 
+#elif defined(__HIPCC__)
+
+#include <hiprand/hiprand_kernel.h>
+
+class philox4x32_10_stateless : private rocrand_device::philox4x32_10_engine {
+public:
+  FQUALIFIERS
+  philox4x32_10_stateless() {}
+
+  FQUALIFIERS
+  uint4 operator()(uint4 counter, uint2 key) {
+    return ten_rounds(counter, key);
+  }
+};
+
+__forceinline__ __device__ uint4 curand_Philox4x32_10(uint4 counter,
+                                                      uint2 key) {
+  philox4x32_10_stateless *e;
+  return (*e)(counter, key);
+}
+
 #endif
-#endif
+
+#endif // CURAND_WRAPPER_HPP
