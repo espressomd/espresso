afcc25fb0efbcd4f6f5cb9ffa1d0bff5044a0369
Sebastian Bindgen <info@bindgen.net>
2018-11-09 16:42:50 +0100

    Experimental changes to Integration loop

--- src/core/integrate.cpp
+++ src/core/integrate.cpp
@@ -680,9 +680,9 @@ void propagate_pos() {
 void propagate_vel_pos() {
   INTEG_TRACE(fprintf(stderr, "%d: propagate_vel_pos:\n", this_node));
 #ifdef LEES_EDWARDS
-double shear_velocity = lees_edwards_get_velocity(sim_time + time_step / 2);
+double shear_velocity = lees_edwards_get_velocity(sim_time + time_step);
 double offset_at_half_time_step =
-          lees_edwards_get_offset(sim_time + time_step / 2);
+          lees_edwards_get_offset(sim_time + time_step);
 #endif
 #ifdef ADDITIONAL_CHECKS
   db_max_force = db_max_vel = 0;
@@ -729,13 +729,11 @@ double offset_at_half_time_step =
       // The update of the velocity at the end of the time step is triggered by
       // the flag and occurs in propagate_vel_finalize_p_inst
       if (p.r.p[lees_edwards_protocol.shearplanenormal] >= box_l[lees_edwards_protocol.shearplanenormal]) {
-        p.m.v[lees_edwards_protocol.sheardir] -= shear_velocity / 2;
         p.r.p[lees_edwards_protocol.sheardir] -= (offset_at_half_time_step -
                      dround(offset_at_half_time_step * box_l_i[lees_edwards_protocol.sheardir]) * box_l[lees_edwards_protocol.sheardir]);
         p.p.lees_edwards_flag = -1; // perform a negative half velocity shift in
                                     // propagate_vel_finalize_p_inst
       } else if (p.r.p[lees_edwards_protocol.shearplanenormal] <= 0.) {
-        p.m.v[lees_edwards_protocol.sheardir] += shear_velocity / 2;
         p.r.p[lees_edwards_protocol.sheardir] += (offset_at_half_time_step -
                      dround(offset_at_half_time_step * box_l_i[lees_edwards_protocol.sheardir]) * box_l[lees_edwards_protocol.sheardir]);
         p.p.lees_edwards_flag = 1; // perform a positive half velocity shift in
