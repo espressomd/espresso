d1641d41228c94f965f24bb0cd67f02dcfb31a55
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-11-23 09:03:05 +0100

    unit_test: fix static assert.

--- src/core/unit_tests/u32_to_u64_test.cpp
+++ src/core/unit_tests/u32_to_u64_test.cpp
@@ -32,5 +32,5 @@ BOOST_AUTO_TEST_CASE(u64_to_u32) {
   constexpr const uint64_t val = (expected_high << 32) | (expected_low);
 
   static_assert(std::pair<uint32_t, uint32_t>{expected_high, expected_low} ==
-                Utils::u64_to_u32(val));
+                Utils::u64_to_u32(val), "");
 }
