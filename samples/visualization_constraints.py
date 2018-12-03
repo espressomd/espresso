c96f8170b33205e80e06fbdda0e967bf65499698
Patrick Kreissl <patrick.kreissl@pa-le.de>
2018-12-03 14:35:27 +0100

    Add # nopep8 to ignore where appropriate.

--- samples/visualization_constraints.py
+++ samples/visualization_constraints.py
@@ -40,44 +40,44 @@ visualizer = espressomd.visualization_opengl.openGLLive(
     rasterize_pointsize=5, camera_position=[150, 25, 25], camera_right=[0, 0, -1])
 
 # Wall
-#system.constraints.add(shape=espressomd.shapes.Wall(
-#    dist=20, normal=[0.1, 0.0, 1]), particle_type=0, penetrable=True)
+#system.constraints.add(shape=espressomd.shapes.Wall(  # nopep8
+#    dist=20, normal=[0.1, 0.0, 1]), particle_type=0, penetrable=True)  # nopep8
 
 # Sphere
-#system.constraints.add(shape=espressomd.shapes.Sphere(
-#    center=[25, 25, 25], radius=15, direction=1), particle_type=0, penetrable=True)
+#system.constraints.add(shape=espressomd.shapes.Sphere(  # nopep8
+#    center=[25, 25, 25], radius=15, direction=1), particle_type=0, penetrable=True)  # nopep8
 
 # Ellipsoid
-#system.constraints.add(shape=espressomd.shapes.Ellipsoid(
-#    center=[25, 25, 25], a=25, b=15, direction=1), particle_type=0, penetrable=True)
+#system.constraints.add(shape=espressomd.shapes.Ellipsoid(  # nopep8
+#    center=[25, 25, 25], a=25, b=15, direction=1), particle_type=0, penetrable=True)  # nopep8
 
 # Cylinder
-#system.constraints.add(shape=espressomd.shapes.Cylinder(center=[25, 25, 25], axis=[
-#                       1, 0, 0], direction=1, radius=10, length=30), particle_type=0, penetrable=True)
+#system.constraints.add(shape=espressomd.shapes.Cylinder(center=[25, 25, 25], axis=[  # nopep8
+#                       1, 0, 0], direction=1, radius=10, length=30), particle_type=0, penetrable=True)  # nopep8
 
 # SpheroCylinder
-#system.constraints.add(shape=espressomd.shapes.SpheroCylinder(center=[25, 25, 25], axis=[
-#                       1, 0, 0], direction=1, radius=10, length=30), particle_type=0, penetrable=True)
+#system.constraints.add(shape=espressomd.shapes.SpheroCylinder(center=[25, 25, 25], axis=[  # nopep8
+#                       1, 0, 0], direction=1, radius=10, length=30), particle_type=0, penetrable=True)  # nopep8
 
 # Maze
-#system.constraints.add(shape=espressomd.shapes.Maze(
-#    cylrad=3, dim=2, nsphere=2, sphrad=8), particle_type=0, penetrable=True)
+#system.constraints.add(shape=espressomd.shapes.Maze(  # nopep8
+#    cylrad=3, dim=2, nsphere=2, sphrad=8), particle_type=0, penetrable=True)  # nopep8
 
 # Stomatocyte
-#system.constraints.add(shape=espressomd.shapes.Stomatocyte(inner_radius=3, outer_radius=7, axis=[
-#                       1.0, 0.0, 0.0], center=[25, 25, 25], layer_width=3, direction=1), particle_type=0, penetrable=True)
+#system.constraints.add(shape=espressomd.shapes.Stomatocyte(inner_radius=3, outer_radius=7, axis=[  # nopep8
+#                       1.0, 0.0, 0.0], center=[25, 25, 25], layer_width=3, direction=1), particle_type=0, penetrable=True)  # nopep8
 
 # SimplePore
-system.constraints.add(shape=espressomd.shapes.SimplePore(center=[25, 25, 25], axis=[
-                       1, 0, 0], length=15, radius=12.5, smoothing_radius=2), particle_type=0, penetrable=True)
+system.constraints.add(shape=espressomd.shapes.SimplePore(center=[25, 25, 25], axis=[  # nopep8
+                       1, 0, 0], length=15, radius=12.5, smoothing_radius=2), particle_type=0, penetrable=True)  # nopep8
 
 # Slitpore
-#system.constraints.add(shape=espressomd.shapes.Slitpore(channel_width=15, lower_smoothing_radius=3,
-#                                                        upper_smoothing_radius=3, pore_length=20, pore_mouth=30, pore_width=5), particle_type=0, penetrable=True)
+#system.constraints.add(shape=espressomd.shapes.Slitpore(channel_width=15, lower_smoothing_radius=3,  # nopep8
+#                                                        upper_smoothing_radius=3, pore_length=20, pore_mouth=30, pore_width=5), particle_type=0, penetrable=True)  # nopep8
 
 # HollowCone
-#system.constraints.add(shape=espressomd.shapes.HollowCone(inner_radius=5, outer_radius=20, opening_angle=np.pi /
-#                                                          4.0, axis=[1.0, 0.0, 0.0], center=[25, 25, 25], width=2, direction=1), particle_type=0, penetrable=True)
+#system.constraints.add(shape=espressomd.shapes.HollowCone(inner_radius=5, outer_radius=20, opening_angle=np.pi /  # nopep8
+#                                                          4.0, axis=[1.0, 0.0, 0.0], center=[25, 25, 25], width=2, direction=1), particle_type=0, penetrable=True)  # nopep8
 
 system.thermostat.set_langevin(kT=10.0, gamma=10)
 
