2b5b8d99d5a720b14a602d434926fee225d96179
Florian Weik <fweik@icp.uni-stuttgart.de>
2018-12-05 21:25:43 +0100

    cmake: Made EspressoConfig into separate target.

--- cmake/MyConfig.cmake
+++ cmake/MyConfig.cmake
@@ -31,13 +31,13 @@ else()
     NO_DEFAULT_PATH)
   # use the default if it is not
   if(NOT MYCONFIG_FILE)
-    set(MYCONFIG_FILE ${CMAKE_SOURCE_DIR}/src/core/myconfig-default.hpp)
+    set(MYCONFIG_FILE ${CMAKE_SOURCE_DIR}/src/config/myconfig-default.hpp)
   endif()
 endif()
 endif()
 
-configure_file(${MYCONFIG_FILE} ${CMAKE_BINARY_DIR}/src/core/myconfig-final.hpp COPYONLY)
-add_custom_target(myconfig DEPENDS ${CMAKE_BINARY_DIR}/src/core/myconfig-final.hpp)
+configure_file(${MYCONFIG_FILE} ${CMAKE_BINARY_DIR}/src/config/myconfig-final.hpp COPYONLY)
+add_custom_target(myconfig DEPENDS ${CMAKE_BINARY_DIR}/src/config/myconfig-final.hpp)
 message(STATUS "Config file: ${MYCONFIG_FILE}")
 # Clear variable, otherwise cmake must be run by hand to detect myconfig
 # Also prevents find_file from skipping when variable is already set
