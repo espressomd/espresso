d48840ddee46d6101dddff5f1bf90df0524c1d1e
Florian Weik <fweik@icp.uni-stuttgart.de>
2018-12-05 23:54:49 +0100

    Fixed regression

--- src/core/electrostatics_magnetostatics/scafacos/Scafacos.hpp
+++ src/core/electrostatics_magnetostatics/scafacos/Scafacos.hpp
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "config.hpp"
 #include <fcs.h>
 #include <list>
 #include <mpi.h>
