59d97e915e8729e81cda50a6fad9518804e84f67
Kai Szuttor <kai@icp.uni-stuttgart.de>
2018-12-12 21:03:18 +0100

    Formatting.

--- src/core/unit_tests/matrix_vector_product.cpp
+++ src/core/unit_tests/matrix_vector_product.cpp
@@ -24,16 +24,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "utils/matrix_vector_product.hpp"
 
-static constexpr std::array<std::array<int, 3>, 3> matrix{{
-    {1, 2, 9},
-    {8, 41, 6},
-    {31, 15, 99}
-}};
+static constexpr std::array<std::array<int, 3>, 3> matrix{
+    {{1, 2, 9}, {8, 41, 6}, {31, 15, 99}}};
 
 BOOST_AUTO_TEST_CASE(inner_product) {
   const std::array<double, 3> vector{{0.5, 1.25, 3.1}};
   auto const result = Utils::matrix_vector_product<double, 3, matrix>(vector);
-  for (int i=0; i<3; ++i) { 
+  for (int i = 0; i < 3; ++i) {
     BOOST_CHECK(result[i] == boost::inner_product(matrix[i], vector, 0.0));
   }
 }
