ea71ea76c5207096d4fcac589a516eed763326aa
Jean-Noel Grad <jgrad@icp.uni-stuttgart.de>
2019-01-17 20:54:17 +0100

    Fix NameError in try/except clause (Python 3)
    In Python 2, "except Exception as e:" stores the exception in
    a global variable "e". In Python 3, "e" is a local variable,
    resulting in a "NameError: name 'e' is not defined" exception
    if "e" is used later. This commit binds "e" to a class attribute.

--- src/python/espressomd/visualization.pyx
+++ src/python/espressomd/visualization.pyx
@@ -8,16 +8,16 @@ try:
         raise ImportError("Cannot connect to X server")
 except ImportError as e:
     class mayaviLive(object):
-
-        def __init__(*args, **kwargs):
-            raise e
+        deferred_ImportError = e
+        def __init__(self, *args, **kwargs):
+            raise self.deferred_ImportError
 
 try:
     from .visualization_opengl import openGLLive
 except ImportError as e:
     class openGLLive(object):
-
-        def __init__(*args, **kwargs):
-            raise e
+        deferred_ImportError = e
+        def __init__(self, *args, **kwargs):
+            raise self.deferred_ImportError
 
 __all__ = ['mayaviLive', 'openGLLive']
