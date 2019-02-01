e8e1af088edd1a8513415b4aa13dfc69644da557
Jean-Noel Grad <jgrad@icp.uni-stuttgart.de>
2019-02-01 17:03:55 +0100

    Separate build from checks in CI pipeline

--- maintainer/CI/build_cmake.sh
+++ maintainer/CI/build_cmake.sh
@@ -66,6 +66,13 @@ function cmd {
 [ -z "$with_cuda" ] && with_cuda="true"
 [ -z "$build_type" ] && build_type="Debug"
 [ -z "$with_ccache" ] && with_ccache="false"
+if [ -z ${CI_JOB_STAGE+x} ]; then
+    stage_build="true"
+    stage_checks="true"
+else
+    [ "${CI_JOB_STAGE}" = "build" ] && stage_build="true" || stage_build="false"
+    [ "${CI_JOB_STAGE}" = "checks" ] && stage_checks="true" || stage_checks="false"
+fi
 
 # If there are no user-provided flags they
 # are added according to with_coverage.
@@ -81,8 +88,10 @@ if [ -z "$cxx_flags" ]; then
     fi
 fi
 
-if [[ ! -z ${with_coverage+x} ]]; then
-  bash <(curl -s https://codecov.io/env) &> /dev/null;
+if $stage_checks; then
+    if $with_coverage; then
+        bash <(curl -s https://codecov.io/env) &> /dev/null;
+    fi
 fi
 
 cmake_params="-DCMAKE_BUILD_TYPE=$build_type -DPYTHON_EXECUTABLE=$(which python$python_version) -DWARNINGS_ARE_ERRORS=ON -DTEST_NP:INT=$check_procs $cmake_params -DWITH_SCAFACOS=ON"
@@ -107,56 +116,58 @@ outp insource srcdir builddir make_check \
     build_procs check_procs \
     python_version with_cuda with_ccache
 
-# check indentation of python files
-pep8_command () {
-    if hash pep8 2> /dev/null; then
-        pep8 "$@"
-    elif hash pycodestyle 2> /dev/null; then
-        pycodestyle "$@"
-    else
-        echo "pep8 not found";
-        exit 1
-    fi
-}
+if $stage_build; then
+    # check indentation of python files
+    pep8_command () {
+        if hash pep8 2> /dev/null; then
+            pep8 "$@"
+        elif hash pycodestyle 2> /dev/null; then
+            pycodestyle "$@"
+        else
+            echo "pep8 not found";
+            exit 1
+        fi
+    }
 
-pep8_command --filename=*.pyx,*.pxd,*.py --select=E111 $srcdir/src/python/espressomd/
-ec=$?
-if [ $ec -eq 0 ]; then
-    echo ""
-    echo "Indentation in Python files correct..."
-    echo ""
-else
-    echo ""
-    echo "Error: Python files are not indented the right way. Please use 4 spaces per indentation level!"
-    echo ""
-    exit $ec
-fi
-# enforce style rules
-pylint_command () {
-    if hash pylint 2> /dev/null; then
-        pylint "$@"
-    elif hash pylint3 2> /dev/null; then
-        pylint3 "$@"
-    elif hash pylint-2 2> /dev/null; then
-        pylint-2 "$@"
-    elif hash pylint-3 2> /dev/null; then
-        pylint-3 "$@"
+    pep8_command --filename=*.pyx,*.pxd,*.py --select=E111 $srcdir/src/python/espressomd/
+    ec=$?
+    if [ $ec -eq 0 ]; then
+        echo ""
+        echo "Indentation in Python files correct..."
+        echo ""
     else
-        echo "pylint not found";
-        exit 1
+        echo ""
+        echo "Error: Python files are not indented the right way. Please use 4 spaces per indentation level!"
+        echo ""
+        exit $ec
     fi
-}
-if [ $(pylint_command --version | grep -o 'pylint.*[0-9]\.[0-9]\.[0-9]' | awk '{ print $2 }' | cut -d'.' -f2) -gt 6 ]; then
-    score_option='--score=no'
-else
-    score_option=''
-fi
-pylint_command $score_option --reports=no --disable=all --enable=C1001 $(find . -name '*.py*') || { echo -e "\nOld-style classes found.\nPlease convert to new-style:\nclass C: => class C(object):\n" && exit 1; }
+    # enforce style rules
+    pylint_command () {
+        if hash pylint 2> /dev/null; then
+            pylint "$@"
+        elif hash pylint3 2> /dev/null; then
+            pylint3 "$@"
+        elif hash pylint-2 2> /dev/null; then
+            pylint-2 "$@"
+        elif hash pylint-3 2> /dev/null; then
+            pylint-3 "$@"
+        else
+            echo "pylint not found";
+            exit 1
+        fi
+    }
+    if [ $(pylint_command --version | grep -o 'pylint.*[0-9]\.[0-9]\.[0-9]' | awk '{ print $2 }' | cut -d'.' -f2) -gt 6 ]; then
+        score_option='--score=no'
+    else
+        score_option=''
+    fi
+    pylint_command $score_option --reports=no --disable=all --enable=C1001 $(find . -name '*.py*') || { echo -e "\nOld-style classes found.\nPlease convert to new-style:\nclass C: => class C(object):\n" && exit 1; }
 
-if ! $insource; then
-    if [ ! -d $builddir ]; then
-        echo "Creating $builddir..."
-        mkdir -p $builddir
+    if ! $insource; then
+        if [ ! -d $builddir ]; then
+            echo "Creating $builddir..."
+            mkdir -p $builddir
+        fi
     fi
 fi
 
@@ -170,107 +181,105 @@ if [ -f "/etc/os-release" ]; then
     grep -q 'rhel\|fedora' /etc/os-release && for f in /etc/profile.d/*module*.sh; do source $f; done && module load mpi
 fi
 
-# CONFIGURE
-start "CONFIGURE"
+if $stage_build; then
+    # CONFIGURE
+    start "CONFIGURE"
 
-if [ $with_fftw = "true" ]; then
-    :
-else
-    cmake_params="-DCMAKE_DISABLE_FIND_PACKAGE_FFTW3=ON $cmake_params"
-fi
-
-if [ $with_python_interface = "true" ]; then
-    cmake_params="-DWITH_PYTHON=ON $cmake_params"
-else
-    cmake_params="-DWITH_PYTHON=OFF $cmake_params"
-fi
-
-if [ $with_coverage = "true" ]; then
-    cmake_params="-DWITH_COVERAGE=ON $cmake_params"
-fi
-
-if [ $with_asan = "true" ]; then
-    cmake_params="-DWITH_ASAN=ON $cmake_params"
-fi
-
-if [ $with_ubsan = "true" ]; then
-    cmake_params="-DWITH_UBSAN=ON $cmake_params"
-fi
-
-if [ $with_static_analysis = "true" ]; then
-    cmake_params="-DWITH_CLANG_TIDY=ON $cmake_params"
-fi
-
-if [ $with_cuda = "true" ]; then
-    :
-else
-    cmake_params="-DWITH_CUDA=OFF $cmake_params"
-fi
-
-MYCONFIG_DIR=$srcdir/maintainer/configs
-if [ "$myconfig" = "default" ]; then
-    echo "Using default myconfig."
-else
-    myconfig_file=$MYCONFIG_DIR/$myconfig.hpp
-    if [ ! -e "$myconfig_file" ]; then
-        echo "$myconfig_file does not exist!"
-        exit 1
+    if [ $with_fftw = "true" ]; then
+        :
+    else
+        cmake_params="-DCMAKE_DISABLE_FIND_PACKAGE_FFTW3=ON $cmake_params"
     fi
-    echo "Copying $myconfig.hpp to $builddir/myconfig.hpp..."
-    cp $myconfig_file $builddir/myconfig.hpp
-fi
 
-cmd "cmake $cmake_params $srcdir" || exit 1
-end "CONFIGURE"
+    if [ $with_python_interface = "true" ]; then
+        cmake_params="-DWITH_PYTHON=ON $cmake_params"
+    else
+        cmake_params="-DWITH_PYTHON=OFF $cmake_params"
+    fi
 
-# BUILD
-start "BUILD"
+    if [ $with_coverage = "true" ]; then
+        cmake_params="-DWITH_COVERAGE=ON $cmake_params"
+    fi
 
-cmd "make -k -j${build_procs}" || cmd "make -k -j1" || exit $?
+    if [ $with_asan = "true" ]; then
+        cmake_params="-DWITH_ASAN=ON $cmake_params"
+    fi
 
-end "BUILD"
+    if [ $with_ubsan = "true" ]; then
+        cmake_params="-DWITH_UBSAN=ON $cmake_params"
+    fi
 
-if $make_check; then
-    start "TEST"
+    if [ $with_static_analysis = "true" ]; then
+        cmake_params="-DWITH_CLANG_TIDY=ON $cmake_params"
+    fi
 
-    if [ -z "$run_tests" ]; then
-        if $check_odd_only; then
-            cmd "make -j${build_procs} check_python_parallel_odd $make_params" || exit 1
-	elif $check_gpu_only; then
-	    cmd "make -j${build_procs} check_python_gpu $make_params" || exit 1
-        else
-            cmd "make -j${build_procs} check_python $make_params" || exit 1
+    if [ $with_cuda = "true" ]; then
+        :
+    else
+        cmake_params="-DWITH_CUDA=OFF $cmake_params"
+    fi
+
+    MYCONFIG_DIR=$srcdir/maintainer/configs
+    if [ "$myconfig" = "default" ]; then
+        echo "Using default myconfig."
+    else
+        myconfig_file=$MYCONFIG_DIR/$myconfig.hpp
+        if [ ! -e "$myconfig_file" ]; then
+            echo "$myconfig_file does not exist!"
+            exit 1
         fi
-    else
-        cmd "make python_tests $make_params"
-        for t in $run_tests; do
-            cmd "ctest --output-on-failure -R $t" || exit 1
-        done
-    fi
-    cmd "make -j${build_procs} check_unit_tests $make_params" || exit 1
-    cmd "make check_cmake_install $make_params" || exit 1
-
-    end "TEST"
-else
-    start "TEST"
-
-    if [ "$HIP_PLATFORM" != "hcc" ]; then
-      cmd "mpiexec -n $check_procs ./pypresso $srcdir/testsuite/python/particle.py" || exit 1
+        echo "Copying $myconfig.hpp to $builddir/myconfig.hpp..."
+        cp $myconfig_file $builddir/myconfig.hpp
     fi
 
-    end "TEST"
+    cmd "cmake $cmake_params $srcdir" || exit 1
+    end "CONFIGURE"
+
+    # BUILD
+    start "BUILD"
+
+    cmd "make -k -j${build_procs}" || cmd "make -k -j1" || exit $?
+
+    end "BUILD"
 fi
 
-if $with_coverage; then
-    cd $builddir
-    lcov -q --directory . --capture --output-file coverage.info # capture coverage info
-    lcov -q --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
-    lcov -q --remove coverage.info '*/doc/*' --output-file coverage.info # filter out docs
-    # Uploading report to CodeCov
-    if [ -z "$CODECOV_TOKEN" ]; then
-        bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+if $stage_checks; then
+    start "TEST"
+    if $make_check; then
+        if [ -z "$run_tests" ]; then
+            if $check_odd_only; then
+                cmd "make -j${build_procs} check_python_parallel_odd $make_params" || exit 1
+            elif $check_gpu_only; then
+                cmd "make -j${build_procs} check_python_gpu $make_params" || exit 1
+            else
+                cmd "make -j${build_procs} check_python $make_params" || exit 1
+            fi
+        else
+            cmd "make python_tests $make_params"
+            for t in $run_tests; do
+                cmd "ctest --output-on-failure -R $t" || exit 1
+            done
+        fi
+        cmd "make -j${build_procs} check_unit_tests $make_params" || exit 1
+        cmd "make check_cmake_install $make_params" || exit 1
     else
-        bash <(curl -s https://codecov.io/bash) -t "$CODECOV_TOKEN" || echo "Codecov did not collect coverage reports"
+        if [ "$HIP_PLATFORM" != "hcc" ]; then
+          cmd "mpiexec -n $check_procs ./pypresso $srcdir/testsuite/python/particle.py" || exit 1
+        fi
+    fi
+    end "TEST"
+
+    if $with_coverage; then
+        cd $builddir
+        lcov -q --directory . --capture --output-file coverage.info # capture coverage info
+        lcov -q --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
+        lcov -q --remove coverage.info '*/doc/*' --output-file coverage.info # filter out docs
+        # Uploading report to CodeCov
+        if [ -z "$CODECOV_TOKEN" ]; then
+            bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+        else
+            bash <(curl -s https://codecov.io/bash) -t "$CODECOV_TOKEN" || echo "Codecov did not collect coverage reports"
+        fi
     fi
 fi
 
