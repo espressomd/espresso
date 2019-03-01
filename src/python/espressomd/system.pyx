dddd114c982818d68980760cf359f72b5c43bdf3
Kai Szuttor <kai@icp.uni-stuttgart.de>
2019-03-01 11:24:24 +0100

    lattice: don't expose lattice_switch to the system class.

--- src/python/espressomd/system.pyx
+++ src/python/espressomd/system.pyx
@@ -294,11 +294,6 @@ cdef class System(object):
         def __get__(self):
             return max_cut_nonbonded
 
-    IF LB or LB_GPU:
-        property lattice_switch:
-            def __get__(self):
-                return lb_lbfluid_get_lattice_switch()
-
     property max_cut_bonded:
         def __get__(self):
             return max_cut_bonded
