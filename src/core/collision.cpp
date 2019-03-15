5649e7ac566a36317a5283ccf74eb3982d59ad28
Florian Weik <florianweik@gmail.com>
2019-03-15 02:41:04 +0100

    coldet: Fixed use after free

--- src/core/collision.cpp
+++ src/core/collision.cpp
@@ -385,7 +385,7 @@ void coldet_do_three_particle_bond(Particle &p, Particle &p1, Particle &p2) {
 #ifdef VIRTUAL_SITES_RELATIVE
 void place_vs_and_relate_to_particle(const int current_vs_pid,
                                      const Vector3d &pos,
-                                     const Particle *relate_to,
+                                     int relate_to,
                                      const Vector3d &initial_pos) {
 
   // The virtual site is placed at initial_pos which will be in the local
@@ -395,8 +395,8 @@ void place_vs_and_relate_to_particle(const int current_vs_pid,
   added_particle(current_vs_pid);
   auto p_vs = local_place_particle(current_vs_pid, initial_pos.data(), 1);
   p_vs->r.p = pos;
-  assert(relate_to);
-  local_vs_relate_to(p_vs, relate_to);
+
+  local_vs_relate_to(p_vs, &get_part(relate_to));
 
   p_vs->p.is_virtual = 1;
   p_vs->p.type = collision_params.vs_particle_type;
@@ -616,7 +616,7 @@ void handle_collisions() {
 
           auto handle_particle = [&](Particle *p, Vector3d const &pos) {
             if (not p->l.ghost) {
-              place_vs_and_relate_to_particle(current_vs_pid, pos, p,
+              place_vs_and_relate_to_particle(current_vs_pid, pos, p->identity(),
                                               initial_pos);
               // Particle storage locations may have changed due to
               // added particle
@@ -680,7 +680,7 @@ void handle_collisions() {
 
           // Vs placement happens on the node that has p1
           if (!attach_vs_to.l.ghost) {
-            place_vs_and_relate_to_particle(current_vs_pid, pos, &attach_vs_to,
+            place_vs_and_relate_to_particle(current_vs_pid, pos, attach_vs_to.identity(),
                                             initial_pos);
             // Particle storage locations may have changed due to
             // added particle
