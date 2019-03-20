c31e0a387e91c2c8ec63b723b61995d72bc94ebf
Florian Weik <fweik@icp.uni-stuttgart.de>
2019-03-20 13:14:37 +0100

    utils: Very important macro refactoring

--- src/core/utils/device_qualifier.hpp
+++ src/core/utils/device_qualifier.hpp
@@ -3,14 +3,10 @@
 
 #if defined(__CUDACC__)
 #define DEVICE_QUALIFIER __host__ __device__
+#define DEVICE_ASSERT(A) void((A))
 #else
 #define DEVICE_QUALIFIER
-#endif
-
-#ifndef __CUDACC__
 #define DEVICE_ASSERT(A) assert((A))
-#else
-#define DEVICE_ASSERT(A) void((A))
 #endif
 
 #endif // ESPRESSO_DEVICE_QUALIFIER_HPP
