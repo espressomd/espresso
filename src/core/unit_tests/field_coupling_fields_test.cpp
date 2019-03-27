5a254047c1090b5a661522c5f57c4819c736aa38
Kai Szuttor <kai@icp.uni-stuttgart.de>
2019-03-27 16:31:09 +0100

    unit-tests: fix field_coupling test.

--- src/core/unit_tests/field_coupling_fields_test.cpp
+++ src/core/unit_tests/field_coupling_fields_test.cpp
@@ -81,7 +81,7 @@ BOOST_AUTO_TEST_CASE(constant_scalar_field) {
   {
     Field field(5.);
 
-    BOOST_CHECK(field.jacobian({1., 2., 3.}).empty());
+    BOOST_CHECK((Vector3d{0.0, 0.0, 0.0} == field.jacobian({1., 2., 3.})));
   }
 }
 
