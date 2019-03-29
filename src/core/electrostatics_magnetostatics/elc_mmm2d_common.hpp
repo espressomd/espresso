4e335cc717651e815b511c5eebb446d186842436
Alexander Reinauer <st144434@stud.uni-stuttgart.de>
2019-03-29 14:09:02 +0100

    removed elc_mmm2d_common.cpp

--- src/core/electrostatics_magnetostatics/elc_mmm2d_common.hpp
+++ src/core/electrostatics_magnetostatics/elc_mmm2d_common.hpp
@@ -59,10 +59,29 @@ elc_add_force_dir(int position, const double *partblk, const double *gblcblk) {
          partblk[position + POQECP] * gblcblk[POQESM];
 }
 
+inline double sum_small(int position, const double *partblk, const double *gblcblk, bool sum) {
+  double sum1 = partblk[position + POQECM] * gblcblk[POQECP] +
+                partblk[position + POQESM] * gblcblk[POQESP];
 
-double sum_small(int position, const double *partblk, const double *gblcblk, bool sum);
+  double sum2 = partblk[position + POQECP] * gblcblk[POQECM] +
+                partblk[position + POQESP] * gblcblk[POQESM];
 
-double sum_large(int position, const double *partblk, const double *gblcblk, bool sum);
+  return sum ? sum1 + sum2 : sum1 - sum2;
+}
+
+inline double sum_large(int position, const double *partblk, const double *gblcblk, bool sum) {
+  double sum1 = partblk[position + PQESSM] * gblcblk[PQESSP] +
+                partblk[position + PQESCM] * gblcblk[PQESCP] +
+                partblk[position + PQECSM] * gblcblk[PQECSP] +
+                partblk[position + PQECCM] * gblcblk[PQECCP];
+
+  double sum2 = partblk[position + PQESSP] * gblcblk[PQESSM] +
+                partblk[position + PQESCP] * gblcblk[PQESCM] +
+                partblk[position + PQECSP] * gblcblk[PQECSM] +
+                partblk[position + PQECCP] * gblcblk[PQECCM];
+
+  return sum ? sum1 + sum2 : sum1 - sum2;
+}
 
 inline double
 elc_add_force_z(int position, const double *partblk, const double *gblcblk) {
