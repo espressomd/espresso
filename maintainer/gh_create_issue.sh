dafc06163b50f0f60d2c3deffae7415d44d2bcc7
Kai Szuttor <kai@icp.uni-stuttgart.de>
2019-04-03 18:19:04 +0200

    Added job for creating an issue if job failed.

---
+++ maintainer/gh_create_issue.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+URL=$(echo "https://gitlab.icp.uni-stuttgart.de/espressomd/espresso/pipelines/${CI_PIPELINE_ID}")
+
+curl -i -s "https://api.github.com/repos/espressomd/espresso/issues" \
+     -H "Accept: application/vnd.github.full+json" \
+     -H "Content-Type: application/json" \
+     -H "Authorization: token $GITHUB_TOKEN" \
+     -X POST \
+     -d "{
+         \"title\": \"CI build failed for merged PR\",
+         \"body\": \"See \"$URL\"\"
+         }"
