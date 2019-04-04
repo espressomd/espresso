0bb9b6b8e1d3bf5a4521cd39be8415a239af52be
Konrad Breitsprecher <konradbreitsprecher@gmail.com>
2019-04-04 15:34:10 +0200

    Update energy.cpp
    Restore p3m sums to only account for particle charges (without ELC contributions).
    Cures ELC sanity check for charge neutrality that is called in the beginning of each integration step.

--- src/core/energy.cpp
+++ src/core/energy.cpp
@@ -199,6 +199,9 @@ void calc_long_range_energies() {
       ELC_P3M_modify_p3m_sums_image();
 
       energy.coulomb[1] -= 0.5 * p3m_calc_kspace_forces(0, 1);
+
+      // restore modified sums 
+      ELC_P3M_restore_p3m_sums();
     }
     energy.coulomb[2] = ELC_energy();
     break;
