883de6b5419ab98daf094fd744394bcc59e59cae
Kai Szuttor <kai@icp.uni-stuttgart.de>
2019-04-04 08:01:59 +0200

    CI: output number of created issue.

--- maintainer/gh_create_issue.sh
+++ maintainer/gh_create_issue.sh
@@ -2,9 +2,10 @@
 
 URL=$(echo "https://gitlab.icp.uni-stuttgart.de/espressomd/espresso/pipelines/${CI_PIPELINE_ID}")
 
-curl -i -s "https://api.github.com/repos/espressomd/espresso/issues" \
+ISSUE_NUMBER=$(curl -s "https://api.github.com/repos/espressomd/espresso/issues" \
      -H "Accept: application/vnd.github.full+json" \
      -H "Content-Type: application/json" \
      -H "Authorization: token $GITHUB_TOKEN" \
      -X POST \
-     -d "{\"title\": \"CI build failed for merged PR\", \"body\": \"$URL\" }"
+     -d "{\"title\": \"CI build failed for merged PR\", \"body\": \"$URL\" }" | jq .number)
+echo $ISSUE_NUMBER
