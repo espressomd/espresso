a94e86bfbbc34fb19025e550e01da741230211f7
Alexander Reinauer <st144434@stud.uni-stuttgart.de>
2019-04-08 13:14:23 +0200

    formatting

--- src/core/electrostatics_magnetostatics/coulomb.cpp
+++ src/core/electrostatics_magnetostatics/coulomb.cpp
@@ -379,7 +379,7 @@ void calc_energy_long_range(Observable_stat &energy) {
       ELC_P3M_modify_p3m_sums_image();
 
       energy.coulomb[1] -= 0.5 * p3m_calc_kspace_forces(0, 1);
-      
+
       // restore modified sums
       ELC_P3M_restore_p3m_sums();
     }
