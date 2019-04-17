ce1f22396845da0d254a0aaf736c0d4b20b388c4
Jan Finkbeiner <st144298@stud.uni-stuttgart.de>
2019-04-17 17:27:16 +0200

    added jonas modifications

--- testsuite/python/widom_insertion.py
+++ testsuite/python/widom_insertion.py
@@ -25,8 +25,11 @@ import unittest as ut
 import numpy as np
 import espressomd  # pylint: disable=import-error
 from espressomd import reaction_ensemble
+from tests_common import lj_potential
 
 
+@ut.skipIf(not espressomd.has_features(["LENNARD_JONES"]),
+           "Features not available, skipping test!")
 class ReactionEnsembleTest(ut.TestCase):
 
     """Test the implementation of the widom insertion (part of the reaction ensemble)."""
@@ -42,8 +45,21 @@ class ReactionEnsembleTest(ut.TestCase):
     lj_sig = 1.0
     lj_cut = 5.0
     box_l = 20.0
+    lj_shift = lj_potential(lj_cut, lj_eps, lj_sig, lj_cut+1, 0.0)
 
-    target_mu_ex = -0.00227888  # analytical solution for this specific system
+    def lj_potential_arr(r, lj_eps, lj_sig, lj_shift):
+        """lennard jones potential (that works for array input)"""
+        r6 = r**(-6)
+        r12 = r6**2
+        ljsig6 = lj_sig**6
+        ljsig12 = ljsig6**2
+        return 4.0 * lj_eps * (ljsig12 * r12 - ljsig6 * r6) + lj_shift
+
+    r = np.linspace(1e-10,lj_cut,1000)
+    integrateSphere = 4*np.pi*np.trapz(r**2*np.exp(-lj_potential_arr(r, lj_eps, lj_sig, lj_shift)/ temperature), x=r) #numerical solution 
+    integreateRest = (box_l**3 - 4.0 / 3.0 * np.pi * lj_cut**3) #numerical solution for V_lj=0 => corresponds to the volume (as exp(0)=1)
+
+    target_mu_ex = -temperature*np.log((integrateSphere + integreateRest)/box_l**3) #calculate excess chemical potential of the system from the integral values
 
     system = espressomd.System(box_l=np.ones(3) * box_l)
     system.seed = system.cell_system.get_state()['n_nodes'] * [2]
@@ -51,7 +67,7 @@ class ReactionEnsembleTest(ut.TestCase):
     system.cell_system.skin = 0.4
     volume = np.prod(system.box_l)  # cuboid box
     
-    unimportant_exclusion_radius = 1.0
+    unimportant_exclusion_radius = np.nan
     Widom = reaction_ensemble.WidomInsertion(
         temperature=temperature,
         exclusion_radius=unimportant_exclusion_radius)    
@@ -89,10 +105,10 @@ class ReactionEnsembleTest(ut.TestCase):
                 0)  # 0 for insertion reaction
         mu_ex = Widom.measure_excess_chemical_potential(0)
         deviation_mu_ex = abs(mu_ex[0] - target_mu_ex)
-        dev_comp_to_err = deviation_mu_ex - 1.5 * mu_ex[1] 
+
         # error
         self.assertLess(
-            dev_comp_to_err,
+            deviation_mu_ex - 0.00015,
             0.0,
             msg="\nExcess chemical potential for single LJ-particle computed via widom insertion gives a wrong value.\n"
             + "  average mu_ex: " + str(mu_ex[0])
