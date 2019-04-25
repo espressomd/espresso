cd84e9c12559d80796ee9e785ca5e4a557ac8bf1
Florian Weik <fweik@icp.uni-stuttgart.de>
2019-04-25 17:40:38 +0200

    ...

--- src/core/utils.hpp
+++ src/core/utils.hpp
@@ -44,8 +44,6 @@ template <typename T1, typename T2> double scalar(const T1 &a, const T2 &b) {
 }
 
 /** calculates the squared length of a vector */
-template <typename T> double sqrlen(T const &v) { return scalar(v, v); }
-
 template <class T, size_t N> T sqrlen(const T (&v)[N]) {
     return boost::inner_product(v, v, T{});
 }
