9ab5cb9a89d349502186c1bb3c5a9e7598b68547
Kai Szuttor <kai@icp.uni-stuttgart.de>
2019-05-24 15:58:03 +0200

    maintainer: propagate manually added cxx flags to nvcc.

--- maintainer/CI/build_cmake.sh
+++ maintainer/CI/build_cmake.sh
@@ -90,7 +90,7 @@ if [ ! -z ${with_coverage+x} ]; then
 fi
 
 cmake_params="-DCMAKE_BUILD_TYPE=$build_type -DPYTHON_EXECUTABLE=$(which python$python_version) -DWARNINGS_ARE_ERRORS=ON -DTEST_NP:INT=$check_procs $cmake_params -DWITH_SCAFACOS=ON"
-cmake_params="$cmake_params -DCMAKE_CXX_FLAGS=$cxx_flags"
+cmake_params="$cmake_params -DCMAKE_CXX_FLAGS=$cxx_flags -DCUDA_NVCC_FLAGS=$cxx_flags"
 cmake_params="$cmake_params -DCMAKE_INSTALL_PREFIX=/tmp/espresso-unit-tests"
 cmake_params="$cmake_params -DTEST_TIMEOUT=$test_timeout"
 if $with_ccache; then
