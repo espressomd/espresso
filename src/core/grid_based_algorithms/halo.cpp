3dd223b796ed3e60d795be2dc419c80d6c97a0cc
Florian Weik <fweik@icp.uni-stuttgart.de>
2019-06-12 14:11:44 +0200

    Formatting

--- src/core/grid_based_algorithms/halo.cpp
+++ src/core/grid_based_algorithms/halo.cpp
@@ -284,8 +284,7 @@ void prepare_halo_communication(HaloCommunicator *const hc,
             hinfo->type = HALO_SEND;
           }
         }
-      } else
-      {
+      } else {
         if (local_node_grid[dir] == 1) {
           hc->halo_info[cnt].type = HALO_LOCL;
         } else {
