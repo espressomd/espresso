fb843cd610ef155949510285f4fd347d06622950
Alexander Reinauer <st144434@stud.uni-stuttgart.de>
2019-06-19 12:49:02 +0200

    formatting

--- src/core/electrostatics_magnetostatics/elc.cpp
+++ src/core/electrostatics_magnetostatics/elc.cpp
@@ -58,8 +58,8 @@
 static double ux, ux2, uy, uy2, uz, height_inverse;
 /*@}*/
 
-ELC_struct elc_params = {1e100, 10, 1, 0, 1, 1, false, 1,
-                         1, false, 0, 0, 0, 0, 0.0};
+ELC_struct elc_params = {1e100, 10,    1, 0, 1, 1, false, 1,
+                         1,     false, 0, 0, 0, 0, 0.0};
 
 /****************************************
  * LOCAL ARRAYS
