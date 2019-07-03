b8ccbd78ad95903fdf6939674567cbb6c8a67273
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2019-07-03 15:25:22 +0200

    Decrease compilation time

--- maintainer/benchmarks/runner.sh
+++ maintainer/benchmarks/runner.sh
@@ -16,7 +16,7 @@ sed 's/#define ADDITIONAL_CHECKS//' ../maintainer/configs/maxset.hpp > myconfig-
 
 # prepare build area
 rm -rf src/ maintainer/
-cmake -DWITH_BENCHMARKS=ON -DTEST_TIMEOUT=600 ..
+cmake -DWITH_BENCHMARKS=ON -DTEST_TIMEOUT=600 -DWITH_CUDA=OFF -DWITH_CCACHE=OFF ..
 cat > benchmarks.csv << EOF
 "config","script","arguments","cores","MPI","mean","ci","steps_per_tick","duration","E1","E2","E3"
 EOF
@@ -29,7 +29,7 @@ do
   make -j$(nproc)
   rm -f benchmarks.csv.part
   touch benchmarks.csv.part
-  make benchmark 2>&1 | tee -a benchmarks.log
+  make -j$(nproc) benchmark 2>&1 | tee -a benchmarks.log
   sed -ri "s/^/\"$(basename ${config})\",/" benchmarks.csv.part
   cat benchmarks.csv.part >> benchmarks.csv
 done
