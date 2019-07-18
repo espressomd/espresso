6e3872a445c5709fc529a56b0940872520500e79
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2019-07-18 19:34:40 +0200

    Simplify management of configuration files
    Apply the same treatment to all configuration files: add features
    required for the benchmarks to run and remove debug features that
    slow down the code.

--- maintainer/benchmarks/runner.sh
+++ maintainer/benchmarks/runner.sh
@@ -4,15 +4,19 @@ cd "$(git rev-parse --show-toplevel)"
 mkdir -p build
 cd build
 
-# manage headers files with different features
-configs="myconfig-minimal.hpp myconfig-default.hpp myconfig-maxset.hpp"
-cat > myconfig-minimal.hpp << EOF
-#define ELECTROSTATICS
-#define LENNARD_JONES
-#define MASS
-EOF
-cp ../src/config/myconfig-default.hpp myconfig-default.hpp
-sed 's/#define ADDITIONAL_CHECKS//' ../maintainer/configs/maxset.hpp > myconfig-maxset.hpp
+# manage configuration files with different features
+configs="empty.hpp default.hpp maxset.hpp"
+cp ../maintainer/configs/empty.hpp .
+cp ../src/config/myconfig-default.hpp default.hpp
+cp ../maintainer/configs/maxset.hpp .
+
+# process configuration files
+for config in ${configs}; do
+  # add minimal features for the benchmarks to run
+  echo -e "#define ELECTROSTATICS\n#define LENNARD_JONES\n#define MASS\n$(cat ${config})" > "${config}"
+  # remove checks
+  sed -ri "s/#define ADDITIONAL_CHECKS//" "${config}"
+done
 
 # prepare build area
 rm -rf src/ maintainer/
@@ -22,8 +26,7 @@ cat > benchmarks.csv << EOF
 EOF
 
 # run benchmarks
-for config in ${configs}
-do
+for config in ${configs}; do
   echo "### ${config}" >> benchmarks.log
   cp ${config} myconfig.hpp
   make -j$(nproc)
