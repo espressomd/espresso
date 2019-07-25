7616a318f51b4a83fca09ee5781a48ba86d5806a
KonradBreitsprecher <konradbreitsprecher@gmail.com>
2019-07-25 14:39:11 +0200

    includes

--- src/core/dpd.hpp
+++ src/core/dpd.hpp
@@ -31,15 +31,14 @@
 
 #ifdef DPD
 #include "particle_data.hpp"
+#include "random.hpp"
 #include "utils/uniform.hpp"
 #include "utils/u32_to_u64.hpp"
 
-#include "random.hpp"
 #include <utils/Vector.hpp>
-
-#include <Random123/philox.h>
 #include <utils/Counter.hpp>
 #include <utils/uniform.hpp>
+#include <Random123/philox.h>
 
 struct IA_parameters;
 
@@ -50,12 +49,32 @@ struct DPDParameters {
   double pref = 0.0;
 };
 
+void dpd_heat_up();
+void dpd_cool_down();
+int dpd_set_params(int part_type_a, int part_type_b, double gamma, double r_c,
+                   int wf, double tgamma, double tr_c, int twf);
+void dpd_init();
+void dpd_update_params(double pref2_scale);
+
+Utils::Vector3d dpd_pair_force(Particle const *p1, Particle const *p2,
+                               const IA_parameters *ia_params, double const *d,
+                               double dist, double dist2);
+Utils::Vector9d dpd_stress();
+
 /** philox functiontality: increment, get/set */
 extern std::unique_ptr<Utils::Counter<uint64_t>> dpd_rng_counter;
 bool dpd_is_seed_required();
 void dpd_rng_counter_increment();
 void dpd_set_rng_state(uint64_t counter);
 uint64_t dpd_get_rng_state();
+
+/** Return a random 4d vector with the philox thermostat.
+    Random numbers depend on
+    1. dpd_rng_counter (initialized by seed) which is increased on
+   integration
+    2. Salt (decorrelates different counter)
+    3. Two particle IDs (order-independent, decorrelates particles, gets rid of seed-per-node)
+*/
 
 inline Utils::Vector3d dpd_noise(uint32_t pid1, uint32_t pid2) {
 
@@ -84,25 +103,6 @@ inline Utils::Vector3d dpd_noise(uint32_t pid1, uint32_t pid2) {
          Utils::Vector3d::broadcast(0.5);
 }
 
-void dpd_heat_up();
-void dpd_cool_down();
-int dpd_set_params(int part_type_a, int part_type_b, double gamma, double r_c,
-                   int wf, double tgamma, double tr_c, int twf);
-void dpd_init();
-void dpd_update_params(double pref2_scale);
-
-Utils::Vector3d dpd_pair_force(Particle const *p1, Particle const *p2,
-                               const IA_parameters *ia_params, double const *d,
-                               double dist, double dist2);
-Utils::Vector9d dpd_stress();
-
-/** Return a random 4d vector with the philox thermostat.
-    Random numbers depend on
-    1. dpd_rng_counter (initialized by seed) which is increased on
-   integration
-    2. Salt (decorrelates different counter)
-    3. Two particle IDs (order-independent, decorrelates particles, gets rid of seed-per-node)
-*/
 
 #endif
 #endif
