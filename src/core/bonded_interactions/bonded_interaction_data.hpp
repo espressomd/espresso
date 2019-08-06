d6518af7b072715ec7ad03b5a3b80c43cc83108c
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2019-08-06 20:22:46 +0200

    Style patch

--- src/core/bonded_interactions/bonded_interaction_data.hpp
+++ src/core/bonded_interactions/bonded_interaction_data.hpp
@@ -168,7 +168,6 @@ struct Bonded_coulomb_sr_bond_parameters {
   double q1q2;
 };
 
-
 /** Parameters for three-body angular potential (harmonic). */
 struct Angle_harmonic_bond_parameters {
   /** bending constant */
