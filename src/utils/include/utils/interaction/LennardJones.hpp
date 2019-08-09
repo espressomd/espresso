61ab7e8fcb0b8f957c47f5c81459173c41d7d367
Kai Szuttor <kai@icp.uni-stuttgart.de>
2019-08-09 09:00:18 +0200

    utils: added LennardJones interaction plus test.

---
+++ src/utils/include/utils/interaction/LennardJones.hpp
@@ -0,0 +1,29 @@
+#ifndef UTILS_LENNARD_JONES
+#define UTILS_LENNARD_JONES
+
+#include "utils/Vector.hpp"
+#include "utils/math/int_pow.hpp"
+#include "utils/math/sqr.hpp"
+
+namespace Utils {
+namespace Interaction{
+
+struct LennardJones {
+  double epsilon = 0.0;
+  double sigma = 0.0;
+
+  double U(double r) const {
+    auto const temp = Utils::int_pow<6>(sigma / r);
+    return 4.0 * epsilon * (Utils::sqr(temp) - temp);
+  }
+
+  Utils::Vector3d F(double r, Utils::Vector3d const &r_ij) const {
+    auto const sigma6 = Utils::int_pow<6>(sigma);
+    return (48.0 * epsilon * Utils::int_pow<2>(sigma6) / Utils::int_pow<13>(r) - 24.0 * epsilon * sigma6 / Utils::int_pow<7>(r)) * r_ij / r_ij.norm();
+  }
+};
+
+
+}
+}
+#endif
---
+++ src/utils/tests/LennardJones_test.cpp
@@ -0,0 +1,56 @@
+/*
+  Copyright (C) 2018 The ESPResSo project
+
+  This file is part of ESPResSo.
+
+  ESPResSo is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  ESPResSo is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define BOOST_TEST_MODULE Utils::Interaction::LennardJones test
+#define BOOST_TEST_DYN_LINK
+
+#include <limits>
+#include <boost/test/unit_test.hpp>
+
+#include "utils/interaction/LennardJones.hpp"
+#include "utils/Vector.hpp"
+
+BOOST_AUTO_TEST_CASE(energy) {
+  Utils::Interaction::LennardJones lennard_jones;
+  lennard_jones.sigma = 1.5;
+  lennard_jones.epsilon = 2.0;
+  // check the minimum
+  BOOST_CHECK(lennard_jones.U(std::pow(2.0, 1./6) * lennard_jones.sigma) == -lennard_jones.epsilon);
+  // check the root
+  BOOST_CHECK(lennard_jones.U(lennard_jones.sigma) == 0.0);
+}
+
+BOOST_AUTO_TEST_CASE(force) {
+  Utils::Interaction::LennardJones lennard_jones;
+  lennard_jones.sigma = 1.5;
+  lennard_jones.epsilon = 2.0;
+  // check the minimum
+  auto const minimum = std::pow(2.0, 1./6) * lennard_jones.sigma;
+  auto const force = lennard_jones.F(minimum, Utils::Vector3d{minimum, 0.0, 0.0});
+  auto const expected = Utils::Vector3d::broadcast(0.0);
+  for (int i=0; i<3; ++i) {
+    BOOST_CHECK_SMALL(force[i] - expected[i], 1e-10);
+  }
+  // Check for sign change in the force
+  auto const epsilon = std::numeric_limits<double>::epsilon();
+  auto const force1 = lennard_jones.F(minimum - epsilon, Utils::Vector3d{minimum - epsilon, 0.0, 0.0});
+  auto const force2 = lennard_jones.F(minimum + epsilon, Utils::Vector3d{minimum + epsilon, 0.0, 0.0});
+  BOOST_CHECK(force1[0] > 0.0);
+  BOOST_CHECK(force2[0] < 0.0);
+}
