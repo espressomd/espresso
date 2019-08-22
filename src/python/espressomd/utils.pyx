d85e112c3f12851d7d660bf52f4c7149bc6162ed
Kai Szuttor <2150555+KaiSzuttor@users.noreply.github.com>
2019-08-22 13:51:29 +0200

    utils: remove unused import

--- src/python/espressomd/utils.pyx
+++ src/python/espressomd/utils.pyx
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import math
 cimport numpy as np
 cimport cython
 import numpy as np
