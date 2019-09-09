89598df4d097248103eb23b0de217fc52f14a6de
itischler <ingo.tischler@gmx.de>
2019-09-09 11:42:53 +0200

    applyed changes are now tested for virtual sites

--- testsuite/python/virtual_sites_relative.py
+++ testsuite/python/virtual_sites_relative.py
@@ -290,6 +290,15 @@ class VirtualSites(ut.TestCase):
             verify_lj_forces(system, 1E-10, 3 *
                              np.arange(int(n / 2), dtype=int))
 
+        # Test applying changes
+        enegry_pre_change = system.analysis.energy()['total']
+        pressure_pre_change = system.analysis.pressure()['total']
+        system.part[0].pos = system.part[0].pos + (2.2,-1.4,4.2)
+        enegry_post_change = system.analysis.energy()['total']
+        pressure_post_change = system.analysis.pressure()['total']
+        self.assertNotAlmostEqual(enegry_pre_change, enegry_post_change)
+        self.assertNotAlmostEqual(pressure_pre_change, pressure_post_change)
+
         # Turn off lj interaction
         system.non_bonded_inter[0, 0].lennard_jones.set_params(
             epsilon=0, sigma=0, cutoff=0, shift=0)
