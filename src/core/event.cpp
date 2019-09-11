55607a3542262fc3db6df0ae3a73d5aba79181b7
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2019-09-11 18:23:20 +0200

    Core: Remove recalc_forces condition from update_dependent_particles()

--- src/core/event.cpp
+++ src/core/event.cpp
@@ -450,15 +450,14 @@ void on_ghost_flags_change() {
 }
 
 void update_dependent_particles() {
-  if (recalc_forces) {
 #ifdef VIRTUAL_SITES
-    if (virtual_sites()->is_relative()) {
-      ghost_communicator(&cell_structure.update_ghost_pos_comm);
-    }
-    virtual_sites()->update();
-#endif
-    cells_update_ghosts();
+  if (virtual_sites()->is_relative()) {
+    ghost_communicator(&cell_structure.update_ghost_pos_comm);
   }
+  virtual_sites()->update();
+#endif
+  cells_update_ghosts();
+
 #ifdef ELECTROSTATICS
   Coulomb::update_dependent_particles();
 #endif
