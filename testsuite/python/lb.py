f70c38a5d2f90f4f89ae9bb1872b963228fa366f
Kai Szuttor <kai@icp.uni-stuttgart.de>
2019-09-13 09:08:05 +0200

    testsuite: add momentum prediction to LB test.

--- testsuite/python/lb.py
+++ testsuite/python/lb.py
@@ -130,8 +130,10 @@ class TestLB:
                                    delta=self.params["mass_prec_per_node"])
 
             # check momentum conservation
+            particle_momentum = np.sum([p.mass * p.v + 0.5 * p.f * self.system.time_step for p in self.system.part], axis=0)
+            fluid_momentum = self.system.analysis.linear_momentum(False, True)
             np.testing.assert_allclose(
-                self.system.analysis.linear_momentum(), self.tot_mom,
+                particle_momentum + fluid_momentum, self.tot_mom,
                 atol=self.params['mom_prec'])
 
             # Calc particle temperature
