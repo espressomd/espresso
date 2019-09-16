e4d5635c2222a29b5f9de4511935425ecaa6962a
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2019-09-16 15:26:20 +0200

    Guard an NPT call

--- src/core/integrate.cpp
+++ src/core/integrate.cpp
@@ -281,7 +281,9 @@ void integrate_vv(int n_steps, int reuse_forces) {
     }
 
 #ifdef NPT
+  if (integ_switch == INTEG_METHOD_NPT_ISO) {
     npt_update_instantaneous_pressure();
+  }
 #endif
 
     if (check_runtime_errors(comm_cart))
