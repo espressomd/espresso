28cbcd0fc2ccfa7a8be6b16c9df54dbc5978ddb2
Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
2019-09-23 16:36:12 +0200

    rectification_simulation decreased timestep so it does not crash at velocities > 5, small cleanup

--- doc/tutorials/06-active_matter/EXERCISES/rectification_simulation.py
+++ doc/tutorials/06-active_matter/EXERCISES/rectification_simulation.py
@@ -79,14 +79,18 @@ except BaseException:
 
 length = 100
 diameter = 20
+padding = 2
 prod_steps = 500
 prod_length = 500
-dt = 0.01
+dt = 0.005
 
 # Setup the MD parameters
 
-system = System(box_l=[length, diameter + 4, diameter + 4])
-system.seed = system.cell_system.get_state()['n_nodes'] * [1234]
+box_l = np.array(
+    [length + 2 * padding,
+     diameter + 2 * padding,
+     diameter + 2 * padding])
+system = espressomd.System(box_l=box_l)
 system.cell_system.skin = 0.1
 system.time_step = dt
 system.min_global_cut = 0.5
@@ -105,7 +109,7 @@ system.thermostat.set_langevin(kT=1.0, gamma=1.0, seed=42)
 ##########################################################################
 
 ## Exercise 2 ##
-# Complete the following from the LB-based Tcl
+# Complete the geometry from the LB-based 
 # script. You need to add types to the walls
 # and cone as well.
 
@@ -115,17 +119,17 @@ system.constraints.add(shape=cylinder, particle_type=1)
 # Setup walls
 
 wall = Wall(...)
-system.constraints.add(shape=wall, particle_type=2)
+system.constraints.add(shape=wall, particle_type=1)
 
 wall = Wall(...)
-system.constraints.add(shape=wall, particle_type=3)
+system.constraints.add(shape=wall, particle_type=1)
 
 # Setup cone
 
 ...
 
 hollow_cone = HollowCone(...)
-system.constraints.add(shape=hollow_cone, particle_type=4)
+system.constraints.add(shape=hollow_cone, particle_type=1)
 
 ##########################################################################
 #
@@ -141,12 +145,6 @@ eps = 1.0
 shift = 0.25
 
 system.non_bonded_inter[0, 1].lennard_jones.set_params(
-    epsilon=eps, sigma=sig, cutoff=cut, shift=shift)
-system.non_bonded_inter[0, 2].lennard_jones.set_params(
-    epsilon=eps, sigma=sig, cutoff=cut, shift=shift)
-system.non_bonded_inter[0, 3].lennard_jones.set_params(
-    epsilon=eps, sigma=sig, cutoff=cut, shift=shift)
-system.non_bonded_inter[0, 4].lennard_jones.set_params(
     epsilon=eps, sigma=sig, cutoff=cut, shift=shift)
 
 ##########################################################################
@@ -169,6 +167,7 @@ for cntr in range(npart):
     x = ...
     y = ...
     z = ...
+    
     quats = ...
 
     system.part.add(pos=[x, y, z], type=0, swimming={'v_swim': vel},
@@ -197,24 +196,23 @@ with open("{}/CMS_{}.dat".format(outdir, vel), "w") as outfile:
 
     dev_sum = 0.0
     dev_av = 0.0
-    time_0 = system.time
+    system.time = 0.
     for i in range(prod_steps):
         # We output the coordinate of the center of mass in
         # the direction of the long axis, here we consider
-        # the deviation from the center
+        # the deviation from the center (keep the padding in mind)
 
         dev = ...
 
-        ...
+        dev_av = ...
 
-        time = system.time - time_0
-
-        print("{} {} {}".format(time, dev, dev_av), file=outfile)
+        print("{} {} {}".format(system.time, dev, dev_av), file=outfile)
 
         system.integrator.run(prod_length)
 
-# Output the final configuration
+# Output the geometry and final configuration
 
+lbf.print_vtk_boundary("{}/boundary.vtk".format(outdir))
 system.part.writevtk("{}/points_{}.vtk".format(outdir, vel), types=[0])
 
 ## Exercise 5 ##
--- doc/tutorials/06-active_matter/SOLUTIONS/rectification_simulation.py
+++ doc/tutorials/06-active_matter/SOLUTIONS/rectification_simulation.py
@@ -75,18 +75,23 @@ try:
 except BaseException:
     print("INFO: Directory \"{}\" exists".format(outdir))
 
-# Setup the box (we pad the diameter to ensure that the LB boundaries
-# and therefore the constraints, are away from the edge of the box)
+# Setup the box (we pad the diameter to ensure that the constraints
+# are away from the edge of the box)
+
 length = 100
 diameter = 20
+padding = 2
 prod_steps = 500
 prod_length = 500
-dt = 0.01
+dt = 0.005
 
 # Setup the MD parameters
-system = espressomd.System(box_l=[length, diameter + 4, diameter + 4])
-system.set_random_state_PRNG()
-system.box_l = [length, diameter + 4, diameter + 4]
+
+box_l = np.array(
+    [length + 2 * padding,
+     diameter + 2 * padding,
+     diameter + 2 * padding])
+system = espressomd.System(box_l=box_l)
 system.cell_system.skin = 0.1
 system.time_step = dt
 system.min_global_cut = 0.5
@@ -102,16 +107,16 @@ system.thermostat.set_langevin(kT=1.0, gamma=1.0, seed=42)
 ##########################################################################
 
 cylinder = Cylinder(
-    center=[length / 2.0, (diameter + 4) / 2.0, (diameter + 4) / 2.0],
+    center=0.5 * box_l,
     axis=[1, 0, 0], radius=diameter / 2.0, length=length, direction=-1)
 system.constraints.add(shape=cylinder, particle_type=1)
 
 # Setup walls
-wall = Wall(dist=2, normal=[1, 0, 0])
-system.constraints.add(shape=wall, particle_type=2)
+wall = Wall(dist=padding, normal=[1, 0, 0])
+system.constraints.add(shape=wall, particle_type=1)
 
-wall = Wall(dist=-(length - 2), normal=[-1, 0, 0])
-system.constraints.add(shape=wall, particle_type=3)
+wall = Wall(dist=-(length + padding), normal=[-1, 0, 0])
+system.constraints.add(shape=wall, particle_type=1)
 
 # Setup cone
 irad = 4.0
@@ -120,14 +125,16 @@ orad = (diameter - irad) / sin(angle)
 shift = 0.25 * orad * cos(angle)
 
 hollow_cone = HollowCone(
-    center=[length / 2.0 + shift, (diameter + 4) / 2.0, (diameter + 4) / 2.0],
+    center=[box_l[0] / 2.0 + shift,
+            box_l[1] / 2.0,
+            box_l[2] / 2.0],
     axis=[-1, 0, 0],
     outer_radius=orad,
     inner_radius=irad,
     width=2.0,
     opening_angle=angle,
     direction=1)
-system.constraints.add(shape=hollow_cone, particle_type=4)
+system.constraints.add(shape=hollow_cone, particle_type=1)
 
 ##########################################################################
 #
@@ -139,16 +146,10 @@ system.constraints.add(shape=hollow_cone, particle_type=4)
 
 sig = 0.5
 cut = 1.12246 * sig
-eps = 1.0
+eps = 1
 shift = 0.25
 
 system.non_bonded_inter[0, 1].lennard_jones.set_params(
-    epsilon=eps, sigma=sig, cutoff=cut, shift=shift)
-system.non_bonded_inter[0, 2].lennard_jones.set_params(
-    epsilon=eps, sigma=sig, cutoff=cut, shift=shift)
-system.non_bonded_inter[0, 3].lennard_jones.set_params(
-    epsilon=eps, sigma=sig, cutoff=cut, shift=shift)
-system.non_bonded_inter[0, 4].lennard_jones.set_params(
     epsilon=eps, sigma=sig, cutoff=cut, shift=shift)
 
 ##########################################################################
@@ -164,14 +165,14 @@ system.non_bonded_inter[0, 4].lennard_jones.set_params(
 npart = 500
 for cntr in range(npart):
     if cntr % 2 == 0:
-        x = 0.25 * length
+        x = 0.25 * box_l[0]
     else:
-        x = 0.75 * length
+        x = 0.75 * box_l[0]
 
-    y = (diameter + 4) / 2.0
-    z = (diameter + 4) / 2.0
+    y = box_l[1] / 2.
+    z = box_l[2] / 2.
 
-    theta = float(2 * np.random.random() * np.pi)
+    theta = float(np.random.random() * np.pi)
     phi = float(2 * np.random.random() * np.pi)
     quats = a2quat(theta, phi)
 
@@ -192,7 +193,7 @@ with open("{}/CMS_{}.dat".format(outdir, vel), "w") as outfile:
     # Production run
     dev_sum = 0.0
     dev_av = 0.0
-    time_0 = system.time
+    system.time = 0.
     for i in range(prod_steps):
         if (i + 1) % 5 == 0:
             print('\rprogress: %.0f%%' % ((i + 1) * 100. / prod_steps), end='')
@@ -200,20 +201,21 @@ with open("{}/CMS_{}.dat".format(outdir, vel), "w") as outfile:
 
         # We output the coordinate of the center of mass in
         # the direction of the long axis, here we consider
-        # the deviation from the center
+        # the deviation from the center (keep the padding in mind)
 
-        dev = system.galilei.system_CMS()[0] - 0.5 * length
+        dev = system.galilei.system_CMS()[0] - 0.5 * box_l[0]
 
         if i > 0:
             dev_sum = dev_sum + dev
             dev_av = dev_sum / i
 
-        time = system.time - time_0
-
-        print("{} {} {}".format(time, dev, dev_av), file=outfile)
+        print("{} {} {}".format(system.time, dev, dev_av), file=outfile)
 
         system.integrator.run(prod_length)
-
-# Output the final configuration
-system.part.writevtk("{}/points_{}.vtk".format(outdir, vel), types=[0])
+        
 print()
+
+# Output the geometry and final configuration
+
+lbf.print_vtk_boundary("{}/boundary.vtk".format(outdir))
+system.part.writevtk("{}/points_{}.vtk".format(outdir, vel), types=[0])
