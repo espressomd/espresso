234cf458c65738f493926ceaa51460c8a7866dc9
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2019-09-26 20:49:11 +0200

    Add missing license headers

--- maintainer/check_features.py
+++ maintainer/check_features.py
@@ -16,8 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+
 # Check whether all features used in the code are defined
-#
 import sys
 import os
 sys.path.append(os.path.join(sys.path[0], '..', 'config'))
--- maintainer/files_with_header.sh
+++ maintainer/files_with_header.sh
@@ -1,21 +1,22 @@
 # Copyright (C) 2012-2018 The ESPResSo project
 # Copyright (C) 2012 Olaf Lenz
-#  
+#
 # This file is part of ESPResSo.
-#  
+#
 # ESPResSo is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#  
+#
 # ESPResSo is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#  
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 #
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 git ls-files --exclude-standard |
 egrep -v '\.(blk|gz|data|dat|tab|chk|jpg|png|pdf|fig|gif|xcf|bib|vtf|vtk|svg|ico|eps)$' |
 egrep -v '^testsuite/configs/|^old/|^cmake/|^config/' |
