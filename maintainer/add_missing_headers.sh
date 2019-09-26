ebe5e018b8c904ffd8a3f9ff7990744de8444035
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2019-09-26 21:06:29 +0200

    Update regexes that add license headers

--- maintainer/add_missing_headers.sh
+++ maintainer/add_missing_headers.sh
@@ -21,20 +21,22 @@
 # Get files without headers
 all_files=`maintainer/files_with_header.sh`
 files_to_check=`grep -iL copyright $all_files`
-py_files=`echo $files_to_check|tr " " "\n" |grep \.py$`
-cpp_files=`echo $files_to_check|tr " " "\n" |grep '\.[c|h]pp$'`
+py_files=`echo $files_to_check|tr " " "\n" |grep -P '\.(pyx?|pxd|sh)$'`
+cpp_files=`echo $files_to_check|tr " " "\n" |grep -P '\.([c|h]pp|cuh?|dox)$'`
 
 tmp=`mktemp`
+# process python/cython/bash files
 for f in `echo $py_files`
 do
-(sed -e 's/^/# /' maintainer/header_template.txt; cat $f) >$tmp
+(sed -e 's/^/# /' maintainer/header_template.txt | sed 's/ $//'; cat $f) >$tmp
 cp $tmp $f
 done
+# process c++/cuda/doxygen files
 for f in `echo $cpp_files`
 do
 (
 echo '/*'
-cat maintainer/header_template.txt
+sed -e 's/^/ * /' maintainer/header_template.txt | sed 's/ $//'
 echo '*/' 
 cat $f) >$tmp
 cp $tmp $f
