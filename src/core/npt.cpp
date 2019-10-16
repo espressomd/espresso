179bec39dd98ecc2f386d0fa40aa4298e06560cb
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2019-10-16 10:01:57 +0200

    Cleanup comments

--- src/core/npt.cpp
+++ src/core/npt.cpp
@@ -16,11 +16,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include "npt.hpp"
 #include "communication.hpp"
 #include "config.hpp"
 #include "errorhandling.hpp"
 #include "integrate.hpp"
-#include "npt.hpp"
 
 #ifdef NPT
 void synchronize_npt_state(int n_steps) {
