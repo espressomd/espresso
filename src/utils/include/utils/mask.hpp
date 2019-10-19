bc07c6432a551811259beb954dadc2033088d1b5
Florian Weik <fweik@icp.uni-stuttgart.de>
2019-10-19 22:15:12 +0200

    utils: Improve docstring of mask

--- src/utils/include/utils/mask.hpp
+++ src/utils/include/utils/mask.hpp
@@ -16,20 +16,28 @@ auto mask_impl(Integral mask, T t, std::index_sequence<I...>) {
 } // namespace detail
 
 /**
- * @brief Set elements of a tuple-like to zero by a bit mask.
+ * @brief Pick elements of a tuple-like by a bit mask.
+ *
+ * E.g. every element of the input for which the corresponding
+ * bit is set in the mask is set is copied to the output unmodified,
+ * the elements that are not set are set to zero (default constructed
+ * instance of the type).
+ *
+ * Example:
+ *   mask(0b1011, {1, 2, 3, 4}) => {1, 0, 3, 4}
  *
  * @tparam T implements the tuple interface(get, tuple_size, ...)
  * @tparam Integral An unsigned integral type
  * @param mask bit mask, if the i-th bit is set, the i-th element
- *        in @param t is set to zero.
- * @param t
+ *        in @param t is copied to the output, otherwise it is set to zero.
+ * @param t input elements
  * @return t partially zeroed out according to mask
  */
 template <class T, class Integral>
-std::enable_if_t<std::is_unsigned<Integral>::value &&
-                     (8 * sizeof(Integral) >= tuple_size<T>::value),
-                 T>
-mask(Integral mask, T t) {
+auto mask(Integral mask, T t)
+    -> std::enable_if_t<std::is_unsigned<Integral>::value &&
+                            (8 * sizeof(Integral) >= tuple_size<T>::value),
+                        T> {
   return detail::mask_impl(mask, t,
                            std::make_index_sequence<tuple_size<T>::value>{});
 }
