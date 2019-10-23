2964e017f117b5bae9334583e1f827dd8fda2070
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2019-10-23 17:48:43 +0200

    Fix velocity Verlet interface
    When setting up the NPT integrator, then the VV integrator, the core
    integrator remains on NPT. Since NVT and VV are the same, we can set
    the core integrator to NVT when the interface is set to VV.

--- src/python/espressomd/integrate.pyx
+++ src/python/espressomd/integrate.pyx
@@ -50,6 +50,8 @@ cdef class Integrator:
         self._method = state['_method']
         if self._method == "STEEPEST_DESCENT":
             self.set_steepest_descent(state['_steepest_descent_params'])
+        elif self._method == "VV":
+            self.set_vv()
         elif self._method == "NVT":
             self.set_nvt()
         elif self._method == "NPT":
@@ -118,6 +120,7 @@ cdef class Integrator:
         Set the integration method to Velocity Verlet.
 
         """
+        integrate_set_nvt()
         self._method = "VV"
 
     def set_nvt(self):
