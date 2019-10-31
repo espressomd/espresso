LB Boundary force: fix unit conversion and tests
Reference solutions for forces on shear planes and for stokes drag need dynamic viscosity, but LB uses kinetmatic viscosity.
This switches the tests to dynamic viscosity and removes a 1/rho from the unit conversion in the script interface.
Also, a force balance test is added, which does not depend on the density.

testsuite/python/lb_shear.py
@@ -37,8 +37,8 @@ TIME_STEP = 0.02
 # Box size will be H +2 AGRID to make room for walls.
 # The number of grid cells should be divisible by four and 3 in all directions
 # for testing on multiple mpi nodes.
-H = 12 * AGRID 
-W = 6 * AGRID  
+H = 12 * AGRID
+W = 6 * AGRID
 SHEAR_VELOCITY = 0.3
 
 LB_PARAMS = {'agrid': AGRID,
@@ -83,7 +83,6 @@ def shear_flow(x, t, nu, v, h, k_max):
 class LBShearCommon:
 
     """Base class of the test that holds the test logic."""
-    lbf = None
     system = espressomd.System(box_l=[H + 2. * AGRID,
                                       W,
                                       W])
@@ -98,9 +97,14 @@ class LBShearCommon:
         """
         self.system.lbboundaries.clear()
         self.system.actors.clear()
+        try: 
+            del self.lbf
+        except BaseException:
+            pass
         self.system.box_l = np.max(
             ((W, W, W), shear_plane_normal * (H + 2 * AGRID)), 0)
 
+        self.lbf = self.lb_class(**LB_PARAMS)
         self.system.actors.add(self.lbf)
 
         wall_shape1 = espressomd.shapes.Wall(
@@ -164,8 +168,8 @@ class LBShearCommon:
             np.copy(wall1.get_force()),
             -np.copy(wall2.get_force()),
             atol=1E-4)
-        np.testing.assert_allclose(np.copy(wall1.get_force()), 
-                                   shear_direction * SHEAR_VELOCITY / H * W**2 * VISC, atol=2E-4)
+        np.testing.assert_allclose(np.dot(np.copy(wall1.get_force()), shear_direction),
+                                   SHEAR_VELOCITY / H * W**2 * dynamic_viscosity, atol=2E-4)
 
     def test(self):
         x = np.array((1, 0, 0), dtype=float)
@@ -185,7 +189,7 @@ class LBCPUShear(ut.TestCase, LBShearCommon):
     """Test for the CPU implementation of the LB."""
 
     def setUp(self):
-        self.lbf = espressomd.lb.LBFluid(**LB_PARAMS)
+        self.lb_class = espressomd.lb.LBFluid
 
 
 @utx.skipIfMissingGPU()
@@ -195,7 +199,7 @@ class LBGPUShear(ut.TestCase, LBShearCommon):
     """Test for the GPU implementation of the LB."""
 
     def setUp(self):
-        self.lbf = espressomd.lb.LBFluidGPU(**LB_PARAMS)
+        self.lb_class = espressomd.lb.LBFluidGPU
 
 
 if __name__ == '__main__':

testsuite/python/lb_stokes_sphere.py
@@ -42,10 +42,10 @@ LB_PARAMS = {'agrid': AGRID,
              'visc': KVISC,
              'tau': TIME_STEP}
 # System setup
-radius = 8 * AGRID 
-box_width = 62 * AGRID
+radius = 7 * AGRID
+box_width = 52 * AGRID
 real_width = box_width + 2 * AGRID
-box_length = 62 * AGRID
+box_length = 54 * AGRID
 c_s = np.sqrt(1. / 3. * AGRID**2 / TIME_STEP**2)
 v = [0, 0, 0.2 * c_s]  # The boundary slip
 
@@ -91,10 +91,11 @@ class Stokes:
             return np.sqrt(tmp)
 
         last_force = -1000.
-        stokes_force = 6 * np.pi * KVISC * radius * size(v)
+        dynamic_viscosity = self.lbf.viscosity * self.lbf.density
+        stokes_force = 6 * np.pi * dynamic_viscosity * radius * size(v)
         self.system.integrator.run(35)
         while True:
-            self.system.integrator.run(10)
+            self.system.integrator.run(5)
             force = np.linalg.norm(sphere.get_force())
             if np.abs(last_force - force) < 0.01 * stokes_force:
                 break