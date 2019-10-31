b89b70eb26800f92dcf8c73f82a2bcf327052e6a
Florian Weik <fweik@icp.uni-stuttgart.de>
2019-10-31 14:04:16 +0100

    core: Simplify particle removal

--- src/core/communication.cpp
+++ src/core/communication.cpp
@@ -281,10 +281,11 @@ void mpi_remove_particle_slave(int pnode, int part) {
   if (part != -1) {
     n_part--;
 
-    if (pnode == this_node)
+    if (pnode == this_node) {
       local_remove_particle(part);
-
-    remove_all_bonds_to(part);
+    } else {
+      remove_all_bonds_to(part);
+    }
   } else
     local_remove_all_particles();
 
