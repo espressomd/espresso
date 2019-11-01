0e6faea513ce6bd744eabf652e66cf0d8d7f9227
Florian Weik <fweik@icp.uni-stuttgart.de>
2019-11-01 10:49:15 +0100

    testsuite: Fixed ArrayLocked issue

--- testsuite/python/lb_buoyancy_force.py
+++ testsuite/python/lb_buoyancy_force.py
@@ -105,7 +105,7 @@ class Buoyancy(object):
         expected_force = np.array(
             [0, -sphere_volume * DENS * G, 0])
         np.testing.assert_allclose(
-            sphere.get_force(), expected_force,
+            np.copy(sphere.get_force()), expected_force,
             atol=np.linalg.norm(expected_force) * 0.02)
 
 
