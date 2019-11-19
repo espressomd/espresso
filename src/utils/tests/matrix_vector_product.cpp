c5e3795c252ff35d7d36a41af101203101f30e95
Florian Weik <fweik@icp.uni-stuttgart.de>
2019-11-19 14:33:19 +0100

    core: Fixed bitwise comparison of floating point numbers

--- src/utils/tests/matrix_vector_product.cpp
+++ src/utils/tests/matrix_vector_product.cpp
@@ -24,6 +24,8 @@
 
 #include "utils/math/matrix_vector_product.hpp"
 
+#include <limits>
+
 extern constexpr std::array<std::array<int, 3>, 3> matrix{
     {{{1, 2, 9}}, {{8, 41, 6}}, {{31, 15, 99}}}};
 
@@ -31,6 +33,7 @@ BOOST_AUTO_TEST_CASE(inner_product) {
   const std::array<double, 3> vector{{0.5, 1.25, 3.1}};
   auto const result = Utils::matrix_vector_product<double, 3, matrix>(vector);
   for (int i = 0; i < 3; ++i) {
-    BOOST_CHECK(result[i] == boost::inner_product(matrix[i], vector, 0.0));
+    BOOST_CHECK_CLOSE(result[i], boost::inner_product(matrix[i], vector, 0.0),
+                      std::numeric_limits<double>::epsilon());
   }
 }
