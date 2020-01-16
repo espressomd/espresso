7baca7331cfa4e14432f9a634494ab7ac3ef717b
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2020-01-16 18:25:02 +0100

    Testsuite: LB Momentum conservation

--- testsuite/python/lb_momentum_conservation.py
+++ testsuite/python/lb_momentum_conservation.py
@@ -23,19 +23,33 @@ import unittest_decorators as utx
 import numpy as np
 
 # Define the LB Parameters
-TIME_STEP = 0.005
+TIME_STEP = 0.004
 AGRID = 1.0
+GRID_SIZE = 6
 KVISC = 5
 DENS = 1
-BOX_SIZE = 6 * AGRID
 F = 0.05
 GAMMA = 5
+
+
+
+# Tolerance in average LB velocity in lattice units
+TOL_V_LATTICE = 1E-7 
+# TOLERANCE FOR MOMENTUM
+# (num latticer sites * total mass * tolerance for lattice_velocity * unit)
+lattice_velocity = AGRID / TIME_STEP
+n_sites = GRID_SIZE**3
+box_volume = n_sites * AGRID**3
+fluid_mass = box_volume * DENS
+tolerance = TOL_V_LATTICE * lattice_velocity * fluid_mass
+print(tolerance)
+
 
 LB_PARAMS = {'agrid': AGRID,
              'dens': DENS,
              'visc': KVISC,
              'tau': TIME_STEP,
-             'ext_force_density': [-.5 * F, F, .8 *F]}
+             'ext_force_density': [-.5 * F, .3 * F, .8 *F]}
 
 
 class Momentum(object):
@@ -46,7 +60,7 @@ class Momentum(object):
 
     """
     lbf = None
-    system = espressomd.System(box_l=[BOX_SIZE] * 3)
+    system = espressomd.System(box_l=[GRID_SIZE * AGRID] * 3)
     system.time_step = TIME_STEP
     system.cell_system.skin = 0.01
 
@@ -66,16 +80,19 @@ class Momentum(object):
 
         p = self.system.part.add(
             pos=(0, 0, 0), ext_force=-applied_force, v=[.1, .2, .3])
-        initial_momentum = self.system.analysis.linear_momentum()
+        initial_momentum = np.array(self.system.analysis.linear_momentum())
         np.testing.assert_allclose(initial_momentum, np.copy(p.v) * p.mass)
         
+        f_half_compensation = np.array(self.lbf.ext_force_density * TIME_STEP *self.system.volume()/2)
+        i=0
         while True: 
             self.system.integrator.run(100)
             measured_momentum = self.system.analysis.linear_momentum()
-            f_half_compensation = self.lbf.ext_force_density * TIME_STEP *DENS *self.system.volume()/2
             stokes_force_compensation = p.f * TIME_STEP  # fluid force is opposed to particle force
-            np.testing.assert_allclose(measured_momentum + f_half_compensation +stokes_force_compensation, initial_momentum, atol=1.4E-4)
-            if np.linalg.norm(stokes_force_compensation) < 1E-4: break
+
+            np.testing.assert_allclose(measured_momentum, 
+                initial_momentum - f_half_compensation - stokes_force_compensation, atol=tolerance)
+            if np.linalg.norm(stokes_force_compensation) < 1E-6: break
 
 
         # Make sure, the particle has crossed the periodic boundaries
@@ -83,7 +100,7 @@ class Momentum(object):
             max(
                 np.abs(p.v) *
                 self.system.time),
-            BOX_SIZE)
+            self.system.box_l[0])
 
 
 @utx.skipIfMissingFeatures(['LB_WALBERLA', 'EXTERNAL_FORCES'])
