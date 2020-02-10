b4e6d0cef6cdc7eaf092768f957a456aaa02e9c0
Patrick Kreissl <patrick.kreissl@pa-le.de>
2020-02-10 12:31:22 +0100

    Get rid of unused variables.

--- src/core/shapes/Ellipsoid.cpp
+++ src/core/shapes/Ellipsoid.cpp
@@ -51,7 +51,6 @@ void Ellipsoid::calculate_dist(const Utils::Vector3d &pos, double &dist,
   }
 
   /* calculate dist and vec */
-  double distance = 0.;
   for (int i = 0; i < 3; i++) {
     vec[i] = (ppos_e[i] - Utils::sqr(m_semiaxes[i]) * ppos_e[i] /
                               (l + Utils::sqr(m_semiaxes[i])));
