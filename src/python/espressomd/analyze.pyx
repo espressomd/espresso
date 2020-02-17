224d1b03358bf4b2e6b2bc1ea5397cf2292b941f
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2020-02-17 16:39:25 +0100

    Fix #3494

--- src/python/espressomd/analyze.pyx
+++ src/python/espressomd/analyze.pyx
@@ -32,6 +32,7 @@ from espressomd.interactions cimport *
 import numpy as np
 cimport numpy as np
 from globals cimport n_configs
+from .globals import Globals
 
 from .utils import array_locked
 
@@ -939,7 +940,7 @@ class Analysis:
                 n_conf = n_configs
 
         if r_max is None:
-            r_max = min_box_l / 2.0
+            r_max = min(Globals().box_l) / 2
 
         cdef vector[double] rdf
         rdf.resize(r_bins)
@@ -1015,7 +1016,7 @@ class Analysis:
             raise ValueError("type_list_b has to be a list!")
 
         if r_max is None:
-            r_max = min_box_l / 2.0
+            r_max = min(Globals().box_l) / 2
 
         if r_min < 0.0 or (log_flag == 1 and r_min == 0.0):
             raise ValueError("r_min was chosen too small!")
