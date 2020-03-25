d0108611856c68d5cea9f077ddec8e53f941a98a
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2020-03-25 11:54:54 +0100

    Fix (for gpu) +test lb velocity interpolation close to boundary

--- testsuite/python/lb_interpolation.py
+++ testsuite/python/lb_interpolation.py
@@ -23,11 +23,11 @@ import espressomd
 import espressomd.shapes
 import espressomd.lb
 
-AGRID = 1.5
+AGRID = 1.5 
 VISC = 4.2
 DENS = 1.3
 FRIC = 1.4
-TAU = 0.2
+TAU = 0.3
 BOX_L = 18.0
 TIME_STEP = TAU
 LB_PARAMETERS = {
@@ -57,9 +57,9 @@ class LBInterpolation:
     def set_boundaries(self, velocity):
         """Place boundaries *not* exactly on a LB node."""
         wall_shape1 = espressomd.shapes.Wall(
-            normal=[1, 0, 0], dist=0.6 * AGRID)
+            normal=[1, 0, 0], dist=AGRID)
         wall_shape2 = espressomd.shapes.Wall(
-            normal=[-1, 0, 0], dist=-(BOX_L - 0.6 * AGRID))
+            normal=[-1, 0, 0], dist=-(BOX_L - AGRID))
         self.system.lbboundaries.add(
             espressomd.lbboundaries.LBBoundary(shape=wall_shape1))
         self.system.lbboundaries.add(
@@ -72,9 +72,21 @@ class LBInterpolation:
         """
         self.set_boundaries([0.0, 0.0, V_BOUNDARY])
         self.system.integrator.run(250)
-        # Shear plane for boundary 1
-        # for pos in itertools.product((AGRID,), np.arange(0.5 * AGRID, BOX_L, AGRID), np.arange(0.5 * AGRID, BOX_L, AGRID)):
-        #     np.testing.assert_almost_equal(self.lbf.get_interpolated_velocity(pos)[2], 0.0)
+        # Check interpolated vel at upper boundary. The node position is at 
+        # box_l[0]-agrid/2.
+        np.testing.assert_allclose(self.lbf.get_interpolated_velocity(
+            [self.system.box_l[0] - AGRID / 2, 0, 0]), np.array([0, 0, V_BOUNDARY]))
+
+        # Cehck interpolated velocity involving boundary and neighboring node
+        # The boundary node index is lbf.shape[0]-1, so -2 refers to the
+        # node in front of the boundary. 
+        node_next_to_boundary = self.lbf[
+            self.lbf.shape[0] - 2, 0, 0]
+        # The midpoint between the boundary and
+        # that node is box_l - agrid.
+        np.testing.assert_allclose(self.lbf.get_interpolated_velocity([self.system.box_l[0] - AGRID, 0, 0]),
+                                   0.5 * (np.array([0, 0, V_BOUNDARY]) + node_next_to_boundary.velocity))
+
         # Bulk
         for pos in itertools.product(
                 np.arange(1.5 * AGRID, BOX_L - 1.5 * AGRID, 0.5 * AGRID),
