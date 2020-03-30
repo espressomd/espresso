14ac4aefa7f63fbc662444d561e84ff13d7649c8
Kai Szuttor <kai@icp.uni-stuttgart.de>
2020-03-30 16:55:19 +0200

    cmake-format.

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -2,27 +2,21 @@ if(GIT_EXECUTABLE)
   # Get the name of the working branch
   execute_process(
     COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_BRANCH
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR} OUTPUT_VARIABLE GIT_BRANCH
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   # Get the latest abbreviated commit hash of the working branch
   execute_process(
     COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_COMMIT_HASH
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
+    OUTPUT_VARIABLE GIT_COMMIT_HASH OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   # Get branch status
   execute_process(
     COMMAND ${GIT_EXECUTABLE} diff-index --quiet HEAD --
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     RESULT_VARIABLE GIT_DIFF_INDEX_RESULT
-    OUTPUT_VARIABLE GIT_DIFF_INDEX_OUTPUT
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
+    OUTPUT_VARIABLE GIT_DIFF_INDEX_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
   if(GIT_DIFF_INDEX_RESULT EQUAL 0)
     set(GIT_STATE "CLEAN")
   else()
@@ -32,4 +26,5 @@ if(GIT_EXECUTABLE)
 endif(GIT_EXECUTABLE)
 
 configure_file(${PROJECT_SOURCE_DIR}/src/config/version.hpp.in version.hpp.tmp)
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different version.hpp.tmp version.hpp)
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different version.hpp.tmp
+                        version.hpp)
