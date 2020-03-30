0c8a23e4db3fe57576073c17cc75aa1f3e7d82e3
Kai Szuttor <kai@icp.uni-stuttgart.de>
2020-03-30 11:12:52 +0200

    maintainer: added new style checking via pre-commit tool.

--- maintainer/CI/fix_style.sh
+++ maintainer/CI/fix_style.sh
@@ -18,10 +18,6 @@
 
 cd "$(git rev-parse --show-toplevel)"
 
-CLANG_FORMAT_VER=6.0
-AUTOPEP8_VER=1.3.4
-PYCODESTYLE_VER=2.3.1
-
 if ! git diff-index --quiet HEAD -- && [ "${1}" != "-f" ]; then
     echo "Warning, your working tree is not clean. Please commit your changes."
     echo "You can also call this script with the -f flag to proceed anyway, but"
@@ -29,25 +25,10 @@ if ! git diff-index --quiet HEAD -- && [ "${1}" != "-f" ]; then
     exit 1
 fi
 
-CLANGFORMAT="$(which clang-format-${CLANG_FORMAT_VER})"
-if [ "${CLANGFORMAT}" = "" ]; then
-    CLANGFORMAT="$(which clang-format)"
-    if ! "${CLANGFORMAT}" --version | grep -qEo "version ${CLANG_FORMAT_VER}\.[0-9]+"; then
-        echo "Could not find clang-format ${CLANG_FORMAT_VER}. ${CLANGFORMAT} is $(${CLANGFORMAT} --version | grep -Eo '[0-9\.]{5}' | head -n 1)."
-        exit 2
-    fi
-fi
 
-AUTOPEP8="$(which autopep8)"
-if ! "${AUTOPEP8}" --version 2>&1 | grep -qFo "autopep8 ${AUTOPEP8_VER} (pycodestyle: ${PYCODESTYLE_VER})"; then
-    echo "Could not find autopep8 ${AUTOPEP8_VER} with pycodestyle ${PYCODESTYLE_VER}"
-    echo "${AUTOPEP8} is $(${AUTOPEP8} --version 2>&1)"
-    exit 2
-fi
-
-find . \( -name '*.hpp' -o -name '*.cpp' -o -name '*.cu' -o -name '*.cuh' \) -not -path './libs/*' | xargs -r -n 5 -P 8 "${CLANGFORMAT}" -i -style=file || exit 3
-find . \( -name '*.py' -o -name '*.pyx' -o -name '*.pxd' \) -not -path './libs/*' | xargs -r -n 5 -P 8 "${AUTOPEP8}" --ignore=E266,W291,W293 --in-place --aggressive || exit 3
-find . -type f -executable ! -name '*.sh' ! -name '*.py' ! -name '*.sh.in' ! -name pypresso.cmakein -not -path './.git/*' | xargs -r -n 5 -P 8 chmod -x || exit 3
+find . \( -name '*.hpp' -o -name '*.cpp' -o -name '*.cu' -o -name '*.cuh' \) -not -path './libs/*' | xargs -n 5 -P 8 bash maintainer/CI/clang-format.sh -i -style=file || exit 3
+find . \( -name '*.py' -o -name '*.pyx' -o -name '*.pxd' \) -not -path './libs/*' | xargs -n 5 -P 8 bash maintainer/CI/autopep8.sh --ignore=E266,W291,W293 --in-place --aggressive || exit 3
+find . -type f -perm +111 ! -name '*.sh' ! -name '*.py' ! -name '*.sh.in' ! -name pypresso.cmakein -not -path './.git/*' | xargs -n 5 -P 8 chmod -x || exit 3
 
 if [ "${CI}" != "" ]; then
     git --no-pager diff > style.patch
