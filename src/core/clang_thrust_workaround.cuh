12dcf03dafa1a225b3ac2f453f447a5bb9144391
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2020-04-09 18:31:29 +0200

    Missing file

---
+++ src/core/clang_thrust_workaround.cuh
@@ -0,0 +1,19 @@
+// This works around a bug which prevents compilation of trhust with Clang
+// https://github.com/thrust/thrust/issues/1032 (comment)
+
+#ifndef CLANG_THRUST_WORKAROUND_CUH
+#define CLANG_THRUST_WORKAROUND_CUH
+
+#ifdef __clang__
+#define THRUST_CUB_NS_PREFIX namespace thrust::cuda_cub {
+#define THRUST_CUB_NS_POSTFIX }
+
+#include <thrust/system/cuda/detail/cub/util_debug.cuh>
+
+using namespace thrust::cuda_cub::cub;
+#include <thrust/device_ptr.h>
+#include <thrust/reduce.h>
+#endif
+
+
+#endif
