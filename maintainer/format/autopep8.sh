4f97bfbc775fa88077b17071be4bda1bd8031138
Kai Szuttor <kai@icp.uni-stuttgart.de>
2020-04-15 14:14:55 +0200

    maintainer: use python -m for autopep8.

--- maintainer/format/autopep8.sh
+++ maintainer/format/autopep8.sh
@@ -20,14 +20,15 @@
 AUTOPEP8_VER=1.3.4
 PYCODESTYLE_VER=2.3.1
 
-if hash autopep8 2>/dev/null; then
-    AUTOPEP8="$(which autopep8)"
+python3 -m autopep8 --help 2>&1 > /dev/null
+if [ "$?" = "0" ]; then
+    AUTOPEP8="python3 -m autopep8"
 else
     echo "No autopep8 found."
     exit 2
 fi
 
-if ! "${AUTOPEP8}" --version 2>&1 | grep -qFo "autopep8 ${AUTOPEP8_VER} (pycodestyle: ${PYCODESTYLE_VER})"; then
+if ! ${AUTOPEP8} --version 2>&1 | grep -qFo "autopep8 ${AUTOPEP8_VER} (pycodestyle: ${PYCODESTYLE_VER})"; then
     echo "Could not find autopep8 ${AUTOPEP8_VER} with pycodestyle ${PYCODESTYLE_VER}"
     echo "${AUTOPEP8} is $(${AUTOPEP8} --version 2>&1)"
     exit 2
