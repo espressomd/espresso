b2218b22329dc917bf56274df53c58a75fc16a2d
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2020-05-29 20:17:45 +0200

    Fix LB samples

--- samples/lbf.py
+++ samples/lbf.py
@@ -21,13 +21,6 @@ Set up a lattice-Boltzmann fluid and apply an external force density on it.
 """
 import matplotlib.pyplot as plt
 import numpy as np
-import argparse
-
-parser = argparse.ArgumentParser(epilog=__doc__)
-group = parser.add_mutually_exclusive_group()
-group.add_argument('--cpu', action='store_true')
-group.add_argument('--gpu', action='store_true')
-args = parser.parse_args()
 
 
 print("""
@@ -37,13 +30,6 @@ print("""
 """)
 
 required_features = ["EXTERNAL_FORCES"]
-if args.gpu:
-    print("Using GPU implementation")
-    required_features.append("CUDA")
-else:
-    print("Using CPU implementation")
-    if not args.cpu:
-        print("(select the implementation with --cpu or --gpu)")
 
 import espressomd
 espressomd.assert_features(required_features)
@@ -58,14 +44,8 @@ system.cell_system.skin = 0.1
 
 system.part.add(pos=[box_l / 2.0] * 3, fix=[True, True, True])
 
-
-lb_params = {'agrid': 1, 'dens': 1, 'visc': 1, 'tau': 0.01,
-             'ext_force_density': [0, 0, -1.0 / (box_l**3)]}
-
-if args.gpu:
-    lbf = espressomd.lb.LBFluidGPU(**lb_params)
-else:
-    lbf = espressomd.lb.LBFluid(**lb_params)
+lbf = espressomd.lb.LBFluidWalberla(agrid=1, dens=1, visc=1, tau=0.01,
+                                    ext_force_density=[0, 0, -1.0 / (box_l**3)])
 system.actors.add(lbf)
 system.thermostat.set_lb(LB_fluid=lbf, gamma=1.0)
 print(lbf.get_params())
