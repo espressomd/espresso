d1968d4a7dc4394fea27d23256457d0b7f7d3075
Jakub Krajniak <jkrajniak@gmail.com>
2020-06-08 17:48:01 +0200

    Remove unnecessary import

--- src/python/espressomd/io/writer/h5md.py
+++ src/python/espressomd/io/writer/h5md.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Interface module for the H5md core implementation."""
-import collections
 import dataclasses
 import sys
 
