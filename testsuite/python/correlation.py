17da023d015d35e0b5ca18fac160795ba281e53e
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2020-06-27 18:16:11 +0200

    correlators: Add tests

--- testsuite/python/correlation.py
+++ testsuite/python/correlation.py
@@ -34,9 +34,20 @@ class CorrelatorTest(ut.TestCase):
     def tearDown(self):
         self.system.part.clear()
 
-    def test(self):
+    def calc_tau(self, time_step, tau_lin, length):
+        tau = []
+        for i in range(tau_lin):
+            tau.append(i)
+        factor = 1
+        while len(tau) < length:
+            p = tau[-1] + factor * 1
+            for i in range(0, tau_lin, 2):
+                tau.append(p + factor * i)
+            factor *= 2
+        return time_step * np.array(tau[:length])
+
+    def test_square_distance_componentwise(self):
         s = self.system
-        s.thermostat.turn_off()
         s.part.add(id=0, pos=(0, 0, 0), v=(1, 2, 3))
 
         obs = espressomd.observables.ParticlePositions(ids=(0,))
@@ -54,11 +65,80 @@ class CorrelatorTest(ut.TestCase):
         acc_unpickeled = pickle.loads(pickle.dumps(acc))
         np.testing.assert_array_equal(corr, acc_unpickeled.result())
 
+        tau = self.calc_tau(s.time_step, acc.tau_lin, corr.shape[0])
+        np.testing.assert_array_almost_equal(corr[:, 0], tau)
         for i in range(corr.shape[0]):
             t = corr[i, 0]
             self.assertAlmostEqual(corr[i, 2], t * t, places=3)
             self.assertAlmostEqual(corr[i, 3], 4 * t * t, places=3)
             self.assertAlmostEqual(corr[i, 4], 9 * t * t, places=3)
+
+    def test_tensor_product(self):
+        s = self.system
+        v = np.array([1, 2, 3])
+        s.part.add(id=0, pos=(0, 0, 0), v=v)
+
+        obs = espressomd.observables.ParticleVelocities(ids=(0,))
+        acc = espressomd.accumulators.Correlator(
+            obs1=obs, tau_lin=10, tau_max=10.0, delta_N=1,
+            corr_operation="tensor_product")
+
+        s.integrator.run(1000)
+        s.auto_update_accumulators.add(acc)
+        s.integrator.run(20000)
+
+        corr = acc.result()
+
+        tau = self.calc_tau(s.time_step, acc.tau_lin, corr.shape[0])
+        np.testing.assert_array_almost_equal(corr[:, 0], tau)
+        for i in range(corr.shape[0]):
+            np.testing.assert_array_almost_equal(corr[i, 2:], np.kron(v, v))
+
+    def test_componentwise_product(self):
+        s = self.system
+        v = np.array([1, 2, 3])
+        s.part.add(id=0, pos=(0, 0, 0), v=v)
+
+        obs = espressomd.observables.ParticleVelocities(ids=(0,))
+        acc = espressomd.accumulators.Correlator(
+            obs1=obs, tau_lin=10, tau_max=10.0, delta_N=1,
+            corr_operation="componentwise_product")
+
+        s.integrator.run(1000)
+        s.auto_update_accumulators.add(acc)
+        s.integrator.run(20000)
+
+        corr = acc.result()
+
+        tau = self.calc_tau(s.time_step, acc.tau_lin, corr.shape[0])
+        np.testing.assert_array_almost_equal(corr[:, 0], tau)
+        for i in range(corr.shape[0]):
+            np.testing.assert_array_almost_equal(corr[i, 2:], v**2)
+
+    def test_scalar_product(self):
+        s = self.system
+        v = np.array([1, 2, 3])
+        s.part.add(id=0, pos=(0, 0, 0), v=v)
+
+        obs = espressomd.observables.ParticleVelocities(ids=(0,))
+        acc = espressomd.accumulators.Correlator(
+            obs1=obs, tau_lin=10, tau_max=10.0, delta_N=1,
+            corr_operation="scalar_product")
+
+        s.integrator.run(1000)
+        s.auto_update_accumulators.add(acc)
+        s.integrator.run(20000)
+
+        corr = acc.result()
+
+        # Check pickling
+        acc_unpickeled = pickle.loads(pickle.dumps(acc))
+        np.testing.assert_array_equal(corr, acc_unpickeled.result())
+
+        tau = self.calc_tau(s.time_step, acc.tau_lin, corr.shape[0])
+        np.testing.assert_array_almost_equal(corr[:, 0], tau)
+        for i in range(corr.shape[0]):
+            np.testing.assert_array_almost_equal(corr[i, 2:], np.sum(v**2))
 
 
 if __name__ == "__main__":
--- testsuite/python/save_checkpoint.py
+++ testsuite/python/save_checkpoint.py
@@ -110,12 +110,16 @@ if espressomd.has_features('P3M') and 'P3M.CPU' in modes:
 
 obs = espressomd.observables.ParticlePositions(ids=[0, 1])
 acc = espressomd.accumulators.MeanVarianceCalculator(obs=obs)
+acc_time_series = espressomd.accumulators.TimeSeries(obs=obs)
 acc.update()
+acc_time_series.update()
 system.part[0].pos = [1.0, 2.0, 3.0]
 acc.update()
+acc_time_series.update()
 
 
 system.auto_update_accumulators.add(acc)
+system.auto_update_accumulators.add(acc_time_series)
 
 # constraints
 system.constraints.add(shape=Sphere(center=system.box_l / 2, radius=0.1),
@@ -187,6 +191,7 @@ if 'THERM.LB' not in modes:
     system.part[1].add_bond((thermalized_bond, 0))
 checkpoint.register("system")
 checkpoint.register("acc")
+checkpoint.register("acc_time_series")
 # calculate forces
 system.integrator.run(0)
 particle_force0 = np.copy(system.part[0].f)
