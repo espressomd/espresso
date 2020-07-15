fed5fcb3e091a4478c8d21318ea0a19193344742
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2020-07-15 16:28:05 +0200

    Fix Walberla test

--- src/core/unit_tests/LbWalberla.cpp
+++ src/core/unit_tests/LbWalberla.cpp
@@ -11,7 +11,7 @@
 
 #include "boost/mpi.hpp"
 #include "grid.hpp"
-#include "grid_based_algorithms/LbWalberlaD3Q19Thermalized.hpp"
+#include "grid_based_algorithms/LbWalberlaD3Q19MRT.hpp"
 #include "grid_based_algorithms/LbWalberla_impl.hpp"
 #include "grid_based_algorithms/lb_interface.hpp"
 #include "grid_based_algorithms/lb_walberla_instance.hpp"
@@ -20,7 +20,7 @@
 
 using Utils::Vector3d;
 using Utils::Vector3i;
-using walberla::LbWalberlaD3Q19Thermalized;
+using walberla::LbWalberlaD3Q19MRT;
 
 double viscosity = 3;
 Vector3d box_dimensions = {10, 12, 14};
@@ -32,33 +32,9 @@ double tau = 0.34;
 double density = 2.5;
 Vector3i mpi_shape;
 
-// BOOST_AUTO_TEST_CASE(field_layout) {
-//  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(viscosity,
-//  density, agrid, tau,
-//                                             box_dimensions, mpi_shape, 2);
-//  lb.add_force_at_pos(Vector3d{{1.5,0.5,3.5}},Vector3d{{1,2,3}});
-//  lb.integrate();
-//  double tol=1E-14;
-//  for (int i=0;i<grid_dimensions[0];i++)
-//    for (int j=0;j<grid_dimensions[1];j++)
-//      for (int k=0;k<grid_dimensions[2];k++) {
-//        auto v = lb.get_node_velocity(Vector3i{{i,j,k}});
-//        if (v) {
-//          if ((*v).norm()>tol) {
-//             printf("%d %d %d: %g %g %g\n",i,j,k,(*v)[0],(*v)[1],(*v)[2]);
-//          }
-//        }
-//        auto f = lb.get_node_last_applied_force(Vector3i{{i,j,k}});
-//        if (f) {
-//          if ((*f).norm()>tol) {
-//             printf("f: %d %d %d: %g %g %g\n",i,j,k,(*f)[0],(*f)[1],(*f)[2]);
-//          }
-//        }
-//      }
-//}
 
 BOOST_AUTO_TEST_CASE(basic_params) {
-  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(
+  LbWalberlaD3Q19MRT lb = LbWalberlaD3Q19MRT(
       viscosity, density, agrid, tau, box_dimensions, mpi_shape, 2);
   BOOST_CHECK(lb.get_grid_dimensions() == grid_dimensions);
   BOOST_CHECK(lb.get_grid_spacing() == agrid);
@@ -72,7 +48,7 @@ BOOST_AUTO_TEST_CASE(basic_params) {
 
 BOOST_AUTO_TEST_CASE(boundary) {
   Vector3d vel = {0.2, 3.8, 4.2};
-  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(
+  LbWalberlaD3Q19MRT lb = LbWalberlaD3Q19MRT(
       viscosity, density, agrid, tau, box_dimensions, mpi_shape, 2);
   for (Vector3i node :
 
@@ -100,33 +76,10 @@ BOOST_AUTO_TEST_CASE(boundary) {
   }
 }
 
-BOOST_AUTO_TEST_CASE(boundary_flow_single_node) {
-  Vector3d vel = {0.2, 3.8, 0};
-  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(
-      viscosity, density, agrid, tau, box_dimensions, mpi_shape, 2);
-  Vector3i node{5, 5, 5};
-  if (lb.node_in_local_halo(node)) {
-    BOOST_CHECK(lb.set_node_velocity_at_boundary(node, vel));
-  }
-  for (int i = 0; i < 100; i++) {
-    lb.integrate();
-  }
-  for (int j = 0; j < 9; j++) {
-    auto v = lb.get_node_velocity(Vector3i{j, node[1], node[2]});
-    if (v) {
-      if (j != node[0]) {
-        printf("%d: %g %g %g\n", j, (*v)[0], (*v)[1], (*v)[2]);
-        BOOST_TEST((*v)[0] > 1E-4);
-        BOOST_TEST((*v)[1] > 1E-4);
-        BOOST_TEST(fabs((*v)[2]) < 1E-11);
-      }
-    }
-  }
-}
 
 BOOST_AUTO_TEST_CASE(boundary_flow_shear) {
   Vector3d vel = {0.2, -0.3, 0};
-  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(
+  LbWalberlaD3Q19MRT lb = LbWalberlaD3Q19MRT(
       viscosity, density, agrid, tau, box_dimensions, mpi_shape, 2);
   int lower = 1, upper = 12;
   for (int x = -1; x <= grid_dimensions[0]; x++) {
@@ -171,8 +124,8 @@ std::vector<Vector3i> all_nodes_incl_ghosts(int n_ghost_layers) {
 
 BOOST_AUTO_TEST_CASE(domain_and_halo) {
   int n_ghost_layers = 2;
-  LbWalberlaD3Q19Thermalized lb =
-      LbWalberlaD3Q19Thermalized(viscosity, density, agrid, tau, box_dimensions,
+  LbWalberlaD3Q19MRT lb =
+      LbWalberlaD3Q19MRT(viscosity, density, agrid, tau, box_dimensions,
                                  mpi_shape, n_ghost_layers);
 
   auto my_left = lb.get_local_domain().first;
@@ -227,8 +180,8 @@ BOOST_AUTO_TEST_CASE(domain_and_halo) {
 
 BOOST_AUTO_TEST_CASE(velocity) {
   int n_ghost_layers = 2;
-  LbWalberlaD3Q19Thermalized lb =
-      LbWalberlaD3Q19Thermalized(viscosity, density, agrid, tau, box_dimensions,
+  LbWalberlaD3Q19MRT lb =
+      LbWalberlaD3Q19MRT(viscosity, density, agrid, tau, box_dimensions,
                                  mpi_shape, n_ghost_layers);
 
   // Values
@@ -287,7 +240,7 @@ BOOST_AUTO_TEST_CASE(velocity) {
 }
 
 BOOST_AUTO_TEST_CASE(total_momentum) {
-  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(
+  LbWalberlaD3Q19MRT lb = LbWalberlaD3Q19MRT(
       viscosity, density, agrid, tau, box_dimensions, mpi_shape, 2);
   auto v = Vector3d{1.5, 2.5, -2.2};
   lb.set_node_velocity(Vector3i{1, 1, 1}, v);
@@ -300,7 +253,7 @@ BOOST_AUTO_TEST_CASE(total_momentum) {
 }
 
 BOOST_AUTO_TEST_CASE(integrate_with_volume_force) {
-  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(
+  LbWalberlaD3Q19MRT lb = LbWalberlaD3Q19MRT(
       viscosity, density, agrid, tau, box_dimensions, mpi_shape, 2);
   auto f = Vector3d{0.01, 0.2, -0.2};
   lb.set_external_force(f);
@@ -309,28 +262,25 @@ BOOST_AUTO_TEST_CASE(integrate_with_volume_force) {
   int n_nodes = grid_dimensions[0] * grid_dimensions[1] * grid_dimensions[2];
   for (int i = 1; i < 30; i++) {
     lb.integrate();
-
-    auto v0 = *(lb.get_node_velocity(Vector3i{{0, 0, 0}}));
-    for (int i = 0; i < grid_dimensions[0]; i++)
-      for (int j = 0; j < grid_dimensions[1]; j++)
-        for (int k = 0; k < grid_dimensions[2]; k++) {
-          BOOST_CHECK_SMALL(
-              (*(lb.get_node_velocity(Vector3i{{i, j, k}})) - v0).norm(),
-              1E-10);
-        }
-
     auto mom = lb.get_momentum() / double(n_nodes);
     auto mom_exp = (i + .5) * f;
     MPI_Allreduce(MPI_IN_PLACE, mom.data(), 3, MPI_DOUBLE, MPI_SUM,
                   MPI_COMM_WORLD);
     BOOST_CHECK_SMALL((mom - mom_exp).norm(), 1E-7);
-    printf("%g %g %g, %g %g %g\n", mom[0], mom[1], mom[2], mom_exp[0],
-           mom_exp[1], mom_exp[2]);
+
+    auto v_exp = mom_exp/density;
+    for (int i = 0; i < grid_dimensions[0]; i++)
+      for (int j = 0; j < grid_dimensions[1]; j++)
+        for (int k = 0; k < grid_dimensions[2]; k++) {
+          auto v = lb.get_node_velocity(Vector3i{{i, j, k}});
+          if (v) BOOST_CHECK_SMALL(((*v)- v_exp).norm(), 1E-10);
+        }
+
   }
 }
 
 BOOST_AUTO_TEST_CASE(integrate_with_point_forces) {
-  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(
+  LbWalberlaD3Q19MRT lb = LbWalberlaD3Q19MRT(
       viscosity, density, agrid, tau, box_dimensions, mpi_shape, 2);
   // auto f = Vector3d{0.15, 0.25, -0.22};
   auto f = Vector3d{0.0006, -0.0013, 0.000528};
@@ -360,7 +310,7 @@ BOOST_AUTO_TEST_CASE(integrate_with_point_forces) {
 }
 
 BOOST_AUTO_TEST_CASE(forces_initial_state) {
-  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(
+  LbWalberlaD3Q19MRT lb = LbWalberlaD3Q19MRT(
       viscosity, density, agrid, tau, box_dimensions, mpi_shape, 2);
 
   for (Vector3i n : all_nodes_incl_ghosts(1)) {
@@ -376,7 +326,7 @@ BOOST_AUTO_TEST_CASE(forces_initial_state) {
 }
 
 BOOST_AUTO_TEST_CASE(forces_interpolation) {
-  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(
+  LbWalberlaD3Q19MRT lb = LbWalberlaD3Q19MRT(
       viscosity, density, agrid, tau, box_dimensions, mpi_shape, 2);
 
   for (Vector3i n : all_nodes_incl_ghosts(1)) {
@@ -402,7 +352,7 @@ BOOST_AUTO_TEST_CASE(forces_interpolation) {
 }
 
 BOOST_AUTO_TEST_CASE(force_book_keeping) {
-  LbWalberlaD3Q19Thermalized lb = LbWalberlaD3Q19Thermalized(
+  LbWalberlaD3Q19MRT lb = LbWalberlaD3Q19MRT(
       viscosity, density, agrid, tau, box_dimensions, mpi_shape, 2);
 
   // Forces added go to force_to_be_applied. After integration, they should be
