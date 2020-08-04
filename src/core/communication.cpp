cbdf8be17c77e206d501c4e5182bcb27de928f47
Michael Kuron <mkuron@users.noreply.github.com>
2020-08-04 10:39:13 +0200

    fix style

--- src/core/communication.cpp
+++ src/core/communication.cpp
@@ -165,7 +165,7 @@ void openmpi_fix_vader() {
  */
 void openmpi_global_namespace() {
   if (OMPI_MAJOR_VERSION >= 3)
-      return;
+    return;
 #ifdef RTLD_NOLOAD
   const int mode = RTLD_NOW | RTLD_GLOBAL | RTLD_NOLOAD;
 #else
