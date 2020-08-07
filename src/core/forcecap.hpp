8aef9595f407a241bfa54eb517e3c7d05eab5e20
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2020-08-07 15:15:31 +0200

    core: Add missing include guards

--- src/core/forcecap.hpp
+++ src/core/forcecap.hpp
@@ -18,6 +18,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#ifndef FORCECAP_HPP
+#define FORCECAP_HPP
 
 #include "ParticleRange.hpp"
 
@@ -25,3 +27,5 @@ void forcecap_set(double forcecap);
 double forcecap_get();
 
 void forcecap_cap(ParticleRange particles);
+
+#endif
