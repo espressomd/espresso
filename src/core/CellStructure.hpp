3e1d418dede4f6be292a30201c6db91a1e5b90c0
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2020-08-26 11:19:50 +0200

    core: cells: Skip non-bonded kernels when inactive
    Skip non-bonded kernels in the short range loop if no non-bonded
    interactions are active.

--- src/core/CellStructure.hpp
+++ src/core/CellStructure.hpp
@@ -33,6 +33,7 @@
 #include "algorithm/link_cell.hpp"
 #include "bond_error.hpp"
 #include "ghosts.hpp"
+#include "nonbonded_interactions/nonbonded_interaction_data.hpp"
 
 #include <boost/algorithm/cxx11/any_of.hpp>
 #include <boost/container/static_vector.hpp>
@@ -516,7 +517,8 @@ public:
    * @param pair_kernel Kernel to apply
    */
   template <class PairKernel> void non_bonded_loop(PairKernel pair_kernel) {
-    link_cell(pair_kernel);
+    if (maximal_cutoff() > 0.)
+      link_cell(pair_kernel);
   }
 
   /** Non-bonded pair loop with potential use
@@ -527,6 +529,8 @@ public:
   template <class PairKernel, class VerletCriterion>
   void non_bonded_loop(PairKernel &&pair_kernel,
                        const VerletCriterion &verlet_criterion) {
+    if (maximal_cutoff() <= 0.)
+      return;
     /* In this case the verlet list update is attached to
      * the pair kernel, and the verlet list is rebuilt as
      * we go. */
