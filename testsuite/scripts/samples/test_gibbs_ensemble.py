062e06766bab4a40fb08fa6d541c62d7d9b953a0
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2020-09-30 15:45:33 +0200

    Gibbs: Test script

--- testsuite/scripts/samples/test_gibbs_ensemble.py
+++ testsuite/scripts/samples/test_gibbs_ensemble.py
@@ -20,13 +20,23 @@ import importlib_wrapper
 
 sample, skipIfMissingFeatures = importlib_wrapper.configure_and_import(
     "@SAMPLES_DIR@/gibbs_ensemble/gibbs_ensemble_socket.py",
-    cmd_arguments=["-S", "1000", "-C", "@SAMPLES_DIR@/gibbs_ensemble/gibbs_ensemble_client.py",
-                   "-E", "@CMAKE_BINARY_DIR@/pypresso"])
+    cmd_arguments=["-S", "2000", "-C", "@SAMPLES_DIR@/gibbs_ensemble/lj_gibbs_client.py",
+                   "-E", "@CMAKE_BINARY_DIR@/pypresso", "-T", "0.1", "-s", "1"])
 
 
 @skipIfMissingFeatures
 class Sample(ut.TestCase):
     sample
+
+    def test(self):
+        self.assertEqual(sample.kT, 0.1) # specified in cmake list
+        
+        # Check densities. The system will not have converged after 1k steps,
+        # but the two densities should be subtantially different
+        last_densities = [sample.densities[0][-1], sample.densities[1][-1]]
+        self.assertLess(min(last_densities), 0.25)
+        self.assertGreater(max(last_densities), 0.4)
+
 
 
 if __name__ == "__main__":
