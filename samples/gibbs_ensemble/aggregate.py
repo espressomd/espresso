405931a4f370cb6283e840c7dc42fc84907964d4
Rudolf Weeber <weeber@icp.uni-stuttgart.de>
2020-10-16 18:55:30 +0200

    Gibbs: Improve aggregation script

--- samples/gibbs_ensemble/aggregate.py
+++ samples/gibbs_ensemble/aggregate.py
@@ -3,20 +3,33 @@ import sys
 import pickle
 import scipy.optimize
 import matplotlib.pyplot as plt
-warmup_length = 100000
 
-delta_rhos = {}
-for f in sys.argv[1:]:
+
+# Number of densities to skip at the beginning of a simulation
+WARMUP_LENGTH = 100000 
+
+delta_rhos = {} # Holds density difference between dense and dilute phase vs kT
+
+for f in sys.argv[1:]: # Iterate over result files
     data = pickle.load(open(f, 'rb'))
+    
     kT = data['args'].temperature
-
     densities = np.array(data['densities'])
     indices = data['list_indices']
-    start = np.where(np.array(indices) > warmup_length)[0][0]
+    
+    
+    # First index to consider
+    start = np.where(np.array(indices) > WARMUP_LENGTH)[0][0]
 
-    low_dens = np.amin(densities[:, start:], axis=0)
-    high_dens = np.amax(densities[:, start:], axis=0)
-    print(kT, np.mean(low_dens), np.mean(high_dens))
+    # Determin which is teh dilute phase from the last density recorded
+    # This way the selection is permanent and the average densities of both
+    # phases will be approximately the same if T > T_c
+    dilute_phase = np.argmin(densities[:,-1])
+    dense_phase = np.argmax(densities[:,-1])
+
+
+    low_dens = densities[dilute_phase, start:]
+    high_dens = densities[dense_phase, start:]
     delta_rho = high_dens-low_dens
     if kT not in delta_rhos:
         delta_rhos[kT] =[]
