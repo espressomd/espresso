985389173c24638306b477dd2b86f41d7f00b8ce
Patrick Kreissl <patrick.kreissl@pa-le.de>
2020-11-03 13:15:49 +0100

    Formatting; simplifications using numpy.

--- testsuite/scripts/tutorials/test_11-ferrofluid_3.py
+++ testsuite/scripts/tutorials/test_11-ferrofluid_3.py
@@ -35,10 +35,10 @@ class Tutorial(ut.TestCase):
 
     def test(self):
         self.assertGreater(
-            tutorial.magnetization_star[1],
+            tutorial.magnetizations[1] / tutorial.N,
             tutorial.L(tutorial.alpha_mean_field(tutorial.alphas[1], tutorial.dip_lambda, tutorial.phi)))
         self.assertLess(
-            tutorial.magnetization_star[1],
+            tutorial.magnetizations[1] / tutorial.N,
             1)
         self.assertAlmostEqual(
             tutorial.chi, reference_chi, delta=0.45)
