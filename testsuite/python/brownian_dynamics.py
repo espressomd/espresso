2ea70da441c9d2f2f77a7da83f126a6fe6db02a5
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2020-11-17 19:46:40 +0100

    tests: Split Brownian/Langevin/DPD tests
    Move checks with long runtime into separate files.

--- testsuite/python/brownian_dynamics.py
+++ testsuite/python/brownian_dynamics.py
@@ -20,19 +20,15 @@ import unittest as ut
 import unittest_decorators as utx
 import espressomd
 import numpy as np
-from espressomd.accumulators import Correlator
-from espressomd.observables import ParticlePositions
-from thermostats_common import ThermostatsCommon
 
 
-class ThermalizedBond(ut.TestCase, ThermostatsCommon):
+class BrownianThermostat(ut.TestCase):
 
-    """Tests velocity distributions and diffusion for Brownian Dynamics"""
+    """Test Brownian Dynamics"""
     system = espressomd.System(box_l=[1.0, 1.0, 1.0])
     system.cell_system.set_domain_decomposition(use_verlet_lists=True)
     system.cell_system.skin = 0
     system.periodicity = [0, 0, 0]
-    system.integrator.set_brownian_dynamics()
 
     def setUp(self):
         np.random.seed(42)
@@ -41,8 +37,8 @@ class ThermalizedBond(ut.TestCase, ThermostatsCommon):
         self.system.time_step = 1e-12
         self.system.cell_system.skin = 0.0
         self.system.part.clear()
-        self.system.auto_update_accumulators.clear()
         self.system.thermostat.turn_off()
+        self.system.integrator.set_vv()
 
     def test_01__rng(self):
         """Test for RNG consistency."""
@@ -69,6 +65,7 @@ class ThermalizedBond(ut.TestCase, ThermostatsCommon):
             system.thermostat.set_brownian(kT=kT, gamma=gamma)
 
         system.thermostat.set_brownian(kT=kT, gamma=gamma, seed=41)
+        system.integrator.set_brownian_dynamics()
 
         # run(0) does not increase the philox counter and should give no force
         p = reset_particle()
@@ -115,103 +112,6 @@ class ThermalizedBond(ut.TestCase, ThermostatsCommon):
             torque4 = np.copy(p.omega_body) / omega2torque
             self.assertTrue(np.all(np.not_equal(torque3, torque4)))
 
-    def check_vel_dist_global_temp(self, recalc_forces, loops):
-        """Test velocity distribution for global temperature parameters.
-
-        Parameters
-        ----------
-        recalc_forces : :obj:`bool`
-            True if the forces should be recalculated after every step.
-        loops : :obj:`int`
-            Number of sampling loops
-        """
-        N = 200
-        system = self.system
-        system.time_step = 1.6
-        kT = 1.1
-        gamma = 3.5
-        system.thermostat.set_brownian(kT=kT, gamma=gamma, seed=41)
-        v_minmax = 5
-        bins = 4
-        error_tol = 0.01
-        self.check_global(
-            N, kT, loops, v_minmax, bins, error_tol, recalc_forces)
-
-    def test_vel_dist_global_temp(self):
-        """Test velocity distribution for global Brownian parameters."""
-        self.check_vel_dist_global_temp(False, loops=200)
-
-    def test_vel_dist_global_temp_initial_forces(self):
-        """Test velocity distribution for global Brownian parameters,
-           when using the initial force calculation.
-        """
-        self.check_vel_dist_global_temp(True, loops=170)
-
-    @utx.skipIfMissingFeatures("BROWNIAN_PER_PARTICLE")
-    def test_vel_dist_per_particle(self):
-        """Test Brownian dynamics with particle-specific kT and gamma. Covers
-           all combinations of particle-specific gamma and temp set or not set.
-        """
-        N = 400
-        system = self.system
-        system.time_step = 1.9
-        kT = 0.9
-        gamma = 3.2
-        gamma2 = 4.3
-        kT2 = 1.5
-        system.thermostat.set_brownian(kT=kT, gamma=gamma, seed=41)
-        loops = 200
-        v_minmax = 5
-        bins = 4
-        error_tol = 0.012
-        self.check_per_particle(
-            N, kT, kT2, gamma2, loops, v_minmax, bins, error_tol)
-
-    def setup_diff_mass_rinertia(self, p):
-        if espressomd.has_features("MASS"):
-            p.mass = 0.5
-        if espressomd.has_features("ROTATION"):
-            p.rotation = [1, 1, 1]
-            # Make sure rinertia does not change diff coeff
-            if espressomd.has_features("ROTATIONAL_INERTIA"):
-                p.rinertia = [0.4, 0.4, 0.4]
-
-    def test_msd_global_temp(self):
-        """Tests diffusion via MSD for global gamma and temperature"""
-
-        gamma = 9.4
-        kT = 0.37
-        dt = 0.5
-
-        system = self.system
-        p = system.part.add(pos=(0, 0, 0), id=0)
-        system.time_step = dt
-        system.thermostat.set_brownian(kT=kT, gamma=gamma, seed=41)
-        system.cell_system.skin = 0.4
-
-        pos_obs = ParticlePositions(ids=(p.id,))
-
-        c_pos = Correlator(obs1=pos_obs, tau_lin=16, tau_max=100., delta_N=1,
-                           corr_operation="square_distance_componentwise",
-                           compress1="discard1")
-        system.auto_update_accumulators.add(c_pos)
-
-        system.integrator.run(30000)
-
-        c_pos.finalize()
-
-        # Check MSD
-        msd = c_pos.result()
-        tau = c_pos.lag_times()
-        system.auto_update_accumulators.clear()
-
-        def expected_msd(x):
-            return 2. * kT / gamma * x
-
-        for i in range(2, 6):
-            np.testing.assert_allclose(
-                msd[i], expected_msd(tau[i]), rtol=0.02)
-
     @utx.skipIfMissingFeatures("VIRTUAL_SITES")
     def test_07__virtual(self):
         system = self.system
@@ -222,6 +122,7 @@ class ThermalizedBond(ut.TestCase, ThermostatsCommon):
 
         system.thermostat.set_brownian(
             kT=0, gamma=1, gamma_rotation=1., act_on_virtual=False, seed=41)
+        system.integrator.set_brownian_dynamics()
 
         system.integrator.run(1)
 
@@ -235,19 +136,6 @@ class ThermalizedBond(ut.TestCase, ThermostatsCommon):
 
         np.testing.assert_almost_equal(np.copy(virtual.v), [0, 0, 0])
         np.testing.assert_almost_equal(np.copy(physical.v), [0, 0, 0])
-
-    def test_08__noise_correlation(self):
-        """Checks that the Brownian noise is uncorrelated"""
-
-        system = self.system
-        system.time_step = 0.01
-        system.cell_system.skin = 0.1
-        kT = 3.2
-        system.thermostat.set_brownian(kT=kT, gamma=2.1, seed=17)
-        system.part.add(id=(0, 1), pos=np.zeros((2, 3)))
-        steps = int(1e4)
-        error_delta = 0.04
-        self.check_noise_correlation(kT, steps, error_delta)
 
 
 if __name__ == "__main__":
--- testsuite/python/dpd.py
+++ testsuite/python/dpd.py
@@ -23,13 +23,12 @@ from itertools import product
 
 import espressomd
 from espressomd.observables import DPDStress
-from thermostats_common import ThermostatsCommon
 
 
 @utx.skipIfMissingFeatures("DPD")
-class DPDThermostat(ut.TestCase, ThermostatsCommon):
+class DPDThermostat(ut.TestCase):
 
-    """Tests the velocity distribution created by the DPD thermostat."""
+    """Test DPD dynamics."""
 
     s = espressomd.System(box_l=3 * [10.0])
     s.time_step = 0.01
@@ -109,123 +108,6 @@ class DPDThermostat(ut.TestCase, ThermostatsCommon):
         system.integrator.run(0, recalc_forces=True)
         force5 = np.copy(p.f)
         self.assertTrue(np.all(np.not_equal(force4, force5)))
-
-    def check_total_zero(self):
-        v_total = np.sum(self.s.part[:].v, axis=0)
-        np.testing.assert_allclose(v_total, np.zeros(3), atol=1e-11)
-
-    def single(self, with_langevin=False):
-        """Test velocity distribution of a dpd fluid with a single type."""
-        N = 500
-        s = self.s
-        s.part.add(pos=s.box_l * np.random.random((N, 3)))
-        kT = 2.3
-        gamma = 1.5
-        if with_langevin:
-            s.thermostat.set_langevin(kT=kT, gamma=gamma, seed=41)
-        s.thermostat.set_dpd(kT=kT, seed=42)
-        s.non_bonded_inter[0, 0].dpd.set_params(
-            weight_function=0, gamma=gamma, r_cut=1.5,
-            trans_weight_function=0, trans_gamma=gamma, trans_r_cut=1.5)
-        s.integrator.run(100)
-        loops = 100
-        v_stored = np.zeros((loops, N, 3))
-        for i in range(loops):
-            s.integrator.run(10)
-            v_stored[i] = s.part[:].v
-        v_minmax = 5
-        bins = 5
-        error_tol = 0.01
-        self.check_velocity_distribution(
-            v_stored.reshape((-1, 3)), v_minmax, bins, error_tol, kT)
-
-        if not with_langevin:
-            self.check_total_zero()
-
-    def test_single(self):
-        self.single()
-
-    def test_single_with_langevin(self):
-        self.single(True)
-
-    def test_binary(self):
-        """Test velocity distribution of binary dpd fluid"""
-        N = 200
-        s = self.s
-        s.part.add(pos=s.box_l * np.random.random((N // 2, 3)),
-                   type=N // 2 * [0])
-        s.part.add(pos=s.box_l * np.random.random((N // 2, 3)),
-                   type=N // 2 * [1])
-        kT = 2.3
-        gamma = 3.5
-        s.thermostat.set_dpd(kT=kT, seed=42)
-        s.non_bonded_inter[0, 0].dpd.set_params(
-            weight_function=0, gamma=gamma, r_cut=1.0,
-            trans_weight_function=0, trans_gamma=gamma, trans_r_cut=1.0)
-        s.non_bonded_inter[1, 1].dpd.set_params(
-            weight_function=0, gamma=gamma, r_cut=1.0,
-            trans_weight_function=0, trans_gamma=gamma, trans_r_cut=1.0)
-        s.non_bonded_inter[0, 1].dpd.set_params(
-            weight_function=0, gamma=gamma, r_cut=1.5,
-            trans_weight_function=0, trans_gamma=gamma, trans_r_cut=1.5)
-        s.integrator.run(100)
-        loops = 250
-        v_stored = np.zeros((loops, N, 3))
-        for i in range(loops):
-            s.integrator.run(10)
-            v_stored[i] = s.part[:].v
-        v_minmax = 5
-        bins = 5
-        error_tol = 0.01
-        self.check_velocity_distribution(
-            v_stored.reshape((-1, 3)), v_minmax, bins, error_tol, kT)
-        self.check_total_zero()
-
-    def test_disable(self):
-        N = 200
-        s = self.s
-        s.time_step = 0.01
-        s.part.add(pos=s.box_l * np.random.random((N, 3)))
-        kT = 2.3
-        gamma = 1.5
-        s.thermostat.set_dpd(kT=kT, seed=42)
-        s.non_bonded_inter[0, 0].dpd.set_params(
-            weight_function=0, gamma=gamma, r_cut=1.5,
-            trans_weight_function=0, trans_gamma=gamma, trans_r_cut=1.5)
-
-        s.integrator.run(10)
-
-        s.thermostat.turn_off()
-
-        # Reset velocities
-        s.part[:].v = [1., 2., 3.]
-
-        s.integrator.run(10)
-
-        # Check that there was neither noise nor friction
-        for v in s.part[:].v:
-            for i in range(3):
-                self.assertEqual(v[i], float(i + 1))
-
-        # Turn back on
-        s.thermostat.set_dpd(kT=kT, seed=42)
-
-        # Reset velocities for faster convergence
-        s.part[:].v = [0., 0., 0.]
-
-        # Equilibrate
-        s.integrator.run(250)
-
-        loops = 250
-        v_stored = np.zeros((loops, N, 3))
-        for i in range(loops):
-            s.integrator.run(10)
-            v_stored[i] = s.part[:].v
-        v_minmax = 5
-        bins = 5
-        error_tol = 0.012
-        self.check_velocity_distribution(
-            v_stored.reshape((-1, 3)), v_minmax, bins, error_tol, kT)
 
     def test_const_weight_function(self):
         s = self.s
