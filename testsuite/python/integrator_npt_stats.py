79f0ccf4e8bdb6dea00e8dd2b85229af6e8875f5
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2020-12-03 15:49:56 +0100

    tests: Cleanup NpT testcase

--- testsuite/python/integrator_npt_stats.py
+++ testsuite/python/integrator_npt_stats.py
@@ -28,48 +28,48 @@ class IntegratorNPT(ut.TestCase):
 
     """This compares pressure and compressibility of a LJ system against
        expected values."""
-    S = espressomd.System(box_l=[1.0, 1.0, 1.0])
-    p_ext = 2.0
+    system = espressomd.System(box_l=[1.0, 1.0, 1.0])
 
     def setUp(self):
+        self.system.box_l = [5] * 3
+        self.system.time_step = 0.01
+        self.system.cell_system.skin = 0.25
 
-        box_l = 5.86326165
-        self.S.box_l = [box_l] * 3
-        self.S.time_step = 0.01
-        self.S.cell_system.skin = 0.25
+    def tearDown(self):
+        self.system.part.clear()
+        self.system.thermostat.turn_off()
+        self.system.integrator.set_vv()
+
+    def test_compressibility(self):
+        system = self.system
+        system.box_l = [5.86326165] * 3
 
         data = np.genfromtxt(tests_common.abspath("data/npt_lj_system.data"))
+        p_ext = 2.0
 
-        # Input format: id pos f
-        for particle in data:
-            pos = particle[:3]
-            v = particle[3:]
-            self.S.part.add(pos=pos, v=v)
+        system.part.add(pos=data[:, :3], v=data[:, 3:])
+        system.non_bonded_inter[0, 0].lennard_jones.set_params(
+            epsilon=1, sigma=1, cutoff=2**(1 / 6), shift=0.25)
 
-        self.S.non_bonded_inter[0, 0].lennard_jones.set_params(
-            epsilon=1, sigma=1, cutoff=1.12246, shift=0.25)
+        system.thermostat.set_npt(kT=1.0, gamma0=2, gammav=0.004, seed=42)
+        system.integrator.set_isotropic_npt(ext_pressure=p_ext, piston=0.0001)
 
-        self.S.thermostat.set_npt(kT=1.0, gamma0=2, gammav=0.004, seed=42)
-        self.S.integrator.set_isotropic_npt(
-            ext_pressure=self.p_ext, piston=0.0001)
-
-    def test_npt(self):
-        self.S.integrator.run(800)
+        system.integrator.run(800)
         avp = 0
         n = 30000
         skip_p = 8
         ls = np.zeros(n)
         for t in range(n):
-            self.S.integrator.run(2)
+            system.integrator.run(2)
             if t % skip_p == 0:
-                avp += self.S.analysis.pressure()['total']
-            ls[t] = self.S.box_l[0]
+                avp += system.analysis.pressure()['total']
+            ls[t] = system.box_l[0]
 
         avp /= (n / skip_p)
         Vs = np.array(ls)**3
         compressibility = np.var(Vs) / np.average(Vs)
 
-        self.assertAlmostEqual(avp, 2.0, delta=0.02)
+        self.assertAlmostEqual(avp, p_ext, delta=0.02)
         self.assertAlmostEqual(compressibility, 0.32, delta=0.02)
 
 
