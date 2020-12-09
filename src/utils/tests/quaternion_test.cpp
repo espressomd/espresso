ec6d211023031db64a13df88cf2c3529334d45ae
Kai Szuttor <kai@icp.uni-stuttgart.de>
2020-12-09 09:44:51 +0100

    added accidentally deleted line.

--- src/utils/tests/quaternion_test.cpp
+++ src/utils/tests/quaternion_test.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2019 The ESPResSo project
  *
+ * This file is part of ESPResSo.
  *
  * ESPResSo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
