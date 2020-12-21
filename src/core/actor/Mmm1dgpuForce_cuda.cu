espressomd/espresso

commit 235480bc1743dcf48c0e945cc6e862ec942139f7

core: Refactor code that uses constants

src/core/actor/Mmm1dgpuForce_cuda.cu
@@ -41,12 +41,11 @@ float multigpu_factors[] = {1.0};
 #include "electrostatics_magnetostatics/mmm1d.hpp"
 #include "mmm-common_cuda.hpp"
 
+#include <utils/constants.hpp>
+
 #if defined(OMPI_MPI_H) || defined(_MPI_H)
 #error CU-file includes mpi.h! This should not happen!
 #endif
-
-const mmm1dgpu_real C_GAMMAf = C_GAMMA;
-const mmm1dgpu_real C_2PIf = C_2PI;
 
 __constant__ mmm1dgpu_real far_switch_radius_2[1] = {0.05 * 0.05};
 __constant__ mmm1dgpu_real boxz[1];
@@ -185,8 +184,9 @@ __global__ void sumKernel(mmm1dgpu_real *data, int N) {
 
 __global__ void besselTuneKernel(int *result, mmm1dgpu_real far_switch_radius,
                                  int maxCut) {
-  mmm1dgpu_real arg = C_2PIf * *uz * far_switch_radius;
-  mmm1dgpu_real pref = 4 * *uz * max(1.0f, C_2PIf * *uz);
+  const mmm1dgpu_real c_2pif = 2 * Utils::pi<mmm1dgpu_real>();
+  mmm1dgpu_real arg = c_2pif * *uz * far_switch_radius;
+  mmm1dgpu_real pref = 4 * *uz * max(1.0f, c_2pif * *uz);
   mmm1dgpu_real err;
   int P = 1;
   do {
@@ -322,6 +322,8 @@ __global__ void forcesKernel(const mmm1dgpu_real *__restrict__ r,
   if (tStop < 0)
     tStop = N * N;
 
+  const mmm1dgpu_real c_2pif = 2 * Utils::pi<mmm1dgpu_real>();
+
   for (int tid =
            static_cast<int>(threadIdx.x + blockIdx.x * blockDim.x) + tStart;
        tid < tStop; tid += static_cast<int>(blockDim.x * gridDim.x)) {
@@ -381,14 +383,14 @@ __global__ void forcesKernel(const mmm1dgpu_real *__restrict__ r,
     } else // far formula
     {
       for (int p = 1; p < *bessel_cutoff; p++) {
-        mmm1dgpu_real arg = C_2PIf * *uz * static_cast<mmm1dgpu_real>(p);
+        mmm1dgpu_real arg = c_2pif * *uz * static_cast<mmm1dgpu_real>(p);
         sum_r +=
             static_cast<mmm1dgpu_real>(p) * dev_K1(arg * rxy) * cos(arg * z);
         sum_z +=
             static_cast<mmm1dgpu_real>(p) * dev_K0(arg * rxy) * sin(arg * z);
       }
-      sum_r *= sqpow(*uz) * 4 * C_2PIf;
-      sum_z *= sqpow(*uz) * 4 * C_2PIf;
+      sum_r *= sqpow(*uz) * 4 * c_2pif;
+      sum_z *= sqpow(*uz) * 4 * c_2pif;
       sum_r += 2 * *uz / rxy;
     }
 
@@ -411,6 +413,9 @@ __global__ void energiesKernel(const mmm1dgpu_real *__restrict__ r,
                                int pairs, int tStart, int tStop) {
   if (tStop < 0)
     tStop = N * N;
+
+  const mmm1dgpu_real c_2pif = 2 * Utils::pi<mmm1dgpu_real>();
+  const mmm1dgpu_real c_gammaf = 2 * Utils::gamma<mmm1dgpu_real>();
 
   HIP_DYNAMIC_SHARED(mmm1dgpu_real, partialsums)
   if (!pairs) {
@@ -451,15 +456,15 @@ __global__ void energiesKernel(const mmm1dgpu_real *__restrict__ r,
       }
 
       sum_e *= -1 * *uz;
-      sum_e -= 2 * *uz * C_GAMMAf;
+      sum_e -= 2 * *uz * c_gammaf;
       sum_e += rsqrt(rxy2 + sqpow(z));
       sum_e += rsqrt(rxy2 + sqpow(z + *boxz));
       sum_e += rsqrt(rxy2 + sqpow(z - *boxz));
     } else // far formula
     {
-      sum_e = -(log(rxy * *uz / 2) + C_GAMMAf) / 2;
+      sum_e = -(log(rxy * *uz / 2) + c_gammaf) / 2;
       for (int p = 1; p < *bessel_cutoff; p++) {
-        mmm1dgpu_real arg = C_2PIf * *uz * static_cast<mmm1dgpu_real>(p);
+        mmm1dgpu_real arg = c_2pif * *uz * static_cast<mmm1dgpu_real>(p);
         sum_e += dev_K0(arg * rxy) * cos(arg * z);
       }
       sum_e *= *uz * 4;

src/core/electrostatics_magnetostatics/elc.cpp
@@ -34,6 +34,8 @@
 #include "electrostatics_magnetostatics/elc.hpp"
 #include "electrostatics_magnetostatics/p3m.hpp"
 
+#include <utils/constants.hpp>
+
 #ifdef P3M
 
 /****************************************
@@ -146,11 +148,12 @@ void ELC_setup_constants() {
 template <size_t dir>
 static std::vector<SCCache> sc_cache(const ParticleRange &particles, int n_freq,
                                      double u) {
+  constexpr double c_2pi = 2 * Utils::pi();
   auto const n_part = particles.size();
   std::vector<SCCache> ret(n_freq * n_part);
 
   for (size_t freq = 1; freq <= n_freq; freq++) {
-    double pref = C_2PI * u * freq;
+    double pref = c_2pi * u * freq;
 
     size_t o = (freq - 1) * n_part;
     for (auto const &part : particles) {
@@ -914,8 +917,9 @@ static void setup_PQ(int p, int q, double omega,
 
 static void add_PQ_force(int p, int q, double omega,
                          const ParticleRange &particles) {
-  double const pref_x = C_2PI * ux * p / omega;
-  double const pref_y = C_2PI * uy * q / omega;
+  constexpr double c_2pi = 2 * Utils::pi();
+  double const pref_x = c_2pi * ux * p / omega;
+  double const pref_y = c_2pi * uy * q / omega;
   int const size = 8;
 
   int ic = 0;
@@ -971,6 +975,7 @@ static double PQ_energy(double omega, int n_part) {
 /*****************************************************************/
 
 void ELC_add_force(const ParticleRange &particles) {
+  constexpr double c_2pi = 2 * Utils::pi();
   auto const n_scxcache = int(ceil(elc_params.far_cut / ux) + 1);
   auto const n_scycache = int(ceil(elc_params.far_cut / uy) + 1);
 
@@ -982,14 +987,14 @@ void ELC_add_force(const ParticleRange &particles) {
 
   /* the second condition is just for the case of numerical accident */
   for (int p = 1; ux * (p - 1) < elc_params.far_cut && p <= n_scxcache; p++) {
-    auto const omega = C_2PI * ux * p;
+    auto const omega = c_2pi * ux * p;
     setup_P(p, omega, particles);
     distribute(4);
     add_P_force(particles);
   }
 
   for (int q = 1; uy * (q - 1) < elc_params.far_cut && q <= n_scycache; q++) {
-    auto const omega = C_2PI * uy * q;
+    auto const omega = c_2pi * uy * q;
     setup_Q(q, omega, particles);
     distribute(4);
     add_Q_force(particles);
@@ -1000,7 +1005,7 @@ void ELC_add_force(const ParticleRange &particles) {
                         elc_params.far_cut2 &&
                     q <= n_scycache;
          q++) {
-      auto const omega = C_2PI * sqrt(Utils::sqr(ux * p) + Utils::sqr(uy * q));
+      auto const omega = c_2pi * sqrt(Utils::sqr(ux * p) + Utils::sqr(uy * q));
       setup_PQ(p, q, omega, particles);
       distribute(8);
       add_PQ_force(p, q, omega, particles);
@@ -1009,6 +1014,7 @@ void ELC_add_force(const ParticleRange &particles) {
 }
 
 double ELC_energy(const ParticleRange &particles) {
+  constexpr double c_2pi = 2 * Utils::pi();
   auto eng = dipole_energy(particles);
   eng += z_energy(particles);
 
@@ -1021,13 +1027,13 @@ double ELC_energy(const ParticleRange &particles) {
 
   /* the second condition is just for the case of numerical accident */
   for (int p = 1; ux * (p - 1) < elc_params.far_cut && p <= n_scxcache; p++) {
-    auto const omega = C_2PI * ux * p;
+    auto const omega = c_2pi * ux * p;
     setup_P(p, omega, particles);
     distribute(4);
     eng += P_energy(omega, n_localpart);
   }
   for (int q = 1; uy * (q - 1) < elc_params.far_cut && q <= n_scycache; q++) {
-    auto const omega = C_2PI * uy * q;
+    auto const omega = c_2pi * uy * q;
     setup_Q(q, omega, particles);
     distribute(4);
     eng += Q_energy(omega, n_localpart);
@@ -1037,7 +1043,7 @@ double ELC_energy(const ParticleRange &particles) {
                         elc_params.far_cut2 &&
                     q <= n_scycache;
          q++) {
-      auto const omega = C_2PI * sqrt(Utils::sqr(ux * p) + Utils::sqr(uy * q));
+      auto const omega = c_2pi * sqrt(Utils::sqr(ux * p) + Utils::sqr(uy * q));
       setup_PQ(p, q, omega, particles);
       distribute(8);
       eng += PQ_energy(omega, n_localpart);

src/core/electrostatics_magnetostatics/mmm-modpsi.cpp
@@ -24,6 +24,8 @@
 #include "mmm-common.hpp"
 #include "mmm-modpsi.hpp"
 
+#include <utils/constants.hpp>
+
 #include <cmath>
 
 std::vector<std::vector<double>> modPsi;
@@ -37,7 +39,7 @@ static void preparePolygammaEven(int n, double binom,
     // psi^0 has a slightly different series expansion
     double maxx = 0.25;
     series.resize(1);
-    series[0] = 2 * (1 - C_GAMMA);
+    series[0] = 2 * (1 - Utils::gamma());
     for (int order = 1;; order += 1) {
       auto const x_order = static_cast<double>(2 * order);
       auto const coeff = -2 * hzeta(x_order + 1, 2);

src/core/electrostatics_magnetostatics/mmm-modpsi.hpp
@@ -29,12 +29,6 @@
 
 #include <vector>
 
-/** \name Math constants */
-/*@{*/
-#define C_2PI (2 * M_PI)
-#define C_GAMMA (0.57721566490153286060651209008)
-/*@}*/
-
 /** Table of the Taylor expansions of the modified polygamma functions */
 extern std::vector<std::vector<double>> modPsi;
 

src/core/electrostatics_magnetostatics/mmm1d.cpp
@@ -43,6 +43,7 @@ using Utils::strcat_alloc;
 #include <utils/math/sqr.hpp>
 
 #include <cstdio>
+#include <limits>
 
 /** How many trial calculations */
 #define TEST_INTEGRATIONS 1000
@@ -178,6 +179,7 @@ void MMM1D_init() {
 
 void add_mmm1d_coulomb_pair_force(double chpref, Utils::Vector3d const &d,
                                   double r, Utils::Vector3d &force) {
+  constexpr double c_2pi = 2 * Utils::pi();
   auto const n_modPsi = static_cast<int>(modPsi.size() >> 1);
   auto const rxy2 = d[0] * d[0] + d[1] * d[1];
   auto const rxy2_d = rxy2 * uz2;
@@ -244,7 +246,7 @@ void add_mmm1d_coulomb_pair_force(double chpref, Utils::Vector3d const &d,
       if (bessel_radii[bp - 1] < rxy)
         break;
 
-      auto const fq = C_2PI * bp;
+      auto const fq = c_2pi * bp;
       double k0, k1;
 #ifdef BESSEL_MACHINE_PREC
       k0 = K0(fq * rxy_d);
@@ -255,8 +257,8 @@ void add_mmm1d_coulomb_pair_force(double chpref, Utils::Vector3d const &d,
       sr += bp * k1 * cos(fq * z_d);
       sz += bp * k0 * sin(fq * z_d);
     }
-    sr *= uz2 * 4 * C_2PI;
-    sz *= uz2 * 4 * C_2PI;
+    sr *= uz2 * 4 * c_2pi;
+    sz *= uz2 * 4 * c_2pi;
 
     auto const pref = sr / rxy + 2 * uz / rxy2;
 
@@ -271,6 +273,7 @@ double mmm1d_coulomb_pair_energy(double const chpref, Utils::Vector3d const &d,
   if (chpref == 0)
     return 0;
 
+  constexpr double c_2pi = 2 * Utils::pi();
   auto const n_modPsi = static_cast<int>(modPsi.size() >> 1);
   auto const rxy2 = d[0] * d[0] + d[1] * d[1];
   auto const rxy2_d = rxy2 * uz2;
@@ -279,7 +282,7 @@ double mmm1d_coulomb_pair_energy(double const chpref, Utils::Vector3d const &d,
 
   if (rxy2 <= mmm1d_params.far_switch_radius_2) {
     /* near range formula */
-    E = -2 * C_GAMMA;
+    E = -2 * Utils::gamma();
 
     /* polygamma summation */
     double r2n = 1.0;
@@ -313,12 +316,12 @@ double mmm1d_coulomb_pair_energy(double const chpref, Utils::Vector3d const &d,
     auto const rxy_d = rxy * uz;
     /* The first Bessel term will compensate a little bit the
        log term, so add them close together */
-    E = -0.25 * log(rxy2_d) + 0.5 * (M_LN2 - C_GAMMA);
+    E = -0.25 * log(rxy2_d) + 0.5 * (M_LN2 - Utils::gamma());
     for (int bp = 1; bp < MAXIMAL_B_CUT; bp++) {
       if (bessel_radii[bp - 1] < rxy)
         break;
 
-      auto const fq = C_2PI * bp;
+      auto const fq = c_2pi * bp;
       E += K0(fq * rxy_d) * cos(fq * z_d);
     }
     E *= 4 * uz;
@@ -331,7 +334,7 @@ int mmm1d_tune(char **log) {
   if (MMM1D_sanity_checks())
     return ES_ERROR;
   char buffer[32 + 2 * ES_DOUBLE_SPACE + ES_INTEGER_SPACE];
-  double min_time = 1e200;
+  double min_time = std::numeric_limits<double>::infinity();
   double min_rad = -1;
   auto const maxrad = box_geo.length()[2];
   double switch_radius;

src/utils/include/utils/constants.hpp
@@ -43,6 +43,13 @@ template <class T = double> DEVICE_QUALIFIER constexpr T sqrt_pi_i() {
   return 0.56418958354775627928034964498;
 }
 
+/**
+ * @brief Euler-Mascheroni constant.
+ */
+template <class T = double> DEVICE_QUALIFIER constexpr T gamma() {
+  return 0.57721566490153286060651209008;
+}
+
 /// error code if no error occurred
 #define ES_OK 0
 /// error code if an error occurred

commit 2b159fbdf2c8ecf017573f6a3943b495a914d5f7

core: Fix MMM1DGPU energy calculation
Fix regression introduced in 235480bc1743dcf48c0e945cc6e862ec942139f7

src/core/actor/Mmm1dgpuForce_cuda.cu
@@ -486,8 +486,8 @@ __global__ void energiesKernel(const mmm1dgpu_real *__restrict__ r,
   if (tStop < 0)
     tStop = N * N;
 
-  const mmm1dgpu_real c_2pif = 2 * Utils::pi<mmm1dgpu_real>();
-  const mmm1dgpu_real c_gammaf = 2 * Utils::gamma<mmm1dgpu_real>();
+  auto const c_2pif = 2 * Utils::pi<mmm1dgpu_real>();
+  auto const c_gammaf = Utils::gamma<mmm1dgpu_real>();
 
   extern __shared__ mmm1dgpu_real partialsums[];
   if (!pairs) {