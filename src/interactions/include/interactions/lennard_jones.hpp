f13e5447f12162bbde3581ec49e24a7dbc2a7ed2
Kai Szuttor <kai@icp.uni-stuttgart.de>
2021-02-23 15:17:12 +0100

    interactions: lj force.

--- src/interactions/include/interactions/lennard_jones.hpp
+++ src/interactions/include/interactions/lennard_jones.hpp
@@ -1,15 +1,27 @@
 #ifndef SRC_INTERACTIONS_INCLUDE_INTERACTIONS_LENNARD_JONES_HPP
 #define SRC_INTERACTIONS_INCLUDE_INTERACTIONS_LENNARD_JONES_HPP
 
+#include <utils/Vector.hpp>
+#include <utils/math/int_pow.hpp>
+
 namespace Interactions {
 
 template <class T> struct LennardJones {
   using value_type = T;
   value_type epsilon;
   value_type sigma;
+
   value_type energy(value_type r) const {
     return 4.0 * epsilon *
-           (std::pow(sigma / r, 12.0) - std::pow(sigma / r, 6.0));
+           (Utils::int_pow<12>(sigma / r) - Utils::int_pow<6>(sigma / r));
+  }
+
+  Utils::Vector<value_type, 3> force(value_type r,
+                                     Utils::Vector<value_type, 3> const &r12) {
+    return r12 *
+           (24. * epsilon * Utils::int_pow<6>(sigma) *
+            (Utils::int_pow<6>(r) - 2. * Utils::int_pow<6>(sigma))) /
+           Utils::int_pow<13>(r);
   }
 };
 
--- src/interactions/tests/lennard_jones.test.cpp
+++ src/interactions/tests/lennard_jones.test.cpp
@@ -3,11 +3,15 @@
 #include <boost/test/unit_test.hpp>
 
 #include "interactions/lennard_jones.hpp"
+#include "utils/Vector.hpp"
 
 constexpr auto tol = std::numeric_limits<double>::epsilon();
 
 BOOST_AUTO_TEST_CASE(lennard_jones_test) {
   Interactions::LennardJones<double> lj{1.5, 2.0};
+  Utils::Vector3d r12{1, 0, 0};
   BOOST_CHECK_SMALL(lj.energy(2.0), tol);
   BOOST_CHECK_SMALL(lj.energy(std::pow(2.0, 1. / 6.) * 2.0) + 1.5, tol);
+  BOOST_CHECK(lj.force(std::pow(2.0, 1. / 6.) * 2.0, r12) ==
+              Utils::Vector3d::broadcast(0.0));
 }
