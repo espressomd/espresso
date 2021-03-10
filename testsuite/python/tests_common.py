4156ffe96a828a45bb309396a3a3942faf462433
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2021-03-10 15:41:42 +0100

    tests: Remove unused utility function

--- testsuite/python/tests_common.py
+++ testsuite/python/tests_common.py
@@ -189,30 +189,6 @@ def rodrigues_rot(vec, axis, angle):
         (1 - np.cos(angle)) * np.dot(axis, vec) * axis
 
 
-def rotation_matrix(axis, theta):
-    """
-    Return the rotation matrix associated with counterclockwise rotation about
-    the given axis by theta radians.
-
-    Parameters
-    ----------
-    axis : array_like :obj:`float`
-        Axis to rotate around.
-    theta : :obj:`float`
-        Rotation angle.
-
-    """
-    axis = np.asarray(axis)
-    axis = axis / np.sqrt(np.dot(axis, axis))
-    a = np.cos(theta / 2.0)
-    b, c, d = -axis * np.sin(theta / 2.0)
-    aa, bb, cc, dd = a * a, b * b, c * c, d * d
-    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
-    return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
-                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
-                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
-
-
 def rotation_matrix_quat(system, part):
     """
     Return the rotation matrix associated with quaternion.
