e4104d28e6f9c23b8115825212f52217b1570fe3
blancoapa <blancoapa@natur.cuni.cz>
2021-04-06 13:12:24 +0200

    Exception error introduced that warns the user if the reaction cannot occur because there are not enough reactants

--- src/core/reaction_ensemble.cpp
+++ src/core/reaction_ensemble.cpp
@@ -1499,7 +1499,8 @@ int ConstantpHEnsemble::do_reaction(int reaction_steps) {
 
 
   for (int reaction_id = 0; reaction_id < reactions.size(); reaction_id++) {
-    if (all_reactant_particles_exist(reaction_id) == false){return 1;
+    if (all_reactant_particles_exist(reaction_id) == false){ 
+      throw std::runtime_error("There is not enough reactants nor products to perform the reaction in either direction");  ;
     }
   }
       
