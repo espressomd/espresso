1c3be47d15d81c98683b04487fd3ee519a9011b6
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2021-04-07 18:30:46 +0200

    tests: Make SF test independent of parameters
    Replace magic values by unit cell parameters. Fix a bug in the
    reflection peaks generator. The test is now independent of the
    box length, sf_order and unit cell parameters.

--- testsuite/python/sf_simple_lattice.py
+++ testsuite/python/sf_simple_lattice.py
@@ -27,7 +27,7 @@ class StructureFactorTest(ut.TestCase):
     '''
     Test structure factor analysis against rectangular lattices.
     We do not check the wavevectors directly, but rather the
-    quantity (wavevectors * 2 / pi)^2, which is more readable.
+    corresponding SF order, which is more readable (integer value).
     '''
 
     box_l = 16
@@ -38,7 +38,14 @@ class StructureFactorTest(ut.TestCase):
     def tearDown(self):
         self.system.part.clear()
 
-    def generate_peaks(self, a, b, c, condition, cutoff=box_l):
+    def order(self, wavevectors, a):
+        """
+        Square and rescale wavevectors to recover the corresponding
+        SF order, which is an integer between 0 and ``self.sf_order``.
+        """
+        return (wavevectors * a / (2 * np.pi))**2
+
+    def generate_peaks(self, a, b, c, conditions):
         '''
         Generate the main diffraction peaks for crystal structures.
 
@@ -50,117 +57,120 @@ class StructureFactorTest(ut.TestCase):
             Length of the unit cell on the y-axis.
         c: :obj:`float`
             Length of the unit cell on the z-axis.
-        condition: :obj:`function`
+        conditions: :obj:`function`
             Reflection conditions for the crystal lattice.
-        cutoff: :obj:`float` (optional)
-            Cutoff value for the generation of reflections.
         '''
-        reflections = [np.linalg.norm([h * a, k * b, l * c])
-                       for (h, k, l) in itertools.product(np.arange(0, 10), repeat=3)
-                       if condition(h, k, l)]
-        return (np.array([x for x in sorted(set(reflections))
-                          if 0. < x < 1.001 * cutoff]) / 4) ** 2
+        hkl_ranges = [
+            range(0, self.sf_order + 1),
+            range(-self.sf_order, self.sf_order + 1),
+            range(-self.sf_order, self.sf_order + 1),
+        ]
+        reflections = [np.linalg.norm([h / a, k / b, l / c]) * 2 * np.pi
+                       for (h, k, l) in itertools.product(*hkl_ranges)
+                       if conditions(h, k, l) and (h + k + l != 0) and
+                       (h**2 + k**2 + l**2) <= self.sf_order**2]
+        return np.unique(reflections)
 
     def test_tetragonal(self):
         """Check tetragonal lattice."""
-        xen = range(0, self.box_l, 2)
-        yen = range(0, self.box_l, 4)
-        zen = range(0, self.box_l, 8)
+        a = 2
+        b = 4
+        c = 8
+        xen = range(0, self.box_l, a)
+        yen = range(0, self.box_l, b)
+        zen = range(0, self.box_l, c)
         for i, j, k in itertools.product(xen, yen, zen):
             self.system.part.add(type=self.part_ty, pos=(i, j, k))
         wavevectors, intensities = self.system.analysis.structure_factor(
             sf_types=[self.part_ty], sf_order=self.sf_order)
-        intensities_int = np.around(intensities).astype(int)
-        # check all reflections
-        # no condition on (h,k,l)
-        peaks_ref = self.generate_peaks(2, 4, 8, lambda h, k, l: True)
-        peaks = (wavevectors[np.nonzero(intensities_int)] * 2 / np.pi)**2
-        np.testing.assert_array_almost_equal(peaks, peaks_ref)
+        intensities = np.around(intensities, 8)
+        # no reflection conditions on (h,k,l)
+        peaks_ref = self.generate_peaks(a, b, c, lambda h, k, l: True)
+        peaks = wavevectors[np.nonzero(intensities)]
+        np.testing.assert_array_almost_equal(
+            self.order(peaks, a), self.order(peaks_ref[:len(peaks)], a))
 
     def test_sc(self):
         """Check simple cubic lattice."""
-        xen = range(0, self.box_l, 4)
+        l0 = 4
+        xen = range(0, self.box_l, l0)
         for i, j, k in itertools.product(xen, repeat=3):
             self.system.part.add(type=self.part_ty, pos=(i, j, k))
         wavevectors, intensities = self.system.analysis.structure_factor(
             sf_types=[self.part_ty], sf_order=self.sf_order)
-        intensities_int = np.around(intensities).astype(int)
-        np.testing.assert_array_almost_equal(intensities, intensities_int)
-        intensities_ref = np.zeros(intensities.shape)
-        intensities_ref[np.nonzero(intensities_int)] = len(self.system.part)
-        np.testing.assert_array_equal(intensities_int, intensities_ref)
-        # check all reflections
-        # no condition on (h,k,l)
-        peaks_ref = self.generate_peaks(4, 4, 4, lambda h, k, l: True)
-        peaks = (wavevectors[np.nonzero(intensities_int)] * 2 / np.pi)**2
-        np.testing.assert_array_almost_equal(peaks, peaks_ref)
+        intensities = np.around(intensities, 8)
+        np.testing.assert_array_equal(
+            intensities[np.nonzero(intensities)], len(self.system.part))
+        # no reflection conditions on (h,k,l)
+        peaks = wavevectors[np.nonzero(intensities)]
+        peaks_ref = self.generate_peaks(l0, l0, l0, lambda h, k, l: True)
+        np.testing.assert_array_almost_equal(
+            self.order(peaks, l0), self.order(peaks_ref[:len(peaks)], l0))
 
     def test_bcc(self):
         """Check body-centered cubic lattice."""
-        xen = range(0, self.box_l, 4)
+        l0 = 4
+        m = l0 / 2
+        xen = range(0, self.box_l, l0)
         for i, j, k in itertools.product(xen, repeat=3):
             self.system.part.add(type=self.part_ty, pos=(i, j, k))
-            self.system.part.add(type=self.part_ty, pos=(i + 2, j + 2, k + 2))
+            self.system.part.add(type=self.part_ty, pos=(i + m, j + m, k + m))
         wavevectors, intensities = self.system.analysis.structure_factor(
             sf_types=[self.part_ty], sf_order=self.sf_order)
-        intensities_int = np.around(intensities).astype(int)
-        np.testing.assert_array_almost_equal(intensities, intensities_int)
-        intensities_ref = np.zeros(intensities.shape)
-        intensities_ref[np.nonzero(intensities_int)] = len(self.system.part)
-        np.testing.assert_array_equal(intensities_int, intensities_ref)
-        # check all reflections
+        intensities = np.around(intensities, 8)
+        np.testing.assert_array_equal(
+            intensities[np.nonzero(intensities)], len(self.system.part))
+        # reflection conditions
         # (h+k+l) even => F = 2f, otherwise F = 0
         peaks_ref = self.generate_peaks(
-            4, 4, 4, lambda h, k, l: (h + k + l) % 2 == 0)
-        peaks = (wavevectors[np.nonzero(intensities_int)] * 2 / np.pi)**2
-        np.testing.assert_array_almost_equal(peaks, peaks_ref)
+            l0, l0, l0, lambda h, k, l: (h + k + l) % 2 == 0)
+        peaks = wavevectors[np.nonzero(intensities)]
+        np.testing.assert_array_almost_equal(
+            self.order(peaks, l0), self.order(peaks_ref[:len(peaks)], l0))
 
     def test_fcc(self):
         """Check face-centered cubic lattice."""
-        xen = range(0, self.box_l, 4)
+        l0 = 4
+        m = l0 / 2
+        xen = range(0, self.box_l, l0)
         for i, j, k in itertools.product(xen, repeat=3):
             self.system.part.add(type=self.part_ty, pos=(i, j, k))
-            self.system.part.add(type=self.part_ty, pos=(i + 2, j + 2, k))
-            self.system.part.add(type=self.part_ty, pos=(i + 2, j, k + 2))
-            self.system.part.add(type=self.part_ty, pos=(i, j + 2, k + 2))
+            self.system.part.add(type=self.part_ty, pos=(i + m, j + m, k))
+            self.system.part.add(type=self.part_ty, pos=(i + m, j, k + m))
+            self.system.part.add(type=self.part_ty, pos=(i, j + m, k + m))
         wavevectors, intensities = self.system.analysis.structure_factor(
             sf_types=[self.part_ty], sf_order=self.sf_order)
-        intensities_int = np.around(intensities).astype(int)
-        np.testing.assert_array_almost_equal(intensities, intensities_int)
-        intensities_ref = np.zeros(intensities.shape)
-        intensities_ref[np.nonzero(intensities_int)] = len(self.system.part)
-        np.testing.assert_array_equal(intensities_int, intensities_ref)
-        # check all reflections
+        intensities = np.around(intensities, 8)
+        np.testing.assert_array_equal(
+            intensities[np.nonzero(intensities)], len(self.system.part))
+        # reflection conditions
         # (h,k,l) all even or odd => F = 4f, otherwise F = 0
         peaks_ref = self.generate_peaks(
-            4, 4, 4, lambda h, k, l:
+            l0, l0, l0, lambda h, k, l:
             h % 2 == 0 and k % 2 == 0 and l % 2 == 0 or
             h % 2 == 1 and k % 2 == 1 and l % 2 == 1)
-        peaks = (wavevectors[np.nonzero(intensities_int)] * 2 / np.pi)**2
-        np.testing.assert_array_almost_equal(peaks, peaks_ref)
+        peaks = wavevectors[np.nonzero(intensities)]
+        np.testing.assert_array_almost_equal(
+            self.order(peaks, l0), self.order(peaks_ref[:len(peaks)], l0))
 
     def test_cco(self):
         """Check c-centered orthorhombic lattice."""
-        xen = range(0, self.box_l, 4)
+        l0 = 4
+        m = l0 / 2
+        xen = range(0, self.box_l, l0)
         for i, j, k in itertools.product(xen, repeat=3):
             self.system.part.add(type=self.part_ty, pos=(i, j, k))
-            self.system.part.add(type=self.part_ty, pos=(i + 2, j + 2, k))
+            self.system.part.add(type=self.part_ty, pos=(i + m, j + m, k))
         wavevectors, intensities = self.system.analysis.structure_factor(
             sf_types=[self.part_ty], sf_order=self.sf_order)
-        intensities_int = np.around(intensities).astype(int)
-        # check all reflections
-        peaks_ref = self.generate_peaks(4, 4, 4, lambda h, k, l: True)
-        peaks = (wavevectors[np.nonzero(intensities_int)] * 2 / np.pi)**2
-        np.testing.assert_array_almost_equal(peaks, peaks_ref)
-        # check main reflections
+        intensities = np.around(intensities, 8)
+        # reflection conditions
         # (h+k) even => F = 2f, otherwise F = 0
-        main_peaks_ref = 4 * self.generate_peaks(
-            4, 4, 4, lambda h, k, l: (h + k) % 2 == 0, self.box_l / 2)
-        peaks = (wavevectors * 2 / np.pi)**2
-        for main_peak in main_peaks_ref:
-            idx = (np.abs(peaks - main_peak)).argmin()
-            self.assertAlmostEqual(peaks[idx], main_peak)
-            self.assertAlmostEqual(intensities[idx], len(self.system.part))
+        peaks_ref = self.generate_peaks(
+            l0, l0, l0, lambda h, k, l: (h + k) % 2 == 0)
+        peaks = wavevectors[np.nonzero(intensities)]
+        np.testing.assert_array_almost_equal(
+            self.order(peaks, l0), self.order(peaks_ref[:len(peaks)], l0))
 
     def test_exceptions(self):
         with self.assertRaisesRegex(ValueError, 'order has to be a strictly positive number'):
