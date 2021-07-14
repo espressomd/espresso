79d0e2a92646c3894de2313db260818cb294ea81
Alexander Reinauer <areinauer@icp.uni-stuttgart.de>
2021-07-14 16:33:26 +0200

    testsuite: ek_noflux: added simple noflux testcase
    Signed-off-by: Alexander Reinauer <areinauer@icp.uni-stuttgart.de>

---
+++ testsuite/python/ek_noflux.py
@@ -0,0 +1,71 @@
+import unittest as ut
+import unittest_decorators as utx
+import espressomd
+import numpy as np
+import espressomd.walberla
+import espressomd.ekin_walberla
+import espressomd.ekboundaries
+import espressomd.shapes
+
+
+@utx.skipIfMissingFeatures(["LB_WALBERLA"])
+class EKNoFlux(ut.TestCase):
+    BOX_L = 15.
+    AGRID = 1.0
+    DENSITY = 1
+    DIFFUSION_COEFFICIENT = 0.1
+    TIME = 50
+    RADIUS = 5.
+
+    system = espressomd.System(box_l=[BOX_L, BOX_L, BOX_L])
+    system.time_step = 1.0
+    system.cell_system.skin = 0.4
+
+    def test_diffusion(self):
+        """
+        Testing the EK noflux boundaries to not leak density outside of a sphere.
+        """
+
+        espressomd.walberla.WalberlaBlockForest(
+            box_size=self.system.box_l, ghost_layers=1, agrid=self.AGRID)
+
+        ekin_walberla = espressomd.ekin_walberla.EKinWalberla(diffusion=self.DIFFUSION_COEFFICIENT,
+                                                              kT=0.0,
+                                                              dens=0.0,
+                                                              tau=1.0)
+        self.system.actors.add(ekin_walberla)
+
+        center = np.asarray(self.system.box_l / 2, dtype=np.int)
+
+        ekin_walberla[center[0], center[1], center[2]].density = self.DENSITY
+
+        sphere = espressomd.ekboundaries.EKBoundary(
+            shape=espressomd.shapes.Sphere(
+                center=self.system.box_l / 2,
+                radius=self.RADIUS,
+                direction=-1))
+        self.system.ekboundaries.add(sphere)
+
+        positions = np.empty((*self.system.box_l.astype(np.int), 3))
+        positions[..., 2], positions[..., 1], positions[..., 0] = np.meshgrid(
+            *map(lambda x: np.arange(0, x) - x / 2, self.system.box_l))
+        positions += 0.5
+
+        mask = np.linalg.norm(positions, axis=-1) < self.RADIUS
+
+        self.system.integrator.run(self.TIME)
+
+        simulated_density = np.copy(ekin_walberla[:, :, :].density)
+
+        # check that the density is conserved
+        np.testing.assert_almost_equal(
+            np.sum(simulated_density), self.DENSITY, 10)
+        if np.any(simulated_density < 0.):
+            self.fail("EK density array contains negative densities!")
+
+        # check that nothing leaked outside
+        np.testing.assert_equal(simulated_density[~mask], 0.)
+
+
+if __name__ == "__main__":
+    ut.main()
