181633d63680c0b8f41bbeaf31acea1fb6b924c0
Alexander Reinauer <areinauer@icp.uni-stuttgart.de>
2021-07-14 11:57:10 +0200

    walberla_bridge: EKinWalberla: use constructor
    Signed-off-by: Alexander Reinauer <areinauer@icp.uni-stuttgart.de>

--- src/walberla_bridge/ekin_walberla_init.cpp
+++ src/walberla_bridge/ekin_walberla_init.cpp
@@ -7,5 +7,5 @@ EKinWalberlaBase<double> *
 new_ekin_walberla(const walberla::WalberlaBlockForest *blockforest,
                   double diffusion, double kT, double density) {
   return new walberla::EKinWalberlaImpl(
-      walberla::EKinWalberlaImpl{blockforest, diffusion, kT, density});
+      walberla::EKinWalberlaImpl(blockforest, diffusion, kT, density));
 }
