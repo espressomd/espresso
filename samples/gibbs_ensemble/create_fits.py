fix math in error calculation

samples/gibbs_ensemble/create_fits.py
@@ -51,7 +51,7 @@ logging.basicConfig(
 )
 
 # Warmup length to skip at beginning
-WARMUP_LENGTH = 3000
+WARMUP_LENGTH = 300000
 
 # Lists for (unsorted) densities and temperature
 dens_liquid = []
@@ -76,8 +76,8 @@ def relaxation_time(x):
     """ Compute the relaxation time """
     corr = autocorr(x)
     popt, _ = scipy.optimize.curve_fit(lambda x, a: np.exp(-a * x),
-                                       range(np.size(corr)), corr, p0=(1. / 15000))
-    return popt[0]
+                                       range(np.size(corr)), corr, p0=(1.))
+    return 1. / popt[0]
 
 
 def std_error_mean(x):
@@ -117,8 +117,8 @@ for f in args.files:
     # Calculate errors
     if density1 < density2:
         key1, key2 = (key2, key1)
-    errors_liquid.append(std_error_mean(data[key2]))
-    errors_gas.append(std_error_mean(data[key1]))
+    errors_liquid.append(std_error_mean(data[key2][WARMUP_LENGTH:]))
+    errors_gas.append(std_error_mean(data[key1][WARMUP_LENGTH:]))
     kTs.append(kT)
 
 # Sort temperatures and densities respectively
@@ -152,7 +152,7 @@ y_rectilinear = 0.5 * (dens_liquid + dens_gas)
 
 # Fit using scaling law
 fit_scaling, p_cov_scaling = scipy.optimize.curve_fit(
-    scaling_law, kTs, y_scaling, p0=(1., 1.), maxfev=6000)
+    scaling_law, kTs, y_scaling, p0=(1., 1.1), maxfev=6000)
 
 # Print fit values
 logging.info(f"Fits using scaling law: B, T_c: {fit_scaling}")