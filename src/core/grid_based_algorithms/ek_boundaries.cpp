28c4845da506805969a808b795cb08b3f3539464
Alexander Reinauer <areinauer@icp.uni-stuttgart.de>
2021-08-05 13:42:11 +0200

    core: ek_boundaries: apply boundaries to all species. This should be changed in the future to apply the boundaries per species
    Signed-off-by: Alexander Reinauer <areinauer@icp.uni-stuttgart.de>

--- src/core/grid_based_algorithms/ek_boundaries.cpp
+++ src/core/grid_based_algorithms/ek_boundaries.cpp
@@ -15,21 +15,25 @@ std::vector<std::shared_ptr<EKBoundary>> ekboundaries;
 void ek_init_boundaries() {
 #ifdef EK_WALBERLA
   if (EK::get_lattice_switch() == EK::ActiveEK::WALBERLA) {
-    ekin_walberla()->clear_boundaries();
-
     auto const agrid = get_walberla_blockforest_params()->get_agrid();
+    std::for_each(get_eks_walberla().begin(), get_eks_walberla().end(),
+                  [agrid](const EKWalberlaInstance &ek_instance) {
+                    const auto ek = ek_instance.get_ek();
+                    ek->clear_boundaries();
 
-    for (auto index_and_pos : ekin_walberla()->node_indices_positions(true)) {
-      // Convert to MD units
-      auto const index = index_and_pos.first;
-      auto const pos = index_and_pos.second * agrid;
+                    for (auto index_and_pos :
+                         ek->node_indices_positions(true)) {
+                      // Convert to MD units
+                      auto const index = index_and_pos.first;
+                      auto const pos = index_and_pos.second * agrid;
 
-      for (auto const &ekboundary : ekboundaries) {
-        if (not ekboundary->shape().is_inside(pos)) {
-          ekin_walberla()->set_node_noflux_boundary(index);
-        }
-      }
-    }
+                      for (auto const &ekboundary : ekboundaries) {
+                        if (not ekboundary->shape().is_inside(pos)) {
+                          ek->set_node_noflux_boundary(index);
+                        }
+                      }
+                    }
+                  });
   }
 #endif // EK_WALBERLA
 }
