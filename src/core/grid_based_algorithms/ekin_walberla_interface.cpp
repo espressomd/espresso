4338b4a9a62a92716a1bbf1b5942ddbcccdc4c70
Alexander Reinauer <areinauer@icp.uni-stuttgart.de>
2021-08-05 12:52:56 +0200

    core: ek_walberla_interface: added id and replaced ekin_walberla with get_ek_walberla
    Signed-off-by: Alexander Reinauer <areinauer@icp.uni-stuttgart.de>

--- src/core/grid_based_algorithms/ekin_walberla_interface.cpp
+++ src/core/grid_based_algorithms/ekin_walberla_interface.cpp
@@ -11,67 +11,74 @@
 
 namespace walberla {
 
-double ek_get_diffusion() { return ekin_walberla()->get_diffusion(); }
-void ek_set_diffusion(double diffusion) {
-  ekin_walberla()->set_diffusion(diffusion);
+double ek_get_diffusion(uint id) {
+  return get_ek_walberla(id)->get_diffusion();
+}
+void ek_set_diffusion(uint id, double diffusion) {
+  get_ek_walberla(id)->set_diffusion(diffusion);
 }
 
 REGISTER_CALLBACK(ek_set_diffusion)
 
-double ek_get_kT() { return ekin_walberla()->get_kT(); }
-void ek_set_kT(double kT) { ekin_walberla()->set_kT(kT); }
+double ek_get_kT(uint id) { return get_ek_walberla(id)->get_kT(); }
+void ek_set_kT(uint id, double kT) { get_ek_walberla(id)->set_kT(kT); }
 
 REGISTER_CALLBACK(ek_set_kT)
 
-double ek_get_tau() { return ek_walberla_params()->get_tau(); }
+double ek_get_tau(uint id) { return get_ek_instance_walberla(id).get_tau(); }
 
-boost::optional<double> ek_get_node_density(const Utils::Vector3i &ind) {
-  auto res = ekin_walberla()->get_node_density(ind);
+boost::optional<double> ek_get_node_density(uint id,
+                                            const Utils::Vector3i &ind) {
+  auto res = get_ek_walberla(id)->get_node_density(ind);
   return res;
 }
 
 REGISTER_CALLBACK_ONE_RANK(ek_get_node_density)
 
-void ek_set_node_density(const Utils::Vector3i &ind, double density) {
-  ekin_walberla()->set_node_density(ind, density);
-  ekin_walberla()->ghost_communication();
+void ek_set_node_density(uint id, const Utils::Vector3i &ind, double density) {
+  get_ek_walberla(id)->set_node_density(ind, density);
+  get_ek_walberla(id)->ghost_communication();
 }
 
 REGISTER_CALLBACK(ek_set_node_density)
 
-boost::optional<bool> ek_get_node_is_boundary(const Utils::Vector3i &ind) {
-  return ekin_walberla()->get_node_is_boundary(ind);
+boost::optional<bool> ek_get_node_is_boundary(uint id,
+                                              const Utils::Vector3i &ind) {
+  return get_ek_walberla(id)->get_node_is_boundary(ind);
 }
 
 REGISTER_CALLBACK_ONE_RANK(ek_get_node_is_boundary)
 
-Utils::Vector3i ek_get_shape() {
-  return ekin_walberla()->get_blockforest()->get_grid_dimensions();
+Utils::Vector3i ek_get_shape(uint id) {
+  return get_ek_walberla(id)->get_blockforest()->get_grid_dimensions();
 }
 
-void ek_create_vtk(unsigned delta_N, unsigned initial_count,
+void ek_create_vtk(uint id, unsigned delta_N, unsigned initial_count,
                    unsigned flag_observables, std::string const &identifier,
                    std::string const &base_folder, std::string const &prefix) {
-  ekin_walberla()->create_vtk(delta_N, initial_count, flag_observables,
-                              identifier, base_folder, prefix);
+  get_ek_walberla(id)->create_vtk(delta_N, initial_count, flag_observables,
+                                  identifier, base_folder, prefix);
 }
 
 REGISTER_CALLBACK(ek_create_vtk)
 
-void ek_write_vtk(std::string const &vtk_uid) {
-  ekin_walberla()->write_vtk(vtk_uid);
+void ek_write_vtk(uint id, std::string const &vtk_uid) {
+  get_ek_walberla(id)->write_vtk(vtk_uid);
 }
 
 REGISTER_CALLBACK(ek_write_vtk)
 
-void ek_switch_vtk(std::string const &vtk_uid, int status) {
-  ekin_walberla()->switch_vtk(vtk_uid, status);
+void ek_switch_vtk(uint id, std::string const &vtk_uid, int status) {
+  get_ek_walberla(id)->switch_vtk(vtk_uid, status);
 }
 
 REGISTER_CALLBACK(ek_switch_vtk)
 
-void ek_propagate() { ekin_walberla()->integrate(); }
-
+void ek_propagate() {
+  std::for_each(get_eks_walberla().begin(), get_eks_walberla().end(),
+                [](const EKWalberlaInstance &ek_instance) {
+                  ek_instance.get_ek()->integrate();
+                });
 } // namespace walberla
 
 #endif // EK_WALBERLA
--- src/core/grid_based_algorithms/ekin_walberla_interface.hpp
+++ src/core/grid_based_algorithms/ekin_walberla_interface.hpp
@@ -8,26 +8,28 @@
 
 namespace walberla {
 
-boost::optional<double> ek_get_node_density(const Utils::Vector3i &ind);
-void ek_set_node_density(const Utils::Vector3i &ind, double density);
+boost::optional<double> ek_get_node_density(uint id,
+                                            const Utils::Vector3i &ind);
+void ek_set_node_density(uint id, const Utils::Vector3i &ind, double density);
 
-boost::optional<bool> ek_get_node_is_boundary(const Utils::Vector3i &ind);
+boost::optional<bool> ek_get_node_is_boundary(uint id,
+                                              const Utils::Vector3i &ind);
 
-double ek_get_diffusion();
-void ek_set_diffusion(double diffusion);
+double ek_get_diffusion(uint id);
+void ek_set_diffusion(uint id, double diffusion);
 
-double ek_get_kT();
-void ek_set_kT(double kT);
+double ek_get_kT(uint id);
+void ek_set_kT(uint id, double kT);
 
-double ek_get_tau();
+double ek_get_tau(uint id);
 
-Utils::Vector3i ek_get_shape();
+Utils::Vector3i ek_get_shape(uint id);
 
-void ek_create_vtk(unsigned delta_N, unsigned initial_count,
+void ek_create_vtk(uint id, unsigned delta_N, unsigned initial_count,
                    unsigned flag_observables, std::string const &identifier,
                    std::string const &base_folder, std::string const &prefix);
-void ek_write_vtk(std::string const &vtk_uid);
-void ek_switch_vtk(std::string const &vtk_uid, int status);
+void ek_write_vtk(uint id, std::string const &vtk_uid);
+void ek_switch_vtk(uint id, std::string const &vtk_uid, int status);
 
 void ek_propagate();
 
