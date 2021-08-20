8bb7f665ca69d6256d5a321a2cfeeec80842dd16
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2021-08-20 19:59:24 +0200

    walberla: Fix AVX LatticeModel classes

--- src/walberla_bridge/LBWalberlaD3Q19FluctuatingMRT.hpp
+++ src/walberla_bridge/LBWalberlaD3Q19FluctuatingMRT.hpp
@@ -1,6 +1,23 @@
+/*
+ * Copyright (C) 2020 The ESPResSo project
+ *
+ * This file is part of ESPResSo.
+ *
+ * ESPResSo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ESPResSo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "LBWalberlaImpl.hpp"
-
-#ifdef __AVX__
+#ifdef __AVX2__
 #include "generated_kernels/CollideSweepThermalizedAVX.h"
 #define CollisionModelName walberla::pystencils::CollideSweepThermalizedAVX
 #include "generated_kernels/FluctuatingMRTLatticeModelAvx.h"
@@ -15,7 +32,6 @@
 namespace walberla {
 class LBWalberlaD3Q19FluctuatingMRT
     : public LBWalberlaImpl<LatticeModelName, CollisionModelName> {
-
   using LatticeModel = LatticeModelName;
 
 public:
