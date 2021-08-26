83c62874633562f6ca4a39d75395a8611de31f67
Alexander Reinauer <areinauer@icp.uni-stuttgart.de>
2021-08-26 10:50:27 +0200

    testsuite: ek_noflux: EKFFT -> EKNone
    Signed-off-by: Alexander Reinauer <areinauer@icp.uni-stuttgart.de>

--- testsuite/python/ek_noflux.py
+++ testsuite/python/ek_noflux.py
@@ -31,9 +31,9 @@ class EKNoFlux(ut.TestCase):
         ekspecies = espressomd.EKSpecies.EKSpecies(
             density=0.0, kT=0.0, diffusion=self.DIFFUSION_COEFFICIENT, valency=0.0)
 
-        ekfft = espressomd.EKSpecies.EKFFT()
+        eksolver = espressomd.EKSpecies.EKNone()
 
-        self.system.ekcontainer.add(ekspecies, tau=1.0, solver=ekfft)
+        self.system.ekcontainer.add(ekspecies, tau=1.0, solver=eksolver)
 
         center = np.asarray(self.system.box_l / 2, dtype=np.int)
 
