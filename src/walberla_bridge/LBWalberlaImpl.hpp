ce51f42d516543d2439af1c8b253465308a758c6
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2021-09-01 20:24:05 +0200

    walberla: Fix LE update kernel

--- src/walberla_bridge/LBWalberlaImpl.hpp
+++ src/walberla_bridge/LBWalberlaImpl.hpp
@@ -371,59 +371,64 @@ protected:
           m_n_ghost_layers(n_ghost_layers), m_callbacks(callbacks) {}
 
     void operator()(IBlock *block) {
+      if (blocks_->atDomainYMaxBorder(*block))
+        kernel(block, true);
+      if (blocks_->atDomainYMinBorder(*block))
+        kernel(block, false);
+    }
+
+  private:
+    void kernel(IBlock *block, bool upper) {
       // TODO should dimension_x contain the ghost layers or not. At the moment
       // value is 64 with GL it is 66. In the lbmpy Lees-Edwards this is fixed.
       // Probably not good
-      auto const is_upper_slice = blocks_->atDomainYMaxBorder(*block);
-      auto const is_lower_slice = blocks_->atDomainYMinBorder(*block);
-      if (is_upper_slice or is_lower_slice) {
-        assert(m_n_ghost_layers == 1u);
-        auto constexpr th = cell_idx_t{1};
-        uint_t const dim_x = blocks_->getNumberOfXCells(*block);
-        uint_t const dim_y = blocks_->getNumberOfYCells(*block);
-        // TODO right now the direction of LE is hardcoded for the Y-axis
-        assert(dim_y >= 2u);
+      assert(m_n_ghost_layers == 1u);
+      auto constexpr th = cell_idx_t{1};
+      uint_t const dim_x = blocks_->getNumberOfXCells(*block);
+      uint_t const dim_y = blocks_->getNumberOfYCells(*block);
+      // TODO right now the direction of LE is hardcoded for the Y-axis
+      assert(dim_y >= 2u);
 
-        auto pdf_field = block->template getData<PdfField>(m_pdf_field_id);
-        auto pdf_tmp_field =
-            block->template getData<PdfField>(m_pdf_tmp_field_id);
-        auto const index_dir = cell_idx_c((is_upper_slice) ? -1 : 1);
-        auto const modes_dir = (is_upper_slice) ? stencil::N : stencil::S;
-        auto const offset = real_c(m_callbacks->get_pos_offset());
-        auto const weight = fmod(offset + real_c(dim_x), real_t{1});
-        CellInterval ci;
-        pdf_field->getGhostRegion(modes_dir, ci, th, true);
+      auto pdf_field = block->template getData<PdfField>(m_pdf_field_id);
+      auto pdf_tmp_field =
+          block->template getData<PdfField>(m_pdf_tmp_field_id);
+      auto const index_dir = (upper) ? cell_idx_c(-1) : cell_idx_c(1);
+      auto const modes_dir = (upper) ? stencil::N : stencil::S;
+      auto const offset = real_c(m_callbacks->get_pos_offset());
+      auto const weight = fmod(offset + real_c(dim_x), real_t{1});
+      CellInterval ci;
+      pdf_field->getGhostRegion(modes_dir, ci, th, true);
 
-        // shift
-        for (auto cell = ci.begin(); cell != ci.end(); ++cell) {
-          cell_idx_t const x = cell->x();
-          cell_idx_t const y = cell->y();
-          cell_idx_t const z = cell->z();
+      // shift
+      for (auto cell = ci.begin(); cell != ci.end(); ++cell) {
+        cell_idx_t const x = cell->x();
+        cell_idx_t const y = cell->y();
+        cell_idx_t const z = cell->z();
 
-          auto const x1 =
-              cell_idx_c(floor(x + index_dir * offset + real_c(dim_x))) %
-              cell_idx_c(dim_x);
-          auto const x2 =
-              cell_idx_c(ceil(x + index_dir * offset + real_c(dim_x))) %
-              cell_idx_c(dim_x);
-          auto const ys = y + index_dir * th;
+        auto const x1 =
+            cell_idx_c(floor(x + index_dir * offset + real_c(dim_x))) %
+            cell_idx_c(dim_x);
+        auto const x2 =
+            cell_idx_c(ceil(x + index_dir * offset + real_c(dim_x))) %
+            cell_idx_c(dim_x);
+        auto const ys = y + index_dir * th;
 
-          for (uint_t q = 0; q < Stencil::Q; ++q) {
-            pdf_tmp_field->get(x, ys, z, 0) =
-                pdf_field->get(x1, ys, z, q) * (1 - weight) +
-                pdf_field->get(x2, ys, z, q) * weight;
-          }
+        for (uint_t q = 0; q < Stencil::Q; ++q) {
+          pdf_tmp_field->get(x, ys, z, q) =
+              pdf_field->get(x1, ys, z, q) * (1 - weight) +
+              pdf_field->get(x2, ys, z, q) * weight;
         }
+      }
 
-        // swap
-        for (auto cell = ci.begin(); cell != ci.end(); ++cell) {
-          cell_idx_t const x = cell->x();
-          cell_idx_t const y = cell->y();
-          cell_idx_t const z = cell->z();
-          auto const ys = y + index_dir * th;
+      // swap
+      for (auto cell = ci.begin(); cell != ci.end(); ++cell) {
+        cell_idx_t const x = cell->x();
+        cell_idx_t const y = cell->y();
+        cell_idx_t const z = cell->z();
+        auto const ys = y + index_dir * th;
 
-          pdf_field->get(x, ys, z, 0) = pdf_tmp_field->get(x, ys, z, 0);
-        }
+        for (uint_t q = 0; q < Stencil::Q; ++q)
+          pdf_field->get(x, ys, z, q) = pdf_tmp_field->get(x, ys, z, q);
       }
     }
 
