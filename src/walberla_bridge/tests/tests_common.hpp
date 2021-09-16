600942d8790140b81ca652c3254966760065962c
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2021-09-16 13:58:43 +0200

    tests: Reduce code complexity

--- src/walberla_bridge/tests/tests_common.hpp
+++ src/walberla_bridge/tests/tests_common.hpp
@@ -1,5 +1,3 @@
-#pragma once
-
 /*
  * Copyright (C) 2019-2020 The ESPResSo project
  *
@@ -18,6 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#pragma once
+
 #include "config.hpp"
 
 #ifdef LB_WALBERLA
