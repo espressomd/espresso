620d573a36bc209d76ae14255a2e309a5cb80a1c
Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
2021-09-22 15:44:43 +0200

    add test

--- testsuite/python/lb_boundary_velocity.py
+++ testsuite/python/lb_boundary_velocity.py
@@ -24,35 +24,124 @@ import numpy as np
 
 @utx.skipIfMissingFeatures(["LB_BOUNDARIES", "LB_WALBERLA"])
 class LBBoundaryVelocityTest(ut.TestCase):
-
-    """Test slip velocity of boundaries.
-
-       In this simple test, a wall with slip velocity is
-       added and the fluid is checked if it has the same velocity.
+    """
+    Various tests to check the interaction of lb velocity boundary conditions and the fluid
     """
 
-    system = espressomd.System(box_l=[10.0, 10.0, 10.0])
-    system.time_step = .5
+    lb_params = {'agrid': 0.6,
+                 'dens': 0.5,
+                 'visc': 3.2,
+                 'tau': 0.7}
+    system = espressomd.System(box_l=3 * [8 * lb_params['agrid']])
+    system.time_step = lb_params['tau']
     system.cell_system.skin = 0.1
 
-    def test(self):
-        system = self.system
+    lb_fluid = None
 
-        lb_fluid = espressomd.lb.LBFluidWalberla(
-            agrid=2.0, dens=.5, visc=5.0, tau=0.5)
-        system.actors.add(lb_fluid)
+    def tearDown(self) -> None:
+        self.system.actors.clear()
+        self.system.lbboundaries.clear()
 
-        v_boundary = [0.03, 0.02, 0.01]
+    def setUp(self) -> None:
+        self.lb_fluid = espressomd.lb.LBFluidWalberla(**self.lb_params)
+        self.system.actors.add(self.lb_fluid)
 
-        wall_shape = espressomd.shapes.Wall(normal=[1, 2, 3], dist=0)
+    def check_wall_slip(self, v_boundary):
+        """
+        Check that the fluid adopts the velocity set by the boundary conditions.
+        """
+
+        agrid = self.lb_params['agrid']
+        wall_shape_left = espressomd.shapes.Wall(normal=[1, 2, 3], dist=0)
+        wall_shape_right = espressomd.shapes.Wall(normal=[-1, 0, 0], dist=-(self.system.box_l[0] - agrid))
+        for shape in [wall_shape_left, wall_shape_right]:
+            wall = espressomd.lbboundaries.LBBoundary(
+                shape=shape, velocity=v_boundary)
+            self.system.lbboundaries.add(wall)
+
+        # velocity in front of the wall must be that of the boundary immediately
+        self.system.integrator.run(100)
+
+        for node in self.lb_fluid.nodes():
+            if not node.is_boundary:
+                print(node.velocity)
+
+        v_fluid = self.lb_fluid[2, 0, 0].velocity
+        np.testing.assert_allclose(v_fluid, v_boundary, atol=1E-5)
+
+        # velocity in the middle needs to propagate first
+        self.system.integrator.run(200)
+        v_fluid = self.lb_fluid[2, 1, 3].velocity
+        np.testing.assert_allclose(v_fluid, v_boundary, atol=1E-5)
+
+    def test_wall_slip_parallel(self):
+        v_boundary = [0, 0, 0.07]
+        self.check_wall_slip(v_boundary)
+
+    def test_wall_slip_nonparallel(self):
+        v_boundary = [0.03, 0.05, 0.07]
+        self.check_wall_slip(v_boundary)
+
+    def test_boundary_readout(self):
+        """
+        Test the read part of the boundary property of lb nodes.
+        """
+        v_boundary = [0.03, 0.05, 0.07]
+        wall_shape = espressomd.shapes.Wall(normal=[1, 0, 0], dist=self.lb_params['agrid'])
         wall = espressomd.lbboundaries.LBBoundary(
             shape=wall_shape, velocity=v_boundary)
-        system.lbboundaries.add(wall)
+        self.system.lbboundaries.add(wall)
 
-        system.integrator.run(2000)
+        # check non_boundary node
+        bound_cond = self.lb_fluid[4, 4, 4].boundary
+        self.assertIsNone(bound_cond)
 
-        v_fluid = lb_fluid[3, 0, 0].velocity
-        np.testing.assert_allclose(v_fluid, v_boundary, atol=1E-5)
+        # on boundary
+        bound_cond = self.lb_fluid[0, 2, 4].boundary
+        np.testing.assert_array_almost_equal(bound_cond.velocity, v_boundary)
+
+        # TODO boundary_force
+
+    def test_velocity_bounce_back_class(self):
+        """
+        Test setters and getters of :ref:`espressomd.lbboundaries.VelocityBounceBack`
+        """
+        with self.assertRaises(ValueError):
+            bound_cond = espressomd.lbboundaries.VelocityBounceBack([1, 2, 3, 4])
+        v = [1, 2, 17.4]
+        bound_cond = espressomd.lbboundaries.VelocityBounceBack(v)
+        np.testing.assert_array_almost_equal(bound_cond.velocity, v)
+
+    def test_boundary_setting(self):
+        """
+        Test setting and un-setting individual lb boundary nodes.
+        """
+        v_boundary = [0.2, 0.1, 0.3]
+        bound_cond = espressomd.lbboundaries.VelocityBounceBack(v_boundary)
+
+        with self.assertRaises(ValueError):
+            self.lb_fluid[1, 2, 3].boundary = 17
+
+        self.lb_fluid[1, 2, 3].boundary = bound_cond
+        np.testing.assert_array_almost_equal(self.lb_fluid[1, 2, 3].boundary.velocity, bound_cond.velocity)
+
+        self.lb_fluid[1, 2, 3].boundary = None
+        self.assertIsNone(self.lb_fluid[1, 2, 3].boundary)
+
+    def test_nodes_in_shape(self):
+        """
+        Test if the get_nodes_in_shape method correctly identifies the grid points inside a cylinder
+        """
+        agrid = self.lb_params['agrid']
+        cyl = espressomd.shapes.Cylinder(center=agrid * np.array([1.5, 2.5, 5.5]),
+                                         axis=[0, 0, 1],
+                                         length=2.1 * agrid,
+                                         radius=0.5 * agrid)
+        nodes_in_cyl = self.lb_fluid.get_nodes_in_shape(cyl)
+        idxs_in_cyl = set(tuple(node.index) for node in nodes_in_cyl)
+
+        idx_should_be = {(1, 2, 5), (1, 2, 4), (1, 2, 6)}
+        self.assertSetEqual(idxs_in_cyl, idx_should_be)
 
 
 if __name__ == "__main__":
