559708d03fb7e13fd1553049f5a058a5ff6efbe4
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2022-10-19 23:36:54 +0200

    walberla: Re-enable LB sedimentation test

--- testsuite/scripts/tutorials/test_lattice_boltzmann_sedimentation.py
+++ testsuite/scripts/tutorials/test_lattice_boltzmann_sedimentation.py
@@ -25,7 +25,7 @@ import scipy.stats
 
 tutorial, skipIfMissingFeatures = importlib_wrapper.configure_and_import(
     "@TUTORIALS_DIR@/lattice_boltzmann/lattice_boltzmann_sedimentation.py",
-    sampling_steps=400)
+    sampling_steps=450)
 
 
 def curl2d(flow, spacing):
@@ -45,7 +45,7 @@ class Tutorial(ut.TestCase):
 
     def test_flow_profile(self):
         # slice trajectory to keep only the flow field onset
-        flow_field = tutorial.data_flowfield[200:400, :, :, 0:2]
+        flow_field = tutorial.data_flowfield[400:450, :, :, 0:2]
         vortices = np.zeros((2, flow_field.shape[0], 2))
         for i in range(flow_field.shape[0]):
             curl = curl2d(flow_field[i], 2 * [tutorial.spacing])
@@ -53,8 +53,8 @@ class Tutorial(ut.TestCase):
             vortices[1, i] = get_peak_position(curl, np.argmin)
 
         # check flow field curl
-        ref_pos_x = [12.5, 7.5]  # LB units
-        ref_pos_y = 2 * [11.]    # LB units
+        ref_pos_x = [5.5, 13.5]  # LB units
+        ref_pos_y = 2 * [13.5]   # LB units
         for i in range(2):
             width = tutorial.n_width  # LB units
             vortex_avg_x = scipy.stats.circmean(vortices[i, :, 0], high=width)
@@ -63,8 +63,8 @@ class Tutorial(ut.TestCase):
             vortex_std_y = np.std(vortices[i, :, 1])
             self.assertAlmostEqual(vortex_avg_x, ref_pos_x[i], delta=2.)
             self.assertAlmostEqual(vortex_avg_y, ref_pos_y[i], delta=2.)
-            self.assertLess(vortex_std_x, 4.)
-            self.assertLess(vortex_std_y, 6.)
+            self.assertLess(vortex_std_x, 2.)
+            self.assertLess(vortex_std_y, 3.)
 
 
 if __name__ == "__main__":
