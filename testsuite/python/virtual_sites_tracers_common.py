espressomd/espresso

commit 29cf77d9c4c24513230c7b45f3794e25f0256179

introduce getters for particle properties

src/core/virtual_sites/lb_inertialess_tracers.cpp
@@ -72,8 +72,8 @@ void IBM_ForcesIntoFluid_CPU() {
   for (auto &p : cell_structure.ghost_particles()) {
     // for ghost particles we have to check if they lie
     // in the range of the local lattice nodes
-    if (in_local_domain(p.r.p)) {
-      if (p.p.is_virtual)
+    if (in_local_domain(p.pos())) {
+      if (p.is_virtual())
         CoupleIBMParticleToFluid(p);
     }
   }
@@ -95,19 +95,13 @@ void IBM_UpdateParticlePositions(ParticleRange const &particles,
 
   // Euler integrator
   for (auto &p : particles) {
-    if (p.p.is_virtual) {
+    if (p.is_virtual()) {
+      for (int axis = 0; axis < 2; axis++) {
 #ifdef EXTERNAL_FORCES
-      if (!(p.p.ext_flag & 2))
+        if (not p.is_fixed_along(axis))
 #endif
-        p.r.p[0] += p.m.v[0] * time_step;
-#ifdef EXTERNAL_FORCES
-      if (!(p.p.ext_flag & 4))
-#endif
-        p.r.p[1] += p.m.v[1] * time_step;
-#ifdef EXTERNAL_FORCES
-      if (!(p.p.ext_flag & 8))
-#endif
-        p.r.p[2] += p.m.v[2] * time_step;
+          p.pos()[axis] += p.v()[axis] * time_step;
+      }
     }
   }
 
@@ -119,12 +113,12 @@ void IBM_UpdateParticlePositions(ParticleRange const &particles,
 /** Put the momentum of a given particle into the LB fluid. */
 void CoupleIBMParticleToFluid(Particle const &p) {
   // Convert units from MD to LB
-  auto const delta_j = p.f.f * Utils::int_pow<4>(lbpar.tau) / lbpar.agrid;
+  auto const delta_j = p.force() * Utils::int_pow<4>(lbpar.tau) / lbpar.agrid;
 
   // Get indices and weights of affected nodes using discrete delta function
   Utils::Vector<std::size_t, 8> node_index{};
   Utils::Vector6d delta{};
-  lblattice.map_position_to_lattice(p.r.p, node_index, delta);
+  lblattice.map_position_to_lattice(p.pos(), node_index, delta);
 
   // Loop over all affected nodes
   for (int z = 0; z < 2; z++) {
@@ -256,10 +250,10 @@ void ParticleVelocitiesFromLB_CPU() {
   // Here all contributions are included: velocity, external force and
   // particle force.
   for (auto &p : cell_structure.local_particles()) {
-    if (p.p.is_virtual) {
+    if (p.is_virtual()) {
       // Get interpolated velocity and store in the force (!) field
       // for later communication (see below)
-      p.f.f = GetIBMInterpolatedVelocity<true>(p.r.p);
+      p.force() = GetIBMInterpolatedVelocity<true>(p.pos());
     }
   }
 
@@ -269,19 +263,19 @@ void ParticleVelocitiesFromLB_CPU() {
     // This criterion include the halo on the left, but excludes the halo on
     // the right
     // Try if we have to use *1.5 on the right
-    if (in_local_domain(p.r.p)) {
-      if (p.p.is_virtual) {
+    if (in_local_domain(p.pos())) {
+      if (p.is_virtual()) {
         // The force stemming from the ghost particle (for
         // communication, see below)
-        p.f.f = GetIBMInterpolatedVelocity<false>(p.r.p);
+        p.force() = GetIBMInterpolatedVelocity<false>(p.pos());
       } else {
         // Reset, necessary because we add all forces below. Also needs to
         // be done for real particles!
-        p.f.f = {};
+        p.force() = {};
       }
     } else {
       // Reset, necessary because we add all forces below
-      p.f.f = {};
+      p.force() = {};
     }
   }
 
@@ -299,8 +293,8 @@ void ParticleVelocitiesFromLB_CPU() {
 
   // Transfer to velocity field
   for (auto &p : cell_structure.local_particles()) {
-    if (p.p.is_virtual) {
-      p.m.v = p.f.f;
+    if (p.is_virtual()) {
+      p.v() = p.force();
     }
   }
 }

src/core/PartCfg.cpp
@@ -50,8 +50,8 @@ void PartCfg::update() {
       m_parts.push_back(get_particle_data(id));
 
       auto &p = m_parts.back();
-      p.r.p += image_shift(p.l.i, box_geo.length());
-      p.l.i = {};
+      p.pos() += image_shift(p.image_box(), box_geo.length());
+      p.image_box() = {};
     }
 
     offset += this_size;

src/core/Particle.hpp
@@ -29,9 +29,20 @@
 
 #include <boost/serialization/vector.hpp>
 
+#include <bitset>
 #include <cstdint>
 #include <stdexcept>
 #include <vector>
+
+namespace detail {
+inline void check_axis_idx_valid(int const axis) {
+  assert(axis >= 0 and axis <= 2);
+}
+
+inline bool get_nth_bit(uint8_t const bitfield, int const bit_idx) {
+  return bitfield & (1u << bit_idx);
+}
+} // namespace detail
 
 enum : uint8_t {
   ROTATION_FIXED = 0u,
@@ -57,9 +68,9 @@ enum : uint8_t {
 struct ParticleParametersSwimming {
   /** Is the particle a swimmer. */
   bool swimming = false;
-  /** Constant velocity to relax to. */
-  double f_swim = 0.;
   /** Imposed constant force. */
+  double f_swim = 0.;
+  /** Constant velocity to relax to. */
   double v_swim = 0.;
   /** Flag for the swimming mode in a LB fluid.
    *  Values:
@@ -421,11 +432,6 @@ struct Particle { // NOLINT(bugprone-exception-escape)
 private:
   BondList bl;
 
-public:
-  auto &bonds() { return bl; }
-  auto const &bonds() const { return bl; }
-
-private:
 #ifdef EXCLUSIONS
   /** list of particles, with which this particle has no non-bonded
    *  interactions
@@ -434,21 +440,151 @@ private:
 #endif
 
 public:
-  std::vector<int> &exclusions() {
-#ifdef EXCLUSIONS
-    return el;
-#else
-    throw std::runtime_error{"Exclusions not enabled."};
-#endif
-  }
+  auto const &id() const { return p.identity; }
+  auto &id() { return p.identity; }
+  auto const &mol_id() const { return p.mol_id; }
+  auto &mol_id() { return p.mol_id; }
+  auto const &type() const { return p.type; }
+  auto &type() { return p.type; }
 
-  std::vector<int> const &exclusions() const {
-#ifdef EXCLUSIONS
-    return el;
+  auto const &bonds() const { return bl; }
+  auto &bonds() { return bl; }
+
+  auto const &pos() const { return r.p; }
+  auto &pos() { return r.p; }
+  auto const &v() const { return m.v; }
+  auto &v() { return m.v; }
+  auto const &force() const { return f.f; }
+  auto &force() { return f.f; }
+
+  bool is_ghost() const { return l.ghost; }
+  void set_ghost(bool const ghost_flag) { l.ghost = ghost_flag; }
+  auto &pos_at_last_verlet_update() { return l.p_old; }
+  auto const &pos_at_last_verlet_update() const { return l.p_old; }
+  auto const &image_box() const { return l.i; }
+  auto &image_box() { return l.i; }
+
+#ifdef MASS
+  auto const &mass() const { return p.mass; }
+  auto &mass() { return p.mass; }
 #else
-    throw std::runtime_error{"Exclusions not enabled."};
+  constexpr auto &mass() const { return p.mass; }
 #endif
+#ifdef ROTATION
+  bool can_rotate() const {
+    return can_rotate_around(0) or can_rotate_around(1) or can_rotate_around(2);
   }
+  bool can_rotate_around(int const axis) const {
+    detail::check_axis_idx_valid(axis);
+    return detail::get_nth_bit(p.rotation, axis);
+  }
+  void set_can_rotate_around(int const axis, bool const rot_flag) {
+    detail::check_axis_idx_valid(axis);
+    if (rot_flag) {
+      p.rotation |= static_cast<uint8_t>(1u << axis);
+    } else {
+      p.rotation &= static_cast<uint8_t>(~(1u << axis));
+    }
+  }
+  void set_can_rotate_all_axes() {
+    for (int axis = 0; axis <= 2; axis++) {
+      set_can_rotate_around(axis, true);
+    }
+  }
+  auto const &quat() const { return r.quat; }
+  auto &quat() { return r.quat; }
+  auto const &torque() const { return f.torque; }
+  auto &torque() { return f.torque; }
+  auto const &omega() const { return m.omega; }
+  auto &omega() { return m.omega; }
+  auto const &ext_torque() const { return p.ext_torque; }
+  auto &ext_torque() { return p.ext_torque; }
+  auto calc_director() const { return r.calc_director(); }
+#else
+  bool can_rotate() const { return false; }
+  bool can_rotate_around(int const axis) const { return false; }
+#endif
+#ifdef DIPOLES
+  auto const &dipm() const { return p.dipm; }
+  auto &dipm() { return p.dipm; }
+#endif
+#ifdef ROTATIONAL_INERTIA
+  auto const &rinertia() const { return p.rinertia; }
+  auto &rinertia() { return p.rinertia; }
+#else
+  constexpr auto &rinertia() const { return p.rinertia; }
+#endif
+#ifdef ELECTROSTATICS
+  auto const &q() const { return p.q; }
+  auto &q() { return p.q; }
+#else
+  constexpr auto &q() const { return p.q; }
+#endif
+#ifdef LB_ELECTROHYDRODYNAMICS
+  auto const &mu_E() const { return p.mu_E; }
+  auto &mu_E() { return p.mu_E; }
+#endif
+#ifdef VIRTUAL_SITES
+  bool is_virtual() const { return p.is_virtual; }
+  void set_virtual(bool const virt_flag) { p.is_virtual = virt_flag; }
+#ifdef VIRTUAL_SITES_RELATIVE
+  auto const &vs_relative() const { return p.vs_relative; }
+  auto &vs_relative() { return p.vs_relative; }
+#endif // VIRTUAL_SITES_RELATIVE
+#else
+  constexpr auto is_virtual() const { return p.is_virtual; }
+#endif
+#ifdef THERMOSTAT_PER_PARTICLE
+  auto const &gamma() const { return p.gamma; }
+  auto &gamma() { return p.gamma; }
+#ifdef ROTATION
+  auto const &gamma_rot() const { return p.gamma_rot; }
+  auto &gamma_rot() { return p.gamma_rot; }
+#endif // ROTATION
+#endif // THERMOSTAT_PER_PARTICLE
+#ifdef EXTERNAL_FORCES
+  bool has_fixed_coordinates() const {
+    return detail::get_nth_bit(p.ext_flag, 0);
+  }
+  bool is_fixed_along(int const axis) const {
+    detail::check_axis_idx_valid(axis);
+    return detail::get_nth_bit(p.ext_flag, axis + 1);
+  }
+  void set_fixed_along(int const axis, bool const fixed_flag) {
+    // set new flag
+    if (fixed_flag) {
+      p.ext_flag |= static_cast<uint8_t>(1u << (axis + 1));
+    } else {
+      p.ext_flag &= static_cast<uint8_t>(~(1u << (axis + 1)));
+    }
+    // check if any flag is set and store that in the 0th bit
+    if (p.ext_flag >> 1) {
+      p.ext_flag |= static_cast<uint8_t>(1u);
+    } else {
+      p.ext_flag &= static_cast<uint8_t>(~1u);
+    }
+  }
+  auto const &ext_force() const { return p.ext_force; }
+  auto &ext_force() { return p.ext_force; }
+
+#else  // EXTERNAL_FORCES
+  constexpr bool has_fixed_coordinates() const { return false; }
+  constexpr bool is_fixed_along(int const axis) const { return false; }
+#endif // EXTERNAL_FORCES
+#ifdef ENGINE
+  auto const &swimming() const { return p.swim; }
+  auto &swimming() { return p.swim; }
+#endif
+#ifdef BOND_CONSTRAINT
+  auto const &pos_last_time_step() const { return r.p_last_timestep; }
+  auto &pos_last_time_step() { return r.p_last_timestep; }
+  auto const &rattle_params() const { return rattle; }
+  auto &rattle_params() { return rattle; }
+#endif
+#ifdef EXCLUSIONS
+  auto const &exclusions() const { return el; }
+  auto &exclusions() { return el; }
+#endif
 
 private:
   friend boost::serialization::access;

src/core/cells.cpp
@@ -70,7 +70,7 @@ std::vector<std::pair<int, int>> get_pairs_filtered(double const distance,
                                                Particle const &p2,
                                                Distance const &d) {
     if (d.dist2 < cutoff2 and filter(p1) and filter(p2))
-      ret.emplace_back(p1.p.identity, p2.p.identity);
+      ret.emplace_back(p1.id(), p2.id());
   };
 
   cell_structure.non_bonded_loop(pair_kernel);
@@ -102,7 +102,7 @@ std::vector<PairInfo> non_bonded_loop_trace() {
   std::vector<PairInfo> ret;
   auto pair_kernel = [&ret](Particle const &p1, Particle const &p2,
                             Distance const &d) {
-    ret.emplace_back(p1.p.identity, p2.p.identity, p1.r.p, p2.r.p, d.vec21,
+    ret.emplace_back(p1.id(), p2.id(), p1.pos(), p2.pos(), d.vec21,
                      comm_cart.rank());
   };
 
@@ -124,7 +124,7 @@ static auto mpi_get_pairs_of_types_local(double const distance,
                                          std::vector<int> const &types) {
   auto pairs = get_pairs_filtered(distance, [types](Particle const &p) {
     return std::any_of(types.begin(), types.end(),
-                       [p](int const type) { return p.p.type == type; });
+                       [p](int const type) { return p.type() == type; });
   });
   Utils::Mpi::gather_buffer(pairs, comm_cart);
   return pairs;
@@ -246,7 +246,7 @@ void cells_update_ghosts(unsigned data_parts) {
     /* Add the ghost particles to the index if we don't already
      * have them. */
     for (auto &part : cell_structure.ghost_particles()) {
-      if (cell_structure.get_local_particle(part.p.identity) == nullptr) {
+      if (cell_structure.get_local_particle(part.id()) == nullptr) {
         cell_structure.update_particle_index(part.identity(), &part);
       }
     }

src/core/cells.hpp
@@ -113,8 +113,8 @@ std::vector<int> mpi_resort_particles(int global_flag);
 /**
  * @brief Find the cell in which a particle is stored.
  *
- * Uses position_to_cell on p.r.p. If this is not on the node's domain,
- * uses position at last Verlet list rebuild (p.l.p_old).
+ * Uses position_to_cell on p.pos(). If this is not on the node's domain,
+ * uses position at last Verlet list rebuild (p.p_old()).
  *
  * @return pointer to the cell or nullptr if the particle is not on the node
  */

src/core/cluster_analysis/Cluster.cpp
@@ -57,15 +57,15 @@ Cluster::center_of_mass_subcluster(std::vector<int> const &particle_ids) {
   // of the cluster is arbitrarily chosen as reference.
 
   auto const reference_position =
-      folded_position(get_particle_data(particles[0]).r.p, box_geo);
+      folded_position(get_particle_data(particles[0]).pos(), box_geo);
   double total_mass = 0.;
   for (int pid : particle_ids) {
     auto const folded_pos =
-        folded_position(get_particle_data(pid).r.p, box_geo);
+        folded_position(get_particle_data(pid).pos(), box_geo);
     auto const dist_to_reference =
         box_geo.get_mi_vector(folded_pos, reference_position);
-    com += dist_to_reference * get_particle_data(pid).p.mass;
-    total_mass += get_particle_data(pid).p.mass;
+    com += dist_to_reference * get_particle_data(pid).mass();
+    total_mass += get_particle_data(pid).mass();
   }
 
   // Normalize by number of particles
@@ -83,8 +83,8 @@ double Cluster::longest_distance() {
   for (auto a = particles.begin(); a != particles.end(); a++) {
     for (auto b = a; ++b != particles.end();) {
       auto const dist = box_geo
-                            .get_mi_vector(get_particle_data(*a).r.p,
-                                           get_particle_data(*b).r.p)
+                            .get_mi_vector(get_particle_data(*a).pos(),
+                                           get_particle_data(*b).pos())
                             .norm();
 
       // Larger than previous largest distance?
@@ -107,7 +107,7 @@ Cluster::radius_of_gyration_subcluster(std::vector<int> const &particle_ids) {
   for (auto const pid : particle_ids) {
     // calculate square length of this distance
     sum_sq_dist +=
-        box_geo.get_mi_vector(com, get_particle_data(pid).r.p).norm2();
+        box_geo.get_mi_vector(com, get_particle_data(pid).pos()).norm2();
   }
 
   return sqrt(sum_sq_dist / static_cast<double>(particle_ids.size()));
@@ -137,7 +137,7 @@ std::pair<double, double> Cluster::fractal_dimension(double dr) {
   std::vector<double> distances;
 
   for (auto const &it : particles) {
-    distances.push_back(box_geo.get_mi_vector(com, get_particle_data(it).r.p)
+    distances.push_back(box_geo.get_mi_vector(com, get_particle_data(it).pos())
                             .norm()); // add distance from the current particle
                                       // to the com in the distances vectors
   }

commit 06f4def082df054ebccb2f890290183a9baac15d

core: Fix inertialess tracers integration
Let the Euler integrator update the position along all coordinates,
not just the x- and y-axes. Adapt the test case to check all three
coordinates are properly integrated. This fixes the regression
introduced by 29cf77d9c4c24513230c7b45f3794e25f0256179.

src/core/virtual_sites/lb_inertialess_tracers.cpp
@@ -92,7 +92,7 @@ void IBM_UpdateParticlePositions(ParticleRange const &particles,
   // Euler integrator
   for (auto &p : particles) {
     if (p.is_virtual()) {
-      for (int axis = 0; axis < 2; axis++) {
+      for (int axis = 0; axis < 3; axis++) {
 #ifdef EXTERNAL_FORCES
         if (not p.is_fixed_along(axis))
 #endif

testsuite/python/virtual_sites_tracers.py
@@ -27,11 +27,7 @@ from virtual_sites_tracers_common import VirtualSitesTracersCommon
     ['VIRTUAL_SITES_INERTIALESS_TRACERS', 'LB_BOUNDARIES'])
 class VirtualSitesTracers(ut.TestCase, VirtualSitesTracersCommon):
 
-    def setUp(self):
-        self.LBClass = espressomd.lb.LBFluid
-
-    def tearDown(self):
-        VirtualSitesTracersCommon.tearDown(self)
+    LBClass = espressomd.lb.LBFluid
 
 
 if __name__ == "__main__":

testsuite/python/virtual_sites_tracers_common.py
@@ -30,13 +30,18 @@ class VirtualSitesTracersCommon:
     system.time_step = 0.05
     system.cell_system.skin = 0.1
 
-    def tearDown(self):
-        self.system.part.clear()
-        self.system.actors.clear()
-        self.system.thermostat.turn_off()
+    def setUp(self):
+        self.system.box_l = (self.box_lw, self.box_lw, self.box_height)
 
-    def reset_lb(self, ext_force_density=(0, 0, 0)):
+    def tearDown(self):
+        self.system.thermostat.turn_off()
         self.system.lbboundaries.clear()
+        self.system.actors.clear()
+        self.system.part.clear()
+
+    def reset_lb(self, ext_force_density=(0, 0, 0), dir_walls=2):
+        self.system.lbboundaries.clear()
+        self.system.actors.clear()
         self.lbf = self.LBClass(
             kT=0.0, agrid=1, dens=1, visc=1.8,
             tau=self.system.time_step, ext_force_density=ext_force_density)
@@ -47,11 +52,14 @@ class VirtualSitesTracersCommon:
             gamma=1)
 
         # Setup boundaries
+        normal = [0, 0, 0]
+        normal[dir_walls] = 1
         walls = [espressomd.lbboundaries.LBBoundary() for k in range(2)]
         walls[0].set_params(shape=espressomd.shapes.Wall(
-            normal=[0, 0, 1], dist=0.5))
+            normal=normal, dist=0.5))
+        normal[dir_walls] = -1
         walls[1].set_params(shape=espressomd.shapes.Wall(
-            normal=[0, 0, -1], dist=-self.box_height - 0.5))
+            normal=normal, dist=-(self.system.box_l[dir_walls] - 0.5)))
 
         for wall in walls:
             self.system.lbboundaries.add(wall)
@@ -70,25 +78,38 @@ class VirtualSitesTracersCommon:
             self.system.virtual_sites, espressomd.virtual_sites.VirtualSitesInertialessTracers)
 
     def test_advection(self):
-        self.reset_lb(ext_force_density=[0.1, 0, 0])
-        # System setup
-        system = self.system
+        for direction in [0, 1, 2]:
+            # System setup
+            system = self.system
+            system.virtual_sites = espressomd.virtual_sites.VirtualSitesInertialessTracers()
 
-        system.virtual_sites = espressomd.virtual_sites.VirtualSitesInertialessTracers()
+            # LB setup with walls
+            ext_force = [0., 0., 0.]
+            ext_force[direction] = 0.1
+            dir_walls = (direction + 2) % 3
+            box_l = 3 * [self.box_lw]
+            box_l[dir_walls] = self.box_height
+            system.box_l = box_l
+            self.reset_lb(ext_force_density=ext_force, dir_walls=dir_walls)
 
-        # Establish steady state flow field
-        p = system.part.add(pos=(0, 5.5, 5.5), virtual=True)
-        system.integrator.run(400)
+            # Establish steady state flow field
+            system.integrator.run(400)
 
-        p.pos = (0, 5.5, 5.5)
-        system.time = 0
+            # Add tracer in the fluid domain
+            pos_initial = [5.5, 5.5, 5.5]
+            p = system.part.add(pos=pos_initial, virtual=True)
 
-        # Perform integration
-        for _ in range(2):
-            system.integrator.run(100)
-            # compute expected position
-            dist = self.lbf.get_interpolated_velocity(p.pos)[0] * system.time
-            self.assertAlmostEqual(p.pos[0] / dist, 1, delta=0.005)
+            # Perform integration
+            system.time = 0
+            for _ in range(2):
+                system.integrator.run(100)
+                # compute expected position
+                lb_vel = self.lbf.get_interpolated_velocity(p.pos)
+                ref_dist = lb_vel[direction] * system.time
+                cur_dist = p.pos[direction] - pos_initial[direction]
+                self.assertAlmostEqual(cur_dist / ref_dist, 1., delta=0.01)
+
+            self.tearDown()
 
     def test_zz_without_lb(self):
         """Check behaviour without lb. Ignore non-virtual particles, complain on

testsuite/python/virtual_sites_tracers_gpu.py
@@ -28,11 +28,7 @@ from virtual_sites_tracers_common import VirtualSitesTracersCommon
     ['VIRTUAL_SITES_INERTIALESS_TRACERS', 'LB_BOUNDARIES'])
 class VirtualSitesTracers(ut.TestCase, VirtualSitesTracersCommon):
 
-    def setUp(self):
-        self.LBClass = espressomd.lb.LBFluidGPU
-
-    def tearDown(self):
-        VirtualSitesTracersCommon.tearDown(self)
+    LBClass = espressomd.lb.LBFluidGPU
 
 
 if __name__ == "__main__":