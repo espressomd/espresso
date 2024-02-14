espressomd/espresso

commit 3fd170980bed30c430a9b0264e9504632b4b7326

Integrate waLBerla library
Replace the lattice-Boltzmann and electrokinetics features by a
new implementation based on waLBerla, using PyStencils and LbmPy.

Co-authored-by: Rudolf Weeber <weeber@icp.uni-stuttgart.de>
Co-authored-by: Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
Co-authored-by: Alexander Reinauer <areinauer@icp.uni-stuttgart.de>
Co-authored-by: Ingo Tischler <ingo.tischler@gmx.de>
Co-authored-by: Sebastian Bindgen <info@bindgen.net>
Co-authored-by: Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
Co-authored-by: Patrick Kreissl <patrick.kreissl@pa-le.de>
Co-authored-by: RiccardoFrenner <riccardo.frenner@gmail.com>
Co-authored-by: stekajack <avantardejack@gmail.com>
Co-authored-by: Kai Szuttor <kai@icp.uni-stuttgart.de>
Co-authored-by: capomav <chinmaypabshettiwar@gmail.com>

samples/lb_circular_couette.py
@@ -1,0 +1,201 @@
+#
+# Copyright (C) 2021-2023 The ESPResSo project
+#
+# This file is part of ESPResSo.
+#
+# ESPResSo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ESPResSo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Simulate a rotating cylinder in a fluid via slip velocity boundary conditions.
+"""
+
+import espressomd.lb
+import espressomd.shapes
+import espressomd.constraints
+import espressomd.observables
+import espressomd.math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors
+import matplotlib.ticker
+import itertools
+import argparse
+
+espressomd.assert_features(["WALBERLA"])
+
+parser = argparse.ArgumentParser(epilog=__doc__)
+parser.add_argument("--visualizer", action="store_true", dest="visualizer",
+                    help="Run the visualizer")
+args = parser.parse_args()
+
+# set up LB system
+agrid = 0.5
+grid_size = np.array([31, 31, 4])
+system = espressomd.System(box_l=grid_size * agrid)
+system.time_step = 0.1
+if args.visualizer:
+    system.time_step = 0.001
+system.cell_system.skin = 0.1
+system.periodicity = [False, False, True]
+lb_fluid = espressomd.lb.LBFluidWalberla(
+    agrid=agrid, density=0.5, kinematic_viscosity=3.2, tau=system.time_step)
+system.actors.add(lb_fluid)
+
+# set up cylinders
+cyl_center = agrid * (grid_size // 2 + 0.5) * [1, 1, 0]
+cylinder_in = espressomd.shapes.Cylinder(
+    center=cyl_center, axis=[0, 0, 1], length=3 * system.box_l[2],
+    radius=8.1 * agrid, direction=1)
+cylinder_out = espressomd.shapes.Cylinder(
+    center=cyl_center, axis=[0, 0, 1], length=3 * system.box_l[2],
+    radius=14.5 * agrid, direction=-1)
+lb_fluid.add_boundary_from_shape(cylinder_in)
+lb_fluid.add_boundary_from_shape(cylinder_out)
+
+# the system needs to be fully symmetric
+mask = np.copy(lb_fluid[:, :, :].is_boundary.astype(int))
+np.testing.assert_array_equal(mask, np.flip(mask, axis=0))
+np.testing.assert_array_equal(mask, np.flip(mask, axis=1))
+np.testing.assert_array_equal(mask, np.flip(mask, axis=2))
+
+# the system needs to be closed in the x and y directions
+np.testing.assert_array_equal(mask[0, :, :], 1)
+np.testing.assert_array_equal(mask[-1, :, :], 1)
+np.testing.assert_array_equal(mask[:, 0, :], 1)
+np.testing.assert_array_equal(mask[:, -1, :], 1)
+
+# add tangential slip velocity to the inner cylinder
+velocity_magnitude = 0.01
+surface_nodes = espressomd.lb.edge_detection(
+    lb_fluid.get_shape_bitmask(cylinder_in), system.periodicity)
+tangents = espressomd.lb.calc_cylinder_tangential_vectors(
+    cylinder_in.center, lb_fluid.agrid, 0.5, surface_nodes)
+for node, tangent in zip(surface_nodes, tangents):
+    lb_fluid[node].boundary = espressomd.lb.VelocityBounceBack(
+        velocity_magnitude * tangent)
+
+if args.visualizer:
+    import espressomd.visualization
+    visualizer = espressomd.visualization.openGLLive(
+        system,
+        LB_draw_velocity_plane=True,
+        LB_plane_dist=0,
+        LB_plane_axis=2,
+        LB_vel_scale=80,
+        LB_vel_radius_scale=0.05,
+        LB_plane_ngrid=15,
+        quality_constraints=128,
+        camera_position=[8, 8, 30],
+        background_color=[1, 1, 1],
+        velocity_arrows_type_colors=[[0, 1, 0]]
+    )
+    system.constraints.add(shape=cylinder_in)
+    system.constraints.add(shape=cylinder_out)
+    system.integrator.run(100)
+    visualizer.run(1)
+
+# add observable for the fluid velocity in cylindrical coordinates
+cyl_transform_params = espressomd.math.CylindricalTransformationParameters(
+    center=cyl_center, axis=[0, 0, 1], orientation=[1, 0, 0])
+observable = espressomd.observables.CylindricalLBVelocityProfile(
+    transform_params=cyl_transform_params,
+    n_r_bins=grid_size[0] // 2,
+    n_phi_bins=1,
+    n_z_bins=1,
+    min_r=0.0,
+    max_r=system.box_l[0] / 2,
+    min_phi=0.,
+    max_phi=2 * np.pi,
+    min_z=-system.box_l[2] / 2,
+    max_z=+system.box_l[2] / 2,
+    axis=[0.0, 0.0, 1.0],
+    sampling_density=1
+)
+obs_data_baseline = observable.calculate()
+
+# equilibrate the fluid
+system.integrator.run(100)
+obs_data = observable.calculate()
+
+# fetch fluid and slip velocities
+boundary_mask = np.squeeze(lb_fluid[:, :, 0].is_boundary.astype(bool))
+quivers_boundary = []
+quivers_fluid = []
+for i, j in itertools.product(range(boundary_mask.shape[0]),
+                              range(boundary_mask.shape[1])):
+    v_fluid = lb_fluid[i, j, 0].velocity
+    if boundary_mask[i, j]:
+        quivers_boundary.append([i, j, v_fluid[0], v_fluid[1]])
+    else:
+        quivers_fluid.append([i, j, v_fluid[0], v_fluid[1]])
+
+# prepare canvas
+plt.rcParams.update({'font.size': 16})
+fig1 = plt.figure()
+fig2 = plt.figure()
+ax1 = fig1.add_subplot(111)
+ax2 = fig2.add_subplot(111)
+
+# plot velocity as a function of distance
+profile_r = observable.bin_centers().reshape([-1, 3])[:, 0]
+profile_v = (obs_data - obs_data_baseline).reshape([-1, 3])
+ax1.plot(profile_r, profile_v[:, 1])
+y_formatter = matplotlib.ticker.ScalarFormatter()
+y_formatter.set_powerlimits((-1e-2, 1e-2))
+ax1.yaxis.set_major_formatter(y_formatter)
+ax1.set(xlabel='Distance from cylinder center', ylabel='Fluid velocity')
+
+# plot boundary geometry
+cmap = matplotlib.colors.ListedColormap(['white', 'silver', 'silver'])
+cmap_bounds = [0, 1, 2]
+cmap_norm = matplotlib.colors.BoundaryNorm(cmap_bounds, cmap.N)
+ax2.imshow(boundary_mask.T, origin='lower', interpolation='nearest', cmap=cmap,
+           norm=cmap_norm)
+
+# add grid lines based on minor ticks
+minor_locator = matplotlib.ticker.FixedLocator(np.arange(0.5, grid_size[0], 1))
+ax2.xaxis.set_minor_locator(minor_locator)
+ax2.yaxis.set_minor_locator(minor_locator)
+ax2.tick_params(axis='both', which='minor', length=0)
+ax2.grid(which='minor', color='w', linestyle='-', linewidth=1.2, zorder=2)
+
+# remove major ticks
+ax2.set_xticks([])
+ax2.set_yticks([])
+
+# add cylinder radii
+# circle_in = plt.Circle(
+#    cyl_center[:2] / agrid - agrid, cylinder_in.radius / agrid,
+#    color='r', fill=False, zorder=3)
+# circle_out = plt.Circle(
+#    cyl_center[:2] / agrid - agrid, cylinder_out.radius / agrid,
+#    color='r', fill=False, zorder=3)
+# ax2.add_patch(circle_in)
+# ax2.add_patch(circle_out)
+
+# plot velocity field
+quivers_boundary = np.array(quivers_boundary)
+quivers_fluid = np.array(quivers_fluid)
+ax2.quiver(quivers_boundary[:, 0], quivers_boundary[:, 1], quivers_boundary[:, 2],
+           quivers_boundary[:, 3], scale=.25, width=0.003, color='black',
+           zorder=4, label='slip velocity')
+ax2.quiver(quivers_fluid[:, 0], quivers_fluid[:, 1], quivers_fluid[:, 2],
+           quivers_fluid[:, 3], scale=.25, width=0.003, color='royalblue',
+           zorder=4, label='fluid velocity')
+ax2.set(xlabel='x-axis', ylabel='y-axis')
+ax2.legend(framealpha=1, loc='upper right')
+
+plt.tight_layout()
+plt.show()

commit d0c9154261f79964487d110a1e84e60e9d23c37b

Rewrite waLBerla MPI communication
Split LB ghost communicator from LB boundaries communicator.
Always communicate bounce-back velocities from the ghost layer.
This fixes the regression introduced by
3fd170980bed30c430a9b0264e9504632b4b7326.

samples/lb_circular_couette.py
@@ -57,7 +57,7 @@ system.lb = lb_fluid
 cyl_center = agrid * (grid_size // 2 + 0.5) * [1, 1, 0]
 cylinder_in = espressomd.shapes.Cylinder(
     center=cyl_center, axis=[0, 0, 1], length=3 * system.box_l[2],
-    radius=8.1 * agrid, direction=1)
+    radius=8.6 * agrid, direction=1)
 cylinder_out = espressomd.shapes.Cylinder(
     center=cyl_center, axis=[0, 0, 1], length=3 * system.box_l[2],
     radius=14.5 * agrid, direction=-1)

src/script_interface/walberla/LBFluid.cpp
@@ -108,7 +108,6 @@ Variant LBFluid::do_call_method(std::string const &name,
   }
   if (name == "clear_boundaries") {
     m_instance->clear_boundaries();
-    m_instance->ghost_communication();
     ::System::get_system().on_lb_boundary_conditions_change();
     return {};
   }
@@ -269,8 +268,8 @@ void LBFluid::load_checkpoint(std::string const &filename, int mode) {
   };
 
   auto const on_success = [&lb_obj]() {
-    lb_obj.reallocate_ubb_field();
     lb_obj.ghost_communication();
+    lb_obj.reallocate_ubb_field();
   };
 
   load_checkpoint_common(*context(), "LB", filename, mode, read_metadata,

src/script_interface/walberla/LBFluidNode.cpp
@@ -53,6 +53,7 @@ Variant LBFluidNode::do_call_method(std::string const &name,
       m_lb_fluid->set_node_velocity_at_boundary(m_index, u);
     }
     m_lb_fluid->ghost_communication();
+    m_lb_fluid->reallocate_ubb_field();
     return {};
   }
   if (name == "get_velocity_at_boundary") {

src/script_interface/walberla/LBFluidSlice.cpp
@@ -99,8 +99,10 @@ Variant LBFluidSlice::do_call_method(std::string const &name,
                 1. / m_conv_velocity);
   }
   if (name == "set_velocity_at_boundary") {
-    return call(&LatticeModel::set_slice_velocity_at_boundary, {1},
-                m_conv_velocity);
+    auto const retval = call(&LatticeModel::set_slice_velocity_at_boundary, {1},
+                             m_conv_velocity);
+    m_lb_fluid->reallocate_ubb_field();
+    return retval;
   }
   if (name == "get_pressure_tensor") {
     return call(&LatticeModel::get_slice_pressure_tensor, {3, 3},

src/walberla_bridge/src/BoundaryPackInfo.hpp
@@ -1,0 +1,147 @@
+/*
+ * Copyright (C) 2024 The ESPResSo project
+ *
+ * This file is part of ESPResSo.
+ *
+ * ESPResSo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ESPResSo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <core/debug/Debug.h>
+#include <core/mpi/RecvBuffer.h>
+#include <core/mpi/SendBuffer.h>
+#include <domain_decomposition/IBlock.h>
+#include <field/communication/PackInfo.h>
+#include <stencil/Directions.h>
+
+#include <memory>
+#include <tuple>
+#include <utility>
+
+namespace walberla {
+namespace field {
+namespace communication {
+
+template <typename GhostLayerField_T, typename Boundary_T>
+class BoundaryPackInfo : public PackInfo<GhostLayerField_T> {
+protected:
+  using PackInfo<GhostLayerField_T>::bdId_;
+
+public:
+  using PackInfo<GhostLayerField_T>::PackInfo;
+  using PackInfo<GhostLayerField_T>::numberOfGhostLayersToCommunicate;
+
+  ~BoundaryPackInfo() override = default;
+
+  void setup_boundary_handle(std::shared_ptr<LatticeWalberla> lattice,
+                             std::shared_ptr<Boundary_T> boundary) {
+    m_lattice = std::move(lattice);
+    m_boundary = std::move(boundary);
+  }
+
+  bool constantDataExchange() const override { return false; }
+  bool threadsafeReceiving() const override { return true; }
+
+  void communicateLocal(IBlock const *sender, IBlock *receiver,
+                        stencil::Direction dir) override {
+    mpi::SendBuffer sBuffer;
+    packDataImpl(sender, dir, sBuffer);
+    mpi::RecvBuffer rBuffer(sBuffer);
+    unpackData(receiver, stencil::inverseDir[dir], rBuffer);
+  }
+
+  void unpackData(IBlock *receiver, stencil::Direction dir,
+                  mpi::RecvBuffer &buffer) override {
+
+    auto *flag_field = receiver->getData<GhostLayerField_T>(bdId_);
+    WALBERLA_ASSERT_NOT_NULLPTR(flag_field);
+    WALBERLA_ASSERT_NOT_NULLPTR(m_boundary);
+    WALBERLA_ASSERT_NOT_NULLPTR(m_lattice);
+
+    auto const boundary_flag = flag_field->getFlag(Boundary_flag);
+    auto const gl = numberOfGhostLayersToCommunicate(flag_field);
+    auto const begin = [gl, dir](auto const *flag_field) {
+      return flag_field->beginGhostLayerOnly(gl, dir);
+    };
+
+#ifndef NDEBUG
+    uint_t xSize, ySize, zSize, bSize;
+    buffer >> xSize >> ySize >> zSize >> bSize;
+    uint_t buf_size{0u};
+    for (auto it = begin(flag_field); it != flag_field->end(); ++it) {
+      if (isFlagSet(it, boundary_flag)) {
+        ++buf_size;
+      }
+    }
+    WALBERLA_ASSERT_EQUAL(xSize, flag_field->xSize());
+    WALBERLA_ASSERT_EQUAL(ySize, flag_field->ySize());
+    WALBERLA_ASSERT_EQUAL(zSize, flag_field->zSize());
+    WALBERLA_ASSERT_EQUAL(bSize, buf_size);
+#endif
+
+    auto const offset = std::get<0>(m_lattice->get_local_grid_range());
+    typename Boundary_T::value_type value;
+    for (auto it = begin(flag_field); it != flag_field->end(); ++it) {
+      if (isFlagSet(it, boundary_flag)) {
+        auto const node = offset + Utils::Vector3i{{it.x(), it.y(), it.z()}};
+        buffer >> value;
+        m_boundary->unpack_node(node, value);
+      }
+    }
+  }
+
+protected:
+  void packDataImpl(IBlock const *sender, stencil::Direction dir,
+                    mpi::SendBuffer &buffer) const override {
+
+    auto const *flag_field = sender->getData<GhostLayerField_T>(bdId_);
+    WALBERLA_ASSERT_NOT_NULLPTR(flag_field);
+    WALBERLA_ASSERT_NOT_NULLPTR(m_boundary);
+    WALBERLA_ASSERT_NOT_NULLPTR(m_lattice);
+
+    auto const boundary_flag = flag_field->getFlag(Boundary_flag);
+    auto const gl = numberOfGhostLayersToCommunicate(flag_field);
+    auto const begin = [gl, dir](auto const *flag_field) {
+      return flag_field->beginSliceBeforeGhostLayer(dir, gl);
+    };
+
+#ifndef NDEBUG
+    uint_t buf_size{0u};
+    for (auto it = begin(flag_field); it != flag_field->end(); ++it) {
+      if (isFlagSet(it, boundary_flag)) {
+        ++buf_size;
+      }
+    }
+    buffer << flag_field->xSize() << flag_field->ySize() << flag_field->zSize()
+           << buf_size;
+#endif
+
+    auto const offset = std::get<0>(m_lattice->get_local_grid_range());
+    for (auto it = begin(flag_field); it != flag_field->end(); ++it) {
+      if (isFlagSet(it, boundary_flag)) {
+        auto const node = offset + Utils::Vector3i{{it.x(), it.y(), it.z()}};
+        buffer << m_boundary->get_node_value_at_boundary(node);
+      }
+    }
+  }
+
+private:
+  std::shared_ptr<LatticeWalberla> m_lattice;
+  std::shared_ptr<Boundary_T> m_boundary;
+};
+
+} // namespace communication
+} // namespace field
+} // namespace walberla

src/walberla_bridge/src/lattice_boltzmann/LBWalberlaImpl.hpp
@@ -45,6 +45,7 @@
 #include <stencil/D3Q27.h>
 
 #include "../BoundaryHandling.hpp"
+#include "../BoundaryPackInfo.hpp"
 #include "InterpolateAndShiftAtBoundary.hpp"
 #include "ResetForce.hpp"
 #include "lb_kernels.hpp"
@@ -235,8 +236,9 @@ protected:
       typename FieldTrait<FloatType, Architecture>::template PackInfo<Field>;
 
   // communicators
-  std::shared_ptr<FullCommunicator> m_full_communication;
-  std::shared_ptr<PDFStreamingCommunicator> m_pdf_streaming_communication;
+  std::shared_ptr<FullCommunicator> m_boundary_communicator;
+  std::shared_ptr<FullCommunicator> m_pdf_full_communicator;
+  std::shared_ptr<PDFStreamingCommunicator> m_pdf_streaming_communicator;
 
   // ResetForce sweep + external force handling
   std::shared_ptr<ResetForce<PdfField, VectorField>> m_reset_force;
@@ -350,27 +352,33 @@ public:
     reset_boundary_handling();
 
     // Set up the communication and register fields
-    m_pdf_streaming_communication =
+    m_pdf_streaming_communicator =
         std::make_shared<PDFStreamingCommunicator>(blocks);
-    m_pdf_streaming_communication->addPackInfo(
+    m_pdf_streaming_communicator->addPackInfo(
         std::make_shared<PackInfo<PdfField>>(m_pdf_field_id, n_ghost_layers));
-    m_pdf_streaming_communication->addPackInfo(
+    m_pdf_streaming_communicator->addPackInfo(
         std::make_shared<PackInfo<VectorField>>(m_last_applied_force_field_id,
                                                 n_ghost_layers));
-    m_pdf_streaming_communication->addPackInfo(
-        std::make_shared<field::communication::PackInfo<FlagField>>(
-            m_flag_field_id, n_ghost_layers));
 
-    m_full_communication = std::make_shared<FullCommunicator>(blocks);
-    m_full_communication->addPackInfo(
+    m_pdf_full_communicator = std::make_shared<FullCommunicator>(blocks);
+    m_pdf_full_communicator->addPackInfo(
         std::make_shared<PackInfo<PdfField>>(m_pdf_field_id, n_ghost_layers));
-    m_full_communication->addPackInfo(std::make_shared<PackInfo<VectorField>>(
-        m_last_applied_force_field_id, n_ghost_layers));
-    m_full_communication->addPackInfo(std::make_shared<PackInfo<VectorField>>(
-        m_velocity_field_id, n_ghost_layers));
-    m_full_communication->addPackInfo(
+    m_pdf_full_communicator->addPackInfo(
+        std::make_shared<PackInfo<VectorField>>(m_last_applied_force_field_id,
+                                                n_ghost_layers));
+    m_pdf_full_communicator->addPackInfo(
+        std::make_shared<PackInfo<VectorField>>(m_velocity_field_id,
+                                                n_ghost_layers));
+
+    m_boundary_communicator = std::make_shared<FullCommunicator>(blocks);
+    m_boundary_communicator->addPackInfo(
         std::make_shared<field::communication::PackInfo<FlagField>>(
             m_flag_field_id, n_ghost_layers));
+    auto boundary_packinfo = std::make_shared<
+        field::communication::BoundaryPackInfo<FlagField, BoundaryModel>>(
+        m_flag_field_id, n_ghost_layers);
+    boundary_packinfo->setup_boundary_handle(m_lattice, m_boundary);
+    m_boundary_communicator->addPackInfo(boundary_packinfo);
 
     // Instantiate the sweep responsible for force double buffering and
     // external forces
@@ -439,13 +447,13 @@ private:
     integrate_reset_force(blocks);
     // LB collide
     integrate_collide(blocks);
-    m_pdf_streaming_communication->communicate();
+    m_pdf_streaming_communicator->communicate();
     // Handle boundaries
     integrate_boundaries(blocks);
     // LB stream
     integrate_stream(blocks);
     // Refresh ghost layers
-    m_full_communication->communicate();
+    ghost_communication_pdfs();
   }
 
   void integrate_pull_scheme() {
@@ -458,7 +466,7 @@ private:
     // LB collide
     integrate_collide(blocks);
     // Refresh ghost layers
-    ghost_communication();
+    ghost_communication_pdfs();
   }
 
 protected:
@@ -474,7 +482,6 @@ protected:
 
 public:
   void integrate() override {
-    reallocate_ubb_field();
     if (has_lees_edwards_bc()) {
       integrate_pull_scheme();
     } else {
@@ -485,7 +492,16 @@ public:
   }
 
   void ghost_communication() override {
-    m_full_communication->communicate();
+    ghost_communication_boundary();
+    ghost_communication_pdfs();
+  }
+
+  void ghost_communication_boundary() {
+    m_boundary_communicator->communicate();
+  }
+
+  void ghost_communication_pdfs() {
+    m_pdf_full_communicator->communicate();
     if (has_lees_edwards_bc()) {
       auto const &blocks = get_lattice().get_blocks();
       apply_lees_edwards_pdf_interpolation(blocks);
@@ -1097,7 +1113,10 @@ public:
 
   void reallocate_ubb_field() override { m_boundary->boundary_update(); }
 
-  void clear_boundaries() override { reset_boundary_handling(); }
+  void clear_boundaries() override {
+    reset_boundary_handling();
+    ghost_communication();
+  }
 
   void
   update_boundary_from_shape(std::vector<int> const &raster_flat,
@@ -1105,6 +1124,8 @@ public:
     auto const grid_size = get_lattice().get_grid_dimensions();
     auto const data = fill_3D_vector_array(data_flat, grid_size);
     set_boundary_from_grid(*m_boundary, get_lattice(), raster_flat, data);
+    ghost_communication();
+    reallocate_ubb_field();
   }
 
   // Pressure tensor

src/walberla_bridge/tests/LBWalberlaImpl_unit_tests.cpp
@@ -202,6 +202,7 @@ BOOST_DATA_TEST_CASE(update_boundary_from_shape, bdata::make(all_lbs()),
     std::vector<double> vel_flat(vel_3d.data(),
                                  vel_3d.data() + vel_3d.num_elements());
     lb->update_boundary_from_shape(raster_flat, vel_flat);
+    lb->ghost_communication();
   }
 
   for (auto const &node : nodes) {

testsuite/python/CMakeLists.txt
@@ -334,6 +334,7 @@ python_test(FILE thole.py MAX_NUM_PROC 4)
 python_test(FILE lb_slice.py MAX_NUM_PROC 2)
 python_test(FILE lb_boundary_velocity.py MAX_NUM_PROC 1)
 # python_test(FILE lb_boundary_volume_force.py MAX_NUM_PROC 2) # TODO
+python_test(FILE lb_boundary_ghost_layer.py MAX_NUM_PROC 2)
 python_test(FILE lb_circular_couette.py MAX_NUM_PROC 2 GPU_SLOTS 1)
 python_test(FILE lb_poiseuille.py MAX_NUM_PROC 4 GPU_SLOTS 1)
 python_test(FILE lb_poiseuille_cylinder.py MAX_NUM_PROC 2 GPU_SLOTS 1)

testsuite/python/lb_boundary_ghost_layer.py
@@ -1,0 +1,103 @@
+#
+# Copyright (C) 2024 The ESPResSo project
+#
+# This file is part of ESPResSo.
+#
+# ESPResSo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ESPResSo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import unittest as ut
+import unittest_decorators as utx
+import numpy as np
+import scipy.optimize
+
+import espressomd.lb
+import espressomd.shapes
+
+AGRID = 0.5
+KINEMATIC_VISC = 2.7
+DENS = 1.7
+TIME_STEP = 0.07
+LB_PARAMS = {"agrid": AGRID, "tau": TIME_STEP, "density": DENS,
+             "kinematic_viscosity": KINEMATIC_VISC}
+
+
+class TestCommon:
+
+    system = espressomd.System(box_l=[16.0, 1.0, 1.0])
+    system.time_step = TIME_STEP
+    system.cell_system.skin = 0.4 * AGRID
+    n_nodes = system.cell_system.get_state()["n_nodes"]
+
+    def setUp(self):
+        self.lbf = self.lb_class(**LB_PARAMS, **self.lb_params)
+        self.system.lb = self.lbf
+        self.ubb = espressomd.lb.VelocityBounceBack([0., 0., 1e-5])
+
+    def tearDown(self):
+        self.system.lb = None
+
+    def get_profile(self):
+        xdata = np.arange(1, self.lbf.shape[0])
+        ydata = []
+        for x in xdata:
+            ydata.append(np.mean(self.lbf[x, :, :].velocity[:, :, 2]))
+        return xdata, np.array(ydata)
+
+    def check_profile(self):
+        def quadratic(x, a, b, c):
+            return a * x**2 + b * x + c
+
+        self.system.integrator.run(40)
+        xdata, ydata = self.get_profile()
+        popt_ref = (4e-8, -1e-6, 1e-5)
+        popt, _ = scipy.optimize.curve_fit(
+            quadratic, xdata, ydata, p0=popt_ref)
+        rtol = 0.3 if self.lbf.single_precision else 0.1
+        np.testing.assert_allclose(popt, popt_ref, rtol=0.5, atol=0.)
+        np.testing.assert_allclose(ydata, quadratic(xdata, *popt),
+                                   rtol=rtol, atol=0.)
+
+    def test_node_setter(self):
+        for i in (0, 1):
+            for j in (0, 1):
+                self.lbf[0, i, j].boundary = self.ubb
+        self.check_profile()
+
+    def test_slice_setter(self):
+        self.lbf[0, :, :].boundary = self.ubb
+        self.check_profile()
+
+    def test_shape_setter(self):
+        shape = espressomd.shapes.Wall(normal=[1, 0, 0], dist=AGRID)
+        self.lbf.add_boundary_from_shape(shape, velocity=self.ubb.velocity)
+        self.check_profile()
+
+
+@utx.skipIfMissingFeatures(["WALBERLA"])
+@ut.skipIf(TestCommon.n_nodes != 2, "only runs for 2 MPI ranks")
+class LBPoiseuilleWalberlaSinglePrecision(TestCommon, ut.TestCase):
+    lb_class = espressomd.lb.LBFluidWalberla
+    lb_params = {"single_precision": True}
+
+
+@utx.skipIfMissingFeatures(["WALBERLA"])
+@ut.skipIf(TestCommon.n_nodes != 2, "only runs for 2 MPI ranks")
+class LBPoiseuilleWalberlaDoublePrecision(TestCommon, ut.TestCase):
+    lb_class = espressomd.lb.LBFluidWalberla
+    lb_params = {"single_precision": False}
+
+
+if __name__ == "__main__":
+    ut.main()

testsuite/python/lb_circular_couette.py
@@ -143,8 +143,8 @@ class LBCircularCouetteCommon:
         a_ref, b_ref = taylor_couette(slip_vel, 0.0, cyl1.radius, cyl2.radius)
         v_phi_ref = a_ref * r + b_ref / r
         v_phi_drift = np.mean(v_phi) - np.mean(v_phi_ref)
-        np.testing.assert_allclose(v_phi_drift, 0., atol=1.2e-4)
-        np.testing.assert_allclose(v_phi - v_phi_drift, v_phi_ref, atol=1e-4)
+        np.testing.assert_allclose(v_phi_drift, 0., atol=4e-4)
+        np.testing.assert_allclose(v_phi - v_phi_drift, v_phi_ref, atol=4e-4)
 
 
 @utx.skipIfMissingFeatures(["WALBERLA"])

testsuite/scripts/samples/test_lb_circular_couette.py
@@ -52,19 +52,18 @@ class Sample(ut.TestCase):
         np.testing.assert_allclose(v_phi[:7], 0., atol=1e-7)
 
         # check azimuthal velocity in the linear regime
-        self.assertGreater(v_phi[7], v_phi[6])
         self.assertGreater(v_phi[8], v_phi[7])
         self.assertGreater(v_phi[9], v_phi[8])
 
         # check azimuthal velocity in the Couette regime
-        xdata = sample.profile_r[9:]
-        ydata = v_phi[9:]
+        xdata = sample.profile_r[9:-1]
+        ydata = v_phi[9:-1]
         a_ref, b_ref = taylor_couette(
             sample.velocity_magnitude, 0.0, sample.cylinder_in.radius,
             sample.cylinder_out.radius, sample.agrid)
         (a_sim, b_sim), _ = scipy.optimize.curve_fit(
             lambda x, a, b: a * x + b / x, xdata, ydata)
-        np.testing.assert_allclose([a_sim, b_sim], [a_ref, b_ref], atol=1e-3)
+        np.testing.assert_allclose([a_sim, b_sim], [a_ref, b_ref], rtol=0.05)
 
 
 if __name__ == "__main__":