d2dc6d4e5386ec186b2e98eb40fc87359076c3de
Ingo Tischler <ingo.tischler@gmx.de>
2025-05-08 15:18:07 +0200

    fixed mpi boundary buf_size not matching

--- src/walberla_bridge/src/BoundaryPackInfo.hpp
+++ src/walberla_bridge/src/BoundaryPackInfo.hpp
@@ -35,6 +35,19 @@ namespace walberla {
 namespace field {
 namespace communication {
 
+template <typename GhostLayerField_T>
+class BoundaryFlagPackInfo : public PackInfo<GhostLayerField_T> {
+
+public:
+  using PackInfo<GhostLayerField_T>::PackInfo;
+  using PackInfo<GhostLayerField_T>::numberOfGhostLayersToCommunicate;
+
+  ~BoundaryFlagPackInfo() override = default;
+
+  bool constantDataExchange() const override { return false; }
+  bool threadsafeReceiving() const override { return false; }
+};
+
 template <typename GhostLayerField_T, typename Boundary_T>
 class BoundaryPackInfo : public PackInfo<GhostLayerField_T> {
 protected:
@@ -61,12 +74,12 @@ public:
 
   void communicateLocal(IBlock const *sender, IBlock *receiver,
                         stencil::Direction dir) override {
+    mpi::SendBuffer sBuffer;
+    packDataImpl(sender, dir, sBuffer);
 #ifdef _OPENMP
-#pragma omp single nowait
+#pragma omp critical
 #endif
     {
-      mpi::SendBuffer sBuffer;
-      packDataImpl(sender, dir, sBuffer);
       mpi::RecvBuffer rBuffer(sBuffer);
       unpackData(receiver, stencil::inverseDir[dir], rBuffer);
     }
