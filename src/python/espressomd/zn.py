a28a887d02873d2895cfb5e78ae2fc29467af59c
Lukas Dorschel <lukas.dorsc@gmail.com>
2025-06-24 14:13:31 +0200

    style

--- src/python/espressomd/zn.py
+++ src/python/espressomd/zn.py
@@ -580,8 +580,8 @@ class Visualizer():
         self.frame_count += 1
 
 
-    def register_setting(self,cls,**kwargs):
-        self.zndraw.register_modifier(cls,**kwargs)
+    def register_setting(self, cls, **kwargs):
+        self.zndraw.register_modifier(cls, **kwargs)
 
     def draw_constraints(self, shapes: list):
         """
