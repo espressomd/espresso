745cf60cbcef0baa524913e332a35d2878a1de73
Lukas Dorschel <lukas.dorsc@gmail.com>
2025-07-15 12:01:27 +0200

    Update zn.py
    Style

--- src/python/espressomd/zn.py
+++ src/python/espressomd/zn.py
@@ -568,7 +568,6 @@ class Visualizer():
 
             self.zndraw.camera = {'position': [
                 x, y, z_dist], 'target': [x, y, z]}
-            #self.zndraw.config.scene.frame_update = False
 
             if self.params["vector_field"] is not None:
                 for key, value in self.arrow_config.items():
@@ -577,8 +576,8 @@ class Visualizer():
         self.frame_count += 1
 
 
-    def register_setting(self,cls,**kwargs):
-        self.zndraw.register_modifier(cls,**kwargs)
+    def register_setting(self, cls, **kwargs):
+        self.zndraw.register_modifier(cls, **kwargs)
 
     def draw_constraints(self, shapes: list):
         """
