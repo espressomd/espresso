27e9e9a7661be17387297c636844bf5f2da62720
Lukas Dorschel <lukas.dorsc@gmail.com>
2025-08-19 13:03:12 +0200

    Update zn_interface.py

--- testsuite/python/zn_interface.py
+++ testsuite/python/zn_interface.py
@@ -3,7 +3,6 @@ import time
 import espressomd.zn
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 
 system = espressomd.System(box_l=[1]*3)
 vis = espressomd.zn.Visualizer(system)
@@ -11,18 +10,21 @@ url = vis.address
 
 #Default Methods
 method_list = [['ConnectedParticles', 'NoneSelection', 'All', 'Invert', 'Range', 'Random', 'IdenticalSpecies', 'Neighbour'],
-['Delete', 'Rotate', 'Translate', 'Duplicate', 'ChangeType', 'AddLineParticles', 'Wrap', 'Center', 'Replicate', 'Connect', 'NewCanvas', 'RemoveAtoms'],
-['Plane', 'Sphere', 'Box', 'Circle', 'Cone', 'Cylinder', 'Dodecahedron', 'Icosahedron', 'Octahedron', 'Ring', 'Tetrahedron', 'Torus', 'TorusKnot', 'Rhomboid', 'Ellipsoid'],
-['Properties1D', 'DihedralAngle', 'Distance', 'Properties2D']]
+               ['Delete', 'Rotate', 'Translate', 'Duplicate', 'ChangeType', 'AddLineParticles',
+                   'Wrap', 'Center', 'Replicate', 'Connect', 'NewCanvas', 'RemoveAtoms'],
+               ['Plane', 'Sphere', 'Box', 'Circle', 'Cone', 'Cylinder', 'Dodecahedron', 'Icosahedron',
+                   'Octahedron', 'Ring', 'Tetrahedron', 'Torus', 'TorusKnot', 'Rhomboid', 'Ellipsoid'],
+               ['Properties1D', 'DihedralAngle', 'Distance', 'Properties2D']]
+
 
 def s(t=1):
     time.sleep(t)
 
+
 class RegisterTestCase(unittest.TestCase):
     def setUp(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("headless")
-        #chrome_options.browser_version = "138" 
+        chrome_options.add_argument("headless") 
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get(url)
 
@@ -33,20 +35,24 @@ class RegisterTestCase(unittest.TestCase):
         next_option_names = []
         idx = 0
         driver = self.driver
-        buttons = driver.find_elements(By.XPATH, '//button[@class="btn btn-outline-tertiary"]')
+        buttons = driver.find_elements(
+            By.XPATH, '//button[@class="btn btn-outline-tertiary"]')
         for button in buttons:
             button.click()
             s(t=0.1)
             elements = driver.find_elements(By.ID, "root[method]switcher")
-            j = len(elements)-1
-            options = [x for x in elements[j].find_elements(By.TAG_NAME, "option")]
+            j = len(elements) - 1
+            options = [x for x in elements[j].find_elements(
+                By.TAG_NAME, "option")]
             option_names = [x.get_attribute("value") for x in options]
             if option_names == next_option_names:
                 continue
             else:
-                assert option_names == method_list[idx], f"Difference: {set(option_names) ^ set(method_list[idx])}"
+                assert option_names == method_list[idx], f"Difference: {
+                    set(option_names) ^ set(method_list[idx])}"
                 idx += 1
                 next_option_names = option_names
+
 
 if __name__ == "__main__":
     unittest.main()
