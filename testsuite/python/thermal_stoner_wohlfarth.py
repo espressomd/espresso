d13e6f363e26818ececab7b8a0d9609d4211a0d3
stekajack <avantardejack@gmail.com>
2025-10-30 22:42:58 +0000

    refactor thermal_stoner_wohlfarth test

--- testsuite/python/thermal_stoner_wohlfarth.py
+++ testsuite/python/thermal_stoner_wohlfarth.py
@@ -16,18 +16,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-
 import espressomd
-import espressomd.observables
 import numpy as np
 import unittest as ut
 import unittest_decorators as utx
 import espressomd.polymer
-import tests_common
 import espressomd.propagation
 Propagation = espressomd.propagation.Propagation
 from espressomd.observables import MagneticDipoleMoment
-
 
 def generate_random_unit_vectors(N_PART):
     z = np.random.uniform(-1, 1, N_PART)
@@ -43,9 +39,12 @@ class Test(ut.TestCase):
     Check the total dipole field for a magnetic LJ fluid (500 particles,
     density approx 0.002, mu^2=1, no PBC).
     """
-    res_dict_fluid={3.4283694213261087:0.9, 1.1427898071087026:0.6, 0.28569745177717565:0.2}
-    res_dict_solid={3.4283694213261087:0.8, 1.1427898071087026:0.54, 0.28569745177717565:0.2}
+    res_dict_fluid={3.4283694213261087:0.91, 1.1427898071087026:0.62, 0.28569745177717565:0.2}
+    res_dict_solid={3.4283694213261087:0.8, 1.1427898071087026:0.54, 0.28569745177717565:0.19}
+    system = espressomd.System(box_l=(29.69314567, 29.69314567, 29.69314567))
     skin = 0.4
+    seed=42
+    np.random.seed(seed)
     time_step = 0.001
     temperature = 1
     kT_KVm_inv=5
@@ -57,27 +56,26 @@ class Test(ut.TestCase):
     tau0_inv=735412234.8230474
     SNAPSHOT_SEPARATION = 12477
     n_part = 100
-    system = espressomd.System(box_l=(29.69314567, 29.69314567, 29.69314567))
-    system.cell_system.skin = 0.4
-    system.time_step = 0.001
-    system.periodicity = [True, True, True]
-    orientor_list = generate_random_unit_vectors(N_PART=n_part)
-    dip_mom_list = dip_reduced*orientor_list
-    positions = espressomd.polymer.linear_polymer_positions(n_polymers=n_part, beads_per_chain=1, min_distance=1., bond_length=1., seed=42)
-    positions = np.reshape(positions, (-1, 3))
+    error=0.05
 
     def tearDown(self):
         self.system.part.clear()
+        self.system.thermostat.turn_off()
 
-    def setUp(self):
+    def _init_particles(self):
         system = self.system
-        particles = system.part.add(pos=self.positions, director=self.orientor_list)
+        self.system.part.clear()
+        orientor_list = generate_random_unit_vectors(N_PART=self.n_part)
+        dip_mom_list = self.dip_reduced*orientor_list
+        positions = espressomd.polymer.linear_polymer_positions(n_polymers=self.n_part, beads_per_chain=1, min_distance=1., bond_length=1., seed=self.seed)
+        positions = np.reshape(positions, (-1, 3))
+        particles = system.part.add(pos=positions, director=orientor_list)
         particles.sw_real = True
         particles.rotation = (True, True, True)
         particles.kT_KVm_inv = self.kT_KVm_inv
         particles.dt_incr = self.dt_incr
         particles.tau0_inv = self.tau0_inv
-        for p1,dipm_el in zip(list(particles),self.dip_mom_list):
+        for p1,dipm_el in zip(list(particles), dip_mom_list):
             p2=system.part.add(
             pos=p1.pos, dip=dipm_el, rotation=[False, False, False],
             sw_virt=True, Hkinv=self.HK_inv,
@@ -85,37 +83,49 @@ class Test(ut.TestCase):
             p2.vs_auto_relate_to(p1)
             p2.propagation = Propagation.TRANS_VS_RELATIVE | Propagation.ROT_VS_INDEPENDENT
 
-    def test_tSW_fluid(self):
+    def _apply_single_field(self,h_reduced):
+        for x in self.system.constraints:
+            self.system.constraints.remove(x)
+        ExtH = espressomd.constraints.HomogeneousMagneticField(H=(0, 0, h_reduced))
+        self.system.constraints.add(ExtH)
+    
+    def _measure_dipole_moment(self):
+        dipm_tot = MagneticDipoleMoment(ids=self.system.part.select(lambda p: p.sw_virt == True).id)
+        norm = 1/(self.dip_reduced*self.n_part)
+        self.system.integrator.run(self.SNAPSHOT_SEPARATION)
+        mag_el = dipm_tot.calculate()*norm
+        return mag_el[-1]
+
+    def setUp(self):
         system = self.system
+        system.cell_system.skin = 0.4
+        system.min_global_cut = 1.
+        system.time_step = 0.001
+        system.periodicity = [True, True, True]
+        system.thermostat.set_langevin(kT=self.temperature, gamma=self.gamma_T,
+                               gamma_rotation=self.gamma_R, seed=self.seed)
+       
+    def test_tSW_fluid(self):
+        self.SNAPSHOT_SEPARATION=12477
+        self.n_part=100
         for h_reduced,res in self.res_dict_fluid.items():
-            print(h_reduced)
-            for x in system.constraints:
-                system.constraints.remove(x)
-            ExtH = espressomd.constraints.HomogeneousMagneticField(H=(0, 0, h_reduced))
-            system.constraints.add(ExtH)
-            dipm_tot = MagneticDipoleMoment(ids=system.part.select(lambda p: p.sw_virt == True).id)
-            norm = 1/(self.dip_reduced*self.n_part)
-            system.integrator.run(self.SNAPSHOT_SEPARATION)
-            mag_el = dipm_tot.calculate()*norm
-            self.assertAlmostEqual(mag_el[-1],res,delta=0.05)
+            self._init_particles()
+            self._apply_single_field(h_reduced)
+            print('test_tSW_fluid: ',h_reduced)
+            self.assertAlmostEqual(self._measure_dipole_moment(),res,delta=self.error)
     
     def test_tSW_solid(self):
+        self.SNAPSHOT_SEPARATION=3447
+        self.n_part=500
         system = self.system
-        part_slice=system.part.select(lambda p: p.sw_real== True)
-        part_slice.rotation = [False, False, False]
-        part_slice.fix = [True, True, True]
-
         for h_reduced,res in self.res_dict_solid.items():
-            print(h_reduced)
-            for x in system.constraints:
-                system.constraints.remove(x)
-            ExtH = espressomd.constraints.HomogeneousMagneticField(H=(0, 0, h_reduced))
-            system.constraints.add(ExtH)
-            dipm_tot = MagneticDipoleMoment(ids=system.part.select(lambda p: p.sw_virt == True).id)
-            norm = 1/(self.dip_reduced*self.n_part)
-            system.integrator.run(self.SNAPSHOT_SEPARATION)
-            mag_el = dipm_tot.calculate()*norm
-            self.assertAlmostEqual(mag_el[-1],res,delta=0.05) 
+            print('test_tSW_solid: ',h_reduced)
+            self._init_particles()
+            part_slice=system.part.select(lambda p: p.sw_real== True)
+            part_slice.rotation = [False, False, False]
+            part_slice.fix = [True, True, True]
+            self._apply_single_field(h_reduced)
+            self.assertAlmostEqual(self._measure_dipole_moment(),res,delta=self.error)
         
 if __name__ == "__main__":
     ut.main()
