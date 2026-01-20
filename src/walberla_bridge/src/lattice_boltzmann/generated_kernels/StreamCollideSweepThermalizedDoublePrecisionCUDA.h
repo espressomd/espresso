9458fd0d102e375692e45c99de1d7ac6f0142109
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2026-01-20 14:26:58 +0100

    Format kernels

--- src/walberla_bridge/src/lattice_boltzmann/generated_kernels/StreamCollideSweepThermalizedDoublePrecisionCUDA.h
+++ src/walberla_bridge/src/lattice_boltzmann/generated_kernels/StreamCollideSweepThermalizedDoublePrecisionCUDA.h
@@ -19,7 +19,7 @@
 
 // kernel generated with pystencils v1.4+1.ge851f4e, lbmpy v1.4+1.ge9efe34,
 // sympy v1.12.1, lbmpy_walberla/pystencils_walberla from waLBerla commit
-// fc0818503b0c5800ec6de9eeb97b1b5dd8ee4a70
+// 272d4a09ec35da50685afc9586645e1b9984b423
 
 #pragma once
 #include "core/DataTypes.h"
@@ -38,8 +38,6 @@
 
 #ifdef __GNUC__
 #define RESTRICT __restrict__
-#elif _MSC_VER
-#define RESTRICT __restrict
 #else
 #define RESTRICT
 #endif
