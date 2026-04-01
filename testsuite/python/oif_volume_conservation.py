127386d9f60258101918e078056b563e78cb4e4b
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2026-04-01 16:34:51 +0200

    WIP: revert test for code coverage

--- testsuite/python/oif_volume_conservation.py
+++ testsuite/python/oif_volume_conservation.py
@@ -46,9 +46,7 @@ class Test(ut.TestCase):
             nodes_file=str(tests_common.data_path("sphere393nodes.dat")),
             triangles_file=str(
                 tests_common.data_path("sphere393triangles.dat")),
-            system=self.system, **kwargs, kb=1.0, kal=1.0, kag=0.1, kv=0.1, kvisc=2e-10,
-            # kvisc must be non-zero to exercise the viscosity code path
-            # in OifLocalForcesBond::calc_forces
+            system=self.system, **kwargs, kb=1.0, kal=1.0, kag=0.1, kv=0.1,
             check_orientation=False, resize=(3.0, 3.0, 3.0))
 
     def check_relaxation(self, **kwargs):
